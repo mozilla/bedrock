d1599a0ef825fb4a664fa527163abfeaf538f389
Paul Osman <paul@eval.ca>
2011-06-03 08:29:02 -0400

    Rough job listings page. Work in progress, needs styling

--- apps/careers/urls.py
+++ apps/careers/urls.py
@@ -4,4 +4,6 @@ from django.conf.urls.defaults import patterns
 urlpatterns = patterns('',
     # Example:
     (r'^careers/?$', 'careers.views.careers'),
+    (r'^careers/(?P<slug>[\w-]+)/$', 'careers.views.department'),
+    (r'^careers/position/(?P<job_id>[\w]+)/$', 'careers.views.position'),
 )
--- apps/careers/views.py
+++ apps/careers/views.py
@@ -1,5 +1,30 @@
 import jingo
 
+from django.shortcuts import get_object_or_404
 
-def careers(request):
-    return jingo.render(request, 'careers/home.html')
+from django_jobvite.models import Position, Category
+
+
+def careers(request, slug=None):
+    categories = Category.objects.all()
+    return jingo.render(request, 'careers/home.html', {
+        'categories': categories,
+    })
+
+
+def department(request, slug):
+    selected = get_object_or_404(Category, slug=slug)
+    categories = Category.objects.all()
+    positions = Position.objects.filter(category=selected)
+    return jingo.render(request, 'careers/home.html', {
+        'categories': categories,
+        'positions': positions,
+        'selected': selected,
+    })
+
+
+def position(request, job_id=None):
+    position = get_object_or_404(Position, job_id=job_id)
+    return jingo.render(request, 'careers/home.html', {
+        'position': position,
+    })
