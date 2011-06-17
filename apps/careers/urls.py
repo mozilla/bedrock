a476ddd18b9f2aea2c45dc88e88b6fb47594a1ff
Paul Osman <paul@eval.ca>
2011-06-17 14:56:13 -0400

    Move careers/benefits above careers/(?P<category>...)

--- apps/careers/urls.py
+++ apps/careers/urls.py
@@ -4,7 +4,7 @@ from django.conf.urls.defaults import patterns
 urlpatterns = patterns('',
     (r'^careers/?$', 'careers.views.careers'),
     (r'^careers/all/?$', 'careers.views.careers_two'),
+    (r'^careers/benefits/?$', 'careers.views.benefits'),
     (r'^careers/(?P<slug>[\w-]+)/$', 'careers.views.department'),
     (r'^careers/position/(?P<job_id>[\w]+)/$', 'careers.views.position'),
-    (r'^careers/benefits/?$', 'careers.views.benefits'),
 )
