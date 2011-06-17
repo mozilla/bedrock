d71c21252997ffb60df803f0097b44876a5191d9
Paul Osman <paul@eval.ca>
2011-06-17 14:55:33 -0400

    Not an example anymore

--- apps/careers/urls.py
+++ apps/careers/urls.py
@@ -2,7 +2,6 @@ from django.conf.urls.defaults import patterns
 
 
 urlpatterns = patterns('',
-    # Example:
     (r'^careers/?$', 'careers.views.careers'),
     (r'^careers/all/?$', 'careers.views.careers_two'),
     (r'^careers/(?P<slug>[\w-]+)/$', 'careers.views.department'),
