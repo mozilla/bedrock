84e46abab634c0ba3739b8cf8530786205adefa9
Paul Osman <paul@eval.ca>
2011-07-08 21:54:31 -0400

    Starting to implement chowse's new UX design for job listings page. CSS still needs cleaning up and lots of todo notes left but the bones are there.

--- apps/careers/urls.py
+++ apps/careers/urls.py
@@ -3,7 +3,6 @@ from django.conf.urls.defaults import patterns
 
 urlpatterns = patterns('',
     (r'^careers/?$', 'careers.views.careers'),
-    (r'^careers/all/?$', 'careers.views.careers_two'),
     (r'^careers/benefits/?$', 'careers.views.benefits'),
     (r'^careers/(?P<slug>[\w-]+)/$', 'careers.views.department'),
     (r'^careers/position/(?P<job_id>[\w]+)/$', 'careers.views.position'),
--- apps/careers/views.py
+++ apps/careers/views.py
@@ -6,40 +6,24 @@ from django_jobvite.models import Position, Category
 
 
 def careers(request, slug=None):
-    categories = Category.objects.all()
+    categories = Category.objects.all().order_by('name')
     return jingo.render(request, 'careers/home.html', {
         'categories': categories,
-    })
-
-
-def careers_two(request):
-    positions = Position.objects.all()
-    categorized = {}
-    categories = Category.objects.all()
-    for category in categories:
-        categorized[category.name] = [p for p in positions
-                                      if category in p.category.all()]
-    return jingo.render(request, 'careers/all.html', {
-        'positions': positions,
-        'categories': categories,
-        'categorized': categorized,
     })
 
 
 def department(request, slug):
-    selected = get_object_or_404(Category, slug=slug)
-    categories = Category.objects.all()
-    positions = Position.objects.filter(category=selected)
-    return jingo.render(request, 'careers/home.html', {
-        'categories': categories,
+    category = get_object_or_404(Category, slug=slug)
+    positions = Position.objects.filter(category=category).order_by('title')
+    return jingo.render(request, 'careers/department.html', {
         'positions': positions,
-        'selected': selected,
+        'category': category
     })
 
 
 def position(request, job_id=None):
     position = get_object_or_404(Position, job_id=job_id)
-    return jingo.render(request, 'careers/home.html', {
+    return jingo.render(request, 'careers/position.html', {
         'position': position,
     })
 
