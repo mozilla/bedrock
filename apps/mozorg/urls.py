1cde900841eaec95bb6a0d359dc2930cd7a831b9
James Long <jlongster@jlongster.com>
2011-10-13 18:25:43 -0400

    playing around with download buttons

--- apps/mozorg/urls.py
+++ apps/mozorg/urls.py
@@ -1,6 +1,7 @@
 from django.conf.urls.defaults import *
-from views import channel
+from views import channel, button
 
 urlpatterns = patterns('',
+    (r'button/', button),
     (r'channel/', channel)
 )
--- apps/mozorg/views.py
+++ apps/mozorg/views.py
@@ -2,3 +2,6 @@ import l10n_utils
 
 def channel(request):
     return l10n_utils.render(request, "mozorg/channel.html")
+
+def button(request):
+    return l10n_utils.render(request, "mozorg/button.html")
