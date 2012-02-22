bd4e3916246fcb7241297f8123b6d50aaac99617
James Long <jlongster@jlongster.com>
2012-02-22 18:20:23 -0500

    force ssl on newsletter page

---
+++ lib/bedrock_util.py
@@ -0,0 +1,13 @@
+from django.conf import settings
+from django.http import HttpResponseRedirect
+
+def secure_required(view_func):
+    """Decorator makes sure URL is accessed over https."""
+    def _wrapped_view_func(request, *args, **kwargs):
+        if not request.is_secure():
+            if not getattr(settings, 'DEV', True):
+                request_url = request.build_absolute_uri(request.get_full_path())
+                secure_url = request_url.replace('http://', 'https://')
+                return HttpResponseRedirect(secure_url)
+        return view_func(request, *args, **kwargs)
+    return _wrapped_view_func
