68e7ba6e218a3a0aecce8891e47bd1a0ba68d798
James Long <jlongster@jlongster.com>
2012-02-22 12:10:24 -0500

    move top-level URLs into top-level urls.py

--- apps/persona/urls.py
+++ apps/persona/urls.py
@@ -2,9 +2,9 @@ from django.conf.urls.defaults import *
 from views import persona, about, developerfaq, termsofservice, privacypolicy
 
 urlpatterns = patterns('',
-    (r'^persona/developer-faq/$', developerfaq),
-    (r'^persona/terms-of-service/$', termsofservice),
-    (r'^persona/privacy-policy/$', privacypolicy),
-    (r'^persona/about/$', about),
-    (r'^persona/$', persona),
+    (r'^developer-faq/$', developerfaq),
+    (r'^terms-of-service/$', termsofservice),
+    (r'^privacy-policy/$', privacypolicy),
+    (r'^about/$', about),
+    (r'^$', persona),
 )
