b1916d5904fc72952f69f5a590e9742e4445be2a
James Long <jlongster@jlongster.com>
2012-03-07 15:33:58 -0500

    add `platform_img` template function

--- settings/base.py
+++ settings/base.py
@@ -109,9 +109,11 @@ MINIFY_BUNDLES = {
         ),
         'common': (
             'js/libs/jquery-1.7.1.min.js',
+            'js/site.js',
         ),
         'firefox': (
             'js/libs/jquery-1.7.1.min.js',
+            'js/site.js',
             'js/nav-main.js',
         ),
         'firefox-customize': (
