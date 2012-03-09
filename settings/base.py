3a26a05b19d97ccc55e920352de0b265dffeaa47
Steven Garrity <steven@silverorange.com>
2012-03-09 12:55:43 -0400

    Underscores rather than dashes

--- settings/base.py
+++ settings/base.py
@@ -64,21 +64,21 @@ MINIFY_BUNDLES = {
         'firefox': (
             'css/firefox/template.less',
         ),
-        'firefox-customize': (
+        'firefox_customize': (
             'css/sandstone/video.less',
             'css/firefox/customize.less',
         ),
-        'firefox-features': (
+        'firefox_features': (
             'css/sandstone/video.less',
             'css/firefox/features.less',
         ),
         'firefox_happy': (
             'css/firefox/happy.less',
         ),
-        'firefox-security': (
+        'firefox_security': (
             'css/firefox/security.less',
         ),
-        'firefox-technology': (
+        'firefox_technology': (
             'css/firefox/technology.less',
             'css/firefox/technology-demos.css',
         ),
@@ -100,7 +100,7 @@ MINIFY_BUNDLES = {
         'persona': (
             'css/persona.less',
         ),
-        'sandstone-guide': (
+        'sandstone_guide': (
             'css/sandstone-guide.less',
         ),
         'video': (
@@ -124,11 +124,11 @@ MINIFY_BUNDLES = {
             'js/site.js',
             'js/nav-main.js',
         ),
-        'firefox-customize': (
+        'firefox_customize': (
             'js/mozilla-video-tools.js',
             'js/firefox/customize.js',
         ),
-        'firefox-features': (
+        'firefox_features': (
             'js/mozilla-video-tools.js',
             'js/firefox/features.js',
         ),
