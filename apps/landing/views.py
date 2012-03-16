1e5fbe92e71224a77a1790af685ebe27ac3b339d
Craig Cook <craigcook@gmail.com>
2012-03-16 14:03:56 -0700

    Fixes and updates
    Removed trans blocks
    Removed mobile locales
    Misc. odds and ends

--- apps/landing/views.py
+++ apps/landing/views.py
@@ -2,4 +2,4 @@ import l10n_utils
 from product_details import product_details
 
 def devices(request):
-    return l10n_utils.render(request, "landing/devices.html", {'latest_version': product_details.firefox_versions['LATEST_FIREFOX_VERSION']})
+    return l10n_utils.render(request, "landing/devices.html", {'latest_version': product_details.firefox_versions['LATEST_FIREFOX_VERSION']} )
