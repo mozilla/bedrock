324c24255b7a26c1a0dd820bb84be89e8fd6d1db
James Long <jlongster@jlongster.com>
2012-03-28 12:51:15 -0400

    remove whitespace

--- media/js/site.js
+++ media/js/site.js
@@ -27,9 +27,9 @@
         // Add the platform as a class name immediately to avoid lots
         // of flickering
         var b = document.body;
-        b.className = b.className.replace("windows", site.platform);        
+        b.className = b.className.replace("windows", site.platform);
     }
 
     init();
-    window.site = site;    
+    window.site = site;
 })();
