cae8c7d3bdd1878cdbe44b402a6cbc51ee99ec5d
James Long <jlongster@jlongster.com>
2012-04-12 14:29:57 -0400

    all all locales to production locales

--- settings/base.py
+++ settings/base.py
@@ -17,8 +17,19 @@ LANGUAGE_CODE = 'en-US'
 SESSION_COOKIE_SECURE = True
 
 # Accepted locales
-PROD_LANGUAGES = ('de', 'en-US', 'es', 'fr',)
-DEV_LANGUAGES = DEV_LANGUAGES + ['en-US']
+PROD_LANGUAGES = ('ab-CD', 'ach', 'af', 'ak', 'ar', 'as', 'ast', 'be', 'bg'
+                  'bin', 'bn-BD', 'bn-IN', 'br', 'bs', 'ca', 'cs', 'csb', 'cy'
+                  'da', 'de', 'el', 'en-GB', 'en-US', 'en-ZA', 'eo', 'es-AR'
+                  'es-CL', 'es-ES', 'es-MX', 'et', 'eu', 'fa', 'ff', 'fi', 'fr'
+                  'fy-NL', 'ga-IE', 'gd', 'gl', 'gu-IN', 'he', 'hi-IN', 'hr'
+                  'hu', 'hy-AM', 'id', 'is', 'it', 'ja', 'ja-JP-mac', 'js',
+                  'ka', 'kk', 'km', 'kn', 'ko', 'ku', 'lg', 'lij', 'lt', 'lv',
+                  'mai', 'mk', 'ml', 'mn', 'mr', 'ms', 'my', 'nb-NO', 'nl',
+                  'nn-NO', 'nso', 'oc', 'or', 'pa-IN', 'pl', 'pt-BR', 'pt-PT',
+                  'rm', 'ro', 'ru', 'si', 'sk', 'sl', 'son', 'sq', 'sr',
+                  'sv-SE', 'sw', 'ta', 'ta-LK', 'te', 'th', 'tr', 'uk',
+                  'vi', 'wo', 'zh-CN', 'zh-TW', 'zu')
+DEV_LANGUAGES = list(DEV_LANGUAGES) + ['en-US']
 
 DOTLANG_CACHE = 60
 
