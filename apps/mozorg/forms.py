0aa765277fe4d034b911a8005a413c5cb9e45ce8
James Long <jlongster@jlongster.com>
2012-04-13 19:18:27 -0400

    add more choices to contribute form (bug 744631)

--- apps/mozorg/forms.py
+++ apps/mozorg/forms.py
@@ -68,9 +68,9 @@ INTEREST_CHOICES = (('', 'Area of interest?'),
                     ('Research', 'User Research'),
                     ('Thunderbird', 'Thunderbird'),
                     ('Accessibility', 'Accessibility'),
-                    (' ', 'Other')),
                     ('Firefox Suggestions', 'I have a suggestion for Firefox'),
-                    ('Firefox Issue', 'I need help with a Firefox issue')
+                    ('Firefox Issue', 'I need help with a Firefox issue'),
+                    (' ', 'Other'))
 
 class ContributeForm(forms.Form):
     email = forms.EmailField(widget=EmailInput(attrs={'required':'true'}))
