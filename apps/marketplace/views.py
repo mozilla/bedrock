b1bb0c711a4bb69a1043abf2420bb43d625d4083
Steven Garrity <steven@silverorange.com>
2012-04-21 21:38:05 -0300

    Initial template setup for Apps Partners page

--- apps/marketplace/views.py
+++ apps/marketplace/views.py
@@ -22,3 +22,18 @@ def marketplace(request):
                              "marketplace/marketplace.html",
                              {'form': form,
                               'success': success})
+
+def partners(request):
+    success = False
+    form = NewsletterForm(request.POST or None)
+
+    if request.method == 'POST':
+        if form.is_valid():
+            data = form.cleaned_data
+            basket.subscribe(data['email'], 'app-dev', format=data['fmt'])
+            success = True
+            
+    return l10n_utils.render(request,
+                             "marketplace/partners.html",
+                             {'form': form,
+                              'success': success})
