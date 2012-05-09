e0e8e1ec9eb72d0ea127e42ec8c3f9156305b8f7
Steven Garrity <steven@silverorange.com>
2012-05-09 10:46:24 -0300

    Switch the main page title/logo for dark-background layouts (Bug 731678)

--- media/js/firefox/channel.js
+++ media/js/firefox/channel.js
@@ -1,18 +1,18 @@
-
 $(document).ready(function() {
+    var $logo = $('#masthead h2 img');
+    var logoOriginalSrc = $logo.attr('src');
+
     var pager = Mozilla.Pager.rootPagers[0];
     var selected_href = false;
 
     function redirect(a) {
         var href = a.href;
 
-        if(href.indexOf('#beta') != -1) {
+        if (href.indexOf('#beta') != -1) {
             window.location = '/firefox/beta/';
-        }
-        else if(href.indexOf('#aurora') != -1) {
+        } else if (href.indexOf('#aurora') != -1) {
             window.location = '/firefox/aurora/';
-        }
-        else if(href.indexOf('#firefox') != -1) {
+        } else if (href.indexOf('#firefox') != -1) {
             window.location = '/firefox/';
         }
     }
@@ -20,8 +20,10 @@ $(document).ready(function() {
     pager.$container.bind('changePage', function(e, tab) {
         if (pager.currentPage.id == 'aurora') {
             $('body').addClass('space');
+            $logo.attr('src', $logo.attr('data-inverse-src'));
         } else {
             $('body').removeClass('space');
+            $logo.attr('src', logoOriginalSrc);
         }
 
         $('.pager-tabs a').unbind('click.outgoing');
@@ -44,9 +46,11 @@ $(document).ready(function() {
     if (pager.currentPage.id == 'aurora') {
         $('body').removeClass('sky');
         $('body').addClass('space');
+        $logo.attr('src', $logo.attr('data-inverse-src'));
     } else {
         $('body').removeClass('space');
         $('body').addClass('sky');
+        $logo.attr('src', logoOriginalSrc);
     }
 
     $('.pager-tabs a.selected').bind('click.outgoing', function() {
