2fac8d41fcb2936d719bea0c5a5ce69192be2c7a
Ross Bruniges <rossbruniges@gmail.com>
2012-05-10 21:13:57 +0100

    Futher fixes from pull request comments

--- apps/webmaker/urls.py
+++ apps/webmaker/urls.py
@@ -1,6 +1,5 @@
 from django.conf.urls.defaults import *
-from views import webmaker
 
 urlpatterns = patterns('',
-    url(r'^$', webmaker, name='webmaker_index'),
+    page('', 'webmaker/index.html')
 )
