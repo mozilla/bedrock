a95de9269c6a36a3dc675b9a39e11adc05a9aa3d
Steven Garrity <steven@silverorange.com>
2012-06-08 09:50:50 -0300

    Update CSV import for devices page from Pull Request #128

--- apps/firefox/views.py
+++ apps/firefox/views.py
@@ -1,4 +1,13 @@
 import l10n_utils
+import csv
+from operator import itemgetter
+
+from django.core.files import File
+from django.core.cache import cache
+import jinja2
+import jingo
+
+from django.conf import settings
 
 def windows_billboards(req):
     major_version = req.GET.get('majorVersion')
@@ -11,13 +20,6 @@ def windows_billboards(req):
             return l10n_utils.render(req, 'firefox/unsupported-winxp.html')
     return l10n_utils.render(req, 'firefox/unsupported-win2k.html')
 
-from django.core.files import File
-import csv
-from operator import itemgetter
-import settings
-import jinja2
-import jingo
-
 @jingo.register.function
 @jinja2.contextfunction
 def platforms(self):
@@ -28,40 +30,49 @@ def platforms(self):
 @jingo.register.function
 @jinja2.contextfunction
 def manufacturers(self, platform):
-    supported_devices = loadDevices(self, platform)
+    supported_devices = load_devices(self, platform)
     return sorted(supported_devices, key=itemgetter(0))
 
 @jingo.register.function
 @jinja2.contextfunction
 def devices(self, platform, manufacturer):
-    supported_devices = loadDevices(self, platform)
+    supported_devices = load_devices(self, platform)
     if manufacturer in supported_devices:
         return supported_devices[manufacturer]
     else:
         return ['No Supported Devices']
 
-@jingo.register.function
-@jinja2.contextfunction
-def loadDevices(self, platform):
-    devices = dict()
-    with open(settings.MEDIA_ROOT + '/devices.csv', 'rb') as file:
-        # TODO: clean up fieldnames and handle whitespace better.
-        # perhaps also case insensitivity, at least on platforms?
-        #fieldnames = 'manufacturer','device', 'platform';
-        reader = csv.DictReader(file, delimiter=',')
-        try:
-            for row in reader:
-                if row[' platform'].lower() == platform.lower():
-                    if row['manufacturer'] in devices:
-                        devices[row['manufacturer']].append(row[' device'])
-                    else:
-                        devices[row['manufacturer']] = [row[' device']]
-                else:
-                    pass
-        except csv.Error, e:
-            sys.exit('file %s, line %d: %s' % (filename, reader.line_num, e))
+def load_devices(self, platform):
+    devices = cache.get('devices')
+    if devices is None:
+        devices = dict()
+        with open(settings.MEDIA_ROOT + '/devices.csv', 'rb') as file:
+            reader = csv.DictReader(
+                file,
+                fieldnames = ('manufacturer','device', 'platform'),
+                delimiter = ',',
+                skipinitialspace = True
+            )
 
-    for manufacturer in devices:
-        devices[manufacturer].sort()
+            # skip header row
+            next(reader)
+
+            try:
+                for row in reader:
+                    if row['platform'].lower() == platform.lower():
+                        #strip leading and trailing whitespace
+                        manufacturer = row['manufacturer'].strip()
+                        device = row['device'].strip()
+                        if manufacturer in devices:
+                            devices[manufacturer].append(device)
+                        else:
+                            devices[manufacturer] = [device]
+            except csv.Error, e:
+                sys.exit('file %s, line %d: %s' % (filename, reader.line_num, e))
+
+        for manufacturer in devices:
+            devices[manufacturer].sort()
+
+        cache.set('devices', devices, 600)
 
     return devices
