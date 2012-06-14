9d83b80b32d8a3baaef703777bd8f8bc98a004b6
Steven Garrity <steven@silverorange.com>
2012-06-14 10:30:50 -0300

    More cleanup for pull request #128

--- apps/firefox/views.py
+++ apps/firefox/views.py
@@ -1,9 +1,9 @@
-import l10n_utils
 import csv
+import l10n_utils
 from operator import itemgetter
 
-from django.core.files import File
 from django.core.cache import cache
+from django.core.files import File
 
 from django.conf import settings
 
@@ -22,11 +22,25 @@ def platforms(request):
     return l10n_utils.render(
         request,
         'firefox/mobile/platforms.html',
-        {'devices': load_devices(request)}
+        {'devices': load_devices(request, settings.MEDIA_ROOT + '/devices.csv')}
     )
 
-def load_devices(self):
-    devices = cache.get('devices')
+def case_insensitive_in(self, list, value):
+    #used in load_devices to lookup existing platform/manufacturer/device
+    #combinations in a case insensitive manner. This prevents duplicates from
+    #being displayed if they are entered twice by accident with different
+    #capitalization. If this happens, capitalization from the first instance
+    #will be used.
+    for i in list:
+        if value.lower() == i.lower():
+            return True
+
+    return False
+
+def load_devices(self, file, cacheDevices = True):
+    devices = None
+    if cacheDevices is True:
+        devices = cache.get('devices')
 
     if devices is None:
         #List of supported platforms. Any row in the csv that doesn't match one
@@ -37,41 +51,65 @@ def load_devices(self):
         for platform in platforms:
             devices[platform] = dict()
 
-        with open(settings.MEDIA_ROOT + '/devices.csv', 'rb') as file:
+        with open(file, 'rb') as file:
             reader = csv.DictReader(
                 file,
-                fieldnames = ('manufacturer', 'device', 'platform'),
                 delimiter = ',',
                 skipinitialspace = True
             )
 
-            #skip header row
-            next(reader)
+            #strip leading and trailing whitespace from each value to normalize
+            #values in the csv. This prevents whitespace from creating double
+            #entries for any platform/manufacturer/device combination.
+            reader = (
+                dict((k, v.strip()) for k, v in row.items())
+                for row in reader
+            )
 
             try:
                 for row in reader:
-                    platform = row['platform'].strip()
-                    if platform in devices:
-                        #strip leading and trailing whitespace
-                        manufacturer = row['manufacturer'].strip()
-                        device = row['device'].strip()
+                    #local variables for code readability
+                    platform = row['platform']
+                    manufacturer = row['manufacturer']
+                    device = row['device']
 
-                        #store the manufacturers in a list so they can easily be
-                        #access after being sorted
-                        if 'manufacturers' not in devices[platform]:
-                            devices[platform]['manufacturers'] = []
+                    if not case_insensitive_in(self, devices, platform):
+                        continue
 
-                        if manufacturer not in devices[platform]['manufacturers']:
-                            devices[platform]['manufacturers'].append(manufacturer)
+                    #store the manufacturers in a list so they can easily be
+                    #access after being sorted. init the list when its a new
+                    #platform
+                    if 'manufacturers' not in devices[platform]:
+                        devices[platform]['manufacturers'] = []
 
-                        #store device names in a nested dict by platform and
-                        #manufacturer
-                        if manufacturer in devices[platform]:
-                            devices[platform][manufacturer].append(device)
-                        else:
-                            devices[platform][manufacturer] = [device]
+                    #add new manufacturers to the list
+                    if not case_insensitive_in(
+                        self,
+                        devices[platform]['manufacturers'],
+                        manufacturer
+                    ):
+                        devices[platform]['manufacturers'].append(manufacturer)
+
+                    # init the list of devices for new manufacturer
+                    if not case_insensitive_in(
+                        self,
+                        devices[platform],
+                        manufacturer
+                    ):
+                        devices[platform][manufacturer] = []
+
+                    #store device names in a nested dict by platform and
+                    #manufacturer
+                    if not case_insensitive_in(
+                        self,
+                        devices[platform][manufacturer],
+                        device
+                    ):
+                        devices[platform][manufacturer].append(device)
             except csv.Error, e:
-                sys.exit('file %s, line %d: %s' % (filename, reader.line_num, e))
+                sys.exit(
+                    'file %s, line %d: %s' % (filename, reader.line_num, e)
+                )
 
         #sort manufacturers lists and device lists
         for platform in devices:
@@ -79,7 +117,8 @@ def load_devices(self):
             for manufacturer in devices[platform]:
                 devices[platform][manufacturer].sort()
 
-        cache.set('devices', devices, 600)
+        if cacheDevices is True:
+            cache.set('devices', devices, 600)
     return devices
 
 def dnt(request):
