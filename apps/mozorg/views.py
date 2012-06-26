498edda5c33f0e2258405ffd76514728651d6aab
Giorgos Logiotatidis <giorgos@mozilla.com>
2012-06-26 16:05:39 +0300

    [fix bug 765783] Implement callback url support.
    - For each successful subscription in /contribute we POST to
      callbackurl GET parameter if available.
    
    - Added /contribute/event/ page that renders the same page as
      /contribute but returns user to the same page after successfull
      subscription, so a new user can directly join.

--- apps/mozorg/views.py
+++ apps/mozorg/views.py
@@ -25,7 +25,7 @@ def handle_contribute_form(request, form):
 
 
 @csrf_exempt
-def contribute(request, template='mozorg/contribute.html'):
+def contribute(request, template, return_to_form):
     def has_contribute_form():
         return (request.method == 'POST' and
                 'contribute-form' in request.POST)
@@ -47,6 +47,10 @@ def contribute(request, template='mozorg/contribute.html'):
     if has_contribute_form():
         form = ContributeForm(request.POST)
         success = handle_contribute_form(request, form)
+        if success:
+            # If form was submitted successfully, return a new, empty
+            # one.
+            form = ContributeForm()
     else:
         form = ContributeForm()
 
@@ -77,7 +81,9 @@ def contribute(request, template='mozorg/contribute.html'):
                              {'form': form,
                               'success': success,
                               'newsletter_form': newsletter_form,
-                              'newsletter_success': newsletter_success})
+                              'newsletter_success': newsletter_success,
+                              'return_to_form': return_to_form})
+
 
 
 def contribute_send(data, locale='en-US'):
