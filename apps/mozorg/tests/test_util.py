66e898931435511871125a087970256fe6b0c788
Anthony Ricaud <anthony@ricaud.me>
2012-07-10 15:58:52 +0200

    Helper function for easy redirects in urlpatterns

---
+++ apps/mozorg/tests/test_util.py
@@ -0,0 +1,22 @@
+import unittest
+
+from django.conf import settings
+from django.test.client import Client
+
+from mock import patch
+from nose.tools import eq_
+
+
+@patch.object(settings, 'ROOT_URLCONF', 'mozorg.tests.urls')
+class TestUrlPatterns(unittest.TestCase):
+    def test_redirect(self):
+	c = Client()
+	response = c.get('/en-US/gloubi-boulga/')
+	eq_(response.status_code, 301)
+	eq_(response['Location'], 'http://testserver/en-US/mock/view/')
+
+    def test_temporary_redirect(self):
+	c = Client()
+	response = c.get('/en-US/gloubi-boulga/tmp/')
+	eq_(response.status_code, 302)
+	eq_(response['Location'], 'http://testserver/en-US/mock/view/')
