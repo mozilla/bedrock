230d6863c5d802c8f838e095401a444edd59dfbf
James Long <jlongster@jlongster.com>
2012-07-20 15:26:20 -0400

    make graphite track each page correctly

--- apps/mozorg/middleware.py
+++ apps/mozorg/middleware.py
@@ -5,6 +5,7 @@ from time import mktime
 from django.conf import settings
 from django.contrib.sessions.middleware import SessionMiddleware
 
+from django_statsd.middleware import GraphiteRequestTimingMiddleware
 
 class CacheMiddleware(object):
 
@@ -43,3 +44,23 @@ class NoVarySessionMiddleware(SessionMiddleware):
             del new_response['Vary']
         return new_response
 
+class MozorgRequestTimingMiddleware(GraphiteRequestTimingMiddleware):
+
+    def process_view(self, request, view, view_args, view_kwargs):
+        if hasattr(view, 'page_name'):
+            request._page_name = view.page_name
+        else:
+            f = super(MozorgRequestTimingMiddleware, self)
+            f.process_view(request, view, view_args, view_kwargs)
+
+    def _record_time(self, request):
+        if hasattr(request, '_page_name'):
+            if hasattr(request, '_start_time'):
+                ms = int((time.time() - request._start_time) * 1000)
+                path = request._page_name.replace('/', '.')
+                method = request.method
+
+                statsd.timing('view.%s.%s' % (path, method), ms)
+                statsd.timing('view.%s' % method)
+        else:
+            super(MozorgRequestTimingMiddleware, self)._record_time(request)
