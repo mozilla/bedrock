ded90ee8a50ec10d353ffdc5b85e18336d60d8e7
James Long <jlongster@jlongster.com>
2012-07-20 15:44:57 -0400

    remove duplicate code in new mozorg graphite timing middleware

--- apps/mozorg/middleware.py
+++ apps/mozorg/middleware.py
@@ -1,6 +1,6 @@
 import datetime
 from email.utils import formatdate
-from time import mktime
+import time
 
 from django.conf import settings
 from django.contrib.sessions.middleware import SessionMiddleware
@@ -15,7 +15,7 @@ class CacheMiddleware(object):
                  'Cache-Control' not in response)
         if cache:
             d = datetime.datetime.now() + datetime.timedelta(minutes=10)
-            stamp = mktime(d.timetuple())
+            stamp = time.mktime(d.timetuple())
 
             response['Cache-Control'] = 'max-age=600'
             response['Expires'] = formatdate(timeval=stamp, localtime=False, usegmt=True)
@@ -48,19 +48,9 @@ class MozorgRequestTimingMiddleware(GraphiteRequestTimingMiddleware):
 
     def process_view(self, request, view, view_args, view_kwargs):
         if hasattr(view, 'page_name'):
-            request._page_name = view.page_name
+            request._view_module = 'page'
+            request._view_name = view.page_name.replace('/', '.')
+            request._start_time = time.time()
         else:
             f = super(MozorgRequestTimingMiddleware, self)
             f.process_view(request, view, view_args, view_kwargs)
-
-    def _record_time(self, request):
-        if hasattr(request, '_page_name'):
-            if hasattr(request, '_start_time'):
-                ms = int((time.time() - request._start_time) * 1000)
-                path = request._page_name.replace('/', '.')
-                method = request.method
-
-                statsd.timing('view.%s.%s' % (path, method), ms)
-                statsd.timing('view.%s' % method)
-        else:
-            super(MozorgRequestTimingMiddleware, self)._record_time(request)
