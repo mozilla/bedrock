d29acc69fac4ad72bb8f6e8224d493031acf8ccc
Anthony Ricaud <anthony@ricaud.me>
2012-07-31 16:46:28 +0200

    Bug 744886 - marketplace/views.py is gone

--- apps/marketplace/urls.py
+++ apps/marketplace/urls.py
@@ -1,9 +1,10 @@
 from django.conf.urls.defaults import *
+
 from mozorg.util import page
 
-import views
-
 urlpatterns = patterns('',
+    # /apps is temporarily redirected to /apps/partners as per
+    # https://bugzilla.mozilla.org/show_bug.cgi?id=751903
     page('', 'marketplace/marketplace.html'),
     page('partners', 'marketplace/partners.html')
 )
