9718d11df0becf318a66264d7ad0fa06192261e1
Steven Garrity <steven@silverorange.com>
2012-08-20 16:59:42 -0300

    Build out more sandstone style guide pages

--- apps/styleguide/urls.py
+++ apps/styleguide/urls.py
@@ -5,4 +5,7 @@ urlpatterns = patterns('',
     page('', 'styleguide/home.html'),
     page('sandstone', 'styleguide/sandstone-intro.html'),
     page('sandstone/grids', 'styleguide/sandstone-grids.html'),
+    page('sandstone/tabzilla', 'styleguide/sandstone-tabzilla.html'),
+    page('sandstone/typefaces', 'styleguide/sandstone-typefaces.html'),
+    page('sandstone/colors', 'styleguide/sandstone-colors.html'),
 )
