08c8309fcdd34a5fe29c9a8cd4c986e86152f5ea
Steven Garrity <steven@silverorange.com>
2012-08-20 13:40:27 -0300

    Build out more of the website section of the style guide

--- apps/styleguide/urls.py
+++ apps/styleguide/urls.py
@@ -3,4 +3,6 @@ from mozorg.util import page
 
 urlpatterns = patterns('',
     page('', 'styleguide/home.html'),
+    page('sandstone', 'styleguide/sandstone-intro.html'),
+    page('sandstone/grids', 'styleguide/sandstone-grids.html'),
 )
