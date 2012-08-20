5de3f96538abe68596a0d557a347a54ff8b4d43c
Ross Bruniges <rossbruniges@gmail.com>
2012-08-20 16:46:51 +0100

    Cleaning up a few bits of code around our initial implementations

--- apps/grants/views.py
+++ apps/grants/views.py
@@ -1,6 +1,7 @@
 from django.http import Http404
 
 import l10n_utils
+import bleach
 
 from grants_db import GRANTS
 
@@ -27,7 +28,7 @@ def grant_info(request, slug):
 
 def grants(request):
 
-    type_filter = request.GET.get('type', '')
+    type_filter = bleach.clean(request.GET.get('type', ''))
 
     if type_filter:
         grants = filter(lambda x: x[1]['type'] == type_filter, GRANTS)
@@ -35,6 +36,7 @@ def grants(request):
         grants = GRANTS
 
     return l10n_utils.render(request, "grants/index.html", {
+            'filter': type_filter,
             'grants': grants,
             'grant_labels': GRANT_LABELS
     })
