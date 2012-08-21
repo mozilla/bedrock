00c38afc7e2eac12f2e3542a6ad9a9752f072408
Ross Bruniges <rossbruniges@gmail.com>
2012-08-21 17:01:22 +0100

    Fixing up a couple of smaller pull request nits from @Rik and @Osmose

--- apps/grants/views.py
+++ apps/grants/views.py
@@ -5,7 +5,7 @@ import bleach
 
 from grants_db import GRANTS
 
-GRANT_LABELS = {
+grant_labels = {
     'open-source-technology': 'Open Source Technology',
     'learning-webmaking': 'Learning & Webmaking',
     'user-sovereignty': 'User Sovereignty',
@@ -14,7 +14,6 @@ GRANT_LABELS = {
 
 
 def grant_info(request, slug):
-
     grant_data = filter(lambda x: x[0] == slug, GRANTS)
 
     if not grant_data:
@@ -22,16 +21,15 @@ def grant_info(request, slug):
 
     return l10n_utils.render(request, "grants/info.html", {
         'grant': grant_data[0][1],  # Using named tuple so need to deep dive
-        'grant_labels': GRANT_LABELS
+        'grant_labels': grant_labels
     })
 
 
 def grants(request):
-
     type_filter = bleach.clean(request.GET.get('type', ''))
 
-    if type_filter != '' and type_filter not in GRANT_LABELS:
-            raise Http404
+    if type_filter and type_filter not in grant_labels:
+        raise Http404
 
     if type_filter:
         grants = filter(lambda x: x[1]['type'] == type_filter, GRANTS)
@@ -41,5 +39,5 @@ def grants(request):
     return l10n_utils.render(request, "grants/index.html", {
             'filter': type_filter,
             'grants': grants,
-            'grant_labels': GRANT_LABELS
+            'grant_labels': grant_labels
     })
