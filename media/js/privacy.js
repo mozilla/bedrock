7df158e7ca16b3e9dff4cf8f079c616b82a736df
ossreleasefeed <sneethling@mozilla.com>
2012-08-27 17:47:36 +0200

    final version of floating toc panel

--- media/js/privacy.js
+++ media/js/privacy.js
@@ -1,3 +1,22 @@
+/*global $:true, isDoNotTrackEnabled:true */
 $(function() {
+    "use strict";
+    var privacyTOCContainer = $(".privacy-toc");
+    var absoluteBottom = window.scrollMaxY;
+    var footerTotalHeight = $("#colophon").outerHeight(true);
+    var topBoundry = parseInt(privacyTOCContainer.css("top"), 10) - 15;
+    var bottomBoundry = absoluteBottom - footerTotalHeight;
+    var scrollTopOffset = 0;
+
+    window.addEventListener("scroll", function(evt) {
+        scrollTopOffset = window.pageYOffset;
+
+        if((scrollTopOffset > topBoundry) && (scrollTopOffset < bottomBoundry)) {
+            privacyTOCContainer.css({
+                "top" : scrollTopOffset
+            });
+        }
+     }, true);
+
     isDoNotTrackEnabled();
 });
