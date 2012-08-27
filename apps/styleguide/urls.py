0687547c4a14c8bf11dad5f35e903502426b55d5
Steven Garrity <steven@silverorange.com>
2012-08-27 13:25:43 -0300

    Add examples page

--- apps/styleguide/urls.py
+++ apps/styleguide/urls.py
@@ -3,12 +3,14 @@ from mozorg.util import page
 
 urlpatterns = patterns('',
     page('', 'styleguide/home.html'),
+    page('identity/firefox/branding', 'styleguide/identity/firefox-branding.html'),
     page('sandstone', 'styleguide/sandstone-intro.html'),
+    page('sandstone/buttons', 'styleguide/sandstone-buttons.html'),
+    page('sandstone/colors', 'styleguide/sandstone-colors.html'),
+    page('sandstone/forms', 'styleguide/sandstone-forms.html'),
     page('sandstone/grids', 'styleguide/sandstone-grids.html'),
+    page('sandstone/tables', 'styleguide/sandstone-tables.html'),
     page('sandstone/tabzilla', 'styleguide/sandstone-tabzilla.html'),
     page('sandstone/typefaces', 'styleguide/sandstone-typefaces.html'),
-    page('sandstone/colors', 'styleguide/sandstone-colors.html'),
-    page('sandstone/buttons', 'styleguide/sandstone-buttons.html'),
-    page('sandstone/tables', 'styleguide/sandstone-tables.html'),
-    page('identity/firefox/branding', 'styleguide/identity/firefox-branding.html'),
+    page('sandstone/examples', 'styleguide/sandstone-examples.html'),
 )
