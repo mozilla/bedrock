9c072b5b3806c3614b5e06c4b30014635a49e39a
Steven Garrity <steven@silverorange.com>
2012-08-30 14:55:19 -0300

    Add Firefox identity pages and start of Communications section

--- apps/styleguide/urls.py
+++ apps/styleguide/urls.py
@@ -4,6 +4,10 @@ from mozorg.util import page
 urlpatterns = patterns('',
     page('', 'styleguide/home.html'),
     page('identity/firefox/branding', 'styleguide/identity/firefox-branding.html'),
+    page('identity/firefox/channels', 'styleguide/identity/firefox-channels.html'),
+    page('identity/firefox/wordmarks', 'styleguide/identity/firefox-wordmarks.html'),
+    page('identity/firefox/color', 'styleguide/identity/firefox-color.html'),
+
     page('sandstone', 'styleguide/sandstone-intro.html'),
     page('sandstone/buttons', 'styleguide/sandstone-buttons.html'),
     page('sandstone/colors', 'styleguide/sandstone-colors.html'),
@@ -13,4 +17,9 @@ urlpatterns = patterns('',
     page('sandstone/tabzilla', 'styleguide/sandstone-tabzilla.html'),
     page('sandstone/typefaces', 'styleguide/sandstone-typefaces.html'),
     page('sandstone/examples', 'styleguide/sandstone-examples.html'),
+
+    page('websites/community/overview', 'styleguide/websites/community-overview.html'),
+    page('websites/domains/overview', 'styleguide/websites/domains-overview.html'),
+
+    page('communications/presentations', 'styleguide/communications/presentations.html'),
 )
