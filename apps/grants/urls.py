5cdfefdde063a82cc380afbcc18986c33bd77c67
Ross Bruniges <rossbruniges@gmail.com>
2012-09-06 13:10:49 +0100

    Removing unrequired inline comments

--- apps/grants/urls.py
+++ apps/grants/urls.py
@@ -6,9 +6,7 @@ import views
 
 urlpatterns = patterns('',
     url(r'^$', views.grants, name='grants'),
-    # attempt to not break existing URL
     url(r'^(?P<slug>[\w-]+).html$', views.grant_info, name='grant_info'),
-    # hard-code in a couple of static pages pulled direct from the old PHP site
     page('reports/gnome-haeger-report', 'grants/reports/gnome-haeger-report.html'),
     page('reports/ushahidi-chile-report', 'grants/reports/ushahidi-chile-report.html'),
 )
