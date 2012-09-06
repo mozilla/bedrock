99d3773f4670b1c57342dac4540f4ced82532ae2
Ross Bruniges <rossbruniges@gmail.com>
2012-09-06 15:11:43 +0100

    Final nit pick from pull review - sorting in place opposed to assigned multiple variables

--- apps/grants/views.py
+++ apps/grants/views.py
@@ -38,13 +38,11 @@ def grants(request):
     else:
         grants = GRANTS
 
-    # first sort by title (alphabetically)
-    sorted_grantees = sorted(grants, key=attrgetter('grantee'))
-    # now sort by year (reverse numeric)
-    sorted_grants = sorted(sorted_grantees, key=attrgetter('year'), reverse=True)
+    grants.sort(key=attrgetter('grantee'))
+    grants.sort(key=attrgetter('year'), reverse=True)
 
     return l10n_utils.render(request, "grants/index.html", {
             'filter': type_filter,
-            'grants': sorted_grants,
+            'grants': grants,
             'grant_labels': grant_labels
     })
