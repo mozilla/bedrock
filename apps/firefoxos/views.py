a8c445f7c063cc6b34703695767b3d2a79cbf0ae
Craig Cook <craigcook@gmail.com>
2012-09-28 10:32:24 -0700

    Reroute faq and about views to the main template
    We'll also redirect the URLs on the server

--- apps/firefoxos/views.py
+++ apps/firefoxos/views.py
@@ -1,12 +1,15 @@
 import l10n_utils
 from django.conf import settings
 
+def firefoxos(request):
+    return l10n_utils.render(request, "firefoxos/firefoxos.html")
+    
 def b2g(request):
     return l10n_utils.render(request, "firefoxos/firefoxos.html")
 
 def about(request):
-    return l10n_utils.render(request, "firefoxos/about.html")
+    return l10n_utils.render(request, "firefoxos/firefoxos.html")
 
 def faq(request):
-    return l10n_utils.render(request, "firefoxos/faq.html")
+    return l10n_utils.render(request, "firefoxos/firefoxos.html")
 
