874107bd844bea965220c935e0f1fc5897dcd2e4
Steven Garrity <steven@silverorange.com>
2012-10-11 14:54:00 -0300

    Redirect Brand Toolkit pages to style guide (Bug 792185)

--- apps/redirects/urls.py
+++ apps/redirects/urls.py
@@ -10,4 +10,20 @@ urlpatterns = patterns('',
     redirect(r'^contribute/areas.html$', 'mozorg.contribute'),  # Bug 781914
     redirect(r'^projects/$', 'mozorg.products'),  # Bug 763665
 
+    # Bug 792185 Brand Toolkit -> Style Guide
+    redirect(r'^firefox/brand/$', 'styleguide.home'),
+    redirect(r'^firefox/brand/platform/$', 'styleguide.identity.firefox-family-platform'),
+    redirect(r'^firefox/brand/identity/$', 'styleguide.identity.firefox-branding'),
+    redirect(r'^firefox/brand/identity/channel-logos/$', 'styleguide.identity.firefox-channels'),
+    redirect(r'^firefox/brand/identity/wordmarks/$', 'styleguide.identity.firefox-wordmarks'),
+    redirect(r'^firefox/brand/identity/typefaces/$', 'styleguide.communications.typefaces'),
+    redirect(r'^firefox/brand/artwork/$', 'styleguide.home'),
+    redirect(r'^firefox/brand/artwork/gear/$', 'styleguide.home'),
+    redirect(r'^firefox/brand/website/$', 'styleguide.websites.sandstone-intro'),
+    redirect(r'^firefox/brand/website/domain-strategy/$', 'styleguide.websites.domains-overview'),
+    redirect(r'^firefox/brand/copy/$', 'styleguide.communications.copy-tone'),
+    redirect(r'^firefox/brand/copy/l10n/$', 'styleguide.communications.translation'),
+    redirect(r'^firefox/brand/copy/rules/$', 'styleguide.communications.copy-rules'),
+    redirect(r'^firefox/brand/downloads/$', 'styleguide.home'),
+
     )
