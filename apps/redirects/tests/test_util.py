b7163e6c43ee7c7491bfb17d6bce91ce7a7f1123
Michael Kelly <mkelly@mozilla.com>
2012-10-17 13:23:21 -0400

    Fix bug 800467: Add redirect from apps/partners to marketplace.
    Updates the redirect utility to support external redirects, and then
    uses that functionality to support the external redirect.

--- apps/redirects/tests/test_util.py
+++ apps/redirects/tests/test_util.py
@@ -7,16 +7,22 @@ from mock import patch
 from nose.tools import eq_
 
 
-@patch.object(settings, 'ROOT_URLCONF', 'mozorg.tests.urls')
+@patch.object(settings, 'ROOT_URLCONF', 'redirects.tests.urls')
 class TestUrlPatterns(unittest.TestCase):
+    def setUp(self):
+        self.client = Client()
+
     def test_redirect(self):
-        c = Client()
-        response = c.get('/en-US/gloubi-boulga/')
+        response = self.client.get('/en-US/gloubi-boulga/')
         eq_(response.status_code, 301)
         eq_(response['Location'], 'http://testserver/en-US/mock/view/')
 
     def test_temporary_redirect(self):
-        c = Client()
-        response = c.get('/en-US/gloubi-boulga/tmp/')
+        response = self.client.get('/en-US/gloubi-boulga/tmp/')
         eq_(response.status_code, 302)
         eq_(response['Location'], 'http://testserver/en-US/mock/view/')
+
+    def test_external_redirect(self):
+        response = self.client.get('/en-US/gloubi-boulga/ext/')
+        eq_(response.status_code, 301)
+        eq_(response['Location'], 'https://marketplace.mozilla.org')
