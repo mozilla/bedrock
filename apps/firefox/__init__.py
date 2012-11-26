0cc8e6cd3594f99712b30924a58e6d9fe6a145f2
Gervase Markham <gerv@gerv.net>
2012-11-26 16:12:39 +0000

    Update license to MPL 2.

--- apps/firefox/__init__.py
+++ apps/firefox/__init__.py
@@ -1,3 +1,7 @@
+# This Source Code Form is subject to the terms of the Mozilla Public
+# License, v. 2.0. If a copy of the MPL was not distributed with this
+# file, You can obtain one at http://mozilla.org/MPL/2.0/.
+
 # Adapted from django-mozilla-product-details
 version_re = (r"\d+"         # major (x in x.y)
               "\.\d+"        # minor1 (y in x.y)
--- apps/grants/views.py
+++ apps/grants/views.py
@@ -1,3 +1,7 @@
+# This Source Code Form is subject to the terms of the Mozilla Public
+# License, v. 2.0. If a copy of the MPL was not distributed with this
+# file, You can obtain one at http://mozilla.org/MPL/2.0/.
+
 from operator import attrgetter
 from django.http import Http404
 
