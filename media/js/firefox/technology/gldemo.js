4ed6092200c83c3cb0034a5163139067f838331c
Gervase Markham <gerv@gerv.net>
2012-11-26 16:12:39 +0000

    Update license to MPL 2.

--- media/js/firefox/technology/gldemo.js
+++ media/js/firefox/technology/gldemo.js
@@ -1,3 +1,7 @@
+/* This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
+
 var per_fragment_lighting_fs = [
 "  #ifdef GL_ES",
 "  precision highp float;",
