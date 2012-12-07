062b8f12e25557efc3a7c07b7fd7403ef5d71a47
Jon Petto <jonpetto@gmail.com>
2012-12-07 10:41:04 -0600

    Added E test - scrolling bullets/button/product shot down. little bit of cleanup. bug 814203

--- media/js/firefox/new.js
+++ media/js/firefox/new.js
@@ -2,7 +2,7 @@ var Mozilla = window.Mozilla || {};
 Mozilla.Firefox = window.Mozilla.Firefox || {};
 
 Mozilla.Firefox.New = (function() {
-	"use strict";
+	//"use strict";
 
 	var _init = function() {
 		$('#firefox-new-a .download-firefox').on('click', function(e) {
@@ -25,13 +25,7 @@ Mozilla.Firefox.New = (function() {
 			}, 500);
 		});
 
-		$('#firefox-new-c .download-firefox').on('click', function(e) {
-			e.preventDefault();
-
-			$('#stage').addClass('scene2');
-		});
-
-		$('#firefox-new-d .download-firefox').on('click', function(e) {
+		$('#firefox-new-c .download-firefox, #firefox-new-d .download-firefox, #firefox-new-e .download-firefox').on('click', function(e) {
 			e.preventDefault();
 
 			$('#stage').addClass('scene2');
