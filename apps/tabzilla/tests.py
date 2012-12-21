10639b0d9b0bc5507980d960481d539e70ce38f9
Paul McLanahan <paul@mclanahan.net>
2012-12-21 09:54:48 -0500

    Bug 823919: Fix content-type of tabzilla.js.

---
+++ apps/tabzilla/tests.py
@@ -0,0 +1,16 @@
+from django.test import Client
+
+from funfactory.urlresolvers import reverse
+
+from mozorg.tests import TestCase
+
+
+class TabzillaViewTests(TestCase):
+    def setUp(self):
+        self.client = Client()
+
+    def test_tabzilla_content_type(self):
+        """ Content-Type header should be text/javascript. """
+        with self.activate('en-US'):
+            resp = self.client.get(reverse('tabzilla'))
+        self.assertEqual(resp['content-type'], 'text/javascript')
--- apps/tabzilla/views.py
+++ apps/tabzilla/views.py
@@ -2,4 +2,5 @@ import l10n_utils
 
 
 def tabzilla_js(request):
-    return l10n_utils.render(request, 'tabzilla/tabzilla.js')
+    return l10n_utils.render(request, 'tabzilla/tabzilla.js',
+                             content_type='text/javascript')
