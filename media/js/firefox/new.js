5d19c590859265df4b3eedfef5f42edb77737b47
Jon Petto <jonpetto@gmail.com>
2013-01-02 19:01:49 -0600

    Fix back button for scene 2 > scene 1. bug 824356
    Add code so back button will take user to scene 1 from scene 2.
    Adds '#download' hash to URL for modern browsers (IE9+).

--- media/js/firefox/new.js
+++ media/js/firefox/new.js
@@ -30,30 +30,16 @@
         }, 300);
     }
 
-    // Bind events on domReady.
-    $(function() {
-        // Pull Firefox download link from the download button and add to the
-        // 'click here' link.
-        // TODO: Remove and generate link in bedrock.
-        var $li = $('.download-list li:visible').filter(':first');
-        var ff_dl_url = $li.find('a:first').attr('href');
-        $('#direct-download-link').attr('href', ff_dl_url).on('click', function(e) {
-            e.preventDefault();
-            ga_track();
-            dl_redirect($(this).attr('href'));
-        });
+    var $scene2 = $('#scene2');
+    var $stage = $('#stage-firefox');
+    var $thankYou = $('.thankyou');
+    // drop support for IE7/8. good?
+    var can_hashchange = ('onhashchange' in window && 'addEventListener' in window);
 
-        // Trigger animation after download.
-        var $scene2 = $('#scene2');
-        var $stage = $('#stage-firefox');
-        var $thankYou = $('.thankyou');
-        $('.download-firefox').on('click', function(e) {
-            e.preventDefault();
-            var $link = $(this);
-            ga_track();
-
+    function show_scene(scene) {
+        if (scene === 2) {
             if (!Modernizr.csstransitions) {
-                $scene2.css('visibility', 'visible');
+                $scene2.css('visibility', 'visible'); // ensure visibility prior to animation
                 $stage.animate({
                     bottom: '-400px'
                 }, 400, function() {
@@ -67,6 +53,60 @@
                 window.setTimeout(function() {
                     $thankYou.focus();
                 }, 500);
+            }
+        } else {
+            if (!Modernizr.csstransitions) {
+                $stage.animate({
+                    bottom: '0px'
+                }, 400, function() {
+                    $scene2.css('visibility', 'hidden'); // revert scene2 to hidden
+                    $thankYou.blur();
+                });
+            } else {
+                $scene2.css('visibility', 'visible'); // ensure visibility through animation
+                $stage.removeClass('scene2'); // remove class - transition to scene 1
+                window.setTimeout(function() {
+                    $scene2.css('visibility', ''); // revert to default (hidden)
+                    $thankYou.blur();
+                }, 500);
+            }
+        }
+    }
+
+    // Bind events on domReady.
+    $(function() {
+        if (can_hashchange) {
+            window.addEventListener('hashchange', function(e) {
+                if (window.location.hash === '#download') {
+                    show_scene(2);
+                } else {
+                    show_scene(1);
+                }
+            });
+        }
+
+        // Pull Firefox download link from the download button and add to the
+        // 'click here' link.
+        // TODO: Remove and generate link in bedrock.
+        var $li = $('.download-list li:visible').filter(':first');
+        var ff_dl_url = $li.find('a:first').attr('href');
+        $('#direct-download-link').attr('href', ff_dl_url).on('click', function(e) {
+            e.preventDefault();
+            ga_track();
+            dl_redirect($(this).attr('href'));
+        });
+
+        // Trigger animation after download.
+        $('.download-firefox').on('click', function(e) {
+            e.preventDefault();
+
+            var $link = $(this);
+            ga_track();
+
+            if (can_hashchange) {
+                window.location.href += '#download';
+            } else {
+                show_scene(2);
             }
 
             dl_redirect($link.attr('href'));
