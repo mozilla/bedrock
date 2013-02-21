3277ee2fe858ce23eba2bcf5135e25b19e341736
Steven Garrity <steven@silverorange.com>
2013-02-21 11:15:58 -0400

    IE style fixes

--- settings/base.py
+++ settings/base.py
@@ -438,6 +438,7 @@ MINIFY_BUNDLES = {
         ),
         'partners': (
             'js/libs/modernizr.custom.shiv-load.js',
+            'js/mozilla-input-placeholder.js',
             'js/mozilla-pager.js',
             'js/firefox/partners.js',
         ),
