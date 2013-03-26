95decbcf0e62b36a4524cf93778253166394718d
Jon Petto <jonpetto@gmail.com>
2013-03-26 17:55:39 -0500

    Added stub for GA tracking slide views.
    Will track 3 separate actions - auto, click, keydown.

--- media/js/mozilla15.js
+++ media/js/mozilla15.js
@@ -2,24 +2,49 @@
  * License, v. 2.0. If a copy of the MPL was not distributed with this
  * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
 
-(function() {
+;(function($) {
     "use strict";
+
+    // default user action to auto
+    var action = 'auto';
 
     var options = {
         nextButton: '.next',
         prevButton: '.prev',
-        autoPlay: false,
+        autoPlay: true,
         autoPlayDelay: 5000,
         pauseOnHover: true,
         fadeFrameWhenSkipped: false,
         animateStartingFrameIn: true,
         moveActiveFrameToTop: false,
-        preloader: true,
-        startingFrameID: '7',
+        preloader: true
     };
 
     var slideshow = $('#slideshow').sequence(options).data('sequence');
 
+    var track = function() {
+        // must use nextFrame here - currentFrame (oddly) isn't updated yet.
+        console.log('tracking ' + slideshow.nextFrame[0].id + ' with action ' + action);
 
+        // reset action to autoplay
+        action = 'auto';
+    };
 
-})();
+    // track animations when next frame comes in
+    slideshow.afterNextFrameAnimatesIn = function() {
+        track();
+    };
+
+    $('.next, .prev').on('click', function(e) {
+        e.preventDefault();
+        action = 'click';
+    });
+
+    $(document).on('keydown', function(e) {
+        var key = e.keyCode;
+
+        if (key === 39 || key === 37) {
+            action = 'keydown';
+        }
+    });
+})(jQuery);
