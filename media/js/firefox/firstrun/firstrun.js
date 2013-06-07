5400df3e6b0d5220693a2ecea9cd72fb4f315562
Jon Petto <jonpetto@gmail.com>
2013-06-07 09:00:57 -0500

    Add GA track to /firefox/firstrun/ newsletter. Bug 880263.
    Remove WebTrends code from email_newsletter_form macro.

--- media/js/firefox/firstrun/firstrun.js
+++ media/js/firefox/firstrun/firstrun.js
@@ -3,9 +3,9 @@
 
   var $video = $('#firstrun-video');
 
-  var gaq_track = function(action) {
+  var gaq_track = function(category, action, label) {
     if (window._gaq) {
-      _gaq.push(['_trackEvent', "first run interaction", action, "First Run Video"]);
+      _gaq.push(['_trackEvent', category, action, label]);
     }
   };
 
@@ -17,6 +17,19 @@
     // using 'ended' results in extra pause tracking.
     var action = ($video[0].currentTime === $video[0].duration) ? 'finish' : 'pause';
 
-    gaq_track(action);
+    gaq_track('first run interaction', action, 'First Run Video');
+  });
+
+  $('#footer_email_submit').on('click', function(e) {
+    // if form is valid, delay submission to wait for GA tracking
+    if ($('#footer-email-form')[0].checkValidity()) {
+      e.preventDefault();
+
+      gaq_track("Newsletter Registration", "submit", "Registered for Firefox Updates");
+
+      setTimeout(function() {
+        $('#footer-email-form').submit();
+      }, 500);
+    }
   });
 })(window.jQuery);
