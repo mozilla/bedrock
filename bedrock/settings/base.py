eb27e5623ea04bf295187e9230a8c66497329386
Craig Cook <craigcook@gmail.com>
2013-08-14 15:46:27 -0700

    Bug 904765 - update Flicks promo on home page

--- bedrock/settings/base.py
+++ bedrock/settings/base.py
@@ -257,8 +257,6 @@ MINIFY_BUNDLES = {
         'home': (
             'css/mozorg/home.less',
             'css/mozorg/home-promo.less',
-            'js/libs/video-js/video-js.css',
-            'js/libs/video-js/video-js-sandstone.css',
         ),
         'marketplace': (
             'css/marketplace/marketplace.less',
@@ -501,8 +499,6 @@ MINIFY_BUNDLES = {
         ),
         'home': (
             'js/base/mozilla-pager.js',
-            'js/libs/video-js/video.dev.js',
-            'js/mozorg/home.js'
         ),
         'marketplace': (
             'js/base/nav-main-resp.js',
