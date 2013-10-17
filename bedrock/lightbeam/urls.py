ffd985682dea5983c730779257d6dc604292ccae
Mavis Ou <mmmavisss@gmail.com>
2013-10-17 11:12:07 -0700

    added filed to /lightbeam

--- bedrock/lightbeam/urls.py
+++ bedrock/lightbeam/urls.py
@@ -7,5 +7,8 @@ from bedrock.mozorg.util import page
 
 urlpatterns = patterns('',
     page('demo', 'lightbeam/demo.html'),
-    page('', 'lightbeam/collusion.html'),
+    page('', 'lightbeam/lightbeam.html'),
+    page('database', 'lightbeam/database.html'),
+    page('about', 'lightbeam/about.html'),
+    page('profile', 'lightbeam/profile.html'),
 )
