569e7cd99a4946e28b97b5b9778ea711d1d8cef7
Paul McLanahan <pmac@mozilla.com>
2013-11-27 12:31:43 -0500

    Bug 931310: Further improve New Relic deploy ping.
    * Move revision.txt handling to update instead of pre_update.
    * Get a real change log instead of just a github url.
    * Steal wonderful description code from chief-james (willkg)

--- bin/update/deploy_base.py
+++ bin/update/deploy_base.py
@@ -5,6 +5,8 @@ Requires commander (https://github.com/oremj/commander) which is installed on
 the systems that need it.
 """
 import os
+import random
+import re
 import urllib
 import urllib2
 
@@ -42,6 +44,13 @@ def update_assets(ctx):
 
 
 @task
+def update_revision_file(ctx):
+    with ctx.lcd(settings.SRC_DIR):
+        ctx.local("mv media/revision.txt media/prev-revision.txt")
+        ctx.local("git rev-parse HEAD > media/revision.txt")
+
+
+@task
 def database(ctx):
     with ctx.lcd(settings.SRC_DIR):
         ctx.local("python2.6 manage.py syncdb --migrate --noinput")
@@ -67,14 +76,10 @@ def update_info(ctx):
         ctx.local("git log -3")
         ctx.local("git status")
         ctx.local("git submodule status")
-        #ctx.local("python ./vendor/src/schematic/schematic -v migrations/")
-        #ctx.local("python ./manage.py migrate --list")
+        ctx.local("python ./manage.py migrate --list")
         with ctx.lcd("locale"):
             ctx.local("svn info")
             ctx.local("svn status")
-
-        ctx.local("mv media/revision.txt media/prev-revision.txt")
-        ctx.local("git rev-parse HEAD > media/revision.txt")
 
 
 @task
@@ -83,14 +88,19 @@ def ping_newrelic(ctx):
         with ctx.lcd(settings.SRC_DIR):
             oldrev = ctx.local('cat media/prev-revision.txt').out.strip()
             newrev = ctx.local('cat media/revision.txt').out.strip()
+            log_cmd = 'git log --oneline {0}..{1}'.format(oldrev, newrev)
+            changelog = ctx.local(log_cmd).out.strip()
 
         print 'Post deployment to New Relic'
+        desc = generate_desc(oldrev, newrev, changelog)
+        if changelog:
+            github_url = GITHUB_URL.format(oldrev=oldrev, newrev=newrev)
+            changelog = '{0}\n\n{1}'.format(changelog, github_url)
         data = urllib.urlencode({
-            'deployment[description]': 'Chief Deployment',
+            'deployment[description]': desc,
             'deployment[revision]': newrev,
             'deployment[app_id]': NEW_RELIC_APP_ID,
-            'deployment[changelog]': GITHUB_URL.format(oldrev=oldrev,
-                                                       newrev=newrev),
+            'deployment[changelog]': changelog,
         })
         headers = {'x-api-key': NEW_RELIC_API_KEY}
         try:
@@ -111,6 +121,7 @@ def update(ctx):
     commands['update_assets']()
     commands['update_locales']()
     commands['database']()
+    commands['update_revision_file']()
 
 
 @task
@@ -125,3 +136,41 @@ def update_bedrock(ctx, tag):
     """Do typical bedrock update"""
     commands['pre_update'](tag)
     commands['update']()
+
+
+## utility functions ##
+# shamelessly stolen from https://github.com/mythmon/chief-james/
+
+
+def get_random_desc():
+    return random.choice([
+        'No bugfixes--must be adding infinite loops.',
+        'No bugfixes--must be rot13ing function names for code security.',
+        'No bugfixes--must be demonstrating our elite push technology.',
+        'No bugfixes--must be testing james.',
+    ])
+
+
+def extract_bugs(changelog):
+    """Takes output from git log --oneline and extracts bug numbers"""
+    bug_regexp = re.compile(r'\bbug (\d+)\b', re.I)
+    bugs = set()
+    for line in changelog:
+        for bug in bug_regexp.findall(line):
+            bugs.add(bug)
+
+    return sorted(list(bugs))
+
+
+def generate_desc(from_commit, to_commit, changelog):
+    """Figures out a good description based on what we're pushing out."""
+    if from_commit.startswith(to_commit):
+        desc = 'Pushing {0} again'.format(to_commit)
+    else:
+        bugs = extract_bugs(changelog.split('\n'))
+        if bugs:
+            bugs = ['bug #{0}'.format(bug) for bug in bugs]
+            desc = 'Fixing: {0}'.format(', '.join(bugs))
+        else:
+            desc = get_random_desc()
+    return desc
