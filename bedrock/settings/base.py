ee659803a8add13a677a96034c9912029fad247d
Francesco Lodolo (:flod) <flod@lodolo.net>
2013-12-04 14:56:45 +0100

    Add more locales to VTCS
    Added locale: sk
    Fixed alphabetical order (hr-hy-AM)

--- bedrock/settings/base.py
+++ bedrock/settings/base.py
@@ -767,15 +767,15 @@ LOCALES_WITH_MOZ15 = ['bg', 'cs', 'de', 'el', 'en-GB', 'en-US', 'es-AR', 'es-CL'
 # Locales with the Lightbeam and Webmaker banners on home page
 LOCALES_WITH_LIGHTBEAM_HOME = ['en-GB', 'en-US', 'en-ZA', 'ar', 'ca', 'cs', 'cy',
                                'da', 'de', 'el', 'es-AR', 'es-CL', 'es-ES', 'es-MX',
-                               'et', 'eu', 'fr', 'fy-NL', 'hy-AM', 'hr', 'is', 'it',
+                               'et', 'eu', 'fr', 'fy-NL', 'hr', 'hy-AM', 'is', 'it',
                                'ko', 'mk', 'nl', 'pl', 'pt-BR', 'ru', 'sq', 'sv-SE',
                                'tr', 'uk', 'xh', 'zh-TW']
 
 # Locales with the Vans Triple Crown banner on home page
 LOCALES_WITH_VTCS_HOME = ['en-GB', 'en-US', 'en-ZA', 'ar', 'ca', 'cs', 'cy', 'da', 'de',
                           'el', 'es-AR', 'es-CL', 'es-ES', 'es-MX', 'et', 'eu', 'fr',
-                          'fy-NL', 'hy-AM', 'hr', 'is', 'it', 'ko', 'mk', 'nl', 'pl',
-                          'pt-BR', 'ru', 'sq', 'sv-SE', 'tr', 'uk', 'zh-TW']
+                          'fy-NL', 'hr', 'hy-AM', 'is', 'it', 'ko', 'mk', 'nl', 'pl',
+                          'pt-BR', 'ru', 'sk', 'sq', 'sv-SE', 'tr', 'uk', 'zh-TW']
 
 # Locales with the Donate banner on home page
 LOCALES_WITH_DONATE_HOME = ['en-US', 'de', 'fr', 'pt-BR']
