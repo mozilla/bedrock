670ceb2502875cdfcf898d960ed485c46633a245
Mavis Ou <mmmavisss@gmail.com>
2014-01-27 13:40:47 -0800

    Add back missing moduels

---
+++ bedrock/gigabit/urls.py
@@ -0,0 +1,11 @@
+# This Source Code Form is subject to the terms of the Mozilla Public
+# License, v. 2.0. If a copy of the MPL was not distributed with this
+# file, You can obtain one at http://mozilla.org/MPL/2.0/.
+
+from django.conf.urls import patterns
+from bedrock.mozorg.util import page
+
+urlpatterns = patterns('',
+    page('', 'gigabit/gigabit.html'),
+    page('faq', 'gigabit/faq.html')
+)
