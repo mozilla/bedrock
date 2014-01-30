3002901ee6e51c7594e182b2a12a6ee1ae2de9b5
Mavis Ou <mmmavisss@gmail.com>
2014-01-30 14:44:40 -0800

    Remove extra white space

--- bedrock/mozorg/helpers/download_buttons.py
+++ bedrock/mozorg/helpers/download_buttons.py
@@ -138,7 +138,6 @@ def download_firefox(ctx, build='release', small=False, icon=True,
             'latest', which bouncer will translate to the funnelcake build.
     :return: The button html.
     """
-
     alt_build = '' if build == 'release' else build
     platform = 'mobile' if mobile else 'desktop'
     locale = locale or get_locale(ctx['request'])
