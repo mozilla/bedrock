bd5e0b5dc761a8a3857172783773c41e8ccebd9e
Alex Gibson <agibson@mozilla.com>
2014-03-10 09:53:37 +0000

    [fix bug 981542] Add callback to the new close button in door-hanger

--- media/js/firefox/australis/australis-uitour.js
+++ media/js/firefox/australis/australis-uitour.js
@@ -4,153 +4,158 @@
 
 // create namespace
 if (typeof Mozilla == 'undefined') {
-    var Mozilla = {};
+	var Mozilla = {};
 }
 
 ;(function($) {
-    'use strict';
+	'use strict';
 
-    // create namespace
-    if (typeof Mozilla.UITour == 'undefined') {
-        Mozilla.UITour = {};
-    }
+	// create namespace
+	if (typeof Mozilla.UITour == 'undefined') {
+		Mozilla.UITour = {};
+	}
 
-    var themeIntervalId = null;
-    function _stopCyclingThemes() {
-        if (themeIntervalId) {
-            clearInterval(themeIntervalId);
-            themeIntervalId = null;
-        }
-    }
+	var themeIntervalId = null;
+	function _stopCyclingThemes() {
+		if (themeIntervalId) {
+			clearInterval(themeIntervalId);
+			themeIntervalId = null;
+		}
+	}
 
-    function _sendEvent(action, data) {
-        var event = new CustomEvent('mozUITour', {
-            bubbles: true,
-            detail: {
-                action: action,
-                data: data || {}
-            }
-        });
+	function _sendEvent(action, data) {
+		var event = new CustomEvent('mozUITour', {
+			bubbles: true,
+			detail: {
+				action: action,
+				data: data || {}
+			}
+		});
 
-        document.dispatchEvent(event);
-    }
+		document.dispatchEvent(event);
+	}
 
-    function _generateCallbackID() {
-        return Math.random().toString(36).replace(/[^a-z]+/g, '');
-    }
+	function _generateCallbackID() {
+		return Math.random().toString(36).replace(/[^a-z]+/g, '');
+	}
 
-    function _waitForCallback(callback) {
-        var id = _generateCallbackID();
+	function _waitForCallback(callback) {
+		var id = _generateCallbackID();
 
-        function listener(event) {
-            if (typeof event.detail != "object")
-                return;
-            if (event.detail.callbackID != id)
-                return;
+		function listener(event) {
+			if (typeof event.detail != "object")
+				return;
+			if (event.detail.callbackID != id)
+				return;
 
-            document.removeEventListener("mozUITourResponse", listener);
-            callback(event.detail.data);
-        }
-        document.addEventListener("mozUITourResponse", listener);
+			document.removeEventListener("mozUITourResponse", listener);
+			callback(event.detail.data);
+		}
+		document.addEventListener("mozUITourResponse", listener);
 
-        return id;
-    }
+		return id;
+	}
 
-    Mozilla.UITour.DEFAULT_THEME_CYCLE_DELAY = 10 * 1000;
+	Mozilla.UITour.DEFAULT_THEME_CYCLE_DELAY = 10 * 1000;
 
-    Mozilla.UITour.registerPageID = function(pageID) {
-        _sendEvent('registerPageID', {
-            pageID: pageID
-        });
-    };
+	Mozilla.UITour.registerPageID = function(pageID) {
+		_sendEvent('registerPageID', {
+			pageID: pageID
+		});
+	};
 
-    Mozilla.UITour.showHighlight = function(target, effect) {
-        _sendEvent('showHighlight', {
-            target: target,
-            effect: effect
-        });
-    };
+	Mozilla.UITour.showHighlight = function(target, effect) {
+		_sendEvent('showHighlight', {
+			target: target,
+			effect: effect
+		});
+	};
 
-    Mozilla.UITour.hideHighlight = function() {
-        _sendEvent('hideHighlight');
-    };
+	Mozilla.UITour.hideHighlight = function() {
+		_sendEvent('hideHighlight');
+	};
 
-    Mozilla.UITour.showInfo = function(target, title, text, icon, buttons) {
-        var buttonData = [];
-        if (Array.isArray(buttons)) {
-            for (var i = 0; i < buttons.length; i++) {
-                buttonData.push({
-                    label: buttons[i].label,
-                    icon: buttons[i].icon,
-                    style: buttons[i].style,
-                    callbackID: _waitForCallback(buttons[i].callback)
-                });
-            }
-        }
+	Mozilla.UITour.showInfo = function(target, title, text, icon, buttons, options) {
+		var buttonData = [];
+		if (Array.isArray(buttons)) {
+			for (var i = 0; i < buttons.length; i++) {
+				buttonData.push({
+					label: buttons[i].label,
+					icon: buttons[i].icon,
+					style: buttons[i].style,
+					callbackID: _waitForCallback(buttons[i].callback)
+				});
+			}
+		}
 
-        _sendEvent('showInfo', {
-            target: target,
-            title: title,
-            text: text,
-            icon: icon,
-            buttons: buttonData
-        });
-    };
+		var closeButtonCallbackID;
+		if (options && options.closeButtonCallback)
+			closeButtonCallbackID = _waitForCallback(options.closeButtonCallback);
 
-    Mozilla.UITour.hideInfo = function() {
-        _sendEvent('hideInfo');
-    };
+		_sendEvent('showInfo', {
+			target: target,
+			title: title,
+			text: text,
+			icon: icon,
+			buttons: buttonData,
+			closeButtonCallbackID: closeButtonCallbackID
+		});
+	};
 
-    Mozilla.UITour.previewTheme = function(theme) {
-        _stopCyclingThemes();
+	Mozilla.UITour.hideInfo = function() {
+		_sendEvent('hideInfo');
+	};
 
-        _sendEvent('previewTheme', {
-            theme: JSON.stringify(theme)
-        });
-    };
+	Mozilla.UITour.previewTheme = function(theme) {
+		_stopCyclingThemes();
 
-    Mozilla.UITour.resetTheme = function() {
-        _stopCyclingThemes();
+		_sendEvent('previewTheme', {
+			theme: JSON.stringify(theme)
+		});
+	};
 
-        _sendEvent('resetTheme');
-    };
+	Mozilla.UITour.resetTheme = function() {
+		_stopCyclingThemes();
 
-    Mozilla.UITour.cycleThemes = function(themes, delay, callback) {
-        _stopCyclingThemes();
+		_sendEvent('resetTheme');
+	};
 
-        if (!delay) {
-            delay = Mozilla.UITour.DEFAULT_THEME_CYCLE_DELAY;
-        }
+	Mozilla.UITour.cycleThemes = function(themes, delay, callback) {
+		_stopCyclingThemes();
 
-        function nextTheme() {
-            var theme = themes.shift();
-            themes.push(theme);
+		if (!delay) {
+			delay = Mozilla.UITour.DEFAULT_THEME_CYCLE_DELAY;
+		}
 
-            _sendEvent('previewTheme', {
-                theme: JSON.stringify(theme),
-                state: true
-            });
+		function nextTheme() {
+			var theme = themes.shift();
+			themes.push(theme);
 
-            callback(theme);
-        }
+			_sendEvent('previewTheme', {
+				theme: JSON.stringify(theme),
+				state: true
+			});
 
-        themeIntervalId = setInterval(nextTheme, delay);
-        nextTheme();
-    };
+			callback(theme);
+		}
 
-    Mozilla.UITour.addPinnedTab = function() {
-        _sendEvent('addPinnedTab');
-    };
+		themeIntervalId = setInterval(nextTheme, delay);
+		nextTheme();
+	};
 
-    Mozilla.UITour.removePinnedTab = function() {
-        _sendEvent('removePinnedTab');
-    };
+	Mozilla.UITour.addPinnedTab = function() {
+		_sendEvent('addPinnedTab');
+	};
 
-    Mozilla.UITour.showMenu = function(name) {
-        _sendEvent('showMenu', {
-            name: name
-        });
-    };
+	Mozilla.UITour.removePinnedTab = function() {
+		_sendEvent('removePinnedTab');
+	};
+
+	Mozilla.UITour.showMenu = function(name) {
+		_sendEvent('showMenu', {
+			name: name
+		});
+	};
 
         Mozilla.UITour.hideMenu = function(name) {
                 _sendEvent('hideMenu', {
@@ -158,21 +163,21 @@ if (typeof Mozilla == 'undefined') {
                 });
         };
 
-    Mozilla.UITour.startUrlbarCapture = function(text, url) {
-        _sendEvent('startUrlbarCapture', {
-            text: text,
-            url: url
-        });
-    };
+	Mozilla.UITour.startUrlbarCapture = function(text, url) {
+		_sendEvent('startUrlbarCapture', {
+			text: text,
+			url: url
+		});
+	};
 
-    Mozilla.UITour.endUrlbarCapture = function() {
-        _sendEvent('endUrlbarCapture');
-    };
+	Mozilla.UITour.endUrlbarCapture = function() {
+		_sendEvent('endUrlbarCapture');
+	};
 
-    Mozilla.UITour.getSyncConfiguration = function(callback) {
-        _sendEvent('getConfiguration', {
-            callbackID: _waitForCallback(callback),
-            configuration: "sync",
-        });
-    };
+	Mozilla.UITour.getSyncConfiguration = function(callback) {
+		_sendEvent('getConfiguration', {
+			callbackID: _waitForCallback(callback),
+			configuration: "sync",
+		});
+	};
 })();
