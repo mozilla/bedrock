2041ef596303bae14edc797a9b21aeabe3ad3a3a
Josh Mize <jgmize@gmail.com>
2014-03-16 21:42:26 +0000

    Alter version_re to match beta to fix bug 984168

--- bedrock/firefox/__init__.py
+++ bedrock/firefox/__init__.py
@@ -3,11 +3,11 @@
 # file, You can obtain one at http://mozilla.org/MPL/2.0/.
 
 # Adapted from django-mozilla-product-details
-version_re = (r"\d+"         # major (x in x.y)
-              "\.\d+"        # minor1 (y in x.y)
-              "\.?(?:\d+)?"  # minor2 (z in x.y.z)
-              "\.?(?:\d+)?"  # minor3 (w in x.y.z.w)
-              "(?:[a|b]?)"   # alpha/beta
-              "(?:\d*)"      # alpha/beta version
-              "(?:pre)?"     # pre release
-              "(?:\d)?")     # pre release version
+version_re = (r"\d+"              # major (x in x.y)
+              "\.\d+"             # minor1 (y in x.y)
+              "\.?(?:\d+)?"       # minor2 (z in x.y.z)
+              "\.?(?:\d+)?"       # minor3 (w in x.y.z.w)
+              "(?:a|b(?:eta)?)?"  # alpha/beta
+              "(?:\d*)"           # alpha/beta version
+              "(?:pre)?"          # pre release
+              "(?:\d)?")          # pre release version
