a9cc66b87b9b91ed88b18a82225cf5307c7e526e
Alex Gibson <agibson@mozilla.com>
2014-04-02 14:14:09 +0100

    [bug 729329] Add Sync animation to firefox/sync

--- bedrock/settings/base.py
+++ bedrock/settings/base.py
@@ -282,6 +282,9 @@ MINIFY_BUNDLES = {
         'firefox_sync': (
             'css/firefox/sync.less',
         ),
+        'firefox_sync_anim': (
+            'css/firefox/sync-animation.less',
+        ),
         'installer_help': (
             'css/base/mozilla-modal.less',
             'css/firefox/template-resp.less',
@@ -565,6 +568,9 @@ MINIFY_BUNDLES = {
         ),
         'firefox_speed': (
             'js/firefox/speed.js',
+        ),
+        'firefox_sync': (
+            'js/firefox/sync.js',
         ),
         'firefox_tech': (
             'js/firefox/technology/tech.js',
