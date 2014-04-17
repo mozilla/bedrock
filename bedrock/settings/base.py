d7d29ce1fd9a98cc48c6a680f59ee5fd162e629b
Alex Gibson <agibson@mozilla.com>
2014-04-17 13:59:42 +0100

    [bug 975156] Intro animation for Firefox Desktop overview page

--- bedrock/settings/base.py
+++ bedrock/settings/base.py
@@ -190,6 +190,7 @@ MINIFY_BUNDLES = {
         ),
         'firefox_desktop': (
             'css/sandstone/sandstone-resp.less',
+            'css/firefox/desktop/intro-anim.less',
             'css/firefox/desktop/index.less',
         ),
         'firefox_desktop_fast': (
@@ -546,7 +547,8 @@ MINIFY_BUNDLES = {
         'firefox_desktop_index': (
             'js/libs/jquery.waypoints.min.js',
             'js/firefox/desktop/common.js',
-            'js/firefox/desktop/speed-graph.js'
+            'js/firefox/desktop/speed-graph.js',
+            'js/firefox/desktop/intro-anim.js',
         ),
         'firefox_features': (
             'js/base/mozilla-video-tools.js',
