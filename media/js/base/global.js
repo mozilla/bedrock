4aee77e718583f9d6e4175a34316ffb60935e21d
Alex Gibson <agibson@mozilla.com>
2014-05-22 09:44:45 +0100

    [fix bug 977391] Fix GA tracking in form signup on /contribute

--- media/js/base/global.js
+++ media/js/base/global.js
@@ -153,31 +153,16 @@ function gaTrack(eventArray, callback) {
     //      $(thing).on('submit', handler);
     // });
 
-    var timer = null;
     var hasCallback = typeof(callback) === 'function';
-    var gaCallback;
 
-    // Only build new function if callback exists.
-    if (hasCallback) {
-        gaCallback = function() {
-            clearTimeout(timer);
-            callback();
-        };
-    }
     if (typeof(window._gaq) === 'object') {
         // send event to GA
         window._gaq.push(eventArray);
         // Only set up timer and hitCallback if a callback exists.
         if (hasCallback) {
-            // Failsafe - be sure we do the callback in a half-second
-            // even if GA isn't able to send in our trackEvent.
-            timer = setTimeout(gaCallback, 500);
-
-            // But ordinarily, we get GA to call us back immediately after
-            // it finishes sending our things.
-            // https://developers.google.com/analytics/devguides/collection/gajs/#PushingFunctions
-            // This is called after GA has sent the current pending data:
-            window._gaq.push(gaCallback);
+            // Need a timeout in order for __utm.gif request to complete in
+            // order to register the GA event, before excecuting the callback.
+            setTimeout(callback, 600);
         }
     } else {
         // GA disabled or blocked or something, make sure we still
