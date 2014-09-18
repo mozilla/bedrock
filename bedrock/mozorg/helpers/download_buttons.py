c685c9285fa677f1787c7e364ebb3e14f8b0b480
Paul McLanahan <pmac@mozilla.com>
2014-09-18 16:42:49 -0400

    Fix bug 1069545: OS X users should get 32.0 until Apple approves 32.0.2.

--- bedrock/mozorg/helpers/download_buttons.py
+++ bedrock/mozorg/helpers/download_buttons.py
@@ -173,10 +173,10 @@ def download_firefox(ctx, build='release', small=False, icon=True,
     if not mobile:
         for plat_os in ['Windows', 'Linux', 'Linux 64', 'OS X']:
             # Bug 1069545
-            # Have to serve OS X 32.0.1 until Apple whitelists 32.0.2 :/
+            # Have to serve OS X 32.0 until Apple whitelists 32.0.2 :/
             # TODO: REMOVE ME WHEN APPLE DOES THEIR JOB
-            if plat_os == 'OS X' and version == '32.0.2':
-                version = '32.0.1'
+            if plat_os == 'OS X' and version.startswith('32.0.'):
+                version = '32.0'
 
             # Fallback to en-US if this plat_os/version isn't available
             # for the current locale
