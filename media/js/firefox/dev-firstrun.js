a3b13db174d9175eabae5a94ece8afa7fad7ce4f
Alex Gibson <agibson@mozilla.com>
2014-10-30 10:31:30 +0000

    Defer loading iframe content until user triggers the modal

--- media/js/firefox/dev-firstrun.js
+++ media/js/firefox/dev-firstrun.js
@@ -9,14 +9,22 @@
         e.preventDefault();
 
         var $this = $(this);
+        var $video = $this.siblings('.video');
+        var $iframe = $('<iframe width="560" height="315" src="" frameborder="0" allowfullscreen></iframe>');
+
+        // defer loading of iframe content until the user clicks to activate the modal
+        // as loading the src triggers UITour panel content to close.
+        $iframe.attr('src', $video.data('src'));
+        $this.siblings('.video').html($iframe);
 
         Mozilla.Modal.createModal(this, $this.nextAll('.video'), {
             // grab the nearby h4 tag as the modal window title
             title: $this.siblings('h4,h2').text(),
             onCreate: function() {
-                $("#modal").fitVids();
+                $('#modal').fitVids();
             }
         });
     });
 
 })(window.jQuery, window.Mozilla);
+
