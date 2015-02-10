25006162824850868c4ba35e15576dd779acedba
Jon Petto <jonpetto@gmail.com>
2015-02-10 11:17:06 -0600

    Hello tour updates:
    - Add GA to "Learn More" links
    - Smooth animation
    - Small CSS fixes

--- media/js/firefox/australis/fx36/common.js
+++ media/js/firefox/australis/fx36/common.js
@@ -1,3 +1,19 @@
 (function() {
     'use strict';
+
+    var $learnMore = $('#learn-more');
+
+    $learnMore.on('click', function(e) {
+        var newTab = (this.target === '_blank' || e.metaKey || e.ctrlKey);
+        var href = this.href;
+
+        if (newTab) {
+            window.gaTrack(['_trackEvent', ' firstrun Page Interactions - New Firefox Tour','button click','Learn more about Hello']);
+        } else {
+            e.preventDefault();
+            window.gaTrack(['_trackEvent', ' firstrun Page Interactions - New Firefox Tour','button click','Learn more about Hello'], function() {
+                window.location = href;
+            });
+        }
+    });
 })();
