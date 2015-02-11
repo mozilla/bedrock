c26850ac773a53f11130b046569817ae8c138992
Nick <nthitz@gmail.com>
2015-02-11 22:09:59 +0000

    update embed scripts with new options

--- media/js/styleguide/mozilla-logo/logo-embed.js
+++ media/js/styleguide/mozilla-logo/logo-embed.js
@@ -2,7 +2,7 @@
 var mozillaIDConfig = {
   selector: '.mozillaLogo',
   size: 200,
-  options: {"controlType":"data","dataSource0":{"key":"sumo","label":"SUMO"},"dataSource1":{"key":"bugzilla","label":"Bugzilla"},"dataSource2":{"key":"firefox","label":"Firefox"},"numDataPoints":4,"graphRange":[0.15,0.5],"curveTightness":0.5,"angleOffset":0,"opacity":90,"colorPalette":"vibrant","fill":"gradient","mozillaLogo":"show","colorIndex0":0,"colorIndex1":1,"colorIndex2":2,"dataKey":{"key":"totalactive","label":"Total Active"},"animationDuration":30000,"mColor":"#fff","animation":"animated","gradientDirection0":true,"gradientDirection1":true,"gradientDirection2":true,"shapeMaxSize":240,"shapeMinSize":72}
+  options: {"controlType":"data","dataSource0":{"key":"sumo","label":"SUMO"},"dataSource1":{"key":"bugzilla","label":"Bugzilla"},"dataSource2":{"key":"firefox","label":"Firefox"},"numDataPoints":4,"graphRange":[0.35,1],"curveTightness":0.5,"angleOffset":0,"opacity":90,"colorPalette":"vibrant","fill":"gradient","mozillaLogo":"show","colorIndex0":0,"colorIndex1":1,"colorIndex2":2,"dataKey":{"key":"totalactive","label":"Total Active"},"animationDuration":30000,"mColor":"#fff","animation":"animated","gradientDirection0":true,"gradientDirection1":true,"gradientDirection2":true,"shapeMaxSize":240,"shapeMinSize":84}
 };
 (function() {
   var ml = document.createElement('script');
