da749aad9a1ba9d4483252e0a68b6a18c6bfefba
Alex Gibson <agibson@mozilla.com>
2015-03-09 10:34:25 +0000

    [fix bug 1139001] Add localized phone images to hero section on /firefox/os/
    - Also remove unused hero section animation code

--- media/js/firefox/os/firefox-os-new.js
+++ media/js/firefox/os/firefox-os-new.js
@@ -16,14 +16,6 @@
     var $apps = $('li', '.apps');
     var $categoryTriggers = $('a', $appGroupSelector);
 
-    var $demoContainer = $('.demo');
-    var $fxosHeroSpace = $('.fxos-hero-space');
-    var $phoneViewContainer = $('.phone');
-
-    var $stage = $('.stage');
-    var $scrollHomescreen = $('.scroll-homescreen');
-    var $swipeApps = $('.swipe-apps');
-
     //get modal logo path from data attribute in template
     var modalLogo = $('#modal-logo').data('src');
 
@@ -95,77 +87,6 @@
                 $currentImg.removeClass('fade');
             }
         });
-    });
-
-    /**
-     * Toggles the active-state class on all links in the container, essentially
-     * causing the newly active link to be set to active.
-     * @params {object} container - The container in which to find the links.
-     * @params {object} [button] - An optional button argument to specify which
-     *                             element to set as active.
-     */
-    function setActiveButton(container, button) {
-        if (button) {
-            $('li > a', container).removeClass('active-state');
-            button.addClass('active-state');
-        } else {
-            $('li > a', container).toggleClass('active-state');
-        }
-    }
-
-    /**
-     * Switches the state of the hero area from full to demo view.
-     * @param {object} [demo] - The demo to enable and animate
-     */
-    function switchState(demo) {
-        // hide both demos and ensure animation is disabled
-        $stage.removeClass('animate');
-
-        // entering demo mode, trim the container height
-        $fxosHeroSpace.toggleClass('trim-height');
-        // scales and fades the large phone image
-        $phoneViewContainer.toggleClass('scale');
-        // activate the demo area
-        $demoContainer.toggleClass('fxos-active');
-
-        // if a demo was specified show and enable animation
-        if (demo) {
-            demo.addClass('animate');
-            setActiveButton($demoContainer, $(demo.data('button')));
-        }
-    }
-
-    $phoneViewContainer.on('click', 'a', function(event) {
-
-        var targetID = event.currentTarget.id;
-
-        // only handle clicks for the demo triggers
-        if (targetID.indexOf('trigger') > -1) {
-            event.preventDefault();
-            var demo = targetID === 'trigger-swipe' ? $swipeApps : $scrollHomescreen;
-            switchState(demo);
-        }
-    });
-
-    $demoContainer.on('click', 'a', function(event) {
-
-        event.preventDefault();
-        var targetID = event.currentTarget.id;
-
-        if (targetID === 'close-demo') {
-            // close the demo view and switch back to default
-            switchState();
-        } else if (targetID === 'scroll-homescreen') {
-            // show the scroll homescreen animation
-            $swipeApps.removeClass('animate');
-            setActiveButton($demoContainer);
-            $scrollHomescreen.addClass('animate');
-        } else if (targetID === 'swipe-apps') {
-            // show swipe apps anim and make it play
-            $scrollHomescreen.removeClass('animate');
-            setActiveButton($demoContainer);
-            $swipeApps.addClass('animate');
-        }
     });
 
     window.pause_ga_tracking = false;
