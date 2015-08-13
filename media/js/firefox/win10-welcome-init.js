1ae6a15df74b66bd1e313b2ff5afdd606a0cff34
Alex Gibson <agibson@mozilla.com>
2015-08-13 18:06:28 +0100

    [bug 1186485] Temporarily disable Windows 10 default CTA

--- media/js/firefox/win10-welcome-init.js
+++ media/js/firefox/win10-welcome-init.js
@@ -5,13 +5,16 @@
 ;(function(Mozilla) {
     'use strict';
 
+    // temporarily disable UITour CTA until Firefox 40.0.1 is released.
+    Mozilla.Win10Welcome.showDefaultContent();
+
     // Conditional content for Firefox Desktop 40 and above only.
-    if (window.isFirefox() && !window.isFirefoxMobile() && window.getFirefoxMasterVersion() >= 40) {
-        Mozilla.Win10Welcome.initPage();
-    } else {
-        // We're not concerned about non-Firefox browsers for this page, but we need to
-        // show them something so here are some nice links.
-        Mozilla.Win10Welcome.showDefaultContent();
-    }
+    // if (window.isFirefox() && !window.isFirefoxMobile() && window.getFirefoxMasterVersion() >= 40) {
+    //     Mozilla.Win10Welcome.initPage();
+    // } else {
+    //     // We're not concerned about non-Firefox browsers for this page, but we need to
+    //     // show them something so here are some nice links.
+    //     Mozilla.Win10Welcome.showDefaultContent();
+    // }
 
 })(window.Mozilla);
