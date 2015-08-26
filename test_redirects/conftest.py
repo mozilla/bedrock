05058fcc107d4aa80ffaa6cc8bc90360b04967b5
Paul McLanahan <pmac@mozilla.com>
2015-08-26 11:03:21 -0400

    Add ability to test local bedrock instance.

--- test_redirects/conftest.py
+++ test_redirects/conftest.py
@@ -1,12 +1,23 @@
-from __future__ import absolute_import
-
-from operator import itemgetter
-
 import pytest
+from pytest_django.fixtures import live_server
 
-from .map_globalconf import URLS as GLOBAL_URLS
+
+def pytest_addoption(parser):
+    group = parser.getgroup('django')
+    group._addoption('--mozorg-url',
+                     action='store', type='string', dest='mozorg_url', default='local',
+                     help='URL of the mozorg server to test (default: local)')
 
 
-@pytest.fixture(params=GLOBAL_URLS, ids=itemgetter('url'))
-def global_conf_url(request):
-    return request.param
+def pytest_configure(config):
+    url = config.getvalue('mozorg_url')
+    config.mozorg_url = url.rstrip('/')
+
+
+@pytest.fixture(scope='session')
+def live_or_remote_server(request):
+    url = request.config.getvalue('mozorg_url').rstrip('/')
+    if url == 'local':
+        return live_server(request).url
+
+    return url
