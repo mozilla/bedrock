829b7f29fa44ca121266a4c24881ec4cb972dedf
Alex Gibson <agibson@mozilla.com>
2015-09-17 13:52:03 +0100

    [fix bug 1205282] Enable auto-downloads for IE 9 and above when landing on scene 2 of /firefox/new/

--- media/js/base/global.js
+++ media/js/base/global.js
@@ -6,16 +6,16 @@
 // download buttons
 
 /**
- * A special function for IE.  Without this hack there is no prompt to download after they click.  sigh.
- * bug 393263
- * Note IE has since removed the 'MSIE' token in version 11. The hack is no longer needed for IE11.
- * http://msdn.microsoft.com/en-us/library/ie/bg182625%28v=vs.85%29.aspx#uaString
- * @param string direct link to download URL
+ * Bug 393263 A special function for IE < 9.
+ * Without this hack there is no prompt to download after they click. sigh.
+ * @param {link} direct link to download URL
+ * @param {userAgent} optional UA string for testing purposes.
  */
-function trigger_ie_download(link, appVersion) {
-    var version = appVersion || navigator.appVersion;
-    // Only open if we got a link and this is IE < 11.
-    if (link && version.indexOf('MSIE') !== -1) {
+function trigger_ie_download(link, userAgent) {
+    'use strict';
+    var ua = userAgent !== undefined ? userAgent : navigator.userAgent;
+    // Only open if we got a link and this is IE < 9.
+    if (link && window.site.platform === 'windows' && /MSIE\s[1-8]\./.test(ua)) {
         window.open(link, 'download_window', 'toolbar=0,location=no,directories=0,status=0,scrollbars=0,resizeable=0,width=1,height=1,top=0,left=0');
         window.focus();
     }
