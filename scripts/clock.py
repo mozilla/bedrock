c0507a5d849aa74cab5ed5bccaac33a952460571
Paul McLanahan <pmac@mozilla.com>
2015-09-21 13:52:37 -0400

    Improve scheduled_job decorator.

--- scripts/clock.py
+++ scripts/clock.py
@@ -1,5 +1,4 @@
 from __future__ import print_function
-from functools import wraps
 
 from django.core.management import call_command
 
@@ -14,6 +13,7 @@ schedule = BlockingScheduler()
 
 
 class scheduled_job(object):
+    """Decorator for scheduled jobs. Takes same args as apscheduler.schedule_job."""
     def __init__(self, *args, **kwargs):
         self.args = args
         self.kwargs = kwargs
@@ -21,28 +21,30 @@ class scheduled_job(object):
     def __call__(self, fn):
         job_name = fn.__name__
         self.name = job_name
+        self.callback = fn
+        schedule.add_job(self.run, id=job_name, *self.args, **self.kwargs)
+        return self.run
 
-        @wraps(fn)
-        def wrapped():
-            print('Starting Clock Job: {}'.format(job_name))
-            try:
-                fn()
-            except Exception as e:
-                print('Clock Job CRASHED: {}: {}'.format(job_name, e))
-                raise
-            else:
-                print('Finished Clock Job: {}'.format(job_name))
+    def run(self):
+        self.log('starting')
+        try:
+            self.callback()
+        except Exception as e:
+            self.log('CRASHED: {}'.format(e))
+            raise
+        else:
+            self.log('finished successfully')
 
-        schedule.add_job(wrapped, id=job_name, *self.args, **self.kwargs)
-        return wrapped
+    def log(self, message):
+        print('Clock job {0}: {1}'.format(self.name, message))
 
 
-@scheduled_job('interval', seconds=10)
+@scheduled_job('interval', seconds=5)
 def job_should_pass():
     print('DO ALL THE THINGS!!!')
 
 
-@scheduled_job('interval', seconds=10)
+@scheduled_job('interval', seconds=5)
 def job_raise_exception():
     raise ValueError("Stuff's broke yo!")
 
