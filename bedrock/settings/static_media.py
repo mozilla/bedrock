ecfa413711f182ecdf058e1a683123d286bac305
schalkneethling <sneethling@mozilla.com>
2015-09-22 12:04:31 +0200

    changing reference in less file directly

--- bedrock/settings/static_media.py
+++ bedrock/settings/static_media.py
@@ -3,7 +3,7 @@
 PIPELINE_CSS = {
     'csrf-failure': {
         'source_filenames': (
-            'css/sandstone/sandstone-rsp.less',
+            'css/sandstone/sandstone-resp.less',
             'css/csrf-failure.less',
         ),
         'output_filename': 'css/csrf-failure-bundle.css',
