03fd9071f75d581804940862438626bf39627cdf
Paul McLanahan <pmac@mozilla.com>
2015-10-02 15:28:05 -0400

    Revert "Fix bug 1210368: Update dev to use locales from git."
    This reverts commit 673d66657a426b61d0b512a1a254f8c86d8d0f44.

--- bin/update-scripts/dev/update-dev-locale.sh
+++ bin/update-scripts/dev/update-dev-locale.sh
@@ -1,14 +1,21 @@
 #!/bin/bash
 
+UPSTREAM=$(svn info https://svn.mozilla.org/projects/mozilla.com/trunk/locales | grep Revision | cut -f2 -d' ')
 cd /data/bedrock-dev/src/www-dev.allizom.org-django/bedrock
 
 pushd locale > /dev/null
-CUR_REV=$(git rev-parse HEAD)
-git pull --ff-only origin master > /dev/null
-NEW_REV=$(git rev-parse HEAD)
+RUNNING=$(svnversion ./ | tr -d '[A-Z]')
+UPDATED=0
+if [ "$RUNNING" != "$UPSTREAM" ]; then
+    svn cleanup
+    svn -q up
+    UPDATED=1
+fi
 popd > /dev/null
 
-if [ "$NEW_REV" != "$CUR_REV" ]; then
-    echo "Deploying new locales: ${NEW_REV:0:10}"
+if [ $UPDATED -eq 1 ]; then
     /data/bedrock-dev/deploy -q www-dev.allizom.org-django/bedrock/locale
+else
+#    echo "Nothing to deploy."
+    exit 0
 fi
