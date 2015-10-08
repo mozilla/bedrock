372dc8cb3a6d825e20fa315065c8e1edf146cc42
Paul McLanahan <pmac@mozilla.com>
2015-10-08 15:02:21 -0400

    Fix django tests in jenkins.
    Also switches both Jenkins and Travis tests to run in-memory sqlite DB.

--- bedrock/settings/travis.py
+++ bedrock/settings/travis.py
@@ -6,20 +6,9 @@ import logging
 ROOT_URLCONF = 'bedrock.urls'
 LOG_LEVEL = logging.ERROR
 
+# need these for some email tests apparently
 ADMINS = ('thedude@example.com',)
 MANAGERS = ADMINS
-
-# Database name has to be set because of sphinx
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.mysql',
-        'NAME': 'bedrock_test',
-        'USER': 'travis',
-        'OPTIONS': {'init_command': 'SET storage_engine=InnoDB'},
-        'TEST_CHARSET': 'utf8',
-        'TEST_COLLATION': 'utf8_general_ci',
-    }
-}
 
 HMAC_KEYS = {
     '2013-01-01': 'prositneujahr',
