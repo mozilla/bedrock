7622cbdd43a3ec78db40b3312cd60ed92f47d4ac
Paul McLanahan <pmac@mozilla.com>
2015-10-09 09:47:17 -0400

    Bug 1210368: Use git-based locale data on dev/demo servers

--- bin/update/deploy_dev_base.py
+++ bin/update/deploy_dev_base.py
@@ -28,3 +28,18 @@ def update_assets(ctx):
     # can't do this in `database` because it needs to run before
     # the file sync from SRC -> WWW.
     management_cmd(ctx, 'update_product_details', use_src_dir=True)
+
+
+@task
+def update_info(ctx):
+    """Add a bunch of info to the deploy log."""
+    # Temporary while dev is using git locales and prod is using svn
+    with ctx.lcd(settings.SRC_DIR):
+        ctx.local("date")
+        ctx.local("git branch")
+        ctx.local("git log -3")
+        ctx.local("git status")
+        ctx.local("git submodule status")
+        with ctx.lcd("locale"):
+            ctx.local("git rev-parse HEAD")
+            ctx.local("git status")
