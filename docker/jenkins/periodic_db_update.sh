c4eb36e99cdb3b4b2014ab21a38c35ac976d3024
Josh Mize <jgmize@gmail.com>
2015-10-12 23:24:42 -0500

    Add periodic db update script for jenkins

---
+++ docker/jenkins/periodic_db_update.sh
@@ -0,0 +1,16 @@
+#!/bin/bash -ex
+# Needs DEIS_CONTROLLER URL, DEIS_USERNAME, DEIS_PASSWORD,
+# DEIS_PROFILE, and DEIS_APPLICATION environment variables.
+#
+# To set them go to Job -> Configure -> Build Environment -> Inject
+# passwords and Inject env variables
+#
+
+deis login $DEIS_CONTROLLER --username=$DEIS_USERNAME --password=$DEIS_PASSWORD
+deis run -a $DEIS_APPLICATION -- '\
+./manage.py rnasync; \
+./manage.py cron update_ical_feeds; \
+./manage.py update_product_details; \
+./manage.py update_externalfiles; \
+./manage.py update_security_advisories; \
+./manage.py cron update_tweets;'
