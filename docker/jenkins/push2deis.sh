515836852025bb2470aa8643bd385517a365b165
Giorgos Logiotatidis <seadog@sealabs.net>
2015-10-15 15:02:41 +0300

    Ping NewRelic on deploy.

--- docker/jenkins/push2deis.sh
+++ docker/jenkins/push2deis.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 # Needs DEIS_CONTROLLER URL, DEIS_USERNAME, DEIS_PASSWORD,
-# DOCKER_REPOSITORY and DEIS_APPLICATION environment variables.
+# DOCKER_REPOSITORY, DEIS_APPLICATION, NEWRELIC_API_KEY and
+# NEWRELIC_APP_NAME environment variables.
 #
 # To set them go to Job -> Configure -> Build Environment -> Inject
 # passwords and Inject env variables
@@ -10,3 +11,8 @@ set -ex
 
 deis login $DEIS_CONTROLLER  --username $DEIS_USERNAME --password $DEIS_PASSWORD
 deis pull $DOCKER_REPOSITORY:$GIT_COMMIT -a $DEIS_APPLICATION
+curl -H "x-api-key:$NEWRELIC_API_KEY" \
+     -d "deployment[app_name]=$NEWRELIC_APP_NAME" \
+     -d "deployment[revision]=$GIT_COMMIT" \
+     -d "deployment[user]=EE Jenkins" \
+     https://api.newrelic.com/deployments.xml
