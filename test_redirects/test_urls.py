c54b064469bc36f15ffa3af936b1ca523c2c65e3
Paul McLanahan <pmac@mozilla.com>
2015-11-05 11:11:51 -0500

    Add press redirects from .htaccess

--- test_redirects/test_urls.py
+++ test_redirects/test_urls.py
@@ -5,12 +5,20 @@ from operator import itemgetter
 import pytest
 
 from .base import assert_valid_url
-from .map_globalconf import URLS as GLOBAL_URLS
 from .map_410 import URLS_410
+from .map_htaccess import URLS as HTA_URLS
+from .map_globalconf import URLS as GLOBAL_URLS
 
 
 @pytest.mark.parametrize('url', GLOBAL_URLS, ids=itemgetter('url'))
 def test_global_conf_url(url, live_or_remote_server):
+    # live_or_remote_server defined in conftest.py
+    url['base_url'] = live_or_remote_server
+    assert_valid_url(**url)
+
+
+@pytest.mark.parametrize('url', HTA_URLS, ids=itemgetter('url'))
+def test_htaccess_url(url, live_or_remote_server):
     # live_or_remote_server defined in conftest.py
     url['base_url'] = live_or_remote_server
     assert_valid_url(**url)
