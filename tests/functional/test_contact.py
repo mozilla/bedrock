1abe0e00752095a9c3be13e490dfdef4244a4270
Dave Hunt <dave.hunt@gmail.com>
2015-12-02 22:46:10 +0000

    Add functional tests for contacts pages

---
+++ tests/functional/test_contact.py
@@ -0,0 +1,85 @@
+# This Source Code Form is subject to the terms of the Mozilla Public
+# License, v. 2.0. If a copy of the MPL was not distributed with this
+# file, You can obtain one at http://mozilla.org/MPL/2.0/.
+
+import pytest
+
+from pages.contact import ContactPage, SpacesPage, CommunitiesPage
+
+
+@pytest.mark.nondestructive
+def test_tab_navigation(base_url, selenium):
+    page = ContactPage(base_url, selenium).open()
+    assert page.contact_tab.is_selected
+    assert not page.spaces_tab.is_selected
+    assert not page.communities_tab.is_selected
+
+    spaces_page = page.click_spaces_tab()
+    assert not spaces_page.contact_tab.is_selected
+    assert spaces_page.spaces_tab.is_selected
+    assert not spaces_page.communities_tab.is_selected
+    assert spaces_page.url in selenium.current_url
+
+    communities_page = spaces_page.click_communities_tab()
+    assert not communities_page.contact_tab.is_selected
+    assert not communities_page.spaces_tab.is_selected
+    assert communities_page.communities_tab.is_selected
+    assert communities_page.url in selenium.current_url
+
+    contact_page = communities_page.click_contact_tab()
+    assert contact_page.contact_tab.is_selected
+    assert not contact_page.spaces_tab.is_selected
+    assert not contact_page.communities_tab.is_selected
+    assert contact_page.url in selenium.current_url
+
+
+@pytest.mark.nondestructive
+def test_spaces_list(base_url, selenium):
+    page = SpacesPage(base_url, selenium).open()
+    assert page.displayed_map_pins == len(page.spaces)
+    for space in page.spaces:
+        space.click()
+        assert space.is_selected
+        assert space.is_displayed
+        assert 1 == page.displayed_map_pins
+
+
+@pytest.mark.nondestructive
+def test_communities_region_list(base_url, selenium):
+    page = CommunitiesPage(base_url, selenium).open()
+    for region in page.regions:
+        region.click()
+        assert region.is_selected
+        assert region.is_displayed
+        key = next(k for k in page.keys if k.id == region.id)
+        assert key.is_selected
+
+
+@pytest.mark.nondestructive
+def test_communities_region_legend(base_url, selenium):
+    page = CommunitiesPage(base_url, selenium).open()
+    for key in page.keys:
+        key.click()
+        assert key.is_selected
+        region = next(r for r in page.regions if r.id == key.id)
+        assert region.is_selected
+        assert region.is_displayed
+
+
+@pytest.mark.nondestructive
+def test_communities_region_menus(base_url, selenium):
+    page = CommunitiesPage(base_url, selenium).open()
+    north_america = page.regions[0]
+    assert not north_america.is_menu_open
+    north_america.click()
+    assert north_america.is_menu_open
+    canada = north_america.communities[0]
+    assert not canada.is_selected
+    assert not canada.is_displayed
+    canada.click()
+    assert canada.is_selected
+    assert canada.is_displayed
+    page.regions[1].click()
+    assert not north_america.is_menu_open
+    assert not canada.is_selected
+    assert not canada.is_displayed
---
+++ tests/pages/contact.py
@@ -0,0 +1,175 @@
+# This Source Code Form is subject to the terms of the Mozilla Public
+# License, v. 2.0. If a copy of the MPL was not distributed with this
+# file, You can obtain one at http://mozilla.org/MPL/2.0/.
+
+from selenium.webdriver.common.by import By
+
+from pages.base import BasePage
+from pages.page import PageRegion
+
+
+class ContactPage(BasePage):
+
+    _url = '{base_url}/{locale}/contact'
+
+    _map_pins_locator = (By.CSS_SELECTOR, '#map img.leaflet-marker-icon')
+    _contact_tab_locator = (By.CSS_SELECTOR, '.category-tabs > li[data-id=contact]')
+    _spaces_tab_locator = (By.CSS_SELECTOR, '.category-tabs > li[data-id=spaces]')
+    _communities_tab_locator = (By.CSS_SELECTOR, '.category-tabs > li[data-id=communities]')
+
+    @property
+    def displayed_map_pins(self):
+        return len([el for el in self.find_elements(self._map_pins_locator) if el.is_displayed()])
+
+    @property
+    def contact_tab(self):
+        el = self.find_element(self._contact_tab_locator)
+        return self.Tab(self, root=el)
+
+    @property
+    def spaces_tab(self):
+        el = self.find_element(self._spaces_tab_locator)
+        return self.Tab(self, root=el)
+
+    @property
+    def communities_tab(self):
+        el = self.find_element(self._communities_tab_locator)
+        return self.Tab(self, root=el)
+
+    def click_contact_tab(self):
+        self.contact_tab.click()
+        return ContactPage(self.base_url, self.selenium).wait_for_page_to_load()
+
+    def click_spaces_tab(self):
+        self.spaces_tab.click()
+        return SpacesPage(self.base_url, self.selenium).wait_for_page_to_load()
+
+    def click_communities_tab(self):
+        self.communities_tab.click()
+        return CommunitiesPage(self.base_url, self.selenium).wait_for_page_to_load()
+
+    class Tab(PageRegion):
+
+        _link_locator = (By.TAG_NAME, 'a')
+
+        @property
+        def is_selected(self):
+            return 'current' in self._root.get_attribute('class')
+
+        def click(self):
+            self.find_element(self._link_locator).click()
+
+
+class SpacesPage(ContactPage):
+
+    _url = '{base_url}/{locale}/contact/spaces'
+
+    _spaces_locator = (By.ID, 'nav-spaces li')
+
+    @property
+    def spaces(self):
+        return [self.Space(self, root=el) for el in
+            self.find_elements(self._spaces_locator)]
+
+    class Space(PageRegion):
+
+        _link_locator = (By.TAG_NAME, 'a')
+
+        @property
+        def is_displayed(self):
+            return self.page.is_element_displayed(
+                (By.ID, self._root.get_attribute('data-id')))
+
+        @property
+        def is_selected(self):
+            return 'current' in self._root.get_attribute('class')
+
+        def click(self):
+            self.find_element(self._link_locator).click()
+            self.wait.until(lambda s: self.is_displayed)
+
+
+class CommunitiesPage(ContactPage):
+
+    _url = '{base_url}/{locale}/contact/communities'
+
+    _keys_locator = (By.CSS_SELECTOR, '#map .legend li')
+    _regions_locator = (By.CSS_SELECTOR, '#nav-communities .region')
+
+    @property
+    def keys(self):
+        return [self.Key(self, root=el) for el in
+            self.find_elements(self._keys_locator)]
+
+    @property
+    def regions(self):
+        return [self.Region(self, root=el) for el in
+            self.find_elements(self._regions_locator)]
+
+    class Key(PageRegion):
+
+        _link_locator = (By.TAG_NAME, 'a')
+
+        @property
+        def id(self):
+            return self._root.get_attribute('data-id')
+
+        @property
+        def is_selected(self):
+            el = self.find_element(self._link_locator)
+            return 'active' in el.get_attribute('class')
+
+        def click(self):
+            self.find_element(self._link_locator).click()
+            region = next(r for r in self.page.regions if r.id == self.id)
+            self.wait.until(lambda s: region.is_displayed)
+
+    class Region(PageRegion):
+
+        _communities_locator = (By.CSS_SELECTOR, '.submenu li')
+        _link_locator = (By.TAG_NAME, 'a')
+
+        @property
+        def id(self):
+            return self._root.get_attribute('data-id')
+
+        @property
+        def communities(self):
+            return [self.Community(self.page, root=el) for el in
+                self.find_elements(self._communities_locator)]
+
+        @property
+        def is_displayed(self):
+            return self.page.is_element_displayed((By.ID, self.id))
+
+        @property
+        def is_selected(self):
+            return 'current' in self._root.get_attribute('class')
+
+        @property
+        def is_menu_open(self):
+            return 'open' in self._root.get_attribute('class')
+
+        def click(self):
+            self.find_element(self._link_locator).click()
+            self.wait.until(lambda s: self.is_displayed)
+
+        class Community(PageRegion):
+
+            _link_locator = (By.TAG_NAME, 'a')
+
+            @property
+            def id(self):
+                return self._root.get_attribute('data-id')
+
+            @property
+            def is_displayed(self):
+                return self.page.is_element_displayed((By.ID, self.id))
+
+            @property
+            def is_selected(self):
+                return 'current' in self._root.get_attribute('class')
+
+            def click(self):
+                self.find_element(self._link_locator).click()
+                self.wait.until(lambda s: self.is_displayed)
