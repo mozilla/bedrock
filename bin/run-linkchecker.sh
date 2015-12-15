5734f43b5bc647e9439df2cd141fc45d078716d2
Dave Hunt <dave.hunt@gmail.com>
2015-12-15 00:05:08 +0000

    Add Dockerfile and scripts for running LinkChecker

---
+++ bin/run-linkchecker.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+# This Source Code Form is subject to the terms of the Mozilla Public
+# License, v. 2.0. If a copy of the MPL was not distributed with this
+# file, You can obtain one at http://mozilla.org/MPL/2.0/.
+
+set -xe
+
+CMD="linkchecker"
+CMD="${CMD} -F html/utf8//results/linkchecker-out.html"
+if [ -n "${THREADS}" ]; then CMD="${CMD} -t ${THREADS}"; fi
+if [ -n "${RECURSION_LEVEL}" ]; then CMD="${CMD} -r ${RECURSION_LEVEL}"; fi
+if [ -n "${VERBOSE}" ]; then CMD="${CMD} -v"; fi
+if [ -n "${CHECK_EXTERNAL}" ]; then CMD="${CMD} --check-extern"; fi
+
+CMD="${CMD} ${URLS}"
+eval ${CMD}
