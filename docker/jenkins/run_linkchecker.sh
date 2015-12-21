f5998701863ca23518dd72d94b6ceb8979ea91a3
Dave Hunt <dave.hunt@gmail.com>
2015-12-21 11:46:56 +0000

    Allow multiple URLs when running LinkChecker in Jenkins

--- docker/jenkins/run_linkchecker.sh
+++ docker/jenkins/run_linkchecker.sh
@@ -1,4 +1,4 @@
 #!/bin/bash -xe
 cp docker/dockerfiles/bedrock_linkchecker Dockerfile
 docker build -t bedrock_linkchecker:${GIT_COMMIT} --pull=true .
-docker run -v `pwd`/results:/results -e URLS=${URLS} -e THREADS=${THREADS} -e RECURSION_LEVEL=${RECURSION_LEVEL} -e CHECK_EXTERNAL=${CHECK_EXTERNAL} -e VERBOSE=${VERBOSE} bedrock_linkchecker:${GIT_COMMIT}
+docker run -v `pwd`/results:/results -e URLS="${URLS}" -e THREADS=${THREADS} -e RECURSION_LEVEL=${RECURSION_LEVEL} -e CHECK_EXTERNAL=${CHECK_EXTERNAL} -e VERBOSE=${VERBOSE} bedrock_linkchecker:${GIT_COMMIT}
