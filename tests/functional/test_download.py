f5dd35479ab76a5c624cae6cc9d368658037b22c
Dave Hunt <dave.hunt@gmail.com>
2016-01-11 16:11:01 +0000

    Mark download link checks as nondestructive

--- tests/functional/test_download.py
+++ tests/functional/test_download.py
@@ -22,6 +22,7 @@ def pytest_generate_tests(metafunc):
 
 
 @pytest.mark.headless
+@pytest.mark.nondestructive
 def test_download_links(url):
     r = requests.head(url, allow_redirects=True)
     assert requests.codes.ok == r.status_code
