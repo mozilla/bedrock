c71e107cf1457c80d6b286d2aa91110ce2c58cfe
Jon Petto <jonpetto@gmail.com>
2016-01-11 18:15:15 -0600

    [fix bug 1238061] Add iOS promos to /firefox/sync.

--- tests/pages/firefox/sync.py
+++ tests/pages/firefox/sync.py
@@ -12,12 +12,17 @@ class FirefoxSyncPage(FirefoxBasePage):
     _url = '{base_url}/{locale}/firefox/sync'
 
     _primary_download_button_locator = (By.ID, 'download-button-desktop-release')
-    _android_download_button_locator = (By.ID, 'cta-android-footer')
+    _play_store_button_locator = (By.ID, 'cta-android-footer')
+    _app_store_button_locator = (By.ID, 'cta-ios-footer')
 
     @property
     def is_primary_download_button_displayed(self):
         return self.is_element_displayed(self._primary_download_button_locator)
 
     @property
-    def is_android_download_button_displayed(self):
-        return self.is_element_displayed(self._android_download_button_locator)
+    def is_play_store_button_displayed(self):
+        return self.is_element_displayed(self._play_store_button_locator)
+
+    @property
+    def is_app_store_button_displayed(self):
+        return self.is_element_displayed(self._app_store_button_locator)
