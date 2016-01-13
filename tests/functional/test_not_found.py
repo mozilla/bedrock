eefe8fed0d319beb0f7348be1db5c85d50896b1b
Alex Gibson <agibson@mozilla.com>
2016-01-13 14:06:54 +0000

    Add functional test 404 page

---
+++ tests/functional/test_not_found.py
@@ -0,0 +1,14 @@
+# This Source Code Form is subject to the terms of the Mozilla Public
+# License, v. 2.0. If a copy of the MPL was not distributed with this
+# file, You can obtain one at http://mozilla.org/MPL/2.0/.
+
+import pytest
+
+from pages.not_found import NotFoundPage
+
+
+@pytest.mark.smoke
+@pytest.mark.nondestructive
+def test_is_download_button_displayed(base_url, selenium):
+    page = NotFoundPage(base_url, selenium).open()
+    assert page.download_button.is_displayed
