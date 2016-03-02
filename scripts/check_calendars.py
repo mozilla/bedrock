1fa01b8d86249ba64a59495a28555ae775471cb3
Paul McLanahan <pmac@mozilla.com>
2016-03-02 14:57:14 -0500

    Add CircleCI support

--- scripts/check_calendars.py
+++ scripts/check_calendars.py
@@ -1,7 +1,8 @@
 #!/usr/bin/env python
 
-
 import os
+import sys
+
 from icalendar import Calendar
 
 
@@ -23,10 +24,10 @@ def check_if_correct_parse(ics_file):
 
 
 def run(*args):
-    calendars_dir = os.path.join('media','caldata')
-    ics_files = map(lambda x: os.path.join(calendars_dir, x), 
+    calendars_dir = os.path.join('media', 'caldata')
+    ics_files = map(lambda x: os.path.join(calendars_dir, x),
                     filter(get_ics, os.listdir(calendars_dir)))
-    
+
     format_str = "Failed to parse the icalendar file: {}. {}"
     check_failed = False
     for f in ics_files:
@@ -37,16 +38,7 @@ def run(*args):
             print format_str.format(f, ve.message)
 
     if check_failed:
-        # Returning a positive error code, since we have nothing to do
-        # with these errors. They simply have to be reported back to
-        # caldata maintainers. Also, we have to return something
-        # other than zero - for travis to fail build over invalid files. 
-        # Please see: http://docs.travis-ci.com/user/build-lifecycle/
-        # """
-        #   When any of the steps in the script stage fails with a non-zero 
-        #   exit code, the build will be marked as failed.
-        # """
-        exit(1)
+        sys.exit(1)
 
 
 # vim: ts=4 sw=4 et ai
