4ad499661866fdbd7d90c98d6392b121ce8217bc
alexgibson <agibson@mozilla.com>
2016-06-01 09:15:02 +0100

    [bug 1275626] Temporarily skip homepage functional tests for prototype redesign

--- tests/functional/test_home.py
+++ tests/functional/test_home.py
@@ -7,6 +7,7 @@ import pytest
 from pages.home import HomePage
 
 
+@pytest.mark.skipif(reason='https://bugzilla.mozilla.org/show_bug.cgi?id=1275626')
 @pytest.mark.smoke
 @pytest.mark.nondestructive
 def test_promo_grid_is_displayed(base_url, selenium):
@@ -14,24 +15,28 @@ def test_promo_grid_is_displayed(base_url, selenium):
     assert page.is_promo_grid_displayed
 
 
+@pytest.mark.skipif(reason='https://bugzilla.mozilla.org/show_bug.cgi?id=1275626')
 @pytest.mark.nondestructive
 def test_promos_are_present(base_url, selenium):
     page = HomePage(selenium, base_url).open()
     assert page.number_of_promos_present == 16
 
 
+@pytest.mark.skipif(reason='https://bugzilla.mozilla.org/show_bug.cgi?id=1275626')
 @pytest.mark.nondestructive
 def test_tweet_is_present(base_url, selenium):
     page = HomePage(selenium, base_url).open()
     assert page.is_tweet_promo_present
 
 
+@pytest.mark.skipif(reason='https://bugzilla.mozilla.org/show_bug.cgi?id=1275626')
 @pytest.mark.nondestructive
 def test_tweet_is_not_present(base_url, selenium):
     page = HomePage(selenium, base_url, 'de').open()
     assert not page.is_tweet_promo_present
 
 
+@pytest.mark.skipif(reason='https://bugzilla.mozilla.org/show_bug.cgi?id=1275626')
 @pytest.mark.sanity
 @pytest.mark.smoke
 @pytest.mark.nondestructive
@@ -40,6 +45,7 @@ def test_download_button_is_displayed(base_url, selenium):
     assert page.download_button.is_displayed
 
 
+@pytest.mark.skipif(reason='https://bugzilla.mozilla.org/show_bug.cgi?id=1275626')
 @pytest.mark.nondestructive
 def test_upcoming_events_are_displayed(base_url, selenium):
     page = HomePage(selenium, base_url).open()
