d0c7edb37b78023a6e64b3f79aacbe8ed87679ee
Paul McLanahan <pmac@mozilla.com>
2016-08-17 16:34:22 -0400

    Remove special jenkins and c9 settings files

--- bedrock/settings/__init__.py
+++ bedrock/settings/__init__.py
@@ -1,20 +1,15 @@
 # This Source Code Form is subject to the terms of the Mozilla Public
 # License, v. 2.0. If a copy of the MPL was not distributed with this
 # file, You can obtain one at http://mozilla.org/MPL/2.0/.
-import os
+
 import sys
 
 from .base import *  # noqa
 
-if os.getenv('JENKINS_HOME', False):
-    from .jenkins import *  # noqa
-else:
-    if os.getenv('C9_USER'):
-        from .c9 import *  # noqa
-    try:
-        from .local import *  # noqa
-    except ImportError as exc:
-        'local.py is supported, but no longer necessary'
+try:
+    from .local import *  # noqa
+except ImportError as exc:
+    'local.py is supported, but no longer necessary'
 
 
 if DEV:
