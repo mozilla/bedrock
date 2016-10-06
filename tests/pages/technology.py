1dc0706052889d0bc792e53be21d0145cc50a2c6
alexgibson <agibson@mozilla.com>
2016-10-06 15:17:43 +0100

    [fix bug 1305955] Create functional tests for technology page

--- tests/pages/technology.py
+++ tests/pages/technology.py
@@ -10,3 +10,14 @@ from pages.base import BasePage
 class TechnologyPage(BasePage):
 
     URL_TEMPLATE = '/{locale}/technology'
+
+    _blog_feed_locator = (By.ID, 'blogs')
+    _blog_feed_articles_locator = (By.CSS_SELECTOR, '#blogs article')
+
+    @property
+    def is_blog_feed_displayed(self):
+        return self.is_element_displayed(*self._blog_feed_locator)
+
+    @property
+    def number_of_blog_articles_present(self):
+        return len(self.find_elements(*self._blog_feed_articles_locator))
