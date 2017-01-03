509b635e2412ea36e5cc3d0582cdb32a2ab05111
Paul McLanahan <pmac@mozilla.com>
2017-01-03 17:01:43 -0500

    Move jenkins IRC notification to bash script
    Add color and bold support to IRC messages

--- Jenkinsfile
+++ Jenkinsfile
@@ -1,4 +1,3 @@
-def branch = env.BRANCH_NAME
 env.DEIS_PROFILE = 'usw'
 env.PRIVATE_REGISTRY = 'localhost:5001'
 
@@ -7,46 +6,30 @@ env.PRIVATE_REGISTRY = 'localhost:5001'
  * @param stage step of build/deploy
  * @param result outcome of build (will be uppercased)
 */
-def ircNotification(stage, result) {
-    def nick = "bedrock-deployer-${env.BUILD_NUMBER}"
-    def channel = '#www'
-    result = result.toUpperCase()
-    def message = "${stage}: ${result}: Branch ${env.BRANCH_NAME} build #${env.BUILD_NUMBER}: ${env.BUILD_URL}"
-    sh """
-        (
-        echo NICK ${nick}
-        echo USER ${nick} 8 * : ${nick}
-        sleep 5
-        echo "JOIN ${channel}"
-        echo "NOTICE ${channel} :${message}"
-        echo QUIT
-        ) | openssl s_client -connect irc.mozilla.org:6697
-    """
+def ircNotification(stage, status) {
+    sh "bin/irc-notify.sh --stage '${stage}' --status '${status}'"
 }
 
-if ( branch == 'master') {
-    node {
+node {
+    def branch = env.BRANCH_NAME
+
+    stage ('git') {
+        checkout scm
+        sh 'git submodule sync'
+        sh 'git submodule update --init --recursive'
+        env.GIT_COMMIT = sh([returnStdout: true, script: 'git rev-parse HEAD']).trim()
+    }
+
+    if ( branch == 'master') {
         ircNotification('Dev Deploy', 'starting')
         build 'bedrock_base_image'
     }
-}
-else if ( branch == 'prod') {
-    node {
-        ircNotification('Prod Deploy', 'starting')
+    else if ( branch == 'prod') {
+        ircNotification('Stage & Prod Deploys', 'starting')
         build 'bedrock_base_image'
     }
-}
-else if ( branch ==~ /^demo__[\w-]+$/ ) {
-    node {
+    else if ( branch ==~ /^demo__[\w-]+$/ ) {
         ircNotification('Demo Deploy', 'starting')
-
-        stage ('git') {
-            checkout scm
-            sh 'git submodule sync'
-            sh 'git submodule update --init --recursive'
-            env.GIT_COMMIT = sh(returnStdout: true, script: 'git rev-parse HEAD').trim()
-        }
-
         try {
             stage ('build') {
                 sh 'make clean'
@@ -72,10 +55,8 @@ else if ( branch ==~ /^demo__[\w-]+$/ ) {
             ircNotification('Demo Deploy', 'failure')
             throw err
         }
-
-        ircNotification('Demo Deploy', 'success')
     }
-}
-else {
-    echo "Doing nothing for ${branch}"
+    else {
+        echo "Doing nothing for ${branch}"
+    }
 }
---
+++ bin/irc-notify.sh
@@ -0,0 +1,91 @@
+#!/bin/bash
+set -eo pipefail
+
+# Required environment variables if using --stage and --status:
+# BRANCH_NAME, BUILD_NUMBER, BUILD_URL
+
+# defaults and constants
+BUILD_NUMBER="${BUILD_NUMBER:-0}"
+NICK="bedrock-deployer-$BUILD_NUMBER"
+CHANNEL="#www"
+SERVER="irc.mozilla.org:6697"
+# colors and styles: values from the following links
+# http://www.mirc.com/colors.html
+# http://stackoverflow.com/a/13382032
+RED=$'\x034'
+GREEN=$'\x039'
+BLUE=$'\x0311'
+BOLD=$'\x02'
+NORMAL=$'\x0F'
+
+# parse cli args
+while [[ $# -gt 1 ]]; do
+    key="$1"
+    case $key in
+        --stage)
+            STAGE="$2"
+            shift # past argument
+            ;;
+        --status)
+            STATUS="$2"
+            shift # past argument
+            ;;
+        --demo_url)
+            DEMO_URL="$2"
+            shift # past argument
+            ;;
+        -m|--message)
+            MESSAGE="$2"
+            shift # past argument
+            ;;
+        --irc_nick)
+            NICK="$2"
+            shift # past argument
+            ;;
+        --irc_server)
+            SERVER="$2"
+            shift # past argument
+            ;;
+        --irc_channel)
+            CHANNEL="$2"
+            shift # past argument
+            ;;
+    esac
+    shift # past argument or value
+done
+
+if [[ -z "$MESSAGE" ]]; then
+    if [[ -n "$STATUS" ]]; then
+        STATUS=$(echo "$STATUS" | tr '[:lower:]' '[:upper:]')
+        case "$STATUS" in
+          'SUCCESS')
+            STATUS_COLOR="${BOLD}${GREEN}"
+            ;;
+          'FAILURE')
+            STATUS_COLOR="${BOLD}${RED}"
+            ;;
+          *)
+            STATUS_COLOR="$BLUE"
+            ;;
+        esac
+        MESSAGE="${STAGE}: ${STATUS_COLOR}${STATUS}${NORMAL}:"
+        MESSAGE="$MESSAGE Branch ${BOLD}${BRANCH_NAME}${NORMAL} build #${BUILD_NUMBER}: ${BUILD_URL}"
+    elif [[ -n "$DEMO_URL" ]]; then
+        MESSAGE="${BOLD}${GREEN}SUCCESS${NORMAL}: Demo deployed: ${DEMO_URL}"
+    else
+        echo "Missing required arguments"
+        echo
+        echo "Usage: irc-notify.sh [--stage STAGE --status STATUS] [--demo_url DEMO_URL]"
+        echo "Optional args: --irc_nick, --irc_server, --irc_channel"
+        exit 1
+    fi
+fi
+
+(
+  echo "NICK ${NICK}"
+  echo "USER ${NICK} 8 * : ${NICK}"
+  sleep 5
+  echo "JOIN ${CHANNEL}"
+  echo "NOTICE ${CHANNEL} :${MESSAGE}"
+  echo "QUIT"
+) | openssl s_client -connect "$SERVER" > /dev/null 2>&1
