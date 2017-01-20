9ea9a589d09d7735ae51cda1f02aa7973d2e2f3b
alexgibson <agibson@mozilla.com>
2017-01-20 15:17:05 +0000

    Skip home page tests in IE due to brand popup

--- tests/functional/test_l10n.py
+++ tests/functional/test_l10n.py
@@ -9,6 +9,7 @@ import pytest
 from pages.home import HomePage
 
 
+@pytest.mark.skip_if_internet_explorer(reason='https://ci.us-west.moz.works/job/bedrock_integration_tests_runner/11169/')
 @pytest.mark.nondestructive
 def test_change_language(base_url, selenium):
     page = HomePage(selenium, base_url).open()
