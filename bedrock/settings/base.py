4fd02108adc35506ce7859fe17e9fd23ba5bc491
alexgibson <agibson@mozilla.com>
2017-02-02 14:35:30 +0000

    [bug 1279291] Default to stub attribution rate of 1 for DEV only

--- bedrock/settings/base.py
+++ bedrock/settings/base.py
@@ -1214,7 +1214,7 @@ ADMINS = MANAGERS = config('ADMINS', cast=json.loads,
 GTM_CONTAINER_ID = config('GTM_CONTAINER_ID', default='')
 GMAP_API_KEY = config('GMAP_API_KEY', default='')
 STUB_ATTRIBUTION_HMAC_KEY = config('STUB_ATTRIBUTION_HMAC_KEY', default='')
-STUB_ATTRIBUTION_RATE = config('STUB_ATTRIBUTION_RATE', default=1, cast=float)
+STUB_ATTRIBUTION_RATE = config('STUB_ATTRIBUTION_RATE', default=1 if DEV else 0, cast=float)
 
 STATSD_CLIENT = config('STATSD_CLIENT', default='django_statsd.clients.normal')
 STATSD_HOST = config('STATSD_HOST', default='127.0.0.1')
