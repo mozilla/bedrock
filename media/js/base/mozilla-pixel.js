a7112e026397f96a79e240d357b74db5410f21df
alexgibson <agibson@mozilla.com>
2017-02-28 10:20:36 +0000

    [fix bug 1343033] Add Yahoo tracking pixels to scene2 of /firefox/new/

---
+++ media/js/base/mozilla-pixel.js
@@ -0,0 +1,58 @@
+/* This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
+
+// Create namespace
+if (typeof Mozilla === 'undefined') {
+    var Mozilla = {};
+}
+
+(function() {
+    'use strict';
+
+    /**
+     * Tracking pixels for /firefox/new/?scene=2 download page.
+     * For more info see websites privacy notice and bugs:
+     * https://www.mozilla.org/privacy/websites/
+     * Yahoo: bug 1343033.
+     * Double Click: bug 1331069.
+     * Pixel switch status: bug 1311423.
+     */
+    var Pixel = {};
+
+    Pixel.getPixelData = function() {
+        return $('#strings').data('pixels');
+    };
+
+    Pixel.setPixels = function() {
+        var $body = $('body');
+        var pixels = Pixel.getPixelData();
+        var $pixel;
+
+        if (typeof pixels !== 'string' || pixels === '') {
+            return;
+        }
+
+        // '::' is a separator for each pixel URL.
+        pixels = pixels.split('::');
+
+        for (var i = 0; i < pixels.length; i++) {
+            $pixel = $('<img />', {
+                width: '1',
+                height: '1',
+                src: pixels[i].replace(/\s/g, '')
+            });
+            $pixel.addClass('moz-px');
+            $body.append($pixel);
+        }
+    };
+
+    Pixel.init = function() {
+        // Do not set pixels if visitor has DNT enabled.
+        if (!window._dntEnabled()) {
+            Pixel.setPixels();
+        }
+    };
+
+    window.Mozilla.Pixel = Pixel;
+})();
