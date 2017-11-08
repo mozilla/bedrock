d9bcb27d13075c328a46a02622bec5591c8a58f7
Craig Cook <craigcook@users.noreply.github.com>
2017-11-07 17:57:40 -0800

    [bug 1410144] Update privacy policy branding
    [fix bug 1414424] Remove UITour widget from Rocket policy

--- media/js/privacy/privacy-quantum.js
+++ media/js/privacy/privacy-quantum.js
@@ -1,36 +1,19 @@
 (function() {
     'use strict';
 
-    var mainContent = document.querySelector('main');
-    var strings = document.getElementById('strings');
-    var tabpanelCloseText = dataAttributeHandler({ elem: strings, property: 'data-tabpanel-close-text' });
-    var tabpanelOpenText = dataAttributeHandler({ elem: strings, property: 'data-tabpanel-open-text' });
-    var topicHeaders = document.querySelectorAll('main > section');
+    var strings;
+    var mainContent;
+    var topicHeaders;
     var topics = [];
+    var tabpanelCloseText;
+    var tabpanelOpenText;
 
     /**
-     * Adds the data choices widget(Firefox only) to the first sub-section under
-     * "Firefox by default shares data to".
-     * @param {Object} section - The section to which the widget
-     * will be added
+     * Check for feature support
      */
-    function addDataChoicesWidget(section) {
-        var container = document.createElement('div');
-        var copyContainer = document.createElement('p');
-        var button = document.createElement('button');
-
-        container.setAttribute('class', 'data-choices');
-
-        copyContainer.textContent = strings.dataset.choicesCopy;
-
-        button.textContent = strings.dataset.choicesButton;
-        button.setAttribute('id', 'choose');
-        button.setAttribute('type', 'button');
-
-        container.appendChild(copyContainer);
-        container.appendChild(button);
-
-        section.appendChild(container);
+    function supportsBaselineJS() {
+        return 'querySelectorAll' in document &&
+               'addEventListener' in window;
     }
 
     /**
@@ -51,7 +34,7 @@
         });
 
         for (var i = 0, l = topicHeaders.length; i < l; i++) {
-            var currentHeading = topicHeaders[i].querySelector('header');
+            var currentHeading = topicHeaders[i].querySelector('h2');
 
             dataAttributeHandler({
                 elem: toggle,
@@ -192,14 +175,6 @@
         initialTopicContent.setAttribute('aria-hidden', false);
 
         initialTopicHeading.classList.remove('collapsed');
-
-        // Ensure this is Fx,
-        if (Mozilla.Client.isFirefoxDesktop) {
-            // and that the UITour works
-            Mozilla.UITour.ping(function() {
-                addDataChoicesWidget(initialTopicContent);
-            });
-        }
     }
 
     /**
@@ -248,8 +223,14 @@
         }
     }
 
-    // do not execute in IE8 and lower
-    if ('addEventListener' in window) {
+    // Don't execute if features aren't supported
+    if (supportsBaselineJS()) {
+        strings = document.getElementById('strings');
+        mainContent = document.querySelector('.privacy-body');
+        topicHeaders = document.querySelectorAll('.privacy-body .content-girdle > section');
+        tabpanelCloseText = dataAttributeHandler({ elem: strings, property: 'data-tabpanel-close-text' });
+        tabpanelOpenText = dataAttributeHandler({ elem: strings, property: 'data-tabpanel-open-text' });
+
         /* add a class to indicate that js is enabled. This will trigger
         the appropriate styling to be applied */
         mainContent.classList.add('interactive-widget');
@@ -278,16 +259,6 @@
             // handle clicks on the master toggle buttons
             if (event.target.classList.contains('toggle')) {
                 toggleMainSectionTopics(event.target);
-            }
-
-            // handle clicks on the data choices "Choose" button
-            if (event.target.id === 'choose') {
-                // if the uitour did not load, just return
-                if (Mozilla.UITour === undefined) {
-                    return;
-                }
-
-                Mozilla.UITour.openPreferences('privacy-reports');
             }
         });
     }
