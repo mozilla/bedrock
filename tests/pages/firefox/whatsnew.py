646cd568b779676b1b7a31a987fb4512bd39845a
Alex Gibson <alexgibson@users.noreply.github.com>
2017-11-29 11:53:25 +0000

    [bug 1343823] Remove old /whatsnew templates

---
+++ tests/pages/firefox/whatsnew.py
@@ -0,0 +1,23 @@
+# This Source Code Form is subject to the terms of the Mozilla Public
+# License, v. 2.0. If a copy of the MPL was not distributed with this
+# file, You can obtain one at http://mozilla.org/MPL/2.0/.
+
+from selenium.webdriver.common.by import By
+
+from pages.firefox.base import FirefoxBasePage
+from pages.regions.send_to_device import SendToDevice
+
+
+class FirefoxWhatsNewPage(FirefoxBasePage):
+
+    URL_TEMPLATE = '/{locale}/firefox/whatsnew/'
+
+    _qr_code_locator = (By.CSS_SELECTOR, '#qr-wrapper > img')
+
+    @property
+    def send_to_device(self):
+        return SendToDevice(self)
+
+    @property
+    def is_qr_code_displayed(self):
+        return self.is_element_displayed(*self._qr_code_locator)
