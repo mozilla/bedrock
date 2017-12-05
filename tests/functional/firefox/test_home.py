d80419b77a6462290e2835aab77d03d211450756
Alex Gibson <alexgibson@users.noreply.github.com>
2017-12-05 17:04:12 +0000

    [fix bug 1416710] Update functional tests for Firefox home page

--- tests/functional/firefox/test_home.py
+++ tests/functional/firefox/test_home.py
@@ -7,9 +7,18 @@ import pytest
 from pages.firefox.home import FirefoxHomePage
 
 
-@pytest.mark.smoke
 @pytest.mark.sanity
 @pytest.mark.nondestructive
 def test_download_buttons_displayed(base_url, selenium):
     page = FirefoxHomePage(selenium, base_url).open()
-    assert page.download_button.is_displayed
+    assert page.primary_download_button.is_displayed
+    assert page.secondary_download_button.is_displayed
+
+
+@pytest.mark.smoke
+@pytest.mark.nondestructive
+def test_play_video(base_url, selenium):
+    page = FirefoxHomePage(selenium, base_url).open()
+    page.play_video()
+    assert not page.is_video_overlay_displayed
+    assert page.is_video_displayed
--- tests/pages/firefox/home.py
+++ tests/pages/firefox/home.py
@@ -12,9 +12,30 @@ class FirefoxHomePage(FirefoxBasePage):
 
     URL_TEMPLATE = '/{locale}/firefox/'
 
-    _download_button_locator = (By.ID, 'download-intro')
+    _primary_download_button_locator = (By.ID, 'download-intro')
+    _secondary_download_button_locator = (By.ID, 'footer-download')
+    _video_overlay_locator = (By.CSS_SELECTOR, '.moz-video-container .moz-video-button')
+    _video_locator = (By.CSS_SELECTOR, '.moz-video-container video')
 
     @property
-    def download_button(self):
-        el = self.find_element(*self._download_button_locator)
+    def primary_download_button(self):
+        el = self.find_element(*self._primary_download_button_locator)
         return DownloadButton(self, root=el)
+
+    @property
+    def secondary_download_button(self):
+        el = self.find_element(*self._secondary_download_button_locator)
+        return DownloadButton(self, root=el)
+
+    @property
+    def is_video_overlay_displayed(self):
+        return self.is_element_displayed(*self._video_overlay_locator)
+
+    @property
+    def is_video_displayed(self):
+        return self.is_element_displayed(*self._video_locator)
+
+    def play_video(self):
+        assert self.is_video_overlay_displayed, 'Video is already displayed'
+        self.find_element(*self._video_overlay_locator).click()
+        self.wait.until(lambda s: not self.is_video_overlay_displayed)
