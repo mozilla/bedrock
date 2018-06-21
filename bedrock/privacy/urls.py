dd24cce78c94404eeec1ed954766728d0078b603
Alex Gibson <alexgibson@users.noreply.github.com>
2018-06-21 09:22:34 +0100

    [fix bug 1469981] Firefox for Fire TV Privacy Notice

--- bedrock/privacy/urls.py
+++ bedrock/privacy/urls.py
@@ -13,6 +13,7 @@ urlpatterns = (
     url(r'^/firefox/$', views.firefox_notices, name='privacy.notices.firefox'),
     url(r'^/firefox-os/$', views.firefox_os_notices, name='privacy.notices.firefox-os'),
     url(r'^/firefox-cliqz/$', views.firefox_cliqz_notices, name='privacy.notices.firefox-cliqz'),
+    url(r'^/firefox-fire-tv/$', views.firefox_fire_tv_notices, name='privacy.notices.firefox-fire-tv'),
     url(r'^/firefox-focus/$', views.firefox_focus_notices, name='privacy.notices.firefox-focus'),
     url(r'^/firefox-rocket/$', views.firefox_rocket_notices, name='privacy.notices.firefox-rocket'),
     # bug 1319207 - special URL for Firefox Focus in de locale
--- tests/redirects/map_globalconf.py
+++ tests/redirects/map_globalconf.py
@@ -1200,7 +1200,6 @@ URLS = flatten((
     url_test('/firefox/organizations/faq/', '/firefox/organizations/'),
 
     # bug 1425865
-    url_test('/privacy/firefox-fire-tv/', '/privacy/firefox-focus/'),
     url_test('/firefox/fire-tv/', 'https://support.mozilla.org/products/firefox-fire-tv/', status_code=302),
 
     # bug 1421584
