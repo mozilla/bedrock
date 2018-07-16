f2952b06bcae0b3f53927a92559a97e47648f301
Alex Gibson <alexgibson@users.noreply.github.com>
2018-07-16 09:19:33 +0100

    Update @mozilla-protocol/core to v2.0.1

---
+++ media/js/base/protocol/init-lang-switcher.js
@@ -0,0 +1,20 @@
+/* This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
+
+/**
+ * Initialize Protocol language switcher.
+ */
+(function() {
+    if (typeof Mozilla === 'undefined' || typeof Mozilla.LangSwitcher === 'undefined') {
+        return;
+    }
+
+    Mozilla.LangSwitcher.init(function(previousLanguage, newLanguage) {
+        window.dataLayer.push({
+            'event': 'change-language',
+            'languageSelected': newLanguage,
+            'previousLanguage': previousLanguage
+        });
+    });
+})();
