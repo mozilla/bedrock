80b593239f1b30717283f270c656aec02acd5928
Paul McLanahan <pmac@mozilla.com>
2018-07-20 14:50:14 -0400

    Use staticfiles.json map to set proper cache headers in s3
    Instead of trying to match filenames it is more accurate
    to use the JSON map Django gives us to move the hashed files
    to a new directory and do the `aws s3 sync` separately for
    each type of file.

--- bin/upload-staticfiles.sh
+++ bin/upload-staticfiles.sh
@@ -9,32 +9,37 @@ if [[ "$BRANCH_NAME" != "prod" ]]; then
     exit 0
 fi
 
+TMP_DIR="s3-static"
+TMP_DIR_HASHED="s3-static-hashed"
 CONTAINER_NAME="bedrock-${BRANCH_AND_COMMIT}"
 
-rm -rf static
+rm -rf "${TMP_DIR}"
+rm -rf "${TMP_DIR_HASHED}"
+
 # have to rerun staticfiles without symlinks or we just copy broken symlinks
 docker-compose run --name "$CONTAINER_NAME" release docker/bin/build_staticfiles.sh --nolink
-docker cp "${CONTAINER_NAME}:/app/static" ./static
+docker cp "${CONTAINER_NAME}:/app/static" "${TMP_DIR}"
 docker rm -f "$CONTAINER_NAME"
+
+# separate the hashed files into another directory
+bin/move_hashed_staticfiles.py "${TMP_DIR}" "${TMP_DIR_HASHED}"
 
 for BUCKET in stage prod; do
     # hashed filenames
     aws s3 sync \
-        --exclude "*" \
-        --include "*.????????????.*" \
         --acl public-read \
         --cache-control "max-age=315360000, public, immutable" \
         --profile bedrock-media \
-        ./static "s3://bedrock-${BUCKET}-media/media/"
+        "./${TMP_DIR_HASHED}" "s3://bedrock-${BUCKET}-media/media/"
     # non-hashed filenames
     # may not need to include non-hashed files
     # TODO look into this if this is slow or makes the bucket too large
     aws s3 sync \
-        --exclude "*.????????????.*" \
         --acl public-read \
         --cache-control "max-age=21600, public" \
         --profile bedrock-media \
-        ./static "s3://bedrock-${BUCKET}-media/media/"
+        "./${TMP_DIR}" "s3://bedrock-${BUCKET}-media/media/"
 done
 
-rm -rf static
+rm -rf "${TMP_DIR}"
+rm -rf "${TMP_DIR_HASHED}"
