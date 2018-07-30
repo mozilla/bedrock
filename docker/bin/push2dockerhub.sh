668c08bde7ae9481a8c58c3c05a582d6832e85a3
Paul McLanahan <pmac@mozilla.com>
2018-07-30 15:52:40 -0400

    Use :latest docker tags for builds from "master"
    Add :prod-latest tags for production images.

--- docker/bin/push2dockerhub.sh
+++ docker/bin/push2dockerhub.sh
@@ -19,9 +19,14 @@ fi
 # Push to docker hub
 docker push $FROM_DOCKER_REPOSITORY:${DOCKER_TAG}
 
+if [[ "$BRANCH_NAME" == "master" ]]; then
+    docker tag $FROM_DOCKER_REPOSITORY:${DOCKER_TAG} $FROM_DOCKER_REPOSITORY:latest
+    docker push $FROM_DOCKER_REPOSITORY:latest
+fi
+
 if [[ "$GIT_TAG_DATE_BASED" == true ]]; then
     docker tag $FROM_DOCKER_REPOSITORY:${DOCKER_TAG} $FROM_DOCKER_REPOSITORY:$GIT_TAG
-    docker tag $FROM_DOCKER_REPOSITORY:${DOCKER_TAG} $FROM_DOCKER_REPOSITORY:latest
+    docker tag $FROM_DOCKER_REPOSITORY:${DOCKER_TAG} $FROM_DOCKER_REPOSITORY:prod-latest
     docker push $FROM_DOCKER_REPOSITORY:$GIT_TAG
-    docker push $FROM_DOCKER_REPOSITORY:latest
+    docker push $FROM_DOCKER_REPOSITORY:prod-latest
 fi
