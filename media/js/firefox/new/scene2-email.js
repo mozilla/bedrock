699f3232a190b808fcdc488eba4470440fe9a81d
Craig Cook <craigcook@users.noreply.github.com>
2018-08-15 10:07:27 -0700

    More review updates
    * Clean up commented JS
    * Use Mozilla.Utils.trans
    * Add firefox/base-protocol.html
    * Add lang files
    * Unhide hidden form in oldIE

--- media/js/firefox/new/scene2-email.js
+++ media/js/firefox/new/scene2-email.js
@@ -5,33 +5,16 @@
 (function() {
     'use strict';
 
-    // var $modalWrapper = $('.email-privacy');
-    // var $modalLink = $('.email-privacy-link');
-
-    // $modalLink.on('click', function(e) {
-    //     e.preventDefault();
-    //     Mozilla.Modal.createModal(this, $modalWrapper, {
-    //         title: $(this).text()
-    //     });
-
-    //     window.dataLayer.push({
-    //         'event': 'in-page-interaction',
-    //         'eAction': 'link click',
-    //         'eLabel': 'How will Mozilla use my email?'
-    //     });
-    // });
-
     var content = document.querySelector('.mzp-u-modal-content');
     var trigger = document.querySelector('.email-privacy-link');
     var title = document.querySelector('.email-privacy h3');
-    var strings = document.getElementById('strings');
 
     trigger.addEventListener('click', function(e) {
         e.preventDefault();
         Mozilla.Modal.createModal(e.target, content, {
             title: title.innerHTML,
             className: 'mzp-t-firefox',
-            closeText: strings.dataset.close
+            closeText: window.Mozilla.Utils.trans('global-close'),
         });
 
         window.dataLayer.push({
