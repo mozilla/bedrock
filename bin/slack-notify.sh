95375bd82935e6cec184e8721c8641ea039573bd
Paul McLanahan <pmac@mozilla.com>
2018-08-22 11:38:38 -0400

    Send additional slack message to alert channel
    When failure occurs during prod build.

--- bin/slack-notify.sh
+++ bin/slack-notify.sh
@@ -6,6 +6,7 @@ set -eo pipefail
 
 # defaults and constants
 CHANNEL="www-notify"
+CHANNEL_FAILURE="meao-alerts"
 BLUE_BUILD_URL="https://ci.us-west.moz.works/blue/organizations/jenkins/bedrock_multibranch_pipeline"
 BLUE_BUILD_URL="${BLUE_BUILD_URL}/detail/${BRANCH_NAME/\//%2f}/${BUILD_NUMBER}/pipeline"
 
@@ -69,3 +70,6 @@ fi
 
 slack-cli -d "${CHANNEL}" "${MESSAGE}"
 
+if [[ "${STATUS}-${BRANCH_NAME}" == "FAILURE-prod" ]]; then
+    slack-cli -d "${CHANNEL_FAILURE}" "${MESSAGE}"
+fi
