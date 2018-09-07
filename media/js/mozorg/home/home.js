990a71ada4b147fdc862bb7e741d7fe72411753d
Amy Churchwell <achurchwell@mozilla.com>
2018-09-07 13:47:00 -0700

    Fix: #6055 August content update and youtube API
    Finish updating card content (minus styles/video)
    
    Resize images and make gifs static
    
    Add YouTube API to modal
    
    Remove unused images
    
    Remove old event listeners, initialize video on click
    
    Refactor video modal
    
    Clean up indentation

--- media/js/mozorg/home/home.js
+++ media/js/mozorg/home/home.js
@@ -2,147 +2,171 @@
  * License, v. 2.0. If a copy of the MPL was not distributed with this
  * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
 
-(function(Waypoint) {
-    'use strict';
+ /* global YT */
+ /* eslint no-unused-vars: [2, { "varsIgnorePattern": "onYouTubeIframeAPIReady" }] */
 
-    function getNextEl(el) {
-        el = el.nextSibling;
-        while (el) {
-            if (el.nodeType === 1) {
-                return el;
-            }
-            el = el.nextSibling;
-        }
-        return null;
-    }
+// YouTube API hook has to be in global scope
+ function onYouTubeIframeAPIReady() {
+     'use strict';
+ }
 
-    /*
-     * Video Tracking
-     */
+ (function($, Waypoint) {
+     'use strict';
 
-    function trackVideoInteraction(title, state) {
-        window.dataLayer.push({
-            'event': 'video-interaction',
-            'videoTitle': title,
-            'interaction': state
-        });
-    }
+     var tag = document.createElement('script');
+     tag.src = 'https://www.youtube.com/iframe_api';
+     var firstScriptTag = document.getElementsByTagName('script')[0];
+     firstScriptTag.parentNode.insertBefore(tag, firstScriptTag);
 
-    function initVideoEvents() {
-        var videos = document.querySelectorAll('video');
-        var videoCards = document.querySelectorAll('.mzp-c-card.has-video-embed .mzp-c-card-block-link');
+     function getNextEl(el) {
+         el = el.nextSibling;
+         while (el) {
+             if (el.nodeType === 1) {
+                 return el;
+             }
+             el = el.nextSibling;
+         }
+         return null;
+     }
+
+     function initVideoEvents() {
+         var videoCards = document.querySelectorAll('.mzp-c-card.has-video-embed .mzp-c-card-block-link');
+
+         function playVideo(event) {
+             var card = event.currentTarget;
+             var title = card.querySelector('.mzp-c-card-title').innerText;
+             var sibling = getNextEl(event.currentTarget);
+             var content = sibling.querySelector('.mzp-c-card-video-content');
+
+             var videoLink = document.querySelector('.video-play');
+             var videoId = videoLink.getAttribute('data-id');
+
+             var player = new YT.Player(videoLink, {
+                 height: '390',
+                 width: '640',
+                 videoId: videoId,
+                 playerVars: {
+                     modestbranding: 1, // hide YouTube logo.
+                     rel: 0, // do not show related videos on end.
+                 },
+                 events: {
+                     'onReady': onPlayerReady,
+                     'onStateChange': onPlayerStateChange
+                 }
+             });
+
+             function onPlayerReady(event) {
+                 event.target.playVideo();
+             }
+
+             function onPlayerStateChange(event) {
+                 var state;
+
+                 switch(event.data) {
+                 case YT.PlayerState.PLAYING:
+                     state = 'video play';
+                     break;
+                 case YT.PlayerState.PAUSED:
+                     state = 'video paused';
+                     break;
+                 case YT.PlayerState.ENDED:
+                     state = 'video complete';
+                     break;
+                 }
+
+                 if (state) {
+                     window.dataLayer.push({
+                         'event': 'onReady',
+                         'videoTitle': title,
+                         'interaction': state
+                     });
+                 }
+             }
+
+             if (content) {
+                 event.preventDefault();
+
+                 Mozilla.Modal.createModal(this, content, {
+                     title: title,
+                     className: 'mzp-has-media',
+                     onDestroy: function() {
+                         player.destroy();
+                     }
+                 });
+             }
+         }
 
         // open and play videos in a modal on click.
-        for (var i = 0; i < videoCards.length; i++) {
-            videoCards[i].addEventListener('click', playVideo, false);
-        }
+         for (var i = 0; i < videoCards.length; i++) {
+             videoCards[i].addEventListener('click', playVideo, false);
+         }
+     }
 
-        // track video interaction events for GA.
-        for (var j = 0; j < videos.length; j++) {
-            videos[j].addEventListener('play', function() {
-                trackVideoInteraction(this.getAttribute('data-ga-label'), 'play');
-            }, false);
+     // Lazyload images
+     Mozilla.LazyLoad.init();
 
-            videos[j].addEventListener('pause', function() {
-                var action = this.currentTime === this.duration ? 'complete' : 'pause';
-                trackVideoInteraction(this.getAttribute('data-ga-label'), action);
-            }, false);
-        }
-    }
+     // Video card interactions.
+     initVideoEvents();
 
-    function playVideo(e) {
-        var card = e.currentTarget;
-        var title = card.querySelector('.mzp-c-card-title').innerText;
-        var sibling = getNextEl(e.currentTarget);
-        var content = sibling.querySelector('.mzp-c-card-video-content');
-
-        if (content) {
-            e.preventDefault();
-            var video = content.querySelector('video');
-
-            Mozilla.Modal.createModal(this, content, {
-                title: title,
-                className: 'mzp-has-media',
-                onCreate: function() {
-                    try {
-                        video.load();
-                        video.play();
-                    } catch(err) {
-                        // fail silently
-                    }
-                },
-                onDestroy: function() {
-                    video.pause();
-                }
-            });
-        }
-    }
-
-    // Lazyload images
-    Mozilla.LazyLoad.init();
-
-    // Video card interactions.
-    initVideoEvents();
-
-    /*
+     /*
      * Sticky CTA
      */
 
-    var $stickyCTA = $(document.getElementById('download-firefox-sticky-cta'));
-    var hasCookies = typeof Mozilla.Cookies !== 'undefined' && Mozilla.Cookies.enabled();
-    $stickyCTA.attr('aria-hidden', 'true');
+     var $stickyCTA = $(document.getElementById('download-firefox-sticky-cta'));
+     var hasCookies = typeof Mozilla.Cookies !== 'undefined' && Mozilla.Cookies.enabled();
+     $stickyCTA.attr('aria-hidden', 'true');
 
-    // init dismiss button
-    function initStickyCTA() {
+     // init dismiss button
+     function initStickyCTA() {
         // add and remove aria-hidden
-        var primaryTop = new Waypoint({
-            element: document.getElementById('download-firefox-primary-cta'),
-            handler: function(direction) {
-                if(direction === 'down') {
+         var primaryTop = new Waypoint({
+             element: document.getElementById('download-firefox-primary-cta'),
+             handler: function(direction) {
+                 if(direction === 'down') {
                     // becomes percivable as the user scrolls down
-                    $stickyCTA.removeAttr('aria-hidden');
-                } else {
+                     $stickyCTA.removeAttr('aria-hidden');
+                 } else {
                     // hidden again as they scroll up
-                    $stickyCTA.attr('aria-hidden', 'true');
-                }
-            }
-        });
+                     $stickyCTA.attr('aria-hidden', 'true');
+                 }
+             }
+         });
 
-        // add button
-        var $dismissButton = $('<button>').addClass('sticky-dismiss').text('Dismiss download prompt.');
-        var $stickyWrapper = $stickyCTA.find('.primary-wrapper');
-        $dismissButton.appendTo($stickyWrapper);
-        // listen for click
-        $dismissButton.one('click', function(){
+         // add button
+         var $dismissButton = $('<button>').addClass('sticky-dismiss').text('Dismiss download prompt.');
+         var $stickyWrapper = $stickyCTA.find('.primary-wrapper');
+         $dismissButton.appendTo($stickyWrapper);
+         // listen for click
+         $dismissButton.one('click', function(){
             // dismiss
-            dismissStickyCTA(primaryTop);
-        });
-    }
+             dismissStickyCTA(primaryTop);
+         });
+     }
 
-    // handle dismiss
-    function dismissStickyCTA(stickyWayPoint) {
-        // destroy waypoint
-        stickyWayPoint.destroy();
-        // remove element
-        $stickyCTA.remove();
-        // set cookie, if cookies are supported
-        if (hasCookies) {
-            var d = new Date();
-            d.setTime(d.getTime() + (30 * 24 * 60 * 60 * 1000)); // 30 days
-            Mozilla.Cookies.setItem('sticky-home-cta-dismissed', 'true', d, '/');
-        }
-    }
+     // handle dismiss
+     function dismissStickyCTA(stickyWayPoint) {
+         // destroy waypoint
+         stickyWayPoint.destroy();
+         // remove element
+         $stickyCTA.remove();
+         // set cookie, if cookies are supported
+         if (hasCookies) {
+             var d = new Date();
+             d.setTime(d.getTime() + (30 * 24 * 60 * 60 * 1000)); // 30 days
+             Mozilla.Cookies.setItem('sticky-home-cta-dismissed', 'true', d, '/');
+         }
+     }
 
-    // Check if previously dismissed
-    if (hasCookies) {
-        if (!Mozilla.Cookies.getItem('sticky-home-cta-dismissed')) {
-            // init the button
-            initStickyCTA();
-        } else {
-            $stickyCTA.remove();
-        }
-    } else {
-        $stickyCTA.remove();
-    }
+     // Check if previously dismissed
+     if (hasCookies) {
+         if (!Mozilla.Cookies.getItem('sticky-home-cta-dismissed')) {
+             // init the button
+             initStickyCTA();
+         } else {
+             $stickyCTA.remove();
+         }
+     } else {
+         $stickyCTA.remove();
+     }
 
-})(window.Waypoint);
+ })(window.Waypoint);
