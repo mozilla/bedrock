962d5cb8847b5fa5717ba254c0e4bbe808391e93
Craig Cook <craigcook@users.noreply.github.com>
2018-11-09 08:23:56 -0800

    [fix #6337] Add FxA benefits CTAs to home page
    [fix #6490] Update home page banner for FFFY

--- tests/functional/test_home.py
+++ tests/functional/test_home.py
@@ -7,12 +7,21 @@ import pytest
 from pages.home import HomePage
 
 
+@pytest.mark.skip_if_firefox(reason='Download button is displayed only to non-Firefox users')
 @pytest.mark.sanity
 @pytest.mark.nondestructive
 def test_download_button_is_displayed_en(base_url, selenium):
     page = HomePage(selenium, base_url).open()
     assert page.primary_download_button.is_displayed
     assert page.secondary_download_button.is_displayed
+
+
+@pytest.mark.skip_if_not_firefox(reason='Firefox Accounts CTA is displayed only to Firefox users')
+@pytest.mark.nondestructive
+def test_accounts_button_is_displayed_en(base_url, selenium):
+    page = HomePage(selenium, base_url).open()
+    assert page.is_primary_accounts_button
+    assert page.is_secondary_accounts_button
 
 
 @pytest.mark.sanity
