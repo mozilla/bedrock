fca505923fcdb83aaa582aca7520c9ef4bf5f464
Chris DeCairos <cade@mozillafoundation.org>
2018-11-19 10:17:29 -0500

    remove locale prefix on foundation redirects

--- bedrock/foundation/redirects.py
+++ bedrock/foundation/redirects.py
@@ -2,12 +2,12 @@ from bedrock.redirects.util import redirect
 
 redirectpatterns = (
 
-    redirect(r'^foundation/$', 'https://foundation.mozilla.org/en/'),
-    redirect(r'^foundation/about/$', 'https://foundation.mozilla.org/en/about/'),
-    redirect(r'^foundation/documents/$', 'https://foundation.mozilla.org/en/about/public-records/'),
-    redirect(r'^foundation/issues/$', 'https://foundation.mozilla.org/en/initiatives/'),
-    redirect(r'^foundation/leadership-network/$', 'https://foundation.mozilla.org/en/'),
-    redirect(r'^foundation/advocacy/$', 'https://foundation.mozilla.org/en/'),
+    redirect(r'^foundation/$', 'https://foundation.mozilla.org/'),
+    redirect(r'^foundation/about/$', 'https://foundation.mozilla.org/about/'),
+    redirect(r'^foundation/documents/$', 'https://foundation.mozilla.org/about/public-records/'),
+    redirect(r'^foundation/issues/$', 'https://foundation.mozilla.org/initiatives/'),
+    redirect(r'^foundation/leadership-network/$', 'https://foundation.mozilla.org/'),
+    redirect(r'^foundation/advocacy/$', 'https://foundation.mozilla.org/'),
     redirect(r'^foundation/trademarks/?$', '/foundation/trademarks/policy/'),
     redirect(r'^foundation/trademarks/faq/$', '/foundation/trademarks/policy/'),
     redirect(r'^foundation/documents/domain-name-license.pdf$', '/foundation/trademarks/policy/'),
