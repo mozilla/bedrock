e13b5674f471824c619b06746fc1f8b41755eb8e
Stephanie Hobson <sjhobson@gmail.com>
2019-01-17 17:00:39 -0800

    Use native video player

--- tests/pages/firefox/home.py
+++ tests/pages/firefox/home.py
@@ -14,8 +14,6 @@ class FirefoxHomePage(FirefoxBasePage):
 
     _primary_download_button_locator = (By.ID, 'download-intro')
     _secondary_download_button_locator = (By.ID, 'footer-download')
-    _video_overlay_locator = (By.CSS_SELECTOR, '.moz-video-container .moz-video-button')
-    _video_locator = (By.CSS_SELECTOR, '.moz-video-container video')
 
     @property
     def primary_download_button(self):
@@ -26,16 +24,3 @@ class FirefoxHomePage(FirefoxBasePage):
     def secondary_download_button(self):
         el = self.find_element(*self._secondary_download_button_locator)
         return DownloadButton(self, root=el)
-
-    @property
-    def is_video_overlay_displayed(self):
-        return self.is_element_displayed(*self._video_overlay_locator)
-
-    @property
-    def is_video_displayed(self):
-        return self.is_element_displayed(*self._video_locator)
-
-    def play_video(self):
-        assert self.is_video_overlay_displayed, 'Video is already displayed'
-        self.find_element(*self._video_overlay_locator).click()
-        self.wait.until(lambda s: not self.is_video_overlay_displayed)
