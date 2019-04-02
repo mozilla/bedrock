582a688b336b0797774e010004525e0ad0c673cc
Alex Gibson <alexgibson@users.noreply.github.com>
2019-04-02 17:16:27 +0100

    Features A/B test on firefox/new (Fixes #6830) (#6927)

--- media/js/firefox/new/scene1_fxa_modal.js
+++ media/js/firefox/new/scene1_fxa_modal.js
@@ -8,38 +8,54 @@
     'use strict';
 
     var client = window.Mozilla.Client;
-    var $downloadButton = $('#download-button-desktop-release .download-link[data-download-os="Desktop"]');
-    var $downloadAnyway = $('#download-fxa-modal .download-link[data-download-os="Desktop"]');
+
+    function showFxAModal(e) {
+        e.preventDefault();
+
+        // Open the modal
+        Mozilla.Modal.createModal(this, $('#firefox-account'), {
+            title: $(this).text(),
+            className: 'firefox-account-modal'
+        });
+
+        // Count the click in GA
+        window.dataLayer.push({
+            'event': 'in-page-interaction',
+            'eAction': 'link click',
+            'eLabel': 'Current Firefox user downloading Firefox'
+        });
+    }
 
     var initFxAccountModal = function() {
+        var downloadLinkPrimary = document.querySelectorAll('#download-button-desktop-release .download-list .download-link[data-download-os="Desktop"]');
+        var downloadLinkSecondary = document.querySelectorAll('#download-call-out-secondary .download-list .download-link[data-download-os="Desktop"]');
+        var downloadAnyway = document.querySelectorAll('#download-fxa-modal .download-list .download-link[data-download-os="Desktop"]');
+
         // To avoid showing users two similar forms back to back,
         // hide the form on the /thanks page by adding a param to
         // download links in the modal.
-        $downloadAnyway.attr('href', function(i, h) {
-            return h + (h.indexOf('?') !== -1 ? '&n=f' : '?n=f');
-        });
+        for(var i = 0; i < downloadAnyway.length; i++) {
+            downloadAnyway[i].href = downloadAnyway[i].href.indexOf('?') !== -1 ? downloadAnyway[i].href + '&n=f' : downloadAnyway[i].href +'?n=f';
+        }
 
-        $downloadButton.on('click', function(e) {
-            e.preventDefault();
+        for(var j = 0; j < downloadLinkPrimary.length; j++) {
+            downloadLinkPrimary[j].addEventListener('click', showFxAModal, false);
+        }
 
-            // Open the modal
-            Mozilla.Modal.createModal(this, $('#firefox-account'), {
-                title: $(this).text(),
-                className: 'firefox-account-modal'
-            });
-
-            // Count the click in GA
-            window.dataLayer.push({
-                'event': 'in-page-interaction',
-                'eAction': 'link click',
-                'eLabel': 'Current Firefox user downloading Firefox'
-            });
-        });
+        for(var k = 0; k < downloadLinkSecondary.length; k++) {
+            downloadLinkSecondary[k].addEventListener('click', showFxAModal, false);
+        }
 
         // Don't trigger the modal for signed in users
         client.getFxaDetails(function(details) {
             if (details.setup) {
-                $downloadButton.unbind('click');
+                for(var i = 0; i < downloadLinkPrimary.length; i++) {
+                    downloadLinkPrimary.removeEventListener('click', showFxAModal, false);
+                }
+
+                for(var j = 0; j < downloadLinkSecondary.length; j++) {
+                    downloadLinkSecondary.removeEventListener('click', showFxAModal, false);
+                }
             }
         });
     };
