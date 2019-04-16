ab227e52e89bc25ee895fc3e4631beb191108b3f
Chris Beaven <smileychris@gmail.com>
2019-04-16 16:38:34 +1200

    Pytest fixes

--- tests/functional/newsletter/test_newsletter_embed.py
+++ tests/functional/newsletter/test_newsletter_embed.py
@@ -48,7 +48,7 @@ def test_newsletter_sign_up_fails_when_missing_required_fields(page_class, base_
 
 @pytest.mark.nondestructive
 @pytest.mark.parametrize(('page_class', 'url_kwargs'), [
-    (ContributePage, None),
+    pytest.param(ContributePage, None, marks=pytest.mark.smoke()),
     (MissionPage, None),
     (FeaturesLandingPage, None),
     (PluginCheckPage, None)])
