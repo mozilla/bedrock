849cadf2b89569d8e259e816628f4ef9b4f9c376
Stephanie Hobson <sjhobson@gmail.com>
2019-04-24 05:47:11 -0700

    Protocol 6.0.1 (#7048)
    * Update Protocol version
    * Update colour tokens
    * Switch high res background declarations to use at2x
    * Remove code to hide curves, curves are gone
    * Blockquote isn't Mozilla styled by default anymore
    * Use mixin for details/summary styles
    * Light links have visited states by default now
    * Add Mozilla theme to Mozilla pages
    * Upgrade to 6.0.1

--- media/js/firefox/fights-for-you.js
+++ media/js/firefox/fights-for-you.js
@@ -5,33 +5,6 @@
 (function() {
     'use strict';
 
-    var client = Mozilla.Client;
-    var survey = document.getElementById('fffy-survey-link');
-    if (survey && !client.isMobile && Math.random() < 0.5) {
-        var link = survey.querySelector('a');
-        var linkHref = link.getAttribute('href');
-
-        // show survey
-        survey.style.display = 'block';
-        window.dataLayer.push({
-            'eLabel': 'Banner Impression',
-            'data-banner-name': 'FFFY Survey Link',
-            'data-banner-impression': '1',
-            'event': 'non-interaction'
-        });
-
-        // survey tracking
-        link.setAttribute('href', linkHref + window.location.search);
-        link.addEventListener('click', function() {
-            window.dataLayer.push({
-                'eLabel': 'Banner Clickthrough',
-                'data-banner-name': 'FFFY Survey Link',
-                'data-banner-click': '1',
-                'event': 'in-page-interaction'
-            });
-        }, false);
-    }
-
     // Set up modal for the Focus link
     var focusContent = document.getElementById('fffy-focus-modal');
     var focusTrigger = document.querySelector('.fffy-t-focus .mzp-c-cta-link');
