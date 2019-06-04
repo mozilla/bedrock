36f6634b8df8e78861a126b468f28d6f35053404
Craig Cook <craigcook@users.noreply.github.com>
2019-06-04 11:10:57 -0700

    [#7132] Clean up switch for trailhead accounts page

--- tests/functional/firefox/test_accounts.py
+++ tests/functional/firefox/test_accounts.py
@@ -7,7 +7,6 @@ import pytest
 from pages.firefox.accounts import FirefoxAccountsPage
 
 
-@pytest.mark.skip(reason='Page is temporarily behind the switch "firefox_accounts_trailhead"')
 @pytest.mark.nondestructive
 def test_account_form(base_url, selenium):
     page = FirefoxAccountsPage(selenium, base_url).open()
