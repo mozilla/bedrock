248e345e72861e0ed937235dfd5d418d98bcda3d
Stephanie Hobson <sjhobson@gmail.com>
2019-07-31 11:36:57 -0700

    Update privacy section to use Protocol base templates (#7358) (#7462)

--- media/js/privacy/privacy-protocol.js
+++ media/js/privacy/privacy-protocol.js
@@ -2,4 +2,20 @@
  * License, v. 2.0. If a copy of the MPL was not distributed with this
  * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
 
-window.Mzp.Details.init('.privacy-body section section > h3');
+
+(function() {
+    'use strict';
+
+    var openText = document.getElementById('strings').getAttribute('data-details-open-text');
+    var closeText = document.getElementById('strings').getAttribute('data-details-close-text');
+    var buttons;
+
+    window.Mzp.Details.init('.format-headings .privacy-body section section > h3');
+    window.Mzp.Details.init('.format-paragraphs .summary');
+
+    buttons = document.querySelectorAll('.is-summary button');
+    for (var i = 0; i < buttons.length; i++) {
+        buttons[i].setAttribute('data-open', openText);
+        buttons[i].setAttribute('data-close', closeText);
+    }
+})();
