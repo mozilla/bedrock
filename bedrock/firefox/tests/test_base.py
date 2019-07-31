229a277b19566303a3ac93cad26ef4d80bcc6026
Alex Gibson <alexgibson@users.noreply.github.com>
2019-07-31 20:16:13 +0100

    Fix test comment

--- bedrock/firefox/tests/test_base.py
+++ bedrock/firefox/tests/test_base.py
@@ -286,7 +286,7 @@ class TestWhatsNew(TestCase):
 
     @override_settings(DEV=True)
     def test_fx_dev_browser_35_0_a2_whatsnew(self, render_mock):
-        """Should default whatsnew template"""
+        """Should show default whatsnew template"""
         req = self.rf.get('/en-US/firefox/whatsnew/')
         self.view(req, version='35.0a2')
         template = render_mock.call_args[0][1]
