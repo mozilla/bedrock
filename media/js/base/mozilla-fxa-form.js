26ef48b4296d545ff4bfbf921eaa99a225569154
Amy Churchwell <achurchwell@mozilla.com>
2019-08-09 14:53:21 -0700

    Add monitor_button params and retrieve deviceId from metrics-flow requests. (Fixes #7531, #7533)

--- media/js/base/mozilla-fxa-form.js
+++ media/js/base/mozilla-fxa-form.js
@@ -7,80 +7,88 @@ if (typeof window.Mozilla === 'undefined') {
     window.Mozilla = {};
 }
 
-Mozilla.FxaForm = (function(Mozilla) {
+(function(Mozilla) {
     'use strict';
 
-    var fxaForm = document.getElementById('fxa-email-form');
-    var fxaSubmitButton = document.getElementById('fxa-email-form-submit');
-    var fxaFormContextField = fxaForm.querySelector('[name="context"]');
-    var fxaFormServiceField = fxaForm.querySelector('[name="service"]');
-    var supportsFetch = 'fetch' in window;
+    var FxaForm = {};
 
-    // swap form action for Fx China re-pack
-    function init() {
-        // disable form while we check distribution
-        fxaSubmitButton.disabled = true;
+    FxaForm.init= function(){
+        var fxaForm = document.getElementById('fxa-email-form');
+        var fxaSubmitButton = document.getElementById('fxa-email-form-submit');
+        var fxaFormContextField = fxaForm.querySelector('[name="context"]');
+        var fxaFormServiceField = fxaForm.querySelector('[name="service"]');
+        var supportsFetch = 'fetch' in window;
 
-        var mozillaonlineAction = fxaForm.dataset.mozillaonlineAction;
+        // swap form action for Fx China re-pack
+        function init() {
+            // disable form while we check distribution
+            fxaSubmitButton.disabled = true;
 
-        if (mozillaonlineAction) {
-            Mozilla.Client.getFirefoxDetails(function(data) {
-                // only switch to China re-pack URL if UITour call is successful
-                // (marked by data.accurate being true)
-                if (data.accurate && data.distribution && data.distribution.toLowerCase() === 'mozillaonline') {
-                    fxaForm.action = mozillaonlineAction;
-                }
+            var mozillaonlineAction = fxaForm.dataset.mozillaonlineAction;
 
+            if (mozillaonlineAction) {
+                Mozilla.Client.getFirefoxDetails(function(data) {
+                    // only switch to China re-pack URL if UITour call is successful
+                    // (marked by data.accurate being true)
+                    if (data.accurate && data.distribution && data.distribution.toLowerCase() === 'mozillaonline') {
+                        fxaForm.action = mozillaonlineAction;
+                    }
+
+                    fetchTokens();
+                });
+            } else {
                 fetchTokens();
+            }
+        }
+
+        // get tokens from FxA for analytics purposes
+        function fetchTokens() {
+            if (!supportsFetch) {
+                return;
+            }
+
+            fxaSubmitButton.disabled = false;
+
+            var destURL = fxaForm.getAttribute('action') + 'metrics-flow';
+            var entrypoint = document.getElementById('fxa-email-form-entrypoint');
+            var utmSource = document.getElementById('fxa-email-form-utm-source');
+            var utmCampaign = document.getElementById('fxa-email-form-utm-campaign');
+
+            // add required params to the token fetch request
+            destURL += '?form_type=email';
+            destURL += '&entrypoint=' + entrypoint.value;
+            destURL += '&utm_source=' + utmSource.value;
+
+            // pass utm_campaign if available
+            if (utmCampaign) {
+                destURL += '&utm_campaign=' + utmCampaign.value;
+            }
+
+            fetch(destURL).then(function(resp) {
+                return resp.json();
+            }).then(function(r) {
+                fxaForm.querySelector('[name="device_id"]').value = r.deviceId;
+                fxaForm.querySelector('[name="flow_id"]').value = r.flowId;
+                fxaForm.querySelector('[name="flow_begin_time"]').value = r.flowBeginTime;
+            }).catch(function() {
+                // silently fail, leaving flow_id and flow_begin_time as default empty value
             });
-        } else {
-            fetchTokens();
-        }
-    }
-
-    // get tokens from FxA for analytics purposes
-    function fetchTokens() {
-        if (!supportsFetch) {
-            return;
         }
 
-        fxaSubmitButton.disabled = false;
-
-        var destURL = fxaForm.getAttribute('action') + 'metrics-flow';
-        var entrypoint = document.getElementById('fxa-email-form-entrypoint');
-        var utmSource = document.getElementById('fxa-email-form-utm-source');
-        var utmCampaign = document.getElementById('fxa-email-form-utm-campaign');
-
-        // add required params to the token fetch request
-        destURL += '?form_type=email';
-        destURL += '&entrypoint=' + entrypoint.value;
-        destURL += '&utm_source=' + utmSource.value;
-
-        // pass utm_campaign if available
-        if (utmCampaign) {
-            destURL += '&utm_campaign=' + utmCampaign.value;
+        if (fxaForm) {
+            // Sync is Firefox for desktop only.
+            if (Mozilla.Client.isFirefoxDesktop) {
+                init();
+            } else {
+                // Omit the fields required for sync.
+                // This allows non-Firefoxes to create accounts.
+                fxaForm.removeChild(fxaFormContextField);
+                fxaForm.removeChild(fxaFormServiceField);
+                fetchTokens();
+            }
         }
+    };
 
-        fetch(destURL).then(function(resp) {
-            return resp.json();
-        }).then(function(r) {
-            fxaForm.querySelector('[name="flow_id"]').value = r.flowId;
-            fxaForm.querySelector('[name="flow_begin_time"]').value = r.flowBeginTime;
-        }).catch(function() {
-            // silently fail, leaving flow_id and flow_begin_time as default empty value
-        });
-    }
+    window.Mozilla.FxaForm = FxaForm;
 
-    if (fxaForm) {
-        // Sync is Firefox for desktop only.
-        if (Mozilla.Client.isFirefoxDesktop) {
-            init();
-        } else {
-            // Omit the fields required for sync.
-            // This allows non-Firefoxes to create accounts.
-            fxaForm.removeChild(fxaFormContextField);
-            fxaForm.removeChild(fxaFormServiceField);
-            fetchTokens();
-        }
-    }
 })(window.Mozilla);
