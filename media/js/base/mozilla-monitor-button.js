07ca1fc089eae62d8a3560a1c6699813512ee187
Amy Churchwell <achurchwell@mozilla.com>
2019-08-14 15:38:52 -0700

    refactor monitor-button and fxa-form

--- media/js/base/mozilla-monitor-button.js
+++ media/js/base/mozilla-monitor-button.js
@@ -7,40 +7,56 @@ if (typeof window.Mozilla === 'undefined') {
     window.Mozilla = {};
 }
 
-Mozilla.MonitorButton = (function() {
+(function() {
     'use strict';
 
-    var monitorButton = document.getElementById('fxa-monitor-submit');
+    var MonitorButton = {};
 
-    // fetch request
-    var supportsFetch = 'fetch' in window;
+    MonitorButton.init = function(buttonId){
 
-    if (!supportsFetch || !monitorButton) {
-        return;
-    }
+        // check for custom buttonId, used in case of multiple buttons.
+        if(!buttonId){
+            buttonId = 'fxa-monitor-submit';
+        }
 
-    var buttonURL = monitorButton.getAttribute('href');
-    // strip url to everything after `?`
-    var buttonURLParams = buttonURL.match(/\?(.*)/)[1];
+        var monitorButton = document.getElementById(buttonId);
 
-    var destURL = monitorButton.getAttribute('data-action') + 'metrics-flow';
+        // fetch request
+        var supportsFetch = 'fetch' in window;
 
-    // collect values from monitor button
-    var params = window._SearchParams.queryStringToObject(buttonURLParams);
+        if (!supportsFetch || !monitorButton) {
+            return;
+        }
 
-    // add required params to the token fetch request
-    destURL += '&' + params;
+        var buttonURL = monitorButton.getAttribute('href');
+        // strip url to everything after `?`
+        var buttonURLParams = buttonURL.match(/\?(.*)/)[1];
 
-    fetch(destURL).then(function(resp) {
-        return resp.json();
-    }).then(function(r) {
-        // add retrieved deviceID, flowBeginTime and flowId values to cta url
-        buttonURL += '&deviceId=' + r.deviceId;
-        buttonURL += '&flowBeginTime=' + r.flowBeginTime;
-        buttonURL += '&flowId=' + r.flowId;
-        monitorButton.setAttribute('href', buttonURL);
-    }).catch(function() {
-        // silently fail: deviceId, flowBeginTime, flowId are not added to url.
-    });
+        var destURL = monitorButton.getAttribute('data-action') + 'metrics-flow';
+
+        // collect values from monitor button
+        var params = window._SearchParams.queryStringToObject(buttonURLParams);
+
+        // add required params to the token fetch request
+        destURL += '&entrypoint=' + params.entrypoint;
+        destURL += '&form_type=' + params.form_type;
+        destURL += '&utm_campaign=' + params.campaign;
+        destURL += '&utm_content=' + params.content;
+        destURL += '&utm_source=' + params.source;
+
+        fetch(destURL).then(function(resp) {
+            return resp.json();
+        }).then(function(r) {
+            // add retrieved deviceID, flowBeginTime and flowId values to cta url
+            buttonURL += '&deviceId=' + r.deviceId;
+            buttonURL += '&flowBeginTime=' + r.flowBeginTime;
+            buttonURL += '&flowId=' + r.flowId;
+            monitorButton.setAttribute('href', buttonURL);
+        }).catch(function() {
+            // silently fail: deviceId, flowBeginTime, flowId are not added to url.
+        });
+    };
+
+    window.Mozilla.MonitorButton = MonitorButton;
 })();
 
