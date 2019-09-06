7f38ae6d738149b33a6ff18ff4b10646ab1eac90
Alex Gibson <alexgibson@users.noreply.github.com>
2019-09-06 13:32:30 +0100

    Detect UITour failure / non-response on what's new page (Fixes #7671)

--- media/js/firefox/whatsnew/whatsnew-69.js
+++ media/js/firefox/whatsnew/whatsnew-69.js
@@ -5,6 +5,27 @@
 (function() {
     'use strict';
 
+    // Issue #7671 measure potential UITour failure for bug 1557153
+    function pingUITour() {
+        var startTime = performance.now();
+        var timeout = window.setTimeout(trackImpression, 1000, 'UITour Timeout');
+
+        Mozilla.UITour.ping(function() {
+            window.clearTimeout(timeout);
+            trackImpression('UITour Response');
+        });
+
+        function trackImpression(eLabel) {
+            var endTime = performance.now() - startTime;
+
+            window.dataLayer.push({
+                'eLabel': eLabel,
+                'data-response-time': endTime,
+                'event': 'non-interaction'
+            });
+        }
+    }
+
     // Prevent double-requesting Flow IDs. Inits signed out button even on non-firefox browsers (highly unlikely on this page, but you never know).
     if (Mozilla.Client.isFirefoxDesktop) {
         Mozilla.Client.getFxaDetails(function(details) {
@@ -17,4 +38,6 @@
     } else {
         Mozilla.MonitorButton.init('monitor-button-signed-out');
     }
+
+    pingUITour();
 })();
