cb795caf8e83d27fe73e232fd3a8d85878c42d69
Errolyn <Errolyn@mozilla.com>
2019-09-12 16:47:30 -0700

    removes unused tests

--- tests/functional/firefox/test_enterprise.py
+++ tests/functional/firefox/test_enterprise.py
@@ -14,37 +14,6 @@ def test_primary_download_button_displayed(base_url, selenium):
 
 
 @pytest.mark.nondestructive
-def test_click_contact_sales_button(base_url, selenium):
-    page = EnterprisePage(selenium, base_url).open()
-    signup_page = page.click_contact_sales_button()
-    assert signup_page.seed_url in selenium.current_url
-
-
-@pytest.mark.nondestructive
-def test_comparison_buttons_displayed_desktop(base_url, selenium):
-    page = EnterprisePage(selenium, base_url).open()
-    assert not page.is_comparison_basic_tab_button_displayed
-    assert not page.is_comparison_premium_tab_button_displayed
-    assert page.is_comparison_download_button_displayed
-    assert page.is_comparison_sales_button_displayed
-
-
-@pytest.mark.nondestructive
-def test_comparison_plan_displayed_mobile(base_url, selenium_mobile):
-    page = EnterprisePage(selenium_mobile, base_url).open()
-    assert page.is_comparison_basic_tab_button_displayed
-    assert page.is_comparison_premium_tab_button_displayed
-    assert page.is_comparison_basic_plan_displayed
-    assert not page.is_comparison_premium_plan_displayed
-    page.click_premium_plan_tab_button()
-    assert page.is_comparison_premium_plan_displayed
-    assert not page.is_comparison_basic_plan_displayed
-    page.click_basic_plan_tab_button()
-    assert not page.is_comparison_premium_plan_displayed
-    assert page.is_comparison_basic_plan_displayed
-
-
-@pytest.mark.nondestructive
 def test_package_download_buttons_displayed(base_url, selenium):
     page = EnterprisePage(selenium, base_url).open()
     assert page.is_package_win64_download_button_displayed
