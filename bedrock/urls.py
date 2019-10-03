088f1712ac5ed6d53cb01127c075d0240a3ee00c
Manoj Tyagi <38884133+neo-0224@users.noreply.github.com>
2019-10-03 19:06:53 +0530

    Changed url() to re_path() for Django 2.2 updates
    I have changed url() function to re_path() function which is more better for Django 2.2 version and also have changed imported modules from django.conf.urls to django.urls

--- bedrock/urls.py
+++ bedrock/urls.py
@@ -3,7 +3,7 @@
 # file, You can obtain one at http://mozilla.org/MPL/2.0/.
 
 from django.conf import settings
-from django.conf.urls import handler404, include, url
+from django.urls import handler404, include, re_path
 
 from django.utils.module_loading import import_string
 
@@ -17,25 +17,25 @@ handler500 = 'bedrock.base.views.server_error_view'
 
 urlpatterns = (
     # Main pages
-    url(r'^foundation/', include('bedrock.foundation.urls')),
-    url(r'^grants/', include('bedrock.grants.urls')),
-    url(r'^about/legal/', include('bedrock.legal.urls')),
-    url(r'^press/', include('bedrock.press.urls')),
-    url(r'^privacy/', include('bedrock.privacy.urls')),
-    url(r'^styleguide/', include('bedrock.styleguide.urls')),
-    url(r'^security/', include('bedrock.security.urls')),
-    url(r'', include('bedrock.firefox.urls')),
-    url(r'', include('bedrock.mozorg.urls')),
-    url(r'', include('bedrock.newsletter.urls')),
-    url(r'^etc/', include('bedrock.etc.urls')),
-    url(r'^exp/', include('bedrock.exp.urls')),
+    re_path(r'^foundation/', include('bedrock.foundation.urls')),
+    re_path(r'^grants/', include('bedrock.grants.urls')),
+    re_path(r'^about/legal/', include('bedrock.legal.urls')),
+    re_path(r'^press/', include('bedrock.press.urls')),
+    re_path(r'^privacy/', include('bedrock.privacy.urls')),
+    re_path(r'^styleguide/', include('bedrock.styleguide.urls')),
+    re_path(r'^security/', include('bedrock.security.urls')),
+    re_path(r'', include('bedrock.firefox.urls')),
+    re_path(r'', include('bedrock.mozorg.urls')),
+    re_path(r'', include('bedrock.newsletter.urls')),
+    re_path(r'^etc/', include('bedrock.etc.urls')),
+    re_path(r'^exp/', include('bedrock.exp.urls')),
 
-    url(r'^healthz/$', watchman_views.ping, name="watchman.ping"),
-    url(r'^readiness/$', watchman_views.status, name="watchman.status"),
-    url(r'^healthz-cron/$', base_views.cron_health_check),
-    url(r'^csp-violation-capture$', base_views.csp_violation_capture,
+    re_path(r'^healthz/$', watchman_views.ping, name="watchman.ping"),
+    re_path(r'^readiness/$', watchman_views.status, name="watchman.status"),
+    re_path(r'^healthz-cron/$', base_views.cron_health_check),
+    re_path(r'^csp-violation-capture$', base_views.csp_violation_capture,
         name='csp-violation-capture'),
-    url(r'^country-code\.json$', base_views.geolocate,
+    re_path(r'^country-code\.json$', base_views.geolocate,
         name='geolocate')
 )
 
@@ -45,6 +45,6 @@ if settings.DEBUG:
         return import_string(handler404)(request, '')
 
     urlpatterns += (
-        url(r'^404/$', show404),
-        url(r'^500/$', import_string(handler500)),
+        re_path(r'^404/$', show404),
+        re_path(r'^500/$', import_string(handler500)),
     )
