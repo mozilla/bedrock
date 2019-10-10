50db5e804eb9401de1cc99ab2aee107cc89d65aa
Paul McLanahan <pmac@users.noreply.github.com>
2019-10-10 10:11:53 -0400

    Add Heroku support for Review apps (#7849)
    * Add heroku.yml for docker deploys
    
    * Grab legal-docs if not present
    
    * Add app.json for Heroku pipelines and review apps

--- bin/sync-all.sh
+++ bin/sync-all.sh
@@ -6,6 +6,13 @@ if [ ! -e ./manage.py ]; then
     cd $script_parent_dir
 fi
 
+# for heroku demos
+# heroku doesn't pull git submodules
+if [[ ! -d ./vendor-local/src/legal-docs/firefox_privacy_notice ]]; then
+    rm -rf ./vendor-local/src/legal-docs
+    git clone --depth 1 https://github.com/mozilla/legal-docs.git vendor-local/src/legal-docs
+fi
+
 ./bin/run-db-download.py --force
 ./manage.py migrate --noinput
 ./bin/run-db-update.sh --all || true
