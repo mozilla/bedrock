ea166314c024b7e96ba6cb9a2f01e0f328a1a820
Sandrocottus <devank1999@gmail.com>
2019-10-17 14:53:57 +0530

    fixed linting issues

--- tests/functional/firefox/test_lockwise.py
+++ tests/functional/firefox/test_lockwise.py
@@ -9,6 +9,8 @@ def test_mobile_button(base_url, selenium):
     assert page.is_ios_download_button_displayed
     assert page.is_android_download_button_displayed
 
+
+@pytest.mark.nondestructive
 def test_firefox_add_on_button(base_url, selenium):
     page = FirefoxLockwisePage(selenium, base_url).open()
     assert page.is_firefox_add_on_button_displayed
