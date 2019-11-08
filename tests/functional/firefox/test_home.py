63003e6c507820bac04d9f8db3ad12a9dcebaf6e
Stephanie Hobson <sjhobson@gmail.com>
2019-11-08 11:15:45 -0800

    remove sanity marker

--- tests/functional/firefox/test_home.py
+++ tests/functional/firefox/test_home.py
@@ -7,9 +7,8 @@ import pytest
 from pages.firefox.home import FirefoxHomePage
 
 
-@pytest.mark.sanity
 @pytest.mark.nondestructive
-def test_download_menu_list_displayes(base_url, selenium):
+def test_download_menu_list_displays(base_url, selenium):
     page = FirefoxHomePage(selenium, base_url).open()
     page.browser_menu_list.click()
     assert page.browser_menu_list.list_is_open
