41d8981c6c77e411a58e3515bf28816819ba5fd0
Amy Churchwell <achurchwell@mozilla.com>
2019-11-20 14:20:22 -0800

    review changes

--- media/js/firefox/welcome4.js
+++ media/js/firefox/welcome4.js
@@ -2,11 +2,17 @@
  * License, v. 2.0. If a copy of the MPL was not distributed with this
  * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
 
-(function () {
+// create namespace
+if (typeof window.Mozilla === 'undefined') {
+    window.Mozilla = {};
+}
+
+(function(Mozilla) {
     'use strict';
 
     var content = document.querySelector('.mzp-u-modal-content');
     var buttons = document.getElementsByClassName('js-modal-link');
+    var sendTo = document.getElementById('send-to-device');
 
     for (var i = 0, len = buttons.length; i < len; i++) {
         var button = buttons[i];
@@ -14,12 +20,17 @@
         button.setAttribute('aria-role', 'button');
     }
 
+    if (sendTo) {
+        var form = new Mozilla.SendToDevice();
+        form.init();
+    }
+
     function openModal(e) {
         e.preventDefault();
 
         Mzp.Modal.createModal(e.target, content, {
-            closeText: 'Close modal'
+            closeText: window.Mozilla.Utils.trans('global-close')
         });
     }
 
-})();
+})(window.Mozilla);
