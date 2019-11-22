d08af4f389bf30435dea51a39803605cfe611b65
Errolyn <Errolyn@mozilla.com>
2019-11-22 13:49:52 -0800

    adding styles compiling

--- webpack.config.js
+++ webpack.config.js
@@ -1,15 +1,41 @@
 const path = require('path');
 
 module.exports = {
-    mode: 'development',
-    entry: {
-        'es6-test': [
-            path.resolve(__dirname,'media/js/components/test.es6.js'),
-            path.resolve(__dirname,'media/js/complied/test.es6.js'),
+  mode: 'development',
+  entry: {
+    'es6-test': [
+        path.resolve(__dirname,'media/js/components/test.es6.js'),
+        path.resolve(__dirname,'media/js/components/test2.es6.js'),
+        
+    ]
+  },
+
+  output: {
+    'filename': '[name].js',
+    'path': path.resolve(__dirname, "static_final/webpacked"),
+  },
+
+  module: {
+    rules: [
+      {
+        test: /\.scss$/,
+        use: [
+          MiniCssExtractPlugin.loader,
+          "css-loader",
+          "sass-loader"
         ]
-    },
-    output: {
-        'filename': '[name].js',
-        'path': path.resolve(__dirname, "static_final/webpacked"),
-    }
+      },
+      {
+        test: /\.less$/,
+        loader: 'less-loader', // compiles Less to CSS
+      },
+      {
+        test: /\.css$/,
+        use: [
+          MiniCssExtractPlugin.loader,
+          "css-loader"
+        ]
+      },
+    ]
+  }
 }
