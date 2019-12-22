e347b58b5272207e94a421808c6fa2aa6ab2ea73
Steven Garrity <steven@silverorange.com>
2019-12-21 20:51:37 -0400

    [fix #7574] Add browser comparison pages

--- bedrock/firefox/urls.py
+++ bedrock/firefox/urls.py
@@ -132,6 +132,10 @@ urlpatterns = (
 
     page('firefox/browsers/compare', 'firefox/compare/index.html'),
     page('firefox/browsers/compare/chrome', 'firefox/compare/chrome.html'),
+    page('firefox/browsers/compare/ie', 'firefox/compare/ie.html'),
+    page('firefox/browsers/compare/opera', 'firefox/compare/opera.html'),
+    page('firefox/browsers/compare/safari', 'firefox/compare/safari.html'),
+    page('firefox/browsers/compare/edge', 'firefox/compare/edge.html'),
 
     # Lockwise
     page('firefox/lockwise', 'firefox/lockwise/lockwise.html'),
