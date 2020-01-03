91e531a4920c710a30315b886da49b34d2ebe8f5
Alex Gibson <alexgibson@users.noreply.github.com>
2020-01-03 10:00:26 +0000

    Remove jQuery code from Manifesto page (Fixes #8368)

--- media/js/mozorg/manifesto.js
+++ media/js/mozorg/manifesto.js
@@ -2,11 +2,11 @@
  * License, v. 2.0. If a copy of the MPL was not distributed with this
  * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
 
-$(function() {
+(function() {
     'use strict';
 
     // Open Twitter in a sub window
-    var openTwitterSubwin = function (url) {
+    function openTwitterSubwin(url) {
         var width = 550;
         var height = 420;
         var options = {
@@ -20,15 +20,25 @@ $(function() {
             'left': Math.round((screen.width / 2) - (width / 2))
         };
 
-        window.open(url, 'twitter_share', $.param(options).replace(/&/g, ',')).focus();
-    };
+        window.open(url, 'twitter_share', window._SearchParams.objectToQueryString(options).replace(/&/g, ',')).focus();
+    }
 
-    // Set up twitter link handler
-    $('.js-manifesto-share').on('click', function (event) {
-        var href = $(this).attr('href');
+    function handleShareLinkClick(e) {
+        var href = e.target.href;
 
         // Open Twitter in a sub window
         openTwitterSubwin(href);
-        event.preventDefault();
-    });
-});
+        e.preventDefault();
+    }
+
+    function onLoad() {
+        // Set up twitter link handler
+        var shareLinks = document.querySelectorAll('.js-manifesto-share');
+
+        for (var i = 0; i < shareLinks.length; i++) {
+            shareLinks[i].addEventListener('click', handleShareLinkClick, false);
+        }
+    }
+
+    window.Mozilla.run(onLoad);
+})();
