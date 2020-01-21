0cccd8a934cd1fec7d386e6d811ca097cac1c0f1
Alex Gibson <alexgibson@users.noreply.github.com>
2020-01-21 09:09:45 +0000

    Add whatsnew page for firefox 73 (Fixes #8436) (#8458)

--- bedrock/firefox/tests/test_base.py
+++ bedrock/firefox/tests/test_base.py
@@ -466,6 +466,19 @@ class TestWhatsNew(TestCase):
 
     # end 72.0 whatsnew tests
 
+    # begin 73.0 whatsnew tests
+
+    @patch.object(fx_views, 'lang_file_is_active', lambda *x: True)
+    def test_fx_73_0_0(self, render_mock):
+        """Should use whatsnew-fx73 template for 73.0"""
+        req = self.rf.get('/firefox/whatsnew/')
+        req.locale = 'en-US'
+        self.view(req, version='73.0')
+        template = render_mock.call_args[0][1]
+        assert template == ['firefox/whatsnew/whatsnew-fx73.html']
+
+    # end 73.0 whatsnew tests
+
 
 @patch('bedrock.firefox.views.l10n_utils.render', return_value=HttpResponse())
 class TestWhatsNewIndia(TestCase):
--- bedrock/firefox/views.py
+++ bedrock/firefox/views.py
@@ -588,6 +588,8 @@ class WhatsnewView(L10nTemplateView):
                 template = 'firefox/whatsnew/index.html'
         elif locale == 'id':
             template = 'firefox/whatsnew/index-lite.id.html'
+        elif version.startswith('73.') and lang_file_is_active('firefox/whatsnew_73', locale):
+            template = 'firefox/whatsnew/whatsnew-fx73.html'
         elif version.startswith('72.') and lang_file_is_active('firefox/whatsnew_71', locale):
             template = 'firefox/whatsnew/whatsnew-fx71.html'
         elif version.startswith('71.') and lang_file_is_active('firefox/whatsnew_71', locale):
