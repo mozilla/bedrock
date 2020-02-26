ff16f1368014ce04371576bbf7c602bfb3979e0e
Errolyn <Errolyn@mozilla.com>
2020-02-25 17:29:07 -0800

    eslint happy

--- media/js/base/error-pages.js
+++ media/js/base/error-pages.js
@@ -1,11 +1,12 @@
 /* This Source Code Form is subject to the terms of the Mozilla Public
  * License, v. 2.0. If a copy of the MPL was not distributed with this
  * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
-
+ 
+// eslint-disable-next-line 
 function goBack(){
     'use strict';
     window.history.back();
-};
+}
 
 (function conditionallyHideBackButton(){ // Hides back button if there is no previous page to go back to.
     'use strict';
