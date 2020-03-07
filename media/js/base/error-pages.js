de5fe0026b5f5afd2c74f61b91db82eec078d13c
Errolyn <Errolyn@mozilla.com>
2020-03-06 16:03:56 -0800

    style fixes

--- media/js/base/error-pages.js
+++ media/js/base/error-pages.js
@@ -1,8 +1,8 @@
 /* This Source Code Form is subject to the terms of the Mozilla Public
  * License, v. 2.0. If a copy of the MPL was not distributed with this
  * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
- 
-// eslint-disable-next-line 
+
+// eslint-disable-next-line
 
 (function(){
     'use strict';
@@ -10,8 +10,8 @@
     if (window.history.length > 1) {
         var div = document.getElementById('go-back');
         div.classList.remove('hide-back');}
-    
+
     document.getElementById('go-back').addEventListener('click', function(){
         window.history.back();
-    }); 
+    });
 }());
