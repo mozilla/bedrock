0d78e966f91263f8cbdaad4a749f29acc38ea94b
Craig Cook <craigcook@users.noreply.github.com>
2020-03-11 13:53:13 -0700

    [fix #8564] Add whatsnew page for Firefox 75

---
+++ media/js/firefox/whatsnew/whatsnew-75.js
@@ -0,0 +1,31 @@
+/* This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
+
+// create namespace
+if (typeof window.Mozilla === 'undefined') {
+    window.Mozilla = {};
+}
+
+(function(Mozilla) {
+    'use strict';
+
+    var client = Mozilla.Client;
+
+    // bug 1419573 - only show "Your Firefox is up to date" if it's the latest version.
+    if (client.isFirefoxDesktop) {
+        client.getFirefoxDetails(function(data) {
+            if (data.isUpToDate) {
+                document.querySelector('.c-page-header').classList.add('show-up-to-date-message');
+            }
+        });
+    }
+
+    client.getFxaDetails(function(details) {
+        // Query param `signed-in=true` is used for integration testing and demo review only.
+        if (details.setup || window.location.search.indexOf('signed-in=true') !== -1) {
+            document.querySelector('.content-wrapper').classList.add('state-is-signed-in');
+        }
+    });
+
+})(window.Mozilla);
