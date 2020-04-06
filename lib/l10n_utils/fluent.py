acc232e038ad2396e3b8dfac7ae6f487edff219d
Paul McLanahan <pmac@users.noreply.github.com>
2020-04-06 13:13:02 -0400

    Update Fluent python view utils with better dev UX (#8751)
    * Update Fluent python view utils with better dev UX
    
    This updates the `ftl`, `ftl_lazy`, and `has_messages` functions that
    are for use in Python code to not require fluent files to be defined
    if using the default files, to allow passing a single fluent file
    as a string instead of a list, and to require fluent files to be
    passed in via the `ftl_files` keyword argument.
    
    * Update docs with new usage pattern for Python ftl function

--- lib/l10n_utils/fluent.py
+++ lib/l10n_utils/fluent.py
@@ -120,7 +120,15 @@ def memoize(f):
 def l10nize(f):
     """Decorator to create and pass in the l10n object"""
     @wraps(f)
-    def inner(ftl_files, *args, **kwargs):
+    def inner(*args, **kwargs):
+        ftl_files = kwargs.get('ftl_files', [])
+        if isinstance(ftl_files, str):
+            ftl_files = [ftl_files]
+        elif isinstance(ftl_files, tuple):
+            ftl_files = list(ftl_files)
+
+        # can not use += here because that mutates the original list
+        ftl_files = ftl_files + settings.FLUENT_DEFAULT_FILES
         locale = kwargs.get('locale') or translation.get_language(True)
         l10n = fluent_l10n([locale, 'en'], ftl_files)
         return f(l10n, *args, **kwargs)
