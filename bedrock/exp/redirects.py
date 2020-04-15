7df2113a7e821fd9eb672111cd4502140d8a66b6
Stephanie Hobson <sjhobson@gmail.com>
2020-04-15 10:03:23 -0700

    Update bedrock/exp/redirects.py
    Co-Authored-By: Alex Gibson <alexgibson@users.noreply.github.com>

--- bedrock/exp/redirects.py
+++ bedrock/exp/redirects.py
@@ -2,6 +2,6 @@ from bedrock.redirects.util import redirect
 
 redirectpatterns = (
 
-    redirect(r'^exp/firefox/new/nav?$', 'firefox.new'),
+    redirect(r'^exp/firefox/new/nav/?$', 'firefox.new'),
 
 )
