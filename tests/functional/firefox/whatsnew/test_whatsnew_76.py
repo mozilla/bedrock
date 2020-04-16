d7539b355f451c2ac2d5136fa3631c58618532d4
Errolyn <Errolyn@mozilla.com>
2020-04-16 14:55:08 -0700

    adding tests

---
+++ tests/functional/firefox/whatsnew/test_whatsnew_76.py
@@ -0,0 +1,23 @@
+# This Source Code Form is subject to the terms of the Mozilla Public
+# License, v. 2.0. If a copy of the MPL was not distributed with this
+# file, You can obtain one at http://mozilla.org/MPL/2.0/.
+
+import pytest
+
+from pages.firefox.whatsnew.whatsnew_76 import FirefoxWhatsNew76Page
+
+
+@pytest.mark.skip_if_not_firefox(reason='Whatsnew pages are shown to Firefox only.')
+@pytest.mark.nondestructive
+def test_facebook_container_picto_block_displayed(base_url, selenium):
+    page = FirefoxWhatsNew76Page(selenium, base_url, params='').open()
+    assert page.is_facebook_container_picto_block_displayed
+    assert page.are_three_columns_displayed
+
+
+@pytest.mark.skip_if_not_firefox(reason='Whatsnew pages are shown to Firefox only.')
+@pytest.mark.nondestructive
+def test_facebook_container_picto_block_not_displayed_in_zh_tw(base_url, selenium):
+    page = FirefoxWhatsNew76Page(selenium, base_url, params='', locale="zh-TW").open()
+    assert not page.is_facebook_container_picto_block_displayed
+    assert page.are_two_columns_displayed
