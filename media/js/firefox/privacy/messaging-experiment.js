6a9eb870ad6c461b6dea4a64e700e6c5c855aa38
Erica Wright <ericaewright@gmail.com>
2020-04-23 11:43:44 -0400

    maybe cleanup

--- media/js/firefox/privacy/messaging-experiment.js
+++ media/js/firefox/privacy/messaging-experiment.js
@@ -2,28 +2,27 @@
  * License, v. 2.0. If a copy of the MPL was not distributed with this
  * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
 
- (function() {
-  'use strict';
+(function() {
+    'use strict';
 
-  var client = Mozilla.Client;
+    var client = Mozilla.Client;
 
-  function handleOpenProtectionReport(e) {
-      e.preventDefault();
-      Mozilla.UITour.showProtectionReport();
+    function handleOpenProtectionReport(e) {
+        e.preventDefault();
+        Mozilla.UITour.showProtectionReport();
 
-      window.dataLayer.push({
-          'event': 'in-page-interaction',
-          'eAction': 'link click',
-          'eLabel': 'See what Firefox has blocked for you'
-      });
-  }
+        window.dataLayer.push({
+            'event': 'in-page-interaction',
+            'eAction': 'link click',
+            'eLabel': 'See what Firefox has blocked for you'
+        });
+    }
 
-  if (client.isFirefoxDesktop && client._getFirefoxMajorVersion() >= 70) {
-      // Intercept link clicks to open about:protections page using UITour.
-      Mozilla.UITour.ping(function() {
-          var protectionReportLink = document.querySelector('.js-open-about-protections');
-          protectionReportLink.addEventListener('click', handleOpenProtectionReport, false);
-      });
-  }
-
+    if (client.isFirefoxDesktop && client._getFirefoxMajorVersion() >= 70) {
+        // Intercept link clicks to open about:protections page using UITour.
+        Mozilla.UITour.ping(function() {
+            var protectionReportLink = document.querySelector('.js-open-about-protections');
+            protectionReportLink.addEventListener('click', handleOpenProtectionReport, false);
+        });
+    }
 })(window.Mozilla);
