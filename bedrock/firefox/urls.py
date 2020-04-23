021814ba06af5badeba4293d9625067ee6a228a6
Erica Wright <ericaewright@gmail.com>
2020-04-23 11:28:05 -0400

    add icons

--- bedrock/firefox/urls.py
+++ bedrock/firefox/urls.py
@@ -113,11 +113,9 @@ urlpatterns = (
     page('firefox/welcome/6', 'firefox/welcome/page6.html'),
     page('firefox/welcome/7', 'firefox/welcome/page7.html'),
 
-
-    page('firefox/privacy_by_default', 'firefox/messaging-experiment/privacy_by_default.html'),
-    page('firefox/privacy_promise', 'firefox/messaging-experiment/privacy_promise.html'),
-    page('firefox/mobile_promo', 'firefox/messaging-experiment/control.html'),
-
+    page('firefox/privacy-by-default', 'firefox/messaging-experiment/privacy_by_default.html'),
+    page('firefox/privacy-promise', 'firefox/messaging-experiment/privacy_promise.html'),
+    page('firefox/mobile-promo', 'firefox/messaging-experiment/control.html'),
 
     page('firefox/switch', 'firefox/switch.html', ftl_files=['firefox/switch', 'firefox/switch-en']),
     page('firefox/pocket', 'firefox/pocket.html'),
