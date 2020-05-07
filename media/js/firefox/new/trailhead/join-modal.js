e0b0073f2a04cfad7af6106d782193bc70005b20
Alex Gibson <alexgibson@users.noreply.github.com>
2020-05-08 00:33:26 +0100

    Remove mozilla-modal.js script (#8842)

--- media/js/firefox/new/trailhead/join-modal.js
+++ media/js/firefox/new/trailhead/join-modal.js
@@ -2,8 +2,6 @@
  * License, v. 2.0. If a copy of the MPL was not distributed with this
  * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
 
-// This relies on base/mozilla-modal.js, included in the firefox_new_scene1 bundle
-
 (function() {
     'use strict';
 
