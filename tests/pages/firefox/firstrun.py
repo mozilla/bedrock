1164a72a5f74e4b05902a2673ee9357dd006e323
Alex Gibson <alexgibson@users.noreply.github.com>
2020-05-07 09:27:08 +0100

    Add functional test for legacy /firstrun page (Fixes #8890)

---
+++ tests/pages/firefox/firstrun.py
@@ -0,0 +1,15 @@
+# This Source Code Form is subject to the terms of the Mozilla Public
+# License, v. 2.0. If a copy of the MPL was not distributed with this
+# file, You can obtain one at http://mozilla.org/MPL/2.0/.
+
+from pages.base import BasePage
+from pages.regions.join_firefox_form import JoinFirefoxForm
+
+
+class FirefoxFirstrunPage(BasePage):
+
+    URL_TEMPLATE = '/{locale}/firefox/firstrun/'
+
+    @property
+    def join_firefox_form(self):
+        return JoinFirefoxForm(self)
