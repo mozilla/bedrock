ede8ac6a4ac7fa8bec821d7e7e04a7d86343e7f9
Paul McLanahan <pmac@mozilla.com>
2020-06-03 10:48:57 -0400

    Add prometheus and remove statsd

--- bedrock/urls.py
+++ bedrock/urls.py
@@ -28,6 +28,7 @@ urlpatterns = (
     url(r'', include('bedrock.mozorg.urls')),
     url(r'', include('bedrock.newsletter.urls')),
     url(r'^exp/', include('bedrock.exp.urls')),
+    url(r'^prometheus/', include('django_prometheus.urls')),
 
     url(r'^healthz/$', watchman_views.ping, name="watchman.ping"),
     url(r'^readiness/$', watchman_views.status, name="watchman.status"),
@@ -35,7 +36,7 @@ urlpatterns = (
     url(r'^csp-violation-capture$', base_views.csp_violation_capture,
         name='csp-violation-capture'),
     url(r'^country-code\.json$', base_views.geolocate,
-        name='geolocate')
+        name='geolocate'),
 )
 
 if settings.DEBUG:
