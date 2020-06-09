035979b11ec60eb79a81894f94bb7c5584807709
Alex Gibson <alexgibson@users.noreply.github.com>
2020-06-09 14:19:06 +0100

    Migrate firefox/products to Fluent (Fixes #9001) [skip l10n]

--- bedrock/firefox/urls.py
+++ bedrock/firefox/urls.py
@@ -32,7 +32,7 @@ urlpatterns = (
     url(r'^firefox/all/$', views.firefox_all, name='firefox.all'),
     page('firefox/accounts', 'firefox/accounts.html', ftl_files=['firefox/accounts']),
     page('firefox/browsers', 'firefox/browsers/index.html', ftl_files=['firefox/browsers']),
-    page('firefox/products', 'firefox/products/index.html'),
+    page('firefox/products', 'firefox/products/index.html', ftl_files=['firefox/products']),
     page('firefox/campaign', 'firefox/campaign/index.html'),
     page('firefox/flashback', 'firefox/flashback/index.html', active_locales=['en-US', 'de', 'fr']),
     page('firefox/channel/desktop', 'firefox/channel/desktop.html'),
