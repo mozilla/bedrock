f95c4ed85781d85e679c5216b7455a970e62424e
Stephanie Hobson <sjhobson@gmail.com>
2020-06-24 14:07:31 -0700

    New /new page. (#9000)

--- bedrock/firefox/views.py
+++ bedrock/firefox/views.py
@@ -738,6 +738,8 @@ class NewView(L10nTemplateView):
 
         if locale == 'ru' and switch('firefox-yandex'):
             template = 'firefox/new/trailhead/download-yandex.html'
+        elif locale == 'en-US' and switch('desktop-download') and variant == 'b':
+            template = 'firefox/new/desktop/download.html'
         elif ftl_file_is_active('firefox/new/download'):
             template = 'firefox/new/trailhead/download.html'
         else:
