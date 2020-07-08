b027b54a34ea6e66a67460a3185afde5afd67ab9
Stephanie Hobson <sjhobson@gmail.com>
2020-07-07 18:04:24 -0700

    EN homepage redesign

--- bedrock/mozorg/views.py
+++ bedrock/mozorg/views.py
@@ -122,7 +122,7 @@ def home_view(request):
 
     if locale.startswith('en-'):
         template_name = 'mozorg/home/home-en.html'
-        ctx['page_content_cards'] = get_page_content_cards('home-2019', 'en-US')
+        ctx['page_content_cards'] = get_page_content_cards('home-en', 'en-US')
     elif locale == 'de':
         template_name = 'mozorg/home/home-de.html'
         ctx['page_content_cards'] = get_page_content_cards('home-de', 'de')
