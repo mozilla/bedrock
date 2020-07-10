f788ea822ba47a203df04cfdae9146ee0620a325
Erica Wright <ericaewright@gmail.com>
2020-07-10 17:06:51 -0400

    Visual treatment test for high velocity testing.

--- bedrock/firefox/urls.py
+++ bedrock/firefox/urls.py
@@ -114,10 +114,12 @@ urlpatterns = (
     page('firefox/welcome/6', 'firefox/welcome/page6.html', ftl_files=['firefox/welcome/page6']),
     page('firefox/welcome/7', 'firefox/welcome/page7.html', ftl_files=['firefox/welcome/page7']),
     page('firefox/welcome/8', 'firefox/welcome/page8.html', ftl_files=['firefox/welcome/page8']),
+    page('firefox/welcome/9', 'firefox/welcome/page9.html', ftl_files=['firefox/welcome/page9']),
 
     page('firefox/privacy-by-default', 'firefox/messaging-experiment/privacy_by_default.html'),
     page('firefox/privacy-tools', 'firefox/messaging-experiment/privacy_tools.html'),
     page('firefox/mobile-promo', 'firefox/messaging-experiment/control.html'),
+
 
     page('firefox/switch', 'firefox/switch.html', ftl_files=['firefox/switch']),
     page('firefox/pocket', 'firefox/pocket.html'),
--- media/js/firefox/welcome/welcome8.js
+++ media/js/firefox/welcome/welcome8.js
@@ -2,8 +2,56 @@
  * License, v. 2.0. If a copy of the MPL was not distributed with this
  * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
 
-(function() {
+(function (Mozilla) {
     'use strict';
+
+    /* update dataLayer with experiment info */
+    var href = window.location.href;
+
+    var initTrafficCop = function () {
+        if (href.indexOf('server-variation=') !== -1) {
+            if (href.indexOf('server-variation=1') !== -1) {
+              // default, no special class needed
+                window.dataLayer.push({
+                    'data-ex-variant': 'experiment-hvt-visual-v1',
+                    'data-ex-name': 'experiment-hvt-visual'
+                });
+            } else if (href.indexOf('server-variation=2') !== -1) {
+              document.body.classList.add("variation-2-image");
+                window.dataLayer.push({
+                    'data-ex-variant': 'experiment-hvt-visual-v2',
+                    'data-ex-name': 'experiment-hvt-visual'
+                });
+            } else if (href.indexOf('server-variation=3') !== -1) {
+              document.body.classList.add("variation-3-animation");
+                window.dataLayer.push({
+                    'data-ex-variant': 'experiment-hvt-visual-v3',
+                    'data-ex-name': 'experiment-hvt-visual'
+                });
+            } else if (href.indexOf('server-variation=4') !== -1) {
+              document.body.classList.add("variation-4-text");
+                window.dataLayer.push({
+                    'data-ex-variant': 'experiment-hvt-visual-v4',
+                    'data-ex-name': 'experiment-hvt-visual'
+                });
+            }
+
+          } else {
+            var cop = new Mozilla.TrafficCop({
+                id: 'experiment_hvt_visual',
+                cookieExpires: 0,
+                variations: {
+                    'server-variation=1': 1,
+                    'server-variation=2': 20,
+                    'server-variation=3': 20,
+                    'server-variation=4': 20,
+                }
+            });
+
+            cop.init();
+        }
+    };
+    initTrafficCop();
 
     function handleOpenProtectionReport(e) {
         e.preventDefault();
@@ -24,4 +72,4 @@
         );
     });
 
-})();
+})(window.Mozilla);
