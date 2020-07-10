b9cc8283f2c2b90bfa1e11666d079abbc517b63d
Stephanie Hobson <sjhobson@gmail.com>
2020-07-10 16:52:57 -0700

    Market desktop browser on /new (#9019)
    - add desktop focused /new page, assets, l10n
    - add updated /thanks page, assets, l10n
    - update view

--- tests/functional/firefox/new/test_download.py
+++ tests/functional/firefox/new/test_download.py
@@ -6,9 +6,6 @@ import pytest
 
 from pages.firefox.new.download import DownloadPage
 
-# ?v=a param added temporarily to evade a traffic cop experiment
-# See https://github.com/mozilla/bedrock/issues/9019
-
 
 @pytest.mark.sanity
 @pytest.mark.nondestructive
@@ -22,14 +19,14 @@ def test_download_button_displayed(base_url, selenium):
 @pytest.mark.skip_if_internet_explorer(reason='https://github.com/SeleniumHQ/selenium/issues/448')
 @pytest.mark.nondestructive
 def test_click_download_button(base_url, selenium):
-    page = DownloadPage(selenium, base_url, params='?v=a').open()
+    page = DownloadPage(selenium, base_url).open()
     thank_you_page = page.download_firefox()
     assert thank_you_page.seed_url in selenium.current_url
 
 
 @pytest.mark.nondestructive
 def test_other_platforms_modal(base_url, selenium):
-    page = DownloadPage(selenium, base_url, params='?v=a').open()
+    page = DownloadPage(selenium, base_url).open()
     modal = page.open_other_platforms_modal()
     assert modal.is_displayed
     modal.close()
@@ -38,7 +35,7 @@ def test_other_platforms_modal(base_url, selenium):
 @pytest.mark.nondestructive
 @pytest.mark.skip_if_not_firefox(reason='Join Firefox form is only displayed to Firefox users')
 def test_firefox_account_modal(base_url, selenium):
-    page = DownloadPage(selenium, base_url, params='?v=a').open()
+    page = DownloadPage(selenium, base_url).open()
     modal = page.open_join_firefox_modal()
     assert modal.is_displayed
     modal.close()
