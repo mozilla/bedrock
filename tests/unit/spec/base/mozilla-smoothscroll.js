d7c34b1e40f30968f7d82bf4652803e99903cb74
vinay-deshmukh <vinay_deshmukh@outlook.com>
2020-07-31 10:53:32 +0530

    Removed jQuery from mozilla-lazy-load.js & mozilla-smoothscroll.js (Fixes #8381)
    For mozilla-lazy-load.js:
    1. Only one occurrence of $, which was replaced with querySelectorAll
    
    For mozilla-smoothscroll.js:
    1. Removed a unit test spec which expected jQuery functions to be called
    2. Modified a unit test spec to not refer to window.jQuery

--- tests/unit/spec/base/mozilla-smoothscroll.js
+++ tests/unit/spec/base/mozilla-smoothscroll.js
@@ -24,38 +24,16 @@ describe('mozilla-smoothscroll.js', function () {
             });
         });
 
-        it('should use jQuery fallback when smooth scrolling is not supported', function () {
-            spyOn(window, 'scrollTo');
-            spyOn($.fn, 'animate');
-
-            Mozilla.smoothScroll({
-                top: 200,
-                unitTest: 'jQuery'
-            });
-
-            expect(window.scrollTo).not.toHaveBeenCalled();
-            expect($.fn.animate).toHaveBeenCalledWith({
-                scrollTop: 200,
-                scrollLeft: 0
-            }, 400);
-        });
-
-        it('should use legacy window.scrollTo when neither smooth scrolling nor jQuery are available', function () {
-            // take away jQuery for a minute
-            var jQueryBack = window.jQuery;
-            window.jQuery = undefined;
+        it('should use legacy window.scrollTo when smooth scrolling is not available', function () {
 
             spyOn(window, 'scrollTo');
 
             Mozilla.smoothScroll({
                 top: 200,
-                unitTest: 'fallback'
+                unitTest: 'fallback' // disable smoothScroll
             });
 
             expect(window.scrollTo).toHaveBeenCalledWith(200, 0);
-
-            // bring jQuery back
-            window.jQuery = jQueryBack;
         });
     });
 });
