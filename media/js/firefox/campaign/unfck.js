08ac4a4870d6037838435ab277ae536ede8585f8
Stephanie Hobson <sjhobson@gmail.com>
2020-09-02 14:00:46 -0700

    Interactions for september campaign landing page (#9334)
    - Removed copy functionality while discussions about what/if should be copied continue
    - Simplified twitter share to only share current page URL (while discussions about what/if shoudl be shared continue)
    - Added GTM tags to share widget and cta links
    - outdented list numbers when page is wide enough
    - renamed macro param from `include_utms` to `link_include_utms`
    - added optional `link_is_external` to macro

--- media/js/firefox/campaign/unfck.js
+++ media/js/firefox/campaign/unfck.js
@@ -1,3 +1,87 @@
 /* This Source Code Form is subject to the terms of the Mozilla Public
 * License, v. 2.0. If a copy of the MPL was not distributed with this
 * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
+
+
+/*
+    Twitter sharing
+*/
+(function() {
+    'use strict';
+
+    // Twitter share
+    function openTwitterSubwin(url) {
+        var width = 550;
+        var height = 420;
+        var options = {
+            'scrollbars': 'yes',
+            'resizable': 'yes',
+            'toolbar': 'no',
+            'location': 'yes',
+            'width': width,
+            'height': height,
+            'top': screen.height > height ? Math.round((screen.height / 2) - (height / 2)) : 0,
+            'left': Math.round((screen.width / 2) - (width / 2))
+        };
+
+        window.open(url, 'twitter_share', window._SearchParams.objectToQueryString(options).replace(/&/g, ',')).focus();
+    }
+
+    function handleShareLinkClick(e) {
+        var item = e.target.closest('.c-item-unfck');
+        if(e.target.tagName == 'SPAN') {
+            var href = e.target.parentElement.href;
+        } else {
+            var href = e.target.href;
+        }
+
+        // Track the event in GA
+        window.dataLayer.push({
+            'event': 'in-page-interaction',
+            'eAction': 'checklist',
+            'eLabel': 'share: ' + item.id,
+        });
+
+        // Open Twitter in a sub window
+        openTwitterSubwin(href);
+        e.preventDefault();
+    }
+
+    function onLoad() {
+        // Set up twitter link handler
+        var shareLinks = document.querySelectorAll('.js-twitter-share');
+
+        for (var i = 0; i < shareLinks.length; i++) {
+            shareLinks[i].addEventListener('click', handleShareLinkClick, false);
+        }
+    }
+
+    window.Mozilla.run(onLoad);
+})();
+
+
+/*
+    Download GIF
+*/
+(function() {
+    'use strict';
+
+    function watchDownloads() {
+        var downloadLinks = document.querySelectorAll('.js-download-gif');
+
+        for (var i = 0; i < downloadLinks.length; i++) {
+            downloadLinks[i].addEventListener('click', function(e) {
+                var item = e.target.closest('.c-item-unfck');
+                // Track the event in GA
+                window.dataLayer.push({
+                    'event': 'in-page-interaction',
+                    'eAction': 'checklist',
+                    'eLabel': 'download: ' + item.id,
+                });
+            }, false);
+        }
+    }
+
+    window.Mozilla.run(watchDownloads);
+
+})();
