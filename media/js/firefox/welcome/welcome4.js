5b957f8e8949dfa2a104df156dd4ee9874c111c8
Amy Churchwell <achurchwell@mozilla.com>
2020-09-02 14:07:20 -0700

    Moments 4 Refresh (#9013)

--- media/js/firefox/welcome/welcome4.js
+++ media/js/firefox/welcome/welcome4.js
@@ -9,28 +9,11 @@ if (typeof window.Mozilla === 'undefined') {
 
 (function(Mozilla) {
     'use strict';
-
-    var content = document.querySelector('.mzp-u-modal-content');
-    var buttons = document.getElementsByClassName('js-modal-link');
     var sendTo = document.getElementById('send-to-device');
-
-    for (var i = 0, len = buttons.length; i < len; i++) {
-        var button = buttons[i];
-        button.addEventListener('click', openModal);
-        button.setAttribute('aria-role', 'button');
-    }
 
     if (sendTo) {
         var form = new Mozilla.SendToDevice();
         form.init();
-    }
-
-    function openModal(e) {
-        e.preventDefault();
-
-        Mzp.Modal.createModal(e.target, content, {
-            closeText: window.Mozilla.Utils.trans('global-close')
-        });
     }
 
 })(window.Mozilla);
--- tests/functional/firefox/welcome/test_welcome_page4.py
+++ tests/functional/firefox/welcome/test_welcome_page4.py
@@ -10,42 +10,26 @@ from pages.firefox.welcome.page4 import FirefoxWelcomePage4
 
 @pytest.mark.skip_if_not_firefox(reason='Welcome pages are shown to Firefox only.')
 @pytest.mark.nondestructive
-def test_modal_button_displayed(base_url, selenium):
-    page = FirefoxWelcomePage4(selenium, base_url).open()
-    assert page.is_primary_modal_button_displayed
-    assert page.is_secondary_modal_button_displayed
-
-
-@pytest.mark.skip_if_not_firefox(reason='Welcome pages are shown to Firefox only.')
-@pytest.mark.nondestructive
 def test_get_firefox_qr_code(base_url, selenium):
     page = FirefoxWelcomePage4(selenium, base_url, locale='sv-SE').open()
-    modal = page.click_primary_modal_button()
-    assert modal.is_displayed
     assert not page.send_to_device.is_displayed
     assert page.is_firefox_qr_code_displayed
-    modal.close()
 
 
 @pytest.mark.skip_if_not_firefox(reason='Welcome pages are shown to Firefox only.')
 @pytest.mark.nondestructive
 def test_primary_get_firefox_send_to_device_success(base_url, selenium):
     page = FirefoxWelcomePage4(selenium, base_url).open()
-    modal = page.click_primary_modal_button()
-    assert modal.is_displayed
     assert not page.is_firefox_qr_code_displayed
     send_to_device = page.send_to_device
     send_to_device.type_email('success@example.com')
     send_to_device.click_send()
     assert send_to_device.send_successful
-    modal.close()
 
 
 @pytest.mark.skip_if_not_firefox(reason='Welcome pages are shown to Firefox only.')
 @pytest.mark.nondestructive
 def test_get_firefox_send_to_device_fails_when_missing_required_fields(base_url, selenium):
     page = FirefoxWelcomePage4(selenium, base_url).open()
-    modal = page.click_primary_modal_button()
-    assert modal.is_displayed
     with pytest.raises(TimeoutException):
         page.send_to_device.click_send()
