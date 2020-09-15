a5239db18f937de037f1076a7ee3ccf449ffc0f5
Alex Gibson <alexgibson@users.noreply.github.com>
2020-09-15 19:18:38 +0100

    Remove legacy /contribute functional test (#9405)

--- tests/pages/contribute/contribute.py
+++ tests/pages/contribute/contribute.py
@@ -2,20 +2,9 @@
 # License, v. 2.0. If a copy of the MPL was not distributed with this
 # file, You can obtain one at http://mozilla.org/MPL/2.0/.
 
-from selenium.webdriver.common.by import By
-
-from pages.contribute.base import ContributeBasePage
-from pages.regions.modal import ModalProtocol
+from pages.base import BasePage
 
 
-class ContributePage(ContributeBasePage):
+class ContributePage(BasePage):
 
     URL_TEMPLATE = '/{locale}/contribute/'
-
-    _video_play_locator = (By.CSS_SELECTOR, '.js-contribute-video')
-
-    def play_video(self):
-        modal = ModalProtocol(self)
-        self.find_element(*self._video_play_locator).click()
-        self.wait.until(lambda s: modal.is_displayed)
-        return modal
