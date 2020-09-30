62abebb2ba4ebe2a2975a54327644f6165f8ed73
Alex Gibson <alexgibson@users.noreply.github.com>
2020-09-30 16:01:20 +0100

    Add functional tests for sept landing pages (Fixes #9465)

---
+++ tests/functional/firefox/test_unfck.py
@@ -0,0 +1,30 @@
+# This Source Code Form is subject to the terms of the Mozilla Public
+# License, v. 2.0. If a copy of the MPL was not distributed with this
+# file, You can obtain one at http://mozilla.org/MPL/2.0/.
+
+import pytest
+
+from pages.firefox.unfck import FirefoxUnfckPage
+
+
+@pytest.mark.skip_if_firefox(reason='Download button is displayed only to non-Firefox users')
+@pytest.mark.nondestructive
+@pytest.mark.parametrize('locale', [
+    ('en-US'),
+    ('de')])
+def test_download_button_displayed(locale, base_url, selenium):
+    page = FirefoxUnfckPage(selenium, base_url, locale=locale).open()
+    assert page.download_button.is_displayed
+
+
+@pytest.mark.skip_if_not_firefox(reason='App Store buttons are displayed only to Firefox users')
+def test_app_store_buttons_displayed(base_url, selenium):
+    page = FirefoxUnfckPage(selenium, base_url).open()
+    assert page.is_play_store_button_displayed
+    assert page.is_app_store_button_displayed
+
+
+@pytest.mark.skip_if_not_firefox(reason='Facebook Container button is displayed only to Firefox users')
+def test_facebook_container_button_displayed(base_url, selenium):
+    page = FirefoxUnfckPage(selenium, base_url, locale='de').open()
+    assert page.is_facebook_container_button_displayed
