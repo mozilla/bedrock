943c0fa86fd31cf3b17774fa89955ea873d508e0
Paul McLanahan <pmac@mozilla.com>
2020-10-09 17:22:11 -0400

    Fix db migration for local db update

--- bin/run-file-clock.sh
+++ bin/run-file-clock.sh
@@ -1,6 +1,7 @@
 #!/bin/bash -xe
 
 bin/run-db-download.py --force
+python manage.py migrate --noinput
 LOCAL_DB_UPDATE=$(echo "${LOCAL_DB_UPDATE:-false}" | tr '[:upper:]' '[:lower:]')
 if [[ "$LOCAL_DB_UPDATE" == "true" ]]; then
     exec python bin/cron.py file db
