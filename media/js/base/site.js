60cc32ac65a717056510851365e74740e44fdf2c
Alex Gibson <alexgibson@users.noreply.github.com>
2020-11-05 14:27:48 +0000

    Prototype Client Hints

--- media/js/base/site.js
+++ media/js/base/site.js
@@ -5,7 +5,56 @@
 (function () {
     'use strict';
     window.site = {
-        getPlatform: function (ua, pf) {
+
+        supportsClientHints: function() {
+            return 'userAgentData' in navigator &&
+                   typeof navigator.userAgentData.getHighEntropyValues !== 'undefined' &&
+                   'Promise' in window;
+        },
+
+        getClientHintsData: function() {
+            return new window.Promise(function(resolve) {
+                navigator.userAgentData.getHighEntropyValues([
+                    'architecture',
+                    'platform',
+                    'platformVersion'
+                ]).then(function(ua) {
+                    if (ua && ua.architecture) {
+                        window.site.clientHintData.architecture = ua.architecture;
+                    }
+                    if (ua && ua.platform) {
+                        window.site.clientHintData.platform = ua.platform;
+                    }
+                    if (ua && ua.platformVersion) {
+                        window.site.clientHintData.platformVersion = ua.platformVersion;
+                    }
+                    resolve();
+                });
+            });
+        },
+
+        getPlatformFromCH: function (platform) {
+            if (platform === 'Windows') {
+                return 'windows';
+            }
+            if (platform === 'Android') {
+                return 'android';
+            }
+            if (platform === 'Linux') {
+                return 'linux';
+            }
+            // TODO: there's no implementation of Client Hints available on iOS?
+            if (platform === 'iOS') {
+                return 'ios';
+            }
+            if (platform === 'Mac OS X') {
+                return 'osx';
+            }
+
+            return 'other';
+        },
+
+        getPlatformFromUA: function (ua, pf) {
             pf = pf || navigator.platform;
             ua = ua || navigator.userAgent;
 
@@ -35,7 +84,23 @@
             return 'other';
         },
 
-        getPlatformVersion: function (ua) {
+        getPlatformVersionFromCH: function (platform, version) {
+            var match;
+
+            // On OS X, Safari and Chrome have underscores instead of dots
+            if (platform === 'Windows') {
+                match = version.match(/\d+\.\d+/);
+            } else if (platform === 'Mac OS X') {
+                match = version.match(/\d+[._]\d+/);
+            } else if (platform === 'Android') {
+                // TODO do we need to detect Android versions?
+                match = version.match(/\d+\.\d+/);
+            }
+
+            return match ? match[0].replace('_', '.') : undefined;
+        },
+
+        getPlatformVersionFromUA: function (ua) {
             ua = ua || navigator.userAgent;
 
             // On OS X, Safari and Chrome have underscores instead of dots
@@ -46,7 +111,24 @@
             return match ? match[1].replace('_', '.') : undefined;
         },
 
-        getArchType: function (ua, pf) {
+        getArchTypeFromCH: function (arch) {
+            // ARM
+            if (/armv\d+/i.test(arch)) {
+                return arch.toLowerCase();
+            }
+
+            // ARMv8 64-bit
+            // TODO what should ARM values be when provided by CH? `ARM64`?
+            if (/aarch64/.test(arch)) {
+                return 'armv8';
+            }
+
+            // We can't detect the type info. It's probably x86 but unsure.
+            // For example, iOS may be running on ARM-based Apple A7 processor
+            return 'x86';
+        },
+
+        getArchTypeFromUA: function (ua, pf) {
             pf = (pf === '') ? '' : pf || navigator.platform;
             ua = ua || navigator.userAgent;
 
@@ -68,7 +150,7 @@
             return 'x86';
         },
 
-        getArchSize: function (ua, pf) {
+        getArchSizeFromUA: function (ua, pf) {
             pf = (pf === '') ? '' : pf || navigator.platform;
             ua = ua || navigator.userAgent;
 
@@ -82,9 +164,78 @@
             return 32;
         },
 
+        getArchSizeFromCH: function (arch) {
+            var re = /x64|x86_64|Win64|WOW64|aarch64/i;
+            if (re.test(arch)) {
+                return 64;
+            }
+
+            // We can't detect the bit info. It's probably 32 but unsure.
+            // For example, OS X may be running on 64-bit Core i7 processor
+            return 32;
+        },
+
         // Universal feature detect to deliver graded browser support (targets IE 11 and above).
         cutsTheMustard: function () {
             return 'classList' in document.createElement('div') && 'MutationObserver' in window;
+        },
+
+        setHTMLClassNames: function() {
+            var h = document.documentElement;
+            var version = window.site.platformVersion ? parseFloat(window.site.platformVersion) : 0;
+
+            // if other than 'windows', immediately replace the platform classname on the html-element
+            // to avoid lots of flickering
+            if (window.site.platform === 'windows') {
+                // Add class for Windows XP/Vista users to display
+                // unsupported messaging on /download/thanks/ page.
+                if (version >= 5.1 && version <= 6) {
+                    h.className += ' xpvista';
+                // Add class for Windows 10 users to display
+                // disclaimer messaging on /download/thanks/ page.
+                } else if (version >= 10.0 && version <= 11) {
+                    h.className += ' windows10';
+                }
+            } else {
+                h.className = h.className.replace('windows', window.site.platform);
+            }
+
+            // Used for Linux ARM processor detection.
+            if (window.site.archType !== 'x86') {
+                h.className = h.className.replace('x86', window.site.archType);
+
+                if (window.site.isARM) {
+                    h.className += ' arm';
+                }
+            }
+
+            // Used for 64bit download link on Linux.
+            if (window.site.archSize === 64) {
+                h.className += ' x64';
+            }
+
+            // Add class to reflect if user agent is Firefox. Cherry-picked from mozilla-client.js.
+            var isFirefox = /\s(Firefox|FxiOS)/.test(navigator.userAgent) && !/Iceweasel|IceCat|SeaMonkey|Camino|like Firefox/i.test(navigator.userAgent);
+
+            if (isFirefox) {
+                h.className += ' is-firefox';
+            }
+
+            // Add class to reflect browsers that get 1st class JS & CSS support.
+            var isModernBrowser = window.site.isModernBrowser = window.site.cutsTheMustard();
+
+            if (isModernBrowser) {
+                h.className += ' is-modern-browser';
+            }
+
+            // Add class to reflect javascript availability for CSS
+            h.className = h.className.replace(/\bno-js\b/, 'js');
+        },
+
+        clientHintData: {
+            architecture: 'x64',
+            platform: 'other',
+            platformVersion: undefined
         },
 
         platform: 'other',
@@ -94,62 +245,24 @@
         isARM: false
     };
     (function () {
-        var h = document.documentElement;
 
-        // if other than 'windows', immediately replace the platform classname on the html-element
-        // to avoid lots of flickering
-        var platform = window.site.platform = window.site.getPlatform();
-        var version = window.site.platformVersion = window.site.getPlatformVersion();
-        var _version = version ? parseFloat(version) : 0;
-
-        if (platform === 'windows') {
-            // Add class for Windows XP/Vista users to display
-            // unsupported messaging on /download/thanks/ page.
-            if (_version >= 5.1 && _version <= 6) {
-                h.className += ' xpvista';
-            // Add class for Windows 10 users to display
-            // disclaimer messaging on /download/thanks/ page.
-            } else if (_version >= 10.0 && _version <= 11) {
-                h.className += ' windows10';
-            }
+        if (window.site.supportsClientHints()) {
+            window.site.getClientHintsData().then(function() {
+                console.log(window.site.clientHintData); // TODO remove me
+                window.site.platform = window.site.getPlatformFromCH(window.site.clientHintData.platform);
+                window.site.platformVersion = window.site.getPlatformVersionFromCH(window.site.clientHintData.platform, window.site.clientHintData.platformVersion);
+                window.site.archType = window.site.getArchTypeFromCH(window.site.clientHintData.architecture, window.site.clientHintData.platform);
+                window.site.archSize = window.site.getArchSizeFromCH(window.site.clientHintData.architecture);
+                window.site.isARM = window.site.archType.match(/armv(\d+)/);
+                window.site.setHTMLClassNames();
+            });
         } else {
-            h.className = h.className.replace('windows', platform);
+            window.site.platform = window.site.getPlatformFromUA();
+            window.site.platformVersion = window.site.getPlatformVersionFromUA();
+            window.site.archType = window.site.getArchTypeFromUA();
+            window.site.archSize = window.site.getArchSizeFromUA();
+            window.site.isARM = window.site.archType.match(/armv(\d+)/);
+            window.site.setHTMLClassNames();
         }
-
-        // Add class to reflect the microprocessor architecture info
-        var archType = window.site.archType = window.site.getArchType();
-        var archSize = window.site.archSize = window.site.getArchSize();
-        var isARM = window.site.isARM = archType.match(/armv(\d+)/);
-
-        // Used for Linux ARM processor detection.
-        if (archType !== 'x86') {
-            h.className = h.className.replace('x86', archType);
-
-            if (isARM) {
-                h.className += ' arm';
-            }
-        }
-
-        // Used for 64bit download link on Linux.
-        if (archSize === 64) {
-            h.className += ' x64';
-        }
-
-        // Add class to reflect if user agent is Firefox. Cherry-picked from mozilla-client.js.
-        var isFirefox = /\s(Firefox|FxiOS)/.test(navigator.userAgent) && !/Iceweasel|IceCat|SeaMonkey|Camino|like Firefox/i.test(navigator.userAgent);
-
-        if (isFirefox) {
-            h.className += ' is-firefox';
-        }
-
-        // Add class to reflect browsers that get 1st class JS & CSS support.
-        var isModernBrowser = window.site.isModernBrowser = window.site.cutsTheMustard();
-
-        if (isModernBrowser) {
-            h.className += ' is-modern-browser';
-        }
-
-        // Add class to reflect javascript availability for CSS
-        h.className = h.className.replace(/\bno-js\b/, 'js');
     })();
 })();
--- tests/unit/spec/base/site.js
+++ tests/unit/spec/base/site.js
@@ -9,110 +9,110 @@ describe('site.js', function () {
 
     'use strict';
 
-    describe('getPlatform', function () {
+    describe('getPlatformFromUA', function () {
 
         it('should identify Windows', function () {
-            expect(window.site.getPlatform('Mozilla/5.0 (compatible; MSIE 8.0; Windows NT 5.1; Trident/4.0; .NET CLR 1.1.4322; .NET CLR 2.0.50727)', 'Win64')).toBe('windows');
-            expect(window.site.getPlatform('Mozilla/5.0 (compatible; MSIE 9.0; Windows NT 7.1; Trident/5.0)', 'Win32')).toBe('windows');
-            expect(window.site.getPlatform('Mozilla/5.0 (Windows NT 6.4; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/36.0.1985.143 Safari/537.36 Edge/12.0', 'Win32')).toBe('windows');
+            expect(window.site.getPlatformFromUA('Mozilla/5.0 (compatible; MSIE 8.0; Windows NT 5.1; Trident/4.0; .NET CLR 1.1.4322; .NET CLR 2.0.50727)', 'Win64')).toBe('windows');
+            expect(window.site.getPlatformFromUA('Mozilla/5.0 (compatible; MSIE 9.0; Windows NT 7.1; Trident/5.0)', 'Win32')).toBe('windows');
+            expect(window.site.getPlatformFromUA('Mozilla/5.0 (Windows NT 6.4; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/36.0.1985.143 Safari/537.36 Edge/12.0', 'Win32')).toBe('windows');
         });
 
         it('should identify Android', function () {
-            expect(window.site.getPlatform('Mozilla/5.0 (Android; Mobile; rv:26.0) Gecko/26.0 Firefox/26.0', 'foo')).toBe('android');
-            expect(window.site.getPlatform('Mozilla/5.0 (Android; Tablet; rv:26.0) Gecko/26.0 Firefox/26.0', 'foo')).toBe('android');
+            expect(window.site.getPlatformFromUA('Mozilla/5.0 (Android; Mobile; rv:26.0) Gecko/26.0 Firefox/26.0', 'foo')).toBe('android');
+            expect(window.site.getPlatformFromUA('Mozilla/5.0 (Android; Tablet; rv:26.0) Gecko/26.0 Firefox/26.0', 'foo')).toBe('android');
         });
 
         it('should identify Linux', function () {
-            expect(window.site.getPlatform('foo', 'Linux')).toBe('linux');
-            expect(window.site.getPlatform('Mozilla/5.0 (X11; Ubuntu; Linux armv7l; rv:32.0) Gecko/20100101 Firefox/32.0', 'Linux armv7l')).toBe('linux');
+            expect(window.site.getPlatformFromUA('foo', 'Linux')).toBe('linux');
+            expect(window.site.getPlatformFromUA('Mozilla/5.0 (X11; Ubuntu; Linux armv7l; rv:32.0) Gecko/20100101 Firefox/32.0', 'Linux armv7l')).toBe('linux');
         });
 
         it('should identify old-Mac', function () {
-            expect(window.site.getPlatform('Mozilla/5.0 (Macintosh; U; PPC Mac OS X 10_5_8; ja-jp) AppleWebKit/533.20.25 (KHTML, like Gecko) Version/5.0.4 Safari/533.20.27', 'foo')).toBe('other');
-            expect(window.site.getPlatform('Mozilla/5.0 (Macintosh; U; PPC Mac OS X 10_4_11; nl-nl) AppleWebKit/533.16 (KHTML, like Gecko) Version/4.1 Safari/533.16', 'foo')).toBe('other');
-            expect(window.site.getPlatform('Mozilla/4.0 (compatible; MSIE 5.23; Mac_PowerPC)', 'foo')).toBe('other');
-            expect(window.site.getPlatform('Mozilla/5.0 (Macintosh; Intel Mac OS X 10_7_3) AppleWebKit/534.55.3 (KHTML, like Gecko) Version/5.1.3 Safari/534.53.10', 'foo')).toBe('other');
-            expect(window.site.getPlatform('Mozilla/5.0 (Macintosh; Intel Mac OS X 10.8; rv:21.0) Gecko/20100101 Firefox/21.0', 'foo')).toBe('other');
-            expect(window.site.getPlatform('Mozilla/4.0 (compatible; MSIE 5.23; Mac_PowerPC)', 'foo')).toBe('other');
+            expect(window.site.getPlatformFromUA('Mozilla/5.0 (Macintosh; U; PPC Mac OS X 10_5_8; ja-jp) AppleWebKit/533.20.25 (KHTML, like Gecko) Version/5.0.4 Safari/533.20.27', 'foo')).toBe('other');
+            expect(window.site.getPlatformFromUA('Mozilla/5.0 (Macintosh; U; PPC Mac OS X 10_4_11; nl-nl) AppleWebKit/533.16 (KHTML, like Gecko) Version/4.1 Safari/533.16', 'foo')).toBe('other');
+            expect(window.site.getPlatformFromUA('Mozilla/4.0 (compatible; MSIE 5.23; Mac_PowerPC)', 'foo')).toBe('other');
+            expect(window.site.getPlatformFromUA('Mozilla/5.0 (Macintosh; Intel Mac OS X 10_7_3) AppleWebKit/534.55.3 (KHTML, like Gecko) Version/5.1.3 Safari/534.53.10', 'foo')).toBe('other');
+            expect(window.site.getPlatformFromUA('Mozilla/5.0 (Macintosh; Intel Mac OS X 10.8; rv:21.0) Gecko/20100101 Firefox/21.0', 'foo')).toBe('other');
+            expect(window.site.getPlatformFromUA('Mozilla/4.0 (compatible; MSIE 5.23; Mac_PowerPC)', 'foo')).toBe('other');
         });
 
         it('should identify iOS', function () {
-            expect(window.site.getPlatform('foo', 'iPhone')).toBe('ios');
-            expect(window.site.getPlatform('foo', 'iPad')).toBe('ios');
-            expect(window.site.getPlatform('foo', 'iPod')).toBe('ios');
-            expect(window.site.getPlatform('foo', 'iPhone Simulator')).toBe('ios');
+            expect(window.site.getPlatformFromUA('foo', 'iPhone')).toBe('ios');
+            expect(window.site.getPlatformFromUA('foo', 'iPad')).toBe('ios');
+            expect(window.site.getPlatformFromUA('foo', 'iPod')).toBe('ios');
+            expect(window.site.getPlatformFromUA('foo', 'iPhone Simulator')).toBe('ios');
         });
 
         it('should identify iPadOS', function () {
             window.navigator.standalone = sinon.stub();
-            expect(window.site.getPlatform('foo', 'MacIntel')).toBe('ios');
+            expect(window.site.getPlatformFromUA('foo', 'MacIntel')).toBe('ios');
         });
 
         it('should identify OSX', function () {
-            expect(window.site.getPlatform('Mozilla/5.0 (Macintosh; Intel Mac OS X 10.12; rv:53.0) Gecko/20100101 Firefox/53.0', 'foo')).toBe('osx');
-            expect(window.site.getPlatform('Mozilla/5.0 (Macintosh; Intel Mac OS X 10_10) AppleWebKit/538.10.3 (KHTML, like Gecko) Chrome/21.0.1180.89 Safari/537.1', 'foo')).toBe('osx');
+            expect(window.site.getPlatformFromUA('Mozilla/5.0 (Macintosh; Intel Mac OS X 10.12; rv:53.0) Gecko/20100101 Firefox/53.0', 'foo')).toBe('osx');
+            expect(window.site.getPlatformFromUA('Mozilla/5.0 (Macintosh; Intel Mac OS X 10_10) AppleWebKit/538.10.3 (KHTML, like Gecko) Chrome/21.0.1180.89 Safari/537.1', 'foo')).toBe('osx');
         });
     });
 
-    describe('getPlatformVersion', function () {
+    describe('getPlatformVersionFromUA', function () {
         it('should identify a Windows version', function () {
-            expect(window.site.getPlatformVersion('Mozilla/5.0 (compatible; MSIE 9.0; Windows NT 7.1; Trident/5.0)')).toBe('7.1');
-            expect(window.site.getPlatformVersion('Mozilla/5.0 (Windows NT 10.0; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/41.0.2249.0 Safari/537.36')).toBe('10.0');
+            expect(window.site.getPlatformVersionFromUA('Mozilla/5.0 (compatible; MSIE 9.0; Windows NT 7.1; Trident/5.0)')).toBe('7.1');
+            expect(window.site.getPlatformVersionFromUA('Mozilla/5.0 (Windows NT 10.0; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/41.0.2249.0 Safari/537.36')).toBe('10.0');
         });
 
         it('should identify an OS X version', function () {
-            expect(window.site.getPlatformVersion('Mozilla/5.0 (Macintosh; Intel Mac OS X 10.8; rv:30.0) Gecko/20100101 Firefox/30.0')).toBe('10.8');
-            expect(window.site.getPlatformVersion('Mozilla/5.0 (Macintosh; Intel Mac OS X 10_10) AppleWebKit/538.10.3 (KHTML, like Gecko) Chrome/21.0.1180.89 Safari/537.1')).toBe('10.10');
+            expect(window.site.getPlatformVersionFromUA('Mozilla/5.0 (Macintosh; Intel Mac OS X 10.8; rv:30.0) Gecko/20100101 Firefox/30.0')).toBe('10.8');
+            expect(window.site.getPlatformVersionFromUA('Mozilla/5.0 (Macintosh; Intel Mac OS X 10_10) AppleWebKit/538.10.3 (KHTML, like Gecko) Chrome/21.0.1180.89 Safari/537.1')).toBe('10.10');
         });
 
         it('should identify an Android version', function () {
-            expect(window.site.getPlatformVersion('Mozilla/5.0 (Linux; U; Android 2.3.3; en-us; HTC_DesireS_S510e Build/GRI40) AppleWebKit/533.1 (KHTML, like Gecko) Version/4.0 Mobile Safari/533.1')).toBe('2.3');
-            expect(window.site.getPlatformVersion('Mozilla/5.0 (Linux; U; Android 4.1.1; en-gb; Build/KLP) AppleWebKit/534.30 (KHTML, like Gecko) Version/4.0 Safari/534.30')).toBe('4.1');
+            expect(window.site.getPlatformVersionFromUA('Mozilla/5.0 (Linux; U; Android 2.3.3; en-us; HTC_DesireS_S510e Build/GRI40) AppleWebKit/533.1 (KHTML, like Gecko) Version/4.0 Mobile Safari/533.1')).toBe('2.3');
+            expect(window.site.getPlatformVersionFromUA('Mozilla/5.0 (Linux; U; Android 4.1.1; en-gb; Build/KLP) AppleWebKit/534.30 (KHTML, like Gecko) Version/4.0 Safari/534.30')).toBe('4.1');
         });
 
         it('should return undefined if a platform version cannot be found in the UA string', function () {
-            expect(window.site.getPlatformVersion('Mozilla/5.0 (Android; Mobile; rv:27.0) Gecko/27.0 Firefox/27.0', 'Linux armv7l')).toBe(undefined);
-            expect(window.site.getPlatformVersion('Mozilla/4.0 (compatible; MSIE 4.01; Windows 95)')).toBe(undefined);
-            expect(window.site.getPlatformVersion('Mozilla/5.0 (X11; Ubuntu; Linux armv7l; rv:32.0) Gecko/20100101 Firefox/32.0')).toBe(undefined);
+            expect(window.site.getPlatformVersionFromUA('Mozilla/5.0 (Android; Mobile; rv:27.0) Gecko/27.0 Firefox/27.0', 'Linux armv7l')).toBe(undefined);
+            expect(window.site.getPlatformVersionFromUA('Mozilla/4.0 (compatible; MSIE 4.01; Windows 95)')).toBe(undefined);
+            expect(window.site.getPlatformVersionFromUA('Mozilla/5.0 (X11; Ubuntu; Linux armv7l; rv:32.0) Gecko/20100101 Firefox/32.0')).toBe(undefined);
         });
     });
 
-    describe('getArchType', function () {
+    describe('getArchTypeFromUA', function () {
 
         it('should identify x86', function () {
-            expect(window.site.getArchType('Mozilla/5.0 (compatible; MSIE 9.0; Windows NT 6.1; Win64; x64; Trident/5.0', 'Win64')).toBe('x86');
-            expect(window.site.getArchType('Mozilla/5.0 (Windows NT 6.3; rv:27.0) Gecko/20100101 Firefox/27.0', 'Win32')).toBe('x86');
-            expect(window.site.getArchType('Mozilla/5.0 (Windows NT 6.3; Trident/7.0; .NET4.0E; .NET4.0C; rv:11.0) like Gecko', 'Win32')).toBe('x86');
-            expect(window.site.getArchType('Mozilla/5.0 (Macintosh; Intel Mac OS X 10.9; rv:27.0) Gecko/20100101 Firefox/27.0', 'MacIntel')).toBe('x86');
-            expect(window.site.getArchType('Mozilla/5.0 (X11; Ubuntu; Linux x86_64; rv:27.0) Gecko/20100101 Firefox/27.0', 'Linux x86_64')).toBe('x86');
-            expect(window.site.getArchType('Mozilla/5.0 (X11; Ubuntu; Linux i686; rv:27.0) Gecko/20100101 Firefox/27.0', 'Linux i686')).toBe('x86');
+            expect(window.site.getArchTypeFromUA('Mozilla/5.0 (compatible; MSIE 9.0; Windows NT 6.1; Win64; x64; Trident/5.0', 'Win64')).toBe('x86');
+            expect(window.site.getArchTypeFromUA('Mozilla/5.0 (Windows NT 6.3; rv:27.0) Gecko/20100101 Firefox/27.0', 'Win32')).toBe('x86');
+            expect(window.site.getArchTypeFromUA('Mozilla/5.0 (Windows NT 6.3; Trident/7.0; .NET4.0E; .NET4.0C; rv:11.0) like Gecko', 'Win32')).toBe('x86');
+            expect(window.site.getArchTypeFromUA('Mozilla/5.0 (Macintosh; Intel Mac OS X 10.9; rv:27.0) Gecko/20100101 Firefox/27.0', 'MacIntel')).toBe('x86');
+            expect(window.site.getArchTypeFromUA('Mozilla/5.0 (X11; Ubuntu; Linux x86_64; rv:27.0) Gecko/20100101 Firefox/27.0', 'Linux x86_64')).toBe('x86');
+            expect(window.site.getArchTypeFromUA('Mozilla/5.0 (X11; Ubuntu; Linux i686; rv:27.0) Gecko/20100101 Firefox/27.0', 'Linux i686')).toBe('x86');
         });
 
         it('should identify ARM', function () {
-            expect(window.site.getArchType('Mozilla/5.0 (Android; Mobile; rv:27.0) Gecko/27.0 Firefox/27.0', 'Linux armv7l')).toBe('armv7');
-            expect(window.site.getArchType('Mozilla/5.0 (Android 5.0.2; Mobile; rv:42.0) Gecko/42.0 Firefox/42.0', 'Linux aarch64')).toBe('armv8');
-            expect(window.site.getArchType('Mozilla/5.0 (X11; U; Linux armv6l; en-US; rv:1.9.1b2pre) Gecko/20081116 Fennec/1.0a2pre', 'Linux armv6l')).toBe('armv6');
-            expect(window.site.getArchType('Mozilla/5.0 (X11; Ubuntu; Linux armv7l; rv:32.0) Gecko/20100101 Firefox/32.0', 'Linux armv7l')).toBe('armv7');
+            expect(window.site.getArchTypeFromUA('Mozilla/5.0 (Android; Mobile; rv:27.0) Gecko/27.0 Firefox/27.0', 'Linux armv7l')).toBe('armv7');
+            expect(window.site.getArchTypeFromUA('Mozilla/5.0 (Android 5.0.2; Mobile; rv:42.0) Gecko/42.0 Firefox/42.0', 'Linux aarch64')).toBe('armv8');
+            expect(window.site.getArchTypeFromUA('Mozilla/5.0 (X11; U; Linux armv6l; en-US; rv:1.9.1b2pre) Gecko/20081116 Fennec/1.0a2pre', 'Linux armv6l')).toBe('armv6');
+            expect(window.site.getArchTypeFromUA('Mozilla/5.0 (X11; Ubuntu; Linux armv7l; rv:32.0) Gecko/20100101 Firefox/32.0', 'Linux armv7l')).toBe('armv7');
         });
     });
 
-    describe('getArchSize', function () {
+    describe('getArchSizeFromUA', function () {
 
         it('should identify 64', function () {
-            expect(window.site.getArchSize('Mozilla/5.0 (compatible; MSIE 9.0; Windows NT 6.1; Win64; x64; Trident/5.0', 'Win64')).toBe(64);
-            expect(window.site.getArchSize('Mozilla/5.0 (X11; Ubuntu; Linux x86_64; rv:27.0) Gecko/20100101 Firefox/27.0', 'Linux x86_64')).toBe(64);
-            expect(window.site.getArchSize('Mozilla/5.0 (Windows NT 6.3; WOW64; Trident/7.0; Touch; rv:11.0) like Gecko', 'Win64')).toBe(64);
-            expect(window.site.getArchSize('Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/41.0.2227.0 Safari/537.36', 'Win64')).toBe(64);
-            expect(window.site.getArchSize('Mozilla/5.0 (Android 5.0.2; Mobile; rv:42.0) Gecko/42.0 Firefox/42.0', 'Linux aarch64')).toBe(64);
+            expect(window.site.getArchSizeFromUA('Mozilla/5.0 (compatible; MSIE 9.0; Windows NT 6.1; Win64; x64; Trident/5.0', 'Win64')).toBe(64);
+            expect(window.site.getArchSizeFromUA('Mozilla/5.0 (X11; Ubuntu; Linux x86_64; rv:27.0) Gecko/20100101 Firefox/27.0', 'Linux x86_64')).toBe(64);
+            expect(window.site.getArchSizeFromUA('Mozilla/5.0 (Windows NT 6.3; WOW64; Trident/7.0; Touch; rv:11.0) like Gecko', 'Win64')).toBe(64);
+            expect(window.site.getArchSizeFromUA('Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/41.0.2227.0 Safari/537.36', 'Win64')).toBe(64);
+            expect(window.site.getArchSizeFromUA('Mozilla/5.0 (Android 5.0.2; Mobile; rv:42.0) Gecko/42.0 Firefox/42.0', 'Linux aarch64')).toBe(64);
         });
 
         it('should identify 32', function () {
-            expect(window.site.getArchSize('Mozilla/5.0 (Windows NT 6.3; rv:27.0) Gecko/20100101 Firefox/27.0', 'Win32')).toBe(32);
-            expect(window.site.getArchSize('Mozilla/5.0 (Windows NT 6.3; Trident/7.0; .NET4.0E; .NET4.0C; rv:11.0) like Gecko', 'Win32')).toBe(32);
-            expect(window.site.getArchSize('Mozilla/5.0 (Macintosh; Intel Mac OS X 10.9; rv:27.0) Gecko/20100101 Firefox/27.0', 'MacIntel')).toBe(32);
-            expect(window.site.getArchSize('Mozilla/5.0 (X11; Ubuntu; Linux i686; rv:27.0) Gecko/20100101 Firefox/27.0', 'Linux i686')).toBe(32);
-            expect(window.site.getArchSize('Mozilla/5.0 (X11; U; Linux armv6l; en-US; rv:1.9.1b2pre) Gecko/20081116 Fennec/1.0a2pre', 'Linux armv6l')).toBe(32);
-            expect(window.site.getArchSize('Mozilla/5.0 (X11; Ubuntu; Linux armv7l; rv:32.0) Gecko/20100101 Firefox/32.0', 'Linux armv7l')).toBe(32);
+            expect(window.site.getArchSizeFromUA('Mozilla/5.0 (Windows NT 6.3; rv:27.0) Gecko/20100101 Firefox/27.0', 'Win32')).toBe(32);
+            expect(window.site.getArchSizeFromUA('Mozilla/5.0 (Windows NT 6.3; Trident/7.0; .NET4.0E; .NET4.0C; rv:11.0) like Gecko', 'Win32')).toBe(32);
+            expect(window.site.getArchSizeFromUA('Mozilla/5.0 (Macintosh; Intel Mac OS X 10.9; rv:27.0) Gecko/20100101 Firefox/27.0', 'MacIntel')).toBe(32);
+            expect(window.site.getArchSizeFromUA('Mozilla/5.0 (X11; Ubuntu; Linux i686; rv:27.0) Gecko/20100101 Firefox/27.0', 'Linux i686')).toBe(32);
+            expect(window.site.getArchSizeFromUA('Mozilla/5.0 (X11; U; Linux armv6l; en-US; rv:1.9.1b2pre) Gecko/20081116 Fennec/1.0a2pre', 'Linux armv6l')).toBe(32);
+            expect(window.site.getArchSizeFromUA('Mozilla/5.0 (X11; Ubuntu; Linux armv7l; rv:32.0) Gecko/20100101 Firefox/32.0', 'Linux armv7l')).toBe(32);
         });
     });
 });
