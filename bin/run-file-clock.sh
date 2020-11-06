ad4993cc024cb26b3ab90cdd32c41f03995dcd91
Paul McLanahan <pmac@mozilla.com>
2020-11-06 13:51:46 -0500

    Run migrations when running in LOCAL_DB_UPDATE mode

--- bin/run-file-clock.sh
+++ bin/run-file-clock.sh
@@ -3,6 +3,7 @@
 bin/run-db-download.py --force
 LOCAL_DB_UPDATE=$(echo "${LOCAL_DB_UPDATE:-false}" | tr '[:upper:]' '[:lower:]')
 if [[ "$LOCAL_DB_UPDATE" == "true" ]]; then
+    python manage.py migrate --noinput
     exec python bin/cron.py file db
 else
     exec python bin/cron.py file
