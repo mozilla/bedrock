b5a1fe9831990cf0c65dc922702ba9f9bbd16c36
Alex Gibson <alexgibson@users.noreply.github.com>
2020-11-12 21:41:36 +0000

    Migrate /firefox/firstrun/ page to Fluent (Fixes #9193) [skip l10n] (#9678)

---
+++ lib/fluent_migrations/firefox/firstrun/firstrun.py
@@ -0,0 +1,66 @@
+from __future__ import absolute_import
+import fluent.syntax.ast as FTL
+from fluent.migrate.helpers import transforms_from
+from fluent.migrate.helpers import VARIABLE_REFERENCE, TERM_REFERENCE
+from fluent.migrate import REPLACE, COPY
+
+firstrun = "firefox/firstrun/firstrun.lang"
+quantum = "firefox/new/quantum.lang"
+
+def migrate(ctx):
+    """Migrate bedrock/firefox/templates/firefox/firstrun/firstrun.html, part {index}."""
+
+    ctx.add_transforms(
+        "firefox/firstrun.ftl",
+        "firefox/firstrun.ftl",
+        transforms_from("""
+firstrun-firefox-browser = { -brand-name-firefox-browser }
+""", quantum=quantum) + [
+            FTL.Message(
+                id=FTL.Identifier("firstrun-welcome-to-firefox"),
+                value=REPLACE(
+                    quantum,
+                    "Welcome to Firefox",
+                    {
+                        "Firefox": TERM_REFERENCE("brand-name-firefox"),
+                    }
+                )
+            ),
+            FTL.Message(
+                id=FTL.Identifier("firstrun-take-firefox-with-you"),
+                value=REPLACE(
+                    quantum,
+                    "Take Firefox with You",
+                    {
+                        "Firefox": TERM_REFERENCE("brand-name-firefox"),
+                    }
+                )
+            ),
+            FTL.Message(
+                id=FTL.Identifier("firstrun-already-using-firefox"),
+                value=REPLACE(
+                    quantum,
+                    "Already using Firefox?",
+                    {
+                        "Firefox": TERM_REFERENCE("brand-name-firefox"),
+                    }
+                )
+            ),
+        ] + transforms_from("""
+firstrun-get-your-bookmarks-history = {COPY(quantum, "Get your bookmarks, history, passwords and other settings on all your devices.",)}
+""", quantum=quantum) + [
+            FTL.Message(
+                id=FTL.Identifier("firstrun-sign-in-to-your-account"),
+                value=REPLACE(
+                    quantum,
+                    "Sign in to your account and we’ll sync the bookmarks, passwords and other great things you’ve saved to Firefox on other devices.",
+                    {
+                        "Firefox": TERM_REFERENCE("brand-name-firefox"),
+                    }
+                )
+            ),
+        ] + transforms_from("""
+firstrun-already-have-an-account = {COPY(quantum, "Already have an account?",)}
+firstrun-sign-in = {COPY(quantum, "Sign In",)}
+""", quantum=quantum)
+        )
