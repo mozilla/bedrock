9d6ddb7c074d251f1ce062f7142140993849d371
Paul McLanahan <pmac@mozilla.com>
2020-11-12 14:50:48 -0500

    Enable multiprocess mode for prometheus metrics gathering

--- bin/run-prod.sh
+++ bin/run-prod.sh
@@ -1,10 +1,15 @@
 #!/bin/bash -xe
 
+export prometheus_multiproc_dir=/tmp/prometheus_metrics
+# ensure the multiproc dir is empty
+rm -rf "$prometheus_multiproc_dir" && mkdir -p "$prometheus_multiproc_dir"
+
 function run-gunicorn () {
     if [[ -z "$NEW_RELIC_LICENSE_KEY" ]]; then
-        gunicorn "$@"
+        exec gunicorn "$@"
     else
-        NEW_RELIC_CONFIG_FILE=newrelic.ini newrelic-admin run-program gunicorn "$@"
+        export NEW_RELIC_CONFIG_FILE=newrelic.ini
+        exec newrelic-admin run-program gunicorn "$@"
     fi
 }
 
@@ -14,9 +19,4 @@ if [[ ! -f "data/bedrock.db" ]]; then
     exit 1
 fi
 
-run-gunicorn wsgi.app:application -b 0.0.0.0:${PORT:-8000} \
-                                  -w ${WEB_CONCURRENCY:-2} \
-                                  --error-logfile - \
-                                  --access-logfile - \
-                                  --log-level ${LOGLEVEL:-info} \
-                                  --worker-class ${GUNICORN_WORKER_CLASS:-meinheld.gmeinheld.MeinheldWorker}
+run-gunicorn wsgi.app:application --config wsgi/config.py
