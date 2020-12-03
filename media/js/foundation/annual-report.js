2363dbc572e8bb4a21f424e26d87541b694af362
Maxx Crawford <maxx.crawford@gmail.com>
2020-12-03 14:46:42 -0600

    Add prev/next functionality, add logic to show/hide buttons if at the beginning or end of the gallery

--- media/js/foundation/annual-report.js
+++ media/js/foundation/annual-report.js
@@ -20,16 +20,31 @@ if (typeof window.Mozilla === 'undefined') {
         '    </button>' +
         '</div>';
 
+    var modalPrevButtonFragment =
+        '<div class="c-modal-prev">' +
+        '    <button type="button" class="c-modal-button-prev" title="Previous">' +
+        '        Previous' +
+        '    </button>' +
+        '</div>';
+
     var articleArray = document.querySelectorAll('[data-modal-id]');
 
-    function showNextArticle(closeBtn, currentArticleId) {
+    function showArticle(dir, closeBtn, currentIndex) {
+
         closeBtn.firstElementChild.click();
 
-        var currentArticle = document.querySelector('[data-modal-id="' + currentArticleId + '"]');
-        var nextArticleId = articleArray[currentArticle.dataset.currentIndex++].dataset.modalId;
-        var nextArticle = document.querySelector('[data-modal-id="' + nextArticleId + '"]');
+        var newArticleIndex = currentIndex;
 
-        nextArticle.click();
+        if (dir === 'next') {
+            newArticleIndex++;
+        } else {
+            newArticleIndex--;
+        }
+
+        var newArticleId = newArticleId = articleArray[newArticleIndex].dataset.modalId;
+        var newArticle = document.querySelector('[data-modal-id="' + newArticleId + '"]');
+
+        newArticle.click();
     }
 
     for (var i = 0; i < modalContainers.length; i++) {
@@ -42,13 +57,14 @@ if (typeof window.Mozilla === 'undefined') {
             e.preventDefault();
 
             var modalId = this.dataset.modalId;
+            var currentIndex = parseInt(this.dataset.currentIndex, 10);
             var modalContent = document.querySelector('[data-modal-parent="' + modalId + '"]').cloneNode(true);
             window.location.hash = modalId;
 
             modalContent.removeAttribute('id');
             modalContent.setAttribute('aria-role', 'article');
 
-            Mzp.Modal.createModal(e.target, content, {
+            Mzp.Modal.createModal(this, content, {
                 allowScroll: false,
                 closeText: window.Mozilla.Utils.trans('global-close'),
                 onCreate: function() {
@@ -56,11 +72,25 @@ if (typeof window.Mozilla === 'undefined') {
 
                     var modalCloseButton = document.querySelector('.mzp-c-modal-close');
                     modalCloseButton.insertAdjacentHTML('beforebegin', modalNextButtonFragment);
+                    modalCloseButton.insertAdjacentHTML('beforebegin', modalPrevButtonFragment);
 
                     var modalNextButton = document.querySelector('.c-modal-next');
+                    var modalPrevButton = document.querySelector('.c-modal-prev');
+
+                    if (currentIndex < 1) {
+                        content.parentNode.classList.add('hide-prev');
+                    } else if (currentIndex === (articleArray.length - 1)) {
+                        content.parentNode.classList.add('hide-next');
+                    }
+                    else {
+                        content.parentNode.classList.remove('hide-prev', 'hide-next');
+                    }
 
                     modalNextButton.addEventListener('click', function(){
-                        showNextArticle(modalCloseButton, modalId);
+                        showArticle('next', modalCloseButton, currentIndex);
+                    });
+                    modalPrevButton.addEventListener('click', function(){
+                        showArticle('prev', modalCloseButton, currentIndex);
                     });
                 },
                 onDestroy: function() {
