d61811bb262d66d2e46d05c397314d8da77ef351
Maxx Crawford <maxx.crawford@gmail.com>
2021-01-12 15:09:14 -0600

    Add sticky promo (non-fx only) to all features/ subpages

--- tests/functional/firefox/features/test_features.py
+++ tests/functional/firefox/features/test_features.py
@@ -18,3 +18,12 @@ from pages.firefox.features.feature import FeaturePage
 def test_download_button_is_displayed(slug, base_url, selenium):
     page = FeaturePage(selenium, base_url, slug=slug).open()
     assert page.download_button.is_displayed
+
+
+@pytest.mark.skip_if_firefox(reason='Sticky promo is displayed only to non-Firefox users')
+@pytest.mark.nondestructive
+def test_sticky_promo(base_url, selenium):
+    page = FeaturePage(selenium, base_url).open()
+    assert page.promo.is_displayed
+    page.promo.close()
+    assert not page.promo.is_displayed
