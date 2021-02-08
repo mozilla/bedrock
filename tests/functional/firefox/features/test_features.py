7c9b5d9d3cc6875b0b7c3b53896d0bd3af93c86e
Maxx Crawford <maxx.crawford@gmail.com>
2021-02-08 16:52:25 -0600

    Revise test to wait until promo is completed loaded before continuing, add test to /firefox/features test

--- tests/functional/firefox/features/test_features.py
+++ tests/functional/firefox/features/test_features.py
@@ -26,6 +26,7 @@ def test_download_button_is_displayed(slug, base_url, selenium):
     ('private-browsing')])
 def test_sticky_promo(slug, base_url, selenium):
     page = FeaturePage(selenium, base_url, slug=slug).open()
+    page.init_promo()
     assert page.promo.is_displayed
     page.promo.close()
     assert not page.promo.is_displayed
--- tests/pages/firefox/new/sticky_promo.py
+++ tests/pages/firefox/new/sticky_promo.py
@@ -3,7 +3,6 @@
 # file, You can obtain one at http://mozilla.org/MPL/2.0/.
 
 from selenium.webdriver.common.by import By
-from selenium.webdriver.support import expected_conditions as expected
 
 from pages.base import BasePage
 from pages.regions.sticky_promo import StickyPromo
@@ -15,14 +14,14 @@ class StickyPromoPage(BasePage):
 
     # Used as a scroll target to move down the page, to trigger the sticky promo element
     _page_highlights_content_locator = (By.CSS_SELECTOR, '.t-highlights')
-    _sticky_promo_modal_content_locator = (By.CSS_SELECTOR, '.mzp-c-sticky-promo.mzp-a-slide-in')
+    _sticky_promo_modal_content_locator = (By.CSS_SELECTOR, '.mzp-c-sticky-promo')
 
     def init_promo(self):
         assert not self.is_promo_displayed, 'Promo detail is not displayed'
         # scroll down page to trigger promo to display
         self.scroll_element_into_view(*self._page_highlights_content_locator)
-        self.wait.until(expected.visibility_of_element_located(
-            self._sticky_promo_modal_content_locator))
+        promo = self.find_element(*self._sticky_promo_modal_content_locator)
+        self.wait.until(lambda s: 'is-displayed' in promo.get_attribute('class'))
 
     @property
     def is_promo_displayed(self):
