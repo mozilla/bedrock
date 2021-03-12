e34c67ca864bd86b14c0704370df5f89b511460f
Maxx Crawford <maxx.crawford@gmail.com>
2021-03-12 15:07:22 -0600

    Fix #1004 - Update /firefox/central redirect

--- bedrock/firefox/redirects.py
+++ bedrock/firefox/redirects.py
@@ -164,10 +164,8 @@ redirectpatterns = (
     redirect(r'^firefox/security/?$', 'firefox.features.independent'),
     redirect(r'^firefox/technology/?$', 'https://developer.mozilla.org/docs/Tools'),
 
-    # Bug 979527
-    redirect(r'^(products/)?firefox/central(/|\.html|-lite\.html)?$', is_firefox_redirector(
-        'https://support.mozilla.org/kb/get-started-firefox-overview-main-features',
-        'firefox.new'), cache_timeout=0),
+    # Previously Bug 979527 / Github #10004 "Getting Started" Page
+    redirect(r'^(products/)?firefox/central(/|\.html|-lite\.html)?$', 'firefox'),
 
     # bug 868169
     redirect(r'^mobile/android-download\.html$',
