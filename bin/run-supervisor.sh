6717f52ee2f3441e3a120c9c1dbec5995a9012d6
Paul McLanahan <pmac@mozilla.com>
2021-03-18 17:49:05 -0400

    Ensure the app will start while run from supervisor

--- bin/run-supervisor.sh
+++ bin/run-supervisor.sh
@@ -1,3 +1,13 @@
 #!/bin/bash -xe
 
+# ensure the app will start while run from supervisor
+# mostly for demos
+STARTUP_FILES=(
+    "data/last-run-update_locales"
+    "data/last-run-download_database"
+)
+for fname in "${STARTUP_FILES[@]}"; do
+    touch "$fname"
+done
+
 exec supervisord -c etc/supervisord.conf
