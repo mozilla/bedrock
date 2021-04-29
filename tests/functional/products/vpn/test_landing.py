6c22b7b4070c3dfbb3ecd4f19d98b6e4f38501f3
Alex Gibson <alexgibson@users.noreply.github.com>
2021-04-29 13:13:36 +0100

    Functional tests for variable pricing on VPN landing page (Fixes #10171)

--- tests/functional/products/vpn/test_landing.py
+++ tests/functional/products/vpn/test_landing.py
@@ -8,30 +8,95 @@ from pages.products.vpn.landing import VPNLandingPage
 
 
 @pytest.mark.nondestructive
-def test_vpn_available_in_country(base_url, selenium):
-    page = VPNLandingPage(selenium, base_url, params='?geo=us').open()
+@pytest.mark.parametrize('country', [('us'), ('ca'), ('my'), ('nz'), ('sg'), ('gb')])
+def test_vpn_fixed_price_available_in_country(country, base_url, selenium):
+    page = VPNLandingPage(selenium, base_url, params='?geo={0}'.format(country)).open()
+    # Hero
     assert page.is_get_vpn_hero_fixed_price_button_displayed
     assert not page.is_join_waitlist_hero_button_displayed
+    assert not page.is_get_vpn_hero_variable_price_anchor_displayed
+
+    # Navigation
     assert page.is_get_vpn_navigation_fixed_price_button_displayed
     assert not page.is_join_waitlist_navigation_button_displayed
+    assert not page.is_get_vpn_navigation_variable_price_anchor_displayed
+
+    # Connect section
     assert page.is_get_vpn_conntect_now_fixed_price_button_displayed
     assert not page.is_join_waitlist_coming_soon_button_displayed
-    assert page.is_get_vpn_fixed_pricing_section_button_displayed
-    assert not page.is_join_waitlist_fixed_pricing_section_button_displayed
+    assert not page.is_get_vpn_conntect_now_variable_price_anchor_displayed
+
+    # Pricing section
+    assert page.is_get_vpn_fixed_price_section_button_displayed
+    assert not page.is_join_waitlist_fixed_price_section_button_displayed
+    assert not page.is_get_vpn_vpn_variable_price_monthly_button_displayed
+    assert not page.is_get_vpn_vpn_variable_price_6_months_button_displayed
+    assert not page.is_get_vpn_vpn_variable_price_12_months_button_displayed
+
+    # Footer
     assert page.is_get_vpn_footer_fixed_price_button_displayed
     assert not page.is_join_waitlist_footer_button_displayed
+    assert not page.is_get_vpn_footer_variable_price_anchor_displayed
+
+
+@pytest.mark.nondestructive
+@pytest.mark.parametrize('country', [('de'), ('fr')])
+def test_vpn_variable_price_available_in_country(country, base_url, selenium):
+    page = VPNLandingPage(selenium, base_url, params='?geo={0}'.format(country)).open()
+    # Hero
+    assert not page.is_get_vpn_hero_fixed_price_button_displayed
+    assert not page.is_join_waitlist_hero_button_displayed
+    assert page.is_get_vpn_hero_variable_price_anchor_displayed
+
+    # Navigation
+    assert not page.is_get_vpn_navigation_fixed_price_button_displayed
+    assert not page.is_join_waitlist_navigation_button_displayed
+    assert page.is_get_vpn_navigation_variable_price_anchor_displayed
+
+    # Connect section
+    assert not page.is_get_vpn_conntect_now_fixed_price_button_displayed
+    assert not page.is_join_waitlist_coming_soon_button_displayed
+    assert page.is_get_vpn_conntect_now_variable_price_anchor_displayed
+
+    # Pricing section
+    assert not page.is_get_vpn_fixed_price_section_button_displayed
+    assert not page.is_join_waitlist_fixed_price_section_button_displayed
+    assert page.is_get_vpn_vpn_variable_price_monthly_button_displayed
+    assert page.is_get_vpn_vpn_variable_price_6_months_button_displayed
+    assert page.is_get_vpn_vpn_variable_price_12_months_button_displayed
+
+    # Footer
+    assert not page.is_get_vpn_footer_fixed_price_button_displayed
+    assert not page.is_join_waitlist_footer_button_displayed
+    assert page.is_get_vpn_footer_variable_price_anchor_displayed
 
 
 @pytest.mark.nondestructive
 def test_vpn_not_available_in_country(base_url, selenium):
     page = VPNLandingPage(selenium, base_url, params='?geo=cn').open()
+    # Hero
     assert not page.is_get_vpn_hero_fixed_price_button_displayed
     assert page.is_join_waitlist_hero_button_displayed
+    assert not page.is_get_vpn_hero_variable_price_anchor_displayed
+
+    # Navigation
     assert not page.is_get_vpn_navigation_fixed_price_button_displayed
     assert page.is_join_waitlist_navigation_button_displayed
+    assert not page.is_get_vpn_navigation_variable_price_anchor_displayed
+
+    # Connect section
     assert not page.is_get_vpn_conntect_now_fixed_price_button_displayed
     assert page.is_join_waitlist_coming_soon_button_displayed
-    assert not page.is_get_vpn_fixed_pricing_section_button_displayed
-    assert page.is_join_waitlist_fixed_pricing_section_button_displayed
+    assert not page.is_get_vpn_conntect_now_variable_price_anchor_displayed
+
+    # Pricing section
+    assert not page.is_get_vpn_fixed_price_section_button_displayed
+    assert page.is_join_waitlist_fixed_price_section_button_displayed
+    assert not page.is_get_vpn_vpn_variable_price_monthly_button_displayed
+    assert not page.is_get_vpn_vpn_variable_price_6_months_button_displayed
+    assert not page.is_get_vpn_vpn_variable_price_12_months_button_displayed
+
+    # Footer
     assert not page.is_get_vpn_footer_fixed_price_button_displayed
     assert page.is_join_waitlist_footer_button_displayed
+    assert not page.is_get_vpn_footer_variable_price_anchor_displayed
--- tests/pages/products/vpn/landing.py
+++ tests/pages/products/vpn/landing.py
@@ -11,59 +11,106 @@ class VPNLandingPage(BasePage):
 
     _URL_TEMPLATE = '/{locale}/products/vpn/{params}'
 
+    # Hero
     _get_vpn_hero_fixed_price_button_locator = (By.CSS_SELECTOR, '.vpn-hero .js-vpn-fixed-pricing .mzp-c-button')
+    _get_vpn_hero_variable_price_anchor_locator = (By.CSS_SELECTOR, '.vpn-hero .js-vpn-variable-pricing .mzp-c-button')
     _join_waitlist_hero_button_locator = (By.CSS_SELECTOR, '.vpn-hero .js-vpn-waitlist .mzp-c-button')
 
+    # Navigation
     _get_vpn_navigation_fixed_price_button_locator = (By.CSS_SELECTOR,
                                                       '.c-navigation-shoulder .mzp-c-button[data-cta-text="Get Mozilla VPN monthly"]')
+    _get_vpn_navigation_variable_price_button_locator = (By.CSS_SELECTOR,
+                                                         '.c-navigation-shoulder .mzp-c-button[data-cta-text="Scroll to pricing"]')
     _join_waitlist_navigation_button_locator = (By.CSS_SELECTOR,
                                                 '.c-navigation-shoulder .mzp-c-button[data-cta-text="Join the VPN Waitlist"]')
 
+    # Connect section
     _get_vpn_conntect_now_fixed_price_button_locator = (By.CSS_SELECTOR,
                                                         '.js-connect-to-countries-and-servers .js-vpn-fixed-pricing .mzp-c-button')
+    _get_vpn_conntect_now_variable_price_anchor_locator = (By.CSS_SELECTOR,
+                                                           '.js-connect-to-countries-and-servers .js-vpn-variable-pricing .mzp-c-button')
     _join_waitlist_coming_soon_button_locator = (By.CSS_SELECTOR, '.js-more-countries-coming-soon .js-vpn-waitlist .mzp-c-button')
 
-    _get_vpn_fixed_pricing_section_button_locator = (By.CSS_SELECTOR, '.vpn-fixed-pricing-block .js-vpn-fixed-pricing .mzp-c-button')
-    _join_waitlist_fixed_pricing_section_button_locator = (By.CSS_SELECTOR, '.vpn-fixed-pricing-block .js-vpn-waitlist .mzp-c-button')
+    # Pricing section
+    _get_vpn_fixed_price_section_button_locator = (By.CSS_SELECTOR, '.vpn-fixed-pricing-block .js-vpn-fixed-pricing .mzp-c-button')
+    _get_vpn_variable_price_monthly_button_locator = (By.CSS_SELECTOR, '.vpn-pricing-monthly .mzp-c-button')
+    _get_vpn_variable_price_6_months_button_locator = (By.CSS_SELECTOR, '.vpn-pricing-6-months .mzp-c-button')
+    _get_vpn_variable_price_12_months_button_locator = (By.CSS_SELECTOR, '.vpn-pricing-12-months .mzp-c-button')
+    _join_waitlist_fixed_price_section_button_locator = (By.CSS_SELECTOR, '.vpn-fixed-pricing-block .js-vpn-waitlist .mzp-c-button')
 
+    # Footer
     _get_vpn_footer_fixed_price_button_locator = (By.CSS_SELECTOR, '.vpn-footer .js-vpn-fixed-pricing .mzp-c-button')
+    _get_vpn_footer_variable_price_anchor_locator = (By.CSS_SELECTOR, '.vpn-footer .js-vpn-variable-pricing .mzp-c-button')
     _join_waitlist_footer_button_locator = (By.CSS_SELECTOR, '.vpn-footer .js-vpn-waitlist .mzp-c-button')
 
+    # Hero
     @property
     def is_get_vpn_hero_fixed_price_button_displayed(self):
         return self.is_element_displayed(*self._get_vpn_hero_fixed_price_button_locator)
 
     @property
+    def is_get_vpn_hero_variable_price_anchor_displayed(self):
+        return self.is_element_displayed(*self._get_vpn_hero_variable_price_anchor_locator)
+
+    @property
     def is_join_waitlist_hero_button_displayed(self):
         return self.is_element_displayed(*self._join_waitlist_hero_button_locator)
 
+    # Navigation
     @property
     def is_get_vpn_navigation_fixed_price_button_displayed(self):
         return self.is_element_displayed(*self._get_vpn_navigation_fixed_price_button_locator)
 
     @property
+    def is_get_vpn_navigation_variable_price_anchor_displayed(self):
+        return self.is_element_displayed(*self._get_vpn_navigation_variable_price_button_locator)
+
+    @property
     def is_join_waitlist_navigation_button_displayed(self):
         return self.is_element_displayed(*self._join_waitlist_navigation_button_locator)
 
+    # Connect section
     @property
     def is_get_vpn_conntect_now_fixed_price_button_displayed(self):
         return self.is_element_displayed(*self._get_vpn_conntect_now_fixed_price_button_locator)
 
     @property
+    def is_get_vpn_conntect_now_variable_price_anchor_displayed(self):
+        return self.is_element_displayed(*self._get_vpn_conntect_now_variable_price_anchor_locator)
+
+    @property
     def is_join_waitlist_coming_soon_button_displayed(self):
         return self.is_element_displayed(*self._join_waitlist_coming_soon_button_locator)
 
+    # Pricing section
     @property
-    def is_get_vpn_fixed_pricing_section_button_displayed(self):
-        return self.is_element_displayed(*self._get_vpn_fixed_pricing_section_button_locator)
+    def is_get_vpn_fixed_price_section_button_displayed(self):
+        return self.is_element_displayed(*self._get_vpn_fixed_price_section_button_locator)
 
     @property
-    def is_join_waitlist_fixed_pricing_section_button_displayed(self):
-        return self.is_element_displayed(*self._join_waitlist_fixed_pricing_section_button_locator)
+    def is_get_vpn_vpn_variable_price_monthly_button_displayed(self):
+        return self.is_element_displayed(*self._get_vpn_variable_price_monthly_button_locator)
 
+    @property
+    def is_get_vpn_vpn_variable_price_6_months_button_displayed(self):
+        return self.is_element_displayed(*self._get_vpn_variable_price_6_months_button_locator)
+
+    @property
+    def is_get_vpn_vpn_variable_price_12_months_button_displayed(self):
+        return self.is_element_displayed(*self._get_vpn_variable_price_12_months_button_locator)
+
+    @property
+    def is_join_waitlist_fixed_price_section_button_displayed(self):
+        return self.is_element_displayed(*self._join_waitlist_fixed_price_section_button_locator)
+
+    # Footer
     @property
     def is_get_vpn_footer_fixed_price_button_displayed(self):
         return self.is_element_displayed(*self._get_vpn_footer_fixed_price_button_locator)
+
+    @property
+    def is_get_vpn_footer_variable_price_anchor_displayed(self):
+        return self.is_element_displayed(*self._get_vpn_footer_variable_price_anchor_locator)
 
     @property
     def is_join_waitlist_footer_button_displayed(self):
