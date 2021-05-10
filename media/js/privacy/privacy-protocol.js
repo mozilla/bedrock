7975070c4470384b20842e2fb9c022639f300318
Alex Gibson <alexgibson@users.noreply.github.com>
2021-05-10 10:19:33 +0100

    Enable target-linking on privacy notice pages (Fixes #10186)

--- media/js/privacy/privacy-protocol.js
+++ media/js/privacy/privacy-protocol.js
@@ -13,20 +13,52 @@
     window.Mzp.Details.init('.format-headings .privacy-body section section > h3');
     window.Mzp.Details.init('.format-paragraphs .summary');
 
-    buttons = document.querySelectorAll('.is-summary button');
-    for (var i = 0; i < buttons.length; i++) {
-        buttons[i].setAttribute('data-open', openText);
-        buttons[i].setAttribute('data-close', closeText);
-    }
-    
-    var cookieButton = document.querySelector('#user-choices button');
-    var cookieText = document.getElementById('expand-formatparagraphssummary-2');
-    
-    if (window.location.hash === '#cookies') {
-        if (cookieButton && cookieText) {
-            cookieButton.setAttribute('aria-expanded', 'true');
-            cookieText.classList.remove('is-closed');
-            cookieText.setAttribute('aria-hidden', 'false');
+    // Add "Learn more" / "Show less" text.
+    if (openText && closeText) {
+        buttons = document.querySelectorAll('.is-summary button');
+
+        for (var i = 0; i < buttons.length; i++) {
+            buttons[i].setAttribute('data-open', openText);
+            buttons[i].setAttribute('data-close', closeText);
         }
     }
+
+    function openPrivacyItem(id) {
+        var item = document.getElementById(id);
+
+        // Pages such as /privacy/firefox/ use 'is-details', whilst other pages like /privacy/websites/ use 'is-summary'.
+        // This is likely due to how the privacy notice markdown file is structured.
+        if (item && (item.classList.contains('is-details') || item.classList.contains('is-summary'))) {
+            var button = item.querySelector('button');
+
+            // Only expand the section if it is hidden.
+            if (button && button.getAttribute('aria-expanded') !== true) {
+                button.click();
+            }
+        }
+    }
+
+    function getHash() {
+        var hash = window.location.hash;
+        if (hash.indexOf('#') > -1) {
+            hash = hash.split('#')[1];
+        }
+
+        return hash;
+    }
+
+    function handleHashChange() {
+        var hash = getHash();
+
+        if (hash) {
+            openPrivacyItem(hash);
+        }
+    }
+
+    // Open relevant Privacy section is URL contains a matching hash.
+    if (window.location.hash) {
+        handleHashChange();
+    }
+
+    window.addEventListener('hashchange', handleHashChange, false);
 })();
