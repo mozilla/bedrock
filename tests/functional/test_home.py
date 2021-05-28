14ad6652eb52e70d5845630f38d4f369eca03b71
Craig Cook <craigcook@users.noreply.github.com>
2021-05-28 13:34:56 -0700

    Fix/update home page tests [#10167] (#10238)

--- tests/functional/test_home.py
+++ tests/functional/test_home.py
@@ -11,11 +11,22 @@ from pages.home import HomePage
 @pytest.mark.sanity
 @pytest.mark.smoke
 @pytest.mark.nondestructive
-@pytest.mark.parametrize('locale', ['en-US', 'de', 'fr', 'es-ES'])
+@pytest.mark.parametrize('locale', ['en-US', 'de', 'fr'])
 def test_download_button_is_displayed(locale, base_url, selenium):
     page = HomePage(selenium, base_url, locale=locale).open()
     assert page.is_primary_download_button_displayed
     assert page.is_secondary_download_button_displayed
+
+
+@pytest.mark.skip_if_not_firefox(reason='Firefox Accounts CTA is displayed only to Firefox users')
+@pytest.mark.nondestructive
+@pytest.mark.parametrize('locale', ['de', 'fr'])
+def test_accounts_button_is_displayed_rest_tier_1(locale, base_url, selenium):
+    page = HomePage(selenium, base_url, locale=locale).open()
+    assert page.is_primary_accounts_button_displayed
+    assert page.is_secondary_accounts_button_displayed
+    assert not page.is_primary_download_button_displayed
+    assert not page.is_secondary_download_button_displayed
 
 
 @pytest.mark.skip_if_not_firefox(reason='Alternative CTA is displayed only to Firefox users')
@@ -26,3 +37,10 @@ def test_accounts_button_is_displayed(base_url, selenium):
     assert page.is_secondary_accounts_button_displayed
     assert not page.is_primary_download_button_displayed
     assert not page.is_secondary_download_button_displayed
+
+
+@pytest.mark.skip_if_firefox(reason='Download button is displayed only to non-Firefox users')
+@pytest.mark.nondestructive
+def test_legacy_download_button_is_displayed(base_url, selenium):
+    page = HomePage(selenium, base_url, locale='it').open()
+    assert page.is_primary_download_button_displayed
