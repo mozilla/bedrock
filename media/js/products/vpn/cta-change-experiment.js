be4497fca85b762d632743f3a83400ed9491b273
Reem H <me@reemify.dev>
2021-09-01 16:00:15 -0400

    created JS file for traffic cop experiment

---
+++ media/js/products/vpn/cta-change-experiment.js
@@ -0,0 +1,41 @@
+/* This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
+
+(function (Mozilla) {
+    'use strict';
+
+    var href = window.location.href;
+
+    var initTrafficCop = function () {
+        if (href.indexOf('cta_experimentation=vpn-landing-page-cta-change&entrypoint_variation=') !== -1) {
+            if (href.indexOf('entrypoint_variation=a') !== -1) {
+                window.dataLayer.push({
+                    'data-ex-variant': 'a',
+                    'data-ex-name': 'vpn-landing-page-cta-change'
+                });
+            } else if (href.indexOf('entrypoint_variation=b') !== -1) {
+                window.dataLayer.push({
+                    'data-ex-variant': 'b',
+                    'data-ex-name': 'vpn-landing-page-cta-change'
+                });
+            }
+        } else {
+            var cop = new Mozilla.TrafficCop({
+                id: 'vpn-landing-page-cta-change-experiment',
+                variations: {
+                    'cta_experimentation=vpn-landing-page-cta-change&entrypoint_variation=a': 50,
+                    'cta_experimentation=vpn-landing-page-cta-change&entrypoint_variation=b': 50,
+                }
+            });
+
+            cop.init();
+        }
+    };
+
+    // Avoid entering automated tests into random experiments.
+    if (href.indexOf('automation=true') === -1) {
+        initTrafficCop();
+    }
+
+})(window.Mozilla);
