d1e9bf524de1c139c539493108a09e0f04be4738
Reem H <me@reemify.dev>
2021-10-05 12:59:38 -0400

    fixed test percentage in js file

--- media/js/products/vpn/image-change-experiment.js
+++ media/js/products/vpn/image-change-experiment.js
@@ -24,8 +24,8 @@
             var cop = new Mozilla.TrafficCop({
                 id: 'vpn-landing-page-image-change-experiment',
                 variations: {
-                    'entrypoint_experiment=vpn-landing-page-image-change&entrypoint_variation=current': 50,
-                    'entrypoint_experiment=vpn-landing-page-image-change&entrypoint_variation=blur': 50
+                    'entrypoint_experiment=vpn-landing-page-image-change&entrypoint_variation=current': 10,
+                    'entrypoint_experiment=vpn-landing-page-image-change&entrypoint_variation=blur': 10
                 }
             });
 
