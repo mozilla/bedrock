0a1c7de67950e778ffc2ced7e7ac8fb98c3de6cf
Nathan Barrett <30009669+nathan-barrett@users.noreply.github.com>
2021-10-07 12:29:59 -0700

    Add voting widget to articles (#10504)

--- media/js/products/vpn/resource-center.js
+++ media/js/products/vpn/resource-center.js
@@ -8,4 +8,29 @@
     // Lazyload images
     Mozilla.LazyLoad.init();
 
+    //event handlers for voting widget on article pages
+    var hasVoted = false;
+    var upvoteBtn = document.querySelector(".vpn-c-vote-btn.up");
+    var downvoteBtn = document.querySelector(".vpn-c-vote-btn.down");
+
+    function handleVote(e) {
+        var el = e.target;
+        var isUpvote = el.classList.contains('up')
+        if (el.classList.contains('active')) {
+            return null;
+        } else {
+            el.classList.add("active");
+            if (isUpvote) {
+                downvoteBtn.classList.remove("active")
+            } else {
+                upvoteBtn.classList.remove("active")
+            }
+        }
+    }
+
+    [upvoteBtn, downvoteBtn].forEach(function(element) {
+        element.addEventListener("click", handleVote)
+    });
+
+
 })();
