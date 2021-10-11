200651a9e018573c3bad246b867c12db94c3379d
Alex Gibson <alexgibson@users.noreply.github.com>
2021-10-11 16:27:03 +0100

    Add helper to determine whether to run experiments (Fixes #10559)

---
+++ tests/unit/spec/base/experiment-utils.js
@@ -0,0 +1,38 @@
+/* For reference read the Jasmine and Sinon docs
+ * Jasmine docs: http://pivotal.github.io/jasmine/
+ * Sinon docs: http://sinonjs.org/docs/
+ */
+
+describe('experiment-utils.es6.js', function () {
+    'use strict';
+
+    describe('isApprovedToRun', function () {
+        it('should return true if experimental params are not found in the page URL', function () {
+            const isApprovedToRun =
+                require('../../../../media/js/base/experiment-utils.es6.js').isApprovedToRun;
+            expect(
+                isApprovedToRun('?utm_source=test&utm_campaign=test')
+            ).toBeTruthy();
+
+            expect(isApprovedToRun('')).toBeTruthy();
+        });
+
+        it('should return false if experimental params are found in the page URL', function () {
+            const isApprovedToRun =
+                require('../../../../media/js/base/experiment-utils.es6.js').isApprovedToRun;
+            expect(isApprovedToRun('?utm_medium=cpc')).toBeFalsy();
+
+            expect(isApprovedToRun('?utm_source=firefox-browser')).toBeFalsy();
+
+            expect(
+                isApprovedToRun(
+                    '?entrypoint_experiment=test&entrypoint_variation=a'
+                )
+            ).toBeFalsy();
+
+            expect(isApprovedToRun('?experiment=test&variation=a')).toBeFalsy();
+
+            expect(isApprovedToRun('?automation=true')).toBeFalsy();
+        });
+    });
+});
