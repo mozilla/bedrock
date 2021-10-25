010ce027ae3fbe8f7ba49f24a2d8510875842106
Steve Jalim <stevejalim@mozilla.com>
2021-10-25 17:15:07 +0100

    10631: Reinstate support for python-coverage
    This changeset makes Bedrock tests use coverage.py once more, but this time via the pytest-cov plugin
    
    The configuration set up ignores a (seemingly) sensible set of files and folders, while also outputting HTML and XML reports, for human and [future] machine enjoyment.
    
    Key changes:
    * Config expanded for coverage.py
    * Main test-running script updated to invoke pytest with the appropriate coverage-logging options
    * Docs updated with a small note.

--- bin/run-tests.sh
+++ bin/run-tests.sh
@@ -13,5 +13,9 @@ python manage.py lint_ftl -q
 python manage.py runscript check_calendars
 python manage.py version
 python manage.py migrate --noinput
-py.test lib bedrock
+py.test lib bedrock \
+    --cov-config=.coveragerc \
+    --cov-report=html \
+    --cov-report=xml:python_coverage/coverage.xml \
+    --cov=. 
 py.test -r a tests/redirects
