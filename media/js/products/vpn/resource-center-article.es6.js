fcc38497042bbe28bf7413d7312afb82fec018f9
Nathan Barrett <30009669+nathan-barrett@users.noreply.github.com>
2021-11-03 08:18:49 -0700

    Add GA to voting widgit

--- media/js/products/vpn/resource-center-article.es6.js
+++ media/js/products/vpn/resource-center-article.es6.js
@@ -1,34 +1,46 @@
-/* This Source Code Form is subject to the terms of the Mozilla Public
+/*
+ * This Source Code Form is subject to the terms of the Mozilla Public
  * License, v. 2.0. If a copy of the MPL was not distributed with this
- * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
+ * file, You can obtain one at https://mozilla.org/MPL/2.0/.
+ */
 
- (function() {
-    'use strict';
-
+(function () {
     // Lazyload images
     Mozilla.LazyLoad.init();
 
     //event handlers for voting widget on article pages
     let hasVoted = false;
-    const upvoteBtn = document.querySelector(".vpn-c-vote-btn.up");
-    const downvoteBtn = document.querySelector(".vpn-c-vote-btn.down");
+    const upvoteBtn = document.querySelector('.vpn-c-vote-btn.up');
+    const downvoteBtn = document.querySelector('.vpn-c-vote-btn.down');
 
-    function handleVote({target}) {
+    function trackVoteInteraction(title, vote) {
+        window.dataLayer.push({
+            event: 'vpn-article-vote',
+            label: title,
+            value: vote
+        });
+    }
+
+    function handleVote({ target }) {
         const isUpvote = target.classList.contains('up');
         if (target.classList.contains('active') && hasVoted) {
             return null;
         } else {
             hasVoted = true;
-            target.classList.add("active");
+            target.classList.add('active');
+            trackVoteInteraction(
+                target.getAttribute('data-ga-article-title'),
+                isUpvote ? 'helpful ' : 'not helpful'
+            );
             if (isUpvote) {
-                downvoteBtn.classList.remove("active")
+                downvoteBtn.classList.remove('active');
             } else {
-                upvoteBtn.classList.remove("active")
+                upvoteBtn.classList.remove('active');
             }
         }
     }
 
-    [upvoteBtn, downvoteBtn].forEach((element) => element.addEventListener("click", handleVote));
-
-
+    [upvoteBtn, downvoteBtn].forEach((element) =>
+        element.addEventListener('click', handleVote)
+    );
 })();
