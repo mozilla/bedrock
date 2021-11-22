deceb5fbf0eeaecb9d9fccc5cd3547b1cb4d0d20
Steve Jalim <stevejalim@mozilla.com>
2021-11-22 13:47:57 +0000

    10778: Add support for VRC pages to sitemap
    This changeset adds support for showing the VPN-specific resource center pages authored in Contentful in the sitemap, plus the index/landing page for the VPN Resource Center iff there are any articles available in the DB
    
    TBC how one sees the results of the sitemap - that hasn't been manually checked yet

---
+++ bedrock/sitemaps/tests/test_utils.py
@@ -0,0 +1,90 @@
+# This Source Code Form is subject to the terms of the Mozilla Public
+# License, v. 2.0. If a copy of the MPL was not distributed with this
+# file, You can obtain one at https://mozilla.org/MPL/2.0/.
+
+from unittest.mock import patch
+
+import pytest
+
+from bedrock.contentful.constants import (
+    CONTENT_CLASSIFICATION_VPN,
+    CONTENT_TYPE_PAGE_RESOURCE_CENTER,
+)
+from bedrock.contentful.models import ContentfulEntry
+from bedrock.sitemaps.utils import _get_vrc_urls, get_contentful_urls, update_sitemaps
+
+pytestmark = pytest.mark.django_db
+
+
+@pytest.fixture
+def dummy_vrc_pages():
+    # No content, just the bare data we need
+    for idx in range(5):
+        ContentfulEntry.objects.create(
+            contentful_id=f"DUMMY-{idx}",
+            slug=f"test-slug-{idx}",
+            # TODO: support different locales
+            locale="en-US",
+            content_type=CONTENT_TYPE_PAGE_RESOURCE_CENTER,
+            classification=CONTENT_CLASSIFICATION_VPN,
+            data={},
+            data_hash="dummy",
+        )
+
+
+def test__get_vrc_urls(dummy_vrc_pages):
+    # TODO: support different locales
+    output = _get_vrc_urls()
+    assert output == {
+        "/products/vpn/resource-center/test-slug-0": ["en-US"],
+        "/products/vpn/resource-center/test-slug-1": ["en-US"],
+        "/products/vpn/resource-center/test-slug-2": ["en-US"],
+        "/products/vpn/resource-center/test-slug-3": ["en-US"],
+        "/products/vpn/resource-center/test-slug-4": ["en-US"],
+        "/products/vpn/resource-center/": ["en-US"],
+    }
+
+
+def test__get_vrc_urls__no_content():
+    # If there are no VRC pages in the DB, don't even show the listing page
+    output = _get_vrc_urls()
+    assert output == {}
+
+
+@patch("bedrock.sitemaps.utils._get_vrc_urls")
+def test_get_contentful_urls(mock__get_vrc_urls):
+    mock__get_vrc_urls.return_value = {"vrc-urls": "dummy-here"}
+
+    output = get_contentful_urls()
+    assert output == {"vrc-urls": "dummy-here"}
+    mock__get_vrc_urls.assert_called_once_with()
+
+
+@patch("bedrock.sitemaps.utils.get_static_urls")
+@patch("bedrock.sitemaps.utils.get_release_notes_urls")
+@patch("bedrock.sitemaps.utils.get_security_urls")
+@patch("bedrock.sitemaps.utils.get_contentful_urls")
+@patch("bedrock.sitemaps.utils.output_json")
+def test_update_sitemaps(
+    mock_output_json,
+    mock_get_contentful_urls,
+    mock_get_security_urls,
+    mock_get_release_notes_urls,
+    mock_get_static_urls,
+):
+    "Light check to ensure we've not added _new_ things we haven't added tests for"
+
+    mock_get_contentful_urls.return_value = {"contentful": "dummy"}
+    mock_get_security_urls.return_value = {"security": "dummy"}
+    mock_get_release_notes_urls.return_value = {"release_notes": "dummy"}
+    mock_get_static_urls.return_value = {"static_urls": "dummy"}
+
+    update_sitemaps()
+    expected = {
+        "contentful": "dummy",
+        "security": "dummy",
+        "release_notes": "dummy",
+        "static_urls": "dummy",
+    }
+
+    mock_output_json.assert_called_once_with(expected)
--- bedrock/sitemaps/utils.py
+++ bedrock/sitemaps/utils.py
@@ -3,6 +3,7 @@
 # file, You can obtain one at https://mozilla.org/MPL/2.0/.
 import json
 import re
+from collections import defaultdict
 
 from django.conf import settings
 from django.http import HttpResponse
@@ -12,6 +13,12 @@ from django.urls import resolvers
 
 from mock import patch
 
+from bedrock.contentful.constants import (
+    CONTENT_CLASSIFICATION_VPN,
+    CONTENT_TYPE_PAGE_RESOURCE_CENTER,
+    VRC_ROOT_PATH,
+)
+from bedrock.contentful.models import ContentfulEntry
 from bedrock.releasenotes.models import ProductRelease
 from bedrock.security.models import SecurityAdvisory
 
@@ -150,10 +157,35 @@ def get_static_urls():
     return urls
 
 
+def _get_vrc_urls():
+
+    urls = defaultdict(list)
+
+    for entry in ContentfulEntry.objects.filter(
+        content_type=CONTENT_TYPE_PAGE_RESOURCE_CENTER,
+        classification=CONTENT_CLASSIFICATION_VPN,
+    ):
+        _path = f"{VRC_ROOT_PATH}{entry.slug}"
+        urls[_path].append(entry.locale)  # In the future one, slug may support multiple locales
+
+    if urls:
+        # We only show the VRC listing page if there are VRC articles in the DB
+        urls[VRC_ROOT_PATH] = ["en-US"]  # TODO: support more locales
+
+    return urls
+
+
+def get_contentful_urls():
+    urls = {}
+    urls.update(_get_vrc_urls())
+    return urls
+
+
 def update_sitemaps():
     urls = get_static_urls()
     urls.update(get_release_notes_urls())
     urls.update(get_security_urls())
+    urls.update(get_contentful_urls())
     # Output static files
     output_json(urls)
 
