c0e669f6a8d86706558404871fe8eae54d1c5da6
Reem H <42309026+reemhamz@users.noreply.github.com>
2021-11-23 15:39:51 -0500

    Pocket platform pages (#10790)
    * updated url
    
    * adding jinja variables and content and updating static bundles
    
    * adding images and trying to render them to page
    
    * figured out how to render image variables
    
    * trying to get stylesheet to work
    
    * removed the important command in css
    
    * added the rest of the platform pages and contents
    
    * fixed quotations
    
    * trying to fix circleCI error
    
    * updated install links for each platform
    
    * simplified split macro
    
    * fixed bundle issue
    
    * removed useless div and updated background colors to reflect pocket colors

--- bedrock/externalpages/urls.py
+++ bedrock/externalpages/urls.py
@@ -1,5 +1,9 @@
 # This Source Code Form is subject to the terms of the Mozilla Public
 # License, v. 2.0. If a copy of the MPL was not distributed with this
+# file, You can obtain one at https://mozilla.org/MPL/2.0/.
+
+# This Source Code Form is subject to the terms of the Mozilla Public
+# License, v. 2.0. If a copy of the MPL was not distributed with this
 # file, You can obtain one at http://mozilla.org/MPL/2.0/.
 
 from bedrock.mozorg.util import page
@@ -8,5 +12,12 @@ urlpatterns = (
     page("pocket", "externalpages/pocket/index.html"),
     page("pocket/about", "externalpages/pocket/about.html"),
     page("pocket/add", "externalpages/pocket/add.html"),
+    page("pocket/platform-pages", "externalpages/pocket/platform-pages.html"),
     page("pocket/android", "externalpages/pocket/android.html"),
+    page("pocket/ios", "externalpages/pocket/ios.html"),
+    page("pocket/chrome", "externalpages/pocket/chrome.html"),
+    page("pocket/safari", "externalpages/pocket/safari.html"),
+    page("pocket/opera", "externalpages/pocket/opera.html"),
+    page("pocket/edge", "externalpages/pocket/edge.html"),
+    page("pocket/safari", "externalpages/pocket/safari.html"),
 )
