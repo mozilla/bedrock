08aeec81638007cdb40da93bc65935824ddc9f89
Reem H <42309026+reemhamz@users.noreply.github.com>
2021-12-06 12:09:07 -0500

    Pocket Welcome page (#10875)
    * added welcome page
    
    * added active and focus pseudo classes for platform nav

--- bedrock/externalpages/urls.py
+++ bedrock/externalpages/urls.py
@@ -13,4 +13,5 @@ urlpatterns = (
     page("pocket/safari", "externalpages/pocket/safari.html"),
     page("pocket/opera", "externalpages/pocket/opera.html"),
     page("pocket/edge", "externalpages/pocket/edge.html"),
+    page("pocket/welcome", "externalpages/pocket/welcome.html"),
 )
