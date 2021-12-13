c6ef8f0f0cb885bf6a72fc3d9c4015052fd69d56
Alex Gibson <alexgibson@users.noreply.github.com>
2021-12-13 16:04:45 +0000

    Stop showing hard-coded English vpn/more pages to locales (Fixes #11017) (#11018)

--- bedrock/products/urls.py
+++ bedrock/products/urls.py
@@ -27,9 +27,9 @@ urlpatterns = (
         "vpn/more/when-to-use-a-vpn", "products/vpn/more/when-to-use.html", ftl_files=["products/vpn/more/when-to-use-a-vpn", "products/vpn/shared"]
     ),
     # VPN pages for Product team (issue #10388)
-    page("vpn/more/why-mozilla-vpn", "products/vpn/more/why-mozilla-vpn.html", ftl_files=["products/vpn/shared"]),
-    page("vpn/more/do-i-need-a-vpn", "products/vpn/more/do-i-need.html", ftl_files=["products/vpn/shared"]),
-    page("vpn/more/what-is-a-vpn-v2", "products/vpn/more/what-is-a-vpn-v2.html", ftl_files=["products/vpn/shared"]),
+    page("vpn/more/why-mozilla-vpn", "products/vpn/more/why-mozilla-vpn.html", ftl_files=["products/vpn/shared"], active_locales=["en-US"]),
+    page("vpn/more/do-i-need-a-vpn", "products/vpn/more/do-i-need.html", ftl_files=["products/vpn/shared"], active_locales=["en-US"]),
+    page("vpn/more/what-is-a-vpn-v2", "products/vpn/more/what-is-a-vpn-v2.html", ftl_files=["products/vpn/shared"], active_locales=["en-US"]),
     # VPN Resource Center
     path(
         "vpn/resource-center/",
