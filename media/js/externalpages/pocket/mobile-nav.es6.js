afa380a6a454402573730e440f56815e35b257d0
maureenlholland <maureen@silverorange.com>
2022-01-20 15:58:15 +0000

    Add tests to pocket mobile navigation (fixes #11029) (#11110)

--- media/js/externalpages/pocket/mobile-nav.es6.js
+++ media/js/externalpages/pocket/mobile-nav.es6.js
@@ -4,11 +4,11 @@
  * file, You can obtain one at https://mozilla.org/MPL/2.0/.
  */
 
-const navOpenBtn = document.querySelector('.global-nav-mobile-menu-btn');
-const mobileNavWrapper = document.querySelector('.mobile-nav-wrapper');
-const mobileNav = document.querySelector('.mobile-nav');
-const navCloseBtn = document.querySelector('.mobile-nav-close-btn');
-const contentWrapper = document.querySelector('body');
+let navOpenBtn;
+let mobileNavWrapper;
+let mobileNav;
+let navCloseBtn;
+let contentWrapper;
 
 const TAB = 9;
 const ESC = 27;
@@ -19,34 +19,34 @@ function detectClickOutside(event) {
     }
 }
 
+function toggleContentWrapperClass(e) {
+    if (e.target === mobileNavWrapper) {
+        contentWrapper.classList.toggle('mobile-nav-open');
+    }
+}
+
 function handleMenuOpen() {
     mobileNavWrapper.classList.add('active');
-    mobileNav.setAttribute('aria-expanded', true);
-    contentWrapper.classList.add('mobile-nav-open');
+    mobileNavWrapper.style.opacity = 1;
+    mobileNav.classList.add('active');
+    navOpenBtn.setAttribute('aria-expanded', true);
+
     document.addEventListener('click', detectClickOutside);
     window.addEventListener('keydown', handleKeyDown);
 
-    // move focus to close button when modal is opened, need to use setTimeout to get the animation working correctly
-    setTimeout(function () {
-        mobileNavWrapper.style.opacity = 1;
-        mobileNav.classList.add('active');
-        navCloseBtn.focus();
-    }, 10);
+    navCloseBtn.focus();
 }
 
 function handleMenuClose() {
     mobileNav.classList.remove('active');
-    mobileNav.setAttribute('aria-expanded', false);
+    mobileNavWrapper.classList.remove('active');
     mobileNavWrapper.style.opacity = 0;
+    navOpenBtn.setAttribute('aria-expanded', false);
+
     document.removeEventListener('click', detectClickOutside);
     window.removeEventListener('keydown', handleKeyDown);
 
-    // move focus to close button when modal is closed need to use setTimeout to get the animation working correctly
-    setTimeout(function () {
-        mobileNavWrapper.classList.remove('active');
-        contentWrapper.classList.remove('mobile-nav-open');
-        navCloseBtn.focus();
-    }, 250);
+    navOpenBtn.focus();
 }
 
 function handleKeyDown(e) {
@@ -82,7 +82,6 @@ function handleKeyDown(e) {
         }
     }
 
-    // function handleForwardTab(){}
     switch (e.keyCode) {
         case TAB:
             if (e.shiftKey) {
@@ -99,5 +98,23 @@ function handleKeyDown(e) {
     }
 }
 
-navOpenBtn.addEventListener('click', handleMenuOpen, false);
-navCloseBtn.addEventListener('click', handleMenuClose, false);
+const init = function () {
+    // set element references
+    navOpenBtn = document.querySelector('.global-nav-mobile-menu-btn');
+    mobileNavWrapper = document.querySelector('.mobile-nav-wrapper');
+    mobileNav = document.querySelector('.mobile-nav');
+    navCloseBtn = document.querySelector('.mobile-nav-close-btn');
+    contentWrapper = document.querySelector('body');
+
+    // add event listeners
+    navOpenBtn.addEventListener('click', handleMenuOpen, false);
+    navCloseBtn.addEventListener('click', handleMenuClose, false);
+    // this event is used both for styling and as a test condition
+    mobileNavWrapper.addEventListener(
+        'transitionend',
+        toggleContentWrapperClass,
+        { capture: true }
+    );
+};
+
+export default init;
