2d79fec36d4aaa4e2d12b2dd5d43083e78ab94a8
Steve Jalim <stevejalim@mozilla.com>
2022-02-07 10:17:28 +0000

    Hard-pin latest working combo of pip + pip-tools in compile-requirements.sh

--- bin/compile-requirements.sh
+++ bin/compile-requirements.sh
@@ -11,8 +11,8 @@ export CUSTOM_COMPILE_COMMAND="make compile-requirements"
 # We need this installed, but we don't want it to live in the main requirements
 # We will need to periodically review this pinning
 
-pip install -U "pip<22"  # Until pip-tools gains compat with pip 22, we need to stay on 21
-pip install pip-tools==6.4.0
+pip install -U pip==22.0.3
+pip install pip-tools==6.5.0
 pip install pip-compile-multi==2.4.3
 
 pip-compile-multi \
