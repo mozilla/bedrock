75c3c4285120a0c5f8709970402641717077e4f4
Reem H <42309026+reemhamz@users.noreply.github.com>
2022-02-24 09:32:52 -0500

    fixed up url paths

--- bedrock/careers/urls.py
+++ bedrock/careers/urls.py
@@ -6,17 +6,23 @@ from django.conf import settings
 from django.urls import path, re_path
 
 from bedrock.mozorg.util import page
+from bedrock.redirects.util import redirect
 
 from . import views
 from .feeds import LatestPositionsFeed
 
 urlpatterns = [
+    # Diversity and inclusion redirect
+    redirect(r"^diversity/$", "careers.diversity.2021.index", name="careers.diversity", locale_prefix=False),
+    # Main paths
     path("", views.HomeView.as_view(), name="careers.home"),
     re_path(r"^position/(?P<source>[\w]+)/(?P<job_id>[\w]+)/$", views.PositionDetailView.as_view(), name="careers.position"),
     path("feed/", LatestPositionsFeed(), name="careers.feed"),
     path("listings/", views.PositionListView.as_view(), name="careers.listings"),
     path("internships/", views.InternshipsView.as_view(), name="careers.internships"),
     path("benefits/", views.BenefitsView.as_view(), name="careers.benefits"),
+    # Diversity and inclusion pages
+    page("diversity/2021", "careers/diversity/2021/index.html"),
 ]
 
 
--- bedrock/urls.py
+++ bedrock/urls.py
@@ -10,9 +10,6 @@ from watchman import views as watchman_views
 
 from bedrock.base import views as base_views
 
-from bedrock.mozorg.util import page
-from bedrock.redirects.util import redirect
-
 # The default django 404 and 500 handler doesn't run the ContextProcessors,
 # which breaks the base template page. So we replace them with views that do!
 handler500 = "bedrock.base.views.server_error_view"
@@ -20,8 +17,6 @@ handler404 = "bedrock.base.views.page_not_found_view"
 
 
 urlpatterns = (
-    # Diversity and inclusion redirect
-    redirect(r"^diversity/$", "careers.diversity.2021.index", name="careers.diversity", locale_prefix=False),
     # Main pages
     path("foundation/", include("bedrock.foundation.urls")),
     path("about/legal/", include("bedrock.legal.urls")),
@@ -39,8 +34,6 @@ urlpatterns = (
     path("healthz/", watchman_views.ping, name="watchman.ping"),
     path("readiness/", watchman_views.status, name="watchman.status"),
     path("healthz-cron/", base_views.cron_health_check),
-    # Diversity and inclusion pages
-    page("careers/diversity/2021", "diversity/2021/index.html")
 )
 
 if settings.DEBUG:
