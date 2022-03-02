dbb5e64cabce14deddf27aad0aa94b387f8d1a4f
maureenlholland <maureen@silverorange.com>
2022-03-02 10:28:43 +0000

    More concise syntax for multi newsletter context boolean
    Co-authored-by: Rob Hudson <robhudson@mozilla.com>

--- bedrock/newsletter/templatetags/helpers.py
+++ bedrock/newsletter/templatetags/helpers.py
@@ -70,7 +70,7 @@ def email_newsletter_form(
             success=success,
             email_label=email_label,
             email_placeholder=email_placeholder,
-            is_multi_newsletter_form=True if "," in newsletters else False,
+            is_multi_newsletter_form="," in newsletters,
         )
     )
 
