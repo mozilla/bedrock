5dac1bcbf487b0be2db8c80db59e20276ea2e827
removedporn <86824510+removedporn@users.noreply.github.com>
2022-04-01 23:28:24 +0800

    Delete bin directory

--- bin/run-db-clock.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash -xe
-
-# This Source Code Form is subject to the terms of the Mozilla Public
-# License, v. 2.0. If a copy of the MPL was not distributed with this
-# file, You can obtain one at https://mozilla.org/MPL/2.0/.
-
-python bin/run-db-download.py --force
-python manage.py migrate --noinput
-exec python bin/cron.py db
--- bin/run-for-integration-tests.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash -xe
-
-# This Source Code Form is subject to the terms of the Mozilla Public
-# License, v. 2.0. If a copy of the MPL was not distributed with this
-# file, You can obtain one at https://mozilla.org/MPL/2.0/.
-
-python manage.py migrate --noinput
-python manage.py update_product_details_files
-exec bin/run-prod.sh
