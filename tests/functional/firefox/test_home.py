a8be5a574d40d2f5b8a6f001119559492ddeac27
Nathan Barrett <30009669+nathan-barrett@users.noreply.github.com>
2022-04-04 15:17:26 -0700

    add first selenium test for dataLayer events

--- tests/functional/firefox/test_home.py
+++ tests/functional/firefox/test_home.py
@@ -3,6 +3,7 @@
 # file, You can obtain one at https://mozilla.org/MPL/2.0/.
 
 import pytest
+from selenium.webdriver.support.ui import WebDriverWait
 
 from pages.firefox.home import FirefoxHomePage
 
@@ -22,3 +23,15 @@ def test_sticky_promo(base_url, selenium):
     assert page.promo.is_displayed
     page.promo.close()
     assert not page.promo.is_displayed
+
+
+@pytest.mark.nondestructive
+def test_desktop_download_datalayer(base_url, selenium):
+    page = FirefoxHomePage(selenium, base_url).open()
+    data_link_type = page.browser_menu_list.desktop_download.get_attribute("data-link-type")
+    assert data_link_type == "Desktop"
+    page.browser_menu_list.desktop_download_click()
+    data_layer = WebDriverWait(selenium, 20).until(lambda selenium: selenium.execute_script("return window.dataLayer"))
+    # currently this event is not added to the dataLayer object
+    print(data_layer)
+    assert True
--- tests/pages/regions/menu_list.py
+++ tests/pages/regions/menu_list.py
@@ -12,6 +12,11 @@ class MenuList(BaseRegion):
     _title_button_locator = (By.CSS_SELECTOR, ".mzp-c-menu-list-title > button")
     _list_locator = (By.CSS_SELECTOR, ".mzp-c-menu-list-list")
 
+    # Elements on page to make are added to window.dataLayer after click
+    _browser_menu_desktop_download_link_locator = (By.CSS_SELECTOR, ".mzp-c-menu-list-item a[data-link-type=Desktop]")
+    _browser_menu_android_download_link_locator = (By.CSS_SELECTOR, ".mzp-c-menu-list-item a[data-link-type=Desktop]")
+    _browser_menu_ios_download_link_locator = (By.CSS_SELECTOR, ".mzp-c-menu-list-item a[data-link-type=Desktop]")
+
     # list is visible
 
     @property
@@ -20,3 +25,19 @@ class MenuList(BaseRegion):
 
     def click(self):
         self.scroll_element_into_view(*self._title_button_locator).click()
+
+    @property
+    def desktop_download(self):
+        return self.find_element(*self._browser_menu_desktop_download_link_locator)
+
+    def desktop_download_click(self):
+        self.scroll_element_into_view(*self._title_button_locator).click()
+        self.find_element(*self._browser_menu_desktop_download_link_locator).click()
+
+    @property
+    def android_download(self):
+        return self.find_element(*self._browser_menu_android_download_link_locator)
+
+    @property
+    def ios_download(self):
+        return self.find_element(*self._browser_menu_ios_download_link_locator)
