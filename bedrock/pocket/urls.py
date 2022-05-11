3ae169e305c2d31875c84f9d802e1ca73df36281
maureenlholland <maureen.holland11@gmail.com>
2022-05-11 18:36:37 +0100

    Add ftl file for pocket contact-info (fixes #11598) (#11612)

--- bedrock/pocket/urls.py
+++ bedrock/pocket/urls.py
@@ -58,11 +58,7 @@ urlpatterns = (
         url_name="pocket.welcome",
         ftl_files=["pocket/platforms"],
     ),
-    page(
-        "contact-info/",
-        "pocket/contact-info.html",
-        url_name="pocket.contact-info",
-    ),
+    page("contact-info/", "pocket/contact-info.html", url_name="pocket.contact-info", ftl_files=["pocket/contact-info"]),
     page(
         "firefox/new_tab_learn_more/",
         "pocket/firefox/new-tab-learn-more.html",
