471daabc222aeaa8b6b32486d595be8c8b4caa4a
Reem H <42309026+reemhamz@users.noreply.github.com>
2022-05-16 15:03:09 +1000

    converted pocket-and-firefox page to fluent

--- bedrock/pocket/urls.py
+++ bedrock/pocket/urls.py
@@ -74,11 +74,13 @@ urlpatterns = (
         "pocket-and-firefox/",
         "pocket/pocket-and-firefox.html",
         url_name="pocket.pocket-and-firefox",
+        ftl_files=["pocket/feature-pages", "pocket/banners/pocket-premium"],
     ),
     page(
         "get-inspired/",
         "pocket/get-inspired.html",
         url_name="pocket.get-inspired",
+        ftl_files=["pocket/feature-pages", "pocket/banners/pocket-premium"],
     ),
     page(
         "jobs/",
@@ -99,6 +101,6 @@ urlpatterns = (
         "save-to-pocket/",
         "pocket/save-to-pocket.html",
         url_name="pocket.save-to-pocket",
-        ftl_files=["pocket/save-to-pocket", "pocket/banners/pocket-premium"],
+        ftl_files=["pocket/feature-pages", "pocket/banners/pocket-premium"],
     ),
 )
