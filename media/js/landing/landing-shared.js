6c90198f71df07ecf98e6ede90f0ed7e82fef321
Nathan Barrett <30009669+nathan-barrett@users.noreply.github.com>
2022-05-23 14:03:27 -0700

    small design tweaks and add tracking to wordmark for testing

---
+++ media/js/landing/landing-shared.js
@@ -0,0 +1,21 @@
+/*
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at https://mozilla.org/MPL/2.0/.
+ */
+
+(function () {
+    'use strict';
+
+    // test to see if users are clicking on the workmark in the header of the SEM landing pages
+    function handleWorkmarkClick(event) {
+        var label = event.target.getAttribute('data-cta-text');
+        window.dataLayer.push({
+            event: 'sem-wordmark-click',
+            label: label + ' SEM landing page'
+        });
+    }
+
+    var wordmark = document.querySelector('.sem-landing-nav-icon');
+    wordmark.addEventListener('click', handleWorkmarkClick);
+})();
