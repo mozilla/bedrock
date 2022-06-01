d97c63b69381ceec93b48a01d3de7981d32f6202
Rob Hudson <robhudson@mozilla.com>
2022-06-01 16:32:39 -0700

    Fix view tests after locale changes

--- bedrock/firefox/tests/test_views.py
+++ bedrock/firefox/tests/test_views.py
@@ -699,13 +699,13 @@ class TestFirefoxGA(TestCase):
                 assert False, f"{link} does not contain attr cta-type or link-type"
 
     def test_firefox_home_GA(self):
-        req = RequestFactory().get("/firefox/")
+        req = RequestFactory().get("/en-US/firefox/")
         view = views.FirefoxHomeView.as_view()
         response = view(req)
         self.assert_ga_attr(response)
 
     def test_firefox_new_GA(self):
-        req = RequestFactory().get("/firefox/new")
+        req = RequestFactory().get("/en-US/firefox/new/")
         view = views.NewView.as_view()
         response = view(req)
         self.assert_ga_attr(response)
