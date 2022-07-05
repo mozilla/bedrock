e5e4315eca2cecd0af4f7b616b52177369404e7e
Alex Gibson <alexgibson@users.noreply.github.com>
2022-07-05 16:06:12 +0100

    Remove obsolete FxA detection code in main nav (Fixes #11854)

--- media/js/base/protocol/init-navigation.js
+++ media/js/base/protocol/init-navigation.js
@@ -35,30 +35,12 @@
         }
 
         var nav = document.querySelector('.c-navigation');
-        var fxaButton = document.querySelector(
-            '.c-navigation .c-navigation-fxa-cta'
-        );
 
         // Nav should be present on page.
-        if (!nav) {
-            return false;
+        if (nav) {
+            // Add a CSS hook for animating the nav button (issue #9009)
+            nav.classList.add('nav-button-is-ready');
         }
-
-        // Check that FxA button exists on the page, and visitor is using Firefox Desktop.
-        if (fxaButton && Mozilla.Client.isFirefoxDesktop) {
-            var fxaButtonAltHref = fxaButton.getAttribute('data-alt-href');
-
-            // Update the button if user is signed in
-            Mozilla.Client.getFxaDetails(function (details) {
-                if (details.setup) {
-                    nav.classList.add('fxa-signed-in');
-                    fxaButton.href = fxaButtonAltHref;
-                }
-            });
-        }
-
-        // Add a CSS hook for animating the nav button (issue #9009)
-        nav.classList.add('nav-button-is-ready');
     }
 
     initNavButton();
