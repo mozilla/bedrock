fac5deec5f92c715b67a26d58dbe1f33c3980442
Alex Gibson <alexgibson@users.noreply.github.com>
2022-07-11 16:08:36 +0100

    WNP 103 for EU (Fixes #11905)

--- bedrock/firefox/tests/test_base.py
+++ bedrock/firefox/tests/test_base.py
@@ -634,6 +634,52 @@ class TestWhatsNew(TestCase):
 
     # end 102.0 whatsnew tests
 
+    # begin 103.0 whatsnew tests
+
+    @override_settings(DEV=False)
+    def test_fx_103_0_0_gb_en_us(self, render_mock):
+        """Should use whatsnew-fx103-gb template in GB with en-US locale"""
+        req = self.rf.get("/firefox/whatsnew/", HTTP_CF_IPCOUNTRY="GB")
+        req.locale = "en-US"
+        self.view(req, version="103.0")
+        template = render_mock.call_args[0][1]
+        assert template == ["firefox/whatsnew/whatsnew-fx103-gb.html"]
+
+    @override_settings(DEV=False)
+    def test_fx_103_0_0_gb_en_gb(self, render_mock):
+        """Should use whatsnew-fx103-gb template in GB with en-GB locale"""
+        req = self.rf.get("/firefox/whatsnew/", HTTP_CF_IPCOUNTRY="GB")
+        req.locale = "en-GB"
+        self.view(req, version="103.0")
+        template = render_mock.call_args[0][1]
+        assert template == ["firefox/whatsnew/whatsnew-fx103-gb.html"]
+
+    def test_fx_103_0_0_de(self, render_mock):
+        """Should use whatsnew-fx103-de template for de locale"""
+        req = self.rf.get("/firefox/whatsnew/")
+        req.locale = "de"
+        self.view(req, version="103.0")
+        template = render_mock.call_args[0][1]
+        assert template == ["firefox/whatsnew/whatsnew-fx103-de.html"]
+
+    def test_fx_103_0_0_fr(self, render_mock):
+        """Should use whatsnew-fx103-fr template for fr locale"""
+        req = self.rf.get("/firefox/whatsnew/")
+        req.locale = "fr"
+        self.view(req, version="103.0")
+        template = render_mock.call_args[0][1]
+        assert template == ["firefox/whatsnew/whatsnew-fx103-fr.html"]
+
+    def test_fx_103_0_0_other_locales(self, render_mock):
+        """Should use default whatsnew template for other locales"""
+        req = self.rf.get("/firefox/whatsnew/")
+        req.locale = "en-US"
+        self.view(req, version="103.0")
+        template = render_mock.call_args[0][1]
+        assert template == ["firefox/whatsnew/index.html"]
+
+    # end 103.0 whatsnew tests
+
 
 @patch("bedrock.firefox.views.l10n_utils.render", return_value=HttpResponse())
 class TestFirstRun(TestCase):
