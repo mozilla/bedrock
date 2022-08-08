399cb7ee23dfc5f926cb9141b9cfaad8eaa03aa7
maureenlholland <maureen.holland11@gmail.com>
2022-08-08 15:03:20 -0300

    Add download button functional test

---
+++ tests/functional/firefox/campaign/families/test_families.py
@@ -0,0 +1,14 @@
+# This Source Code Form is subject to the terms of the Mozilla Public
+# License, v. 2.0. If a copy of the MPL was not distributed with this
+# file, You can obtain one at https://mozilla.org/MPL/2.0/.
+
+import pytest
+
+from pages.firefox.campaign.families.landing import CampaignFamiliesPage
+
+
+# confirm if download button is actual download or link to download page
+@pytest.mark.skip_if_firefox(reason="Download button is displayed only to non-Firefox users")
+def test_firefox_download_button_is_displayed(base_url, selenium):
+    page = CampaignFamiliesPage(selenium, base_url).open()
+    assert page.is_firefox_download_button_displayed
---
+++ tests/pages/firefox/campaign/families/landing.py
@@ -0,0 +1,18 @@
+# This Source Code Form is subject to the terms of the Mozilla Public
+# License, v. 2.0. If a copy of the MPL was not distributed with this
+# file, You can obtain one at https://mozilla.org/MPL/2.0/.
+
+from selenium.webdriver.common.by import By
+
+from pages.base import BasePage
+
+
+class CampaignFamiliesPage(BasePage):
+
+    _URL_TEMPLATE = "/{locale}/firefox/families/"
+
+    _firefox_download_button_locator = (By.CSS_SELECTOR, "[data-link-type='download']")
+
+    @property
+    def is_firefox_download_button_displayed(self):
+        return self.is_element_displayed(*self._firefox_download_button_locator)
