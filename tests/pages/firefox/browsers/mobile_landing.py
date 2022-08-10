ac303574ea99301bac46a074e9fa74d4da34df1b
Alex Gibson <alexgibson@users.noreply.github.com>
2022-08-10 15:52:21 +0100

    Fix broken Firefox mobile page (Fixes #12025)

---
+++ tests/pages/firefox/browsers/mobile_landing.py
@@ -0,0 +1,35 @@
+# This Source Code Form is subject to the terms of the Mozilla Public
+# License, v. 2.0. If a copy of the MPL was not distributed with this
+# file, You can obtain one at https://mozilla.org/MPL/2.0/.
+
+from selenium.webdriver.common.by import By
+
+from pages.base import BasePage
+from pages.regions.menu_list import MenuList
+from pages.regions.send_to_device import SendToDevice
+
+
+class FirefoxMobilePage(BasePage):
+
+    _URL_TEMPLATE = "/{locale}/firefox/browsers/mobile/{params}"
+
+    _android_download_link_locator = (By.ID, "android-download")
+    _ios_download_link_locator = (By.ID, "ios-download")
+    _focus_menu_list_locator = (By.ID, "menu-focus-wrapper")
+
+    @property
+    def focus_menu_list(self):
+        el = self.find_element(*self._focus_menu_list_locator)
+        return MenuList(self, root=el)
+
+    @property
+    def send_to_device(self):
+        return SendToDevice(self)
+
+    @property
+    def is_android_download_link_displayed(self):
+        return self.is_element_displayed(*self._android_download_link_locator)
+
+    @property
+    def is_ios_download_link_displayed(self):
+        return self.is_element_displayed(*self._ios_download_link_locator)
