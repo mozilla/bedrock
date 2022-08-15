dee4f3a3720628885e429986d98f707df8369215
Alex Gibson <alexgibson@users.noreply.github.com>
2022-08-15 15:58:22 +0100

    Fix VPN coupon experiment redirect (Fixes #12013) (#12036)

--- tests/unit/spec/base/experiment-utils.js
+++ tests/unit/spec/base/experiment-utils.js
@@ -9,34 +9,28 @@
  * Sinon docs: http://sinonjs.org/docs/
  */
 
+import { isApprovedToRun } from '../../../../media/js/base/experiment-utils.es6';
+
 describe('experiment-utils.es6.js', function () {
     describe('isApprovedToRun', function () {
         it('should return true if experimental params are not found in the page URL', function () {
-            const isApprovedToRun =
-                require('../../../../media/js/base/experiment-utils.es6.js').isApprovedToRun;
             expect(
                 isApprovedToRun('?utm_source=test&utm_campaign=test')
-            ).toBeTruthy();
-
-            expect(isApprovedToRun('')).toBeTruthy();
+            ).toBeTrue();
+            expect(isApprovedToRun('')).toBeTrue();
         });
 
         it('should return false if experimental params are found in the page URL', function () {
-            const isApprovedToRun =
-                require('../../../../media/js/base/experiment-utils.es6.js').isApprovedToRun;
-            expect(isApprovedToRun('?utm_medium=cpc')).toBeFalsy();
-
-            expect(isApprovedToRun('?utm_source=firefox-browser')).toBeFalsy();
-
+            expect(isApprovedToRun('?utm_medium=cpc')).toBeFalse();
+            expect(isApprovedToRun('?utm_source=firefox-browser')).toBeFalse();
             expect(
                 isApprovedToRun(
                     '?entrypoint_experiment=test&entrypoint_variation=a'
                 )
-            ).toBeFalsy();
-
-            expect(isApprovedToRun('?experiment=test&variation=a')).toBeFalsy();
-
-            expect(isApprovedToRun('?automation=true')).toBeFalsy();
+            ).toBeFalse();
+            expect(isApprovedToRun('?experiment=test&variation=a')).toBeFalse();
+            expect(isApprovedToRun('?automation=true')).toBeFalse();
+            expect(isApprovedToRun('?cjevent=1234567890')).toBeFalse();
         });
     });
 });
