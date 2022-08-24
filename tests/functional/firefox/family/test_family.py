93cdfb41234c64561e22ac3d844fe1cec18da170
Nathan Barrett <30009669+nathan-barrett@users.noreply.github.com>
2022-08-24 12:14:04 -0700

    add tests and data attributes to pdf link

--- tests/functional/firefox/family/test_family.py
+++ tests/functional/firefox/family/test_family.py
@@ -12,3 +12,8 @@ from pages.firefox.family.landing import FamilyPage
 def test_firefox_download_button_is_displayed(base_url, selenium):
     page = FamilyPage(selenium, base_url).open()
     assert page.is_firefox_download_button_displayed
+
+
+def text_firefox_pdf_download_button_is_displayed(base_url, selenium):
+    page = CampaignFamiliesPage(selenium, base_url).open()
+    assert page.is_firefox_pdf_download_button_displayed
