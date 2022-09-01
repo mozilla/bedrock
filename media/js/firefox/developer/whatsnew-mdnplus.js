d6da7b7b5d810d268300984b2fb0f3d5a80f9b1e
Craig Cook <craigcook@users.noreply.github.com>
2022-09-01 06:48:55 -0700

    Dev WNP, only animate mandala when visible [#11793] (#12070)

---
+++ media/js/firefox/developer/whatsnew-mdnplus.js
@@ -0,0 +1,41 @@
+/*
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at https://mozilla.org/MPL/2.0/.
+ */
+
+(function () {
+    'use strict';
+
+    var stopAnimTimeout;
+    var mandalaElement = document.getElementById('mandala');
+
+    // Check for support of the Page Visbility API
+    if (typeof document.hidden === 'undefined') {
+        return;
+    }
+
+    // If the page is hidden, stop the mandala.
+    // If the page is visible, animate the mandala.
+    function handleVisibilityChange() {
+        if (document.hidden) {
+            mandalaElement.classList.remove('animated');
+            clearTimeout(stopAnimTimeout);
+        } else {
+            mandalaElement.classList.add('animated');
+            // Stop animating after five minutes
+            stopAnimTimeout = setTimeout(function () {
+                mandalaElement.classList.remove('animated');
+                document.title = 'timeout';
+            }, 300000);
+        }
+    }
+
+    document.addEventListener(
+        'visibilitychange',
+        handleVisibilityChange,
+        false
+    );
+
+    window.Mozilla.run(handleVisibilityChange);
+})(window.Mozilla);
