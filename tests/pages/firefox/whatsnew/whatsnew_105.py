e8da2c5060770a08d274df400846a32b089d84cb
Alex Gibson <alexgibson@users.noreply.github.com>
2022-09-01 14:45:30 +0100

    Add WNP 105 for EU (Fixes #12075)

---
+++ tests/pages/firefox/whatsnew/whatsnew_105.py
@@ -0,0 +1,18 @@
+# This Source Code Form is subject to the terms of the Mozilla Public
+# License, v. 2.0. If a copy of the MPL was not distributed with this
+# file, You can obtain one at https://mozilla.org/MPL/2.0/.
+
+from selenium.webdriver.common.by import By
+
+from pages.base import BasePage
+
+
+class FirefoxWhatsNew105Page(BasePage):
+
+    _URL_TEMPLATE = "/{locale}/firefox/105.0/whatsnew/{params}"
+
+    _pocket_button_locator = (By.CSS_SELECTOR, ".wnp-main-cta .mzp-c-button")
+
+    @property
+    def is_pocket_button_displayed(self):
+        return self.is_element_displayed(*self._pocket_button_locator)
