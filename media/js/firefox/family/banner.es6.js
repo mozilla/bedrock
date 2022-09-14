9f3938427dde04f8647de5d9b1d048c5bc5c4b26
Reem H <42309026+reemhamz@users.noreply.github.com>
2022-09-14 14:26:50 +0100

    Family page: add bubbling emojis animation (fixes #12162)

--- media/js/firefox/family/banner.es6.js
+++ media/js/firefox/family/banner.es6.js
@@ -6,12 +6,20 @@
 
 let dadJokesBanner;
 let dadJokesBannerClose;
+let emojiWrapper;
+
+function motionAllowed() {
+    return (
+        'matchMedia' in window &&
+        window.matchMedia('(prefers-reduced-motion: no-preference)').matches
+    );
+}
 
 function showBanner() {
     // remove unneeded event listener
     document.removeEventListener('scroll', showBanner);
-    // show banner (potentially use setTimeout?)
-    dadJokesBanner.setAttribute('aria-hidden', 'false');
+    dadJokesBanner.classList.remove('hide-banner');
+    dadJokesBanner.classList.add('fade-in-banner');
     // allow dismissal click
     dadJokesBannerClose.addEventListener('click', hideBanner);
 }
@@ -23,16 +31,57 @@ function hideBanner() {
         'data-banner-dismissal': '1',
         event: 'in-page-interaction'
     });
-    // hide banner
-    dadJokesBanner.setAttribute('aria-hidden', 'true');
-    // remove unusable event listener
+    // start emoji animation according to user preference
+    const userPrefAnimation = motionAllowed()
+        ? 'animate-bubbles'
+        : 'animate-emoji-appearance';
+    emojiWrapper.classList.add(userPrefAnimation);
+    dadJokesBannerClose.setAttribute('disabled', 'true');
+    // fade out banner
+    emojiWrapper.addEventListener('animationend', (e) => fadeOutBanner(e));
+    // remove banner from accessibility tree
+    dadJokesBanner.addEventListener('transitionend', removeBanner);
+    // remove unusable event listeners
     dadJokesBannerClose.removeEventListener('click', hideBanner);
+    emojiWrapper.removeEventListener('animationend', (e) => fadeOutBanner(e));
+    dadJokesBanner.removeEventListener('transitionend', removeBanner);
+}
+
+function fadeOutBanner(e) {
+    if (!e.target.nextSibling) {
+        dadJokesBanner.classList.remove('fade-in-banner');
+    }
+}
+
+function removeBanner() {
+    dadJokesBanner.classList.add('hide-banner');
 }
 
 const init = function () {
     // set element references
     dadJokesBanner = document.getElementById('dad-jokes-banner');
     dadJokesBannerClose = document.getElementById('dad-jokes-banner-close');
+
+    // create emoji wrapper and images inside banner
+    const eyeroll = '/media/img/firefox/family/banner-emoji-eyeroll.svg';
+    const grimace = '/media/img/firefox/family/banner-emoji-grimace.svg';
+
+    const emojis = [eyeroll, grimace, grimace, eyeroll, eyeroll];
+
+    emojiWrapper = document.createElement('div');
+    emojiWrapper.setAttribute('class', 'emoji-wrapper');
+
+    emojis.forEach((src) => {
+        const img = document.createElement('img');
+        img.setAttribute('src', src);
+        img.setAttribute('alt', '');
+        emojiWrapper.appendChild(img);
+    });
+
+    dadJokesBanner
+        .querySelector('.c-dad-jokes-banner-button-wrapper')
+        .appendChild(emojiWrapper);
+
     // add event listeners
     document.addEventListener('scroll', showBanner);
 };
