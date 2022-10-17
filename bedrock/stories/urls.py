5f01b9da3cfa899037ac9f7c3262a08c074b5bf9
Paul McLanahan <pmac@mozilla.com>
2022-10-17 13:35:48 -0400

    Add temporary redirect for stories URL

--- bedrock/stories/urls.py
+++ bedrock/stories/urls.py
@@ -4,10 +4,13 @@
 
 
 from bedrock.mozorg.util import page
+from bedrock.redirects.util import redirect
 
 urlpatterns = (
     page("", "stories/landing.html"),
     page("art-of-engagement/", "stories/articles/art-of-engagement.html"),
     page("build-together/", "stories/articles/build-together.html"),
     page("community-champion/", "stories/articles/community-champion.html"),
+    # REMOVE WHEN REAL PAGE GOES LIVE
+    redirect(r"^joy-of-color/?$", "https://blog.mozilla.org/en/products/firefox/firefox-news/independent-voices/", permanent=False),
 )
