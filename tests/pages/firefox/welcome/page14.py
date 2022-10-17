bf90269916165520246003af28df0cff69f2fc6f
Alex Gibson <alexgibson@users.noreply.github.com>
2022-10-17 09:09:36 +0100

    Fix 500 error on /firefox/welcome/14/

---
+++ tests/pages/firefox/welcome/page14.py
@@ -0,0 +1,18 @@
+# This Source Code Form is subject to the terms of the Mozilla Public
+# License, v. 2.0. If a copy of the MPL was not distributed with this
+# file, You can obtain one at https://mozilla.org/MPL/2.0/.
+
+from selenium.webdriver.common.by import By
+
+from pages.base import BasePage
+
+
+class FirefoxWelcomePage14(BasePage):
+
+    _URL_TEMPLATE = "/{locale}/firefox/welcome/14/"
+
+    _get_vpn_button_locator = (By.CSS_SELECTOR, ".c-main-cta .mzp-c-button")
+
+    @property
+    def is_get_vpn_button_displayed(self):
+        return self.is_element_displayed(*self._get_vpn_button_locator)
