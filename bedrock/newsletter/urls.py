9756a5e2b282233f8339ca88b335d4719af7bdf1
Alex Gibson <alexgibson@users.noreply.github.com>
2022-10-20 11:03:38 +0100

    Add Firefox for families newsletter signup page (Fixes #12227) (#12257)

--- bedrock/newsletter/urls.py
+++ bedrock/newsletter/urls.py
@@ -36,4 +36,5 @@ urlpatterns = (
     page("newsletter/country/success/", "newsletter/country_success.html", ftl_files=["mozorg/newsletters"]),
     page("newsletter/fxa-error/", "newsletter/fxa-error.html", ftl_files=["mozorg/newsletters"]),
     page("newsletter/knowledge-is-power/", "newsletter/knowledge-is-power.html", ftl_files=["mozorg/newsletters"]),
+    page("newsletter/family/", "newsletter/family.html", ftl_files=["mozorg/newsletters"], active_locales=["en-US"]),
 )
