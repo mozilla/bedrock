a499b46a42ed00ef83351383ae385bde1cf8b003
Alex Gibson <alexgibson@users.noreply.github.com>
2022-11-21 13:28:23 +0000

    Add /whatsnew page for Firefox 108 (Fixes #12231)

--- bedrock/firefox/tests/test_base.py
+++ bedrock/firefox/tests/test_base.py
@@ -911,6 +911,113 @@ class TestWhatsNew(TestCase):
 
     # end 107.0 whatsnew tests
 
+    # begin 108.0 whatsnew tests
+
+    @override_settings(DEV=True)
+    @patch.dict(os.environ, SWITCH_FIREFOX_WHATSNEW_108_EN_SHOW_FEATURE="False")
+    def test_fx_108_0_0_curation_en(self, render_mock):
+        """Should use whatsnew-fx108-curation-en template for en-US locale"""
+        req = self.rf.get("/firefox/whatsnew/")
+        req.locale = "en-US"
+        self.view(req, version="108.0")
+        template = render_mock.call_args[0][1]
+        assert template == ["firefox/whatsnew/whatsnew-fx108-curation-en.html"]
+
+    @override_settings(DEV=True)
+    @patch.dict(os.environ, SWITCH_FIREFOX_WHATSNEW_108_EN_SHOW_FEATURE="True")
+    def test_fx_108_0_0_feature_en(self, render_mock):
+        """Should use whatsnew-fx108-feature-en template when switch is on"""
+        req = self.rf.get("/firefox/whatsnew/")
+        req.locale = "en-US"
+        self.view(req, version="108.0")
+        template = render_mock.call_args[0][1]
+        assert template == ["firefox/whatsnew/whatsnew-fx108-feature-en.html"]
+
+    @override_settings(DEV=True)
+    def test_fx_108_0_0_curation_variation_en(self, render_mock):
+        """Should use whatsnew-fx108-curation-en variation for en-US locale"""
+        req = self.rf.get("/firefox/whatsnew/?v=1")
+        req.locale = "en-US"
+        self.view(req, version="108.0")
+        template = render_mock.call_args[0][1]
+        assert template == ["firefox/whatsnew/whatsnew-fx108-curation-en.html"]
+
+    @override_settings(DEV=True)
+    def test_fx_108_0_0_feature_variation_en(self, render_mock):
+        """Should use whatsnew-fx108-feature-en variation for en-US locale"""
+        req = self.rf.get("/firefox/whatsnew/?v=2")
+        req.locale = "en-US"
+        self.view(req, version="108.0")
+        template = render_mock.call_args[0][1]
+        assert template == ["firefox/whatsnew/whatsnew-fx108-feature-en.html"]
+
+    @override_settings(DEV=True)
+    @patch.dict(os.environ, SWITCH_FIREFOX_WHATSNEW_108_DE_SHOW_FEATURE="False")
+    def test_fx_108_0_0_curation_de(self, render_mock):
+        """Should use whatsnew-fx108-curation-de template for de locale"""
+        req = self.rf.get("/firefox/whatsnew/")
+        req.locale = "de"
+        self.view(req, version="108.0")
+        template = render_mock.call_args[0][1]
+        assert template == ["firefox/whatsnew/whatsnew-fx108-curation-de.html"]
+
+    @override_settings(DEV=True)
+    @patch.dict(os.environ, SWITCH_FIREFOX_WHATSNEW_108_DE_SHOW_FEATURE="True")
+    def test_fx_108_0_0_feature_de(self, render_mock):
+        """Should use whatsnew-fx108-feature-de template when switch is on"""
+        req = self.rf.get("/firefox/whatsnew/")
+        req.locale = "de"
+        self.view(req, version="108.0")
+        template = render_mock.call_args[0][1]
+        assert template == ["firefox/whatsnew/whatsnew-fx108-feature-de.html"]
+
+    @override_settings(DEV=True)
+    def test_fx_108_0_0_curation_variation_de(self, render_mock):
+        """Should use whatsnew-fx108-curation-de variation for de locale"""
+        req = self.rf.get("/firefox/whatsnew/?v=1")
+        req.locale = "de"
+        self.view(req, version="108.0")
+        template = render_mock.call_args[0][1]
+        assert template == ["firefox/whatsnew/whatsnew-fx108-curation-de.html"]
+
+    @override_settings(DEV=True)
+    def test_fx_108_0_0_feature_variation_de(self, render_mock):
+        """Should use whatsnew-fx108-feature-de variation for de locale"""
+        req = self.rf.get("/firefox/whatsnew/?v=2")
+        req.locale = "de"
+        self.view(req, version="108.0")
+        template = render_mock.call_args[0][1]
+        assert template == ["firefox/whatsnew/whatsnew-fx108-feature-de.html"]
+
+    @override_settings(DEV=True)
+    def test_fx_108_0_0_fr(self, render_mock):
+        """Should use whatsnew-fx108-fr template for fr locale"""
+        req = self.rf.get("/firefox/whatsnew/")
+        req.locale = "fr"
+        self.view(req, version="108.0")
+        template = render_mock.call_args[0][1]
+        assert template == ["firefox/whatsnew/whatsnew-fx108-fr.html"]
+
+    @override_settings(DEV=True)
+    def test_fx_108_0_0_en_gb(self, render_mock):
+        """Should use whatsnew-fx108-uk template for en-GB locale"""
+        req = self.rf.get("/firefox/whatsnew/")
+        req.locale = "en-GB"
+        self.view(req, version="108.0")
+        template = render_mock.call_args[0][1]
+        assert template == ["firefox/whatsnew/whatsnew-fx108-uk.html"]
+
+    @override_settings(DEV=True)
+    def test_fx_108_0_0_uk_geo(self, render_mock):
+        """Should use whatsnew-fx108-uk template for en-US locale when county is GB"""
+        req = self.rf.get("/firefox/whatsnew/", HTTP_CF_IPCOUNTRY="GB")
+        req.locale = "en-US"
+        self.view(req, version="108.0")
+        template = render_mock.call_args[0][1]
+        assert template == ["firefox/whatsnew/whatsnew-fx108-uk.html"]
+
+    # end 108.0 whatsnew tests
+
 
 @patch("bedrock.firefox.views.l10n_utils.render", return_value=HttpResponse())
 class TestFirstRun(TestCase):
---
+++ tests/pages/firefox/whatsnew/whatsnew_108.py
@@ -0,0 +1,26 @@
+# This Source Code Form is subject to the terms of the Mozilla Public
+# License, v. 2.0. If a copy of the MPL was not distributed with this
+# file, You can obtain one at https://mozilla.org/MPL/2.0/.
+
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support import expected_conditions as expected
+
+from pages.base import BasePage
+
+
+class FirefoxWhatsNew108Page(BasePage):
+
+    _URL_TEMPLATE = "/{locale}/firefox/108.0/whatsnew/{params}"
+
+    _pocket_button_locator = (By.CSS_SELECTOR, ".wnp-main-cta .mzp-c-button")
+
+    def wait_for_page_to_load(self):
+        self.wait.until(lambda s: self.seed_url in s.current_url)
+        el = self.find_element(By.TAG_NAME, "html")
+        self.wait.until(lambda s: "loaded" in el.get_attribute("class"))
+        self.wait.until(expected.visibility_of_element_located(self._pocket_button_locator))
+        return self
+
+    @property
+    def is_pocket_button_displayed(self):
+        return self.is_element_displayed(*self._pocket_button_locator)
