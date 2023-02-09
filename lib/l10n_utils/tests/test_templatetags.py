2f0eafde45aa0a2e469b3a6d2d744787310e936a
Steve Jalim <stevejalim@mozilla.com>
2023-02-09 14:16:26 +0000

    Support sanitisation of Fluent strings before rendering
    This changeset adds a layer of protection against unexpected markup being
    introduced via Fluent strings as part of the localisation process.
    
    We now use `bleach` to sanitise each Fluent string's content before declaring
    it as safe for Django to render without escaping (via `Markup`).
    
    The sanitisation only allows a specific, small, set of HTML tags and attributes
    to be allowed through; everything else will be escaped.
    
    We've drawn our initial set of allowed tags and attributes from the current
    en-US fluent templates in the repo, following the logic that: a) we trust these
    because we added them; and b) if someone tries to add in malevolent markup
    via a Fluent string in a particular locale, we _should_ be sanitising that
    because the translation should use the same tags and elements that the source
    does.
    
    There is a test added to show the sanitisation working, plus a test that
    inspects the current state of the Fluent files in the l10n/ and l10n-pocket/
    directories and ensures they do not contain any tags that are not currently
    in the relevant allowlist. If we want to support another tag or attr in our
    Fluent strings, that's fine - we just expand the allowlist, and a test will
    fail to warn us that we haven't updated the allowlist.

---
+++ lib/l10n_utils/tests/test_templatetags.py
@@ -0,0 +1,114 @@
+# This Source Code Form is subject to the terms of the Mozilla Public
+# License, v. 2.0. If a copy of the MPL was not distributed with this
+# file, You can obtain one at https://mozilla.org/MPL/2.0/.
+
+import os
+import re
+from typing import List, MutableSet
+from unittest.mock import patch
+
+import pytest
+
+from lib.l10n_utils.templatetags.fluent import (
+    ATTRS_ALLOWED_IN_FLUENT_STRINGS,
+    TAGS_ALLOWED_IN_FLUENT_STRINGS,
+    ftl,
+)
+
+
+@pytest.mark.parametrize(
+    "mock_return_value, expected",
+    (
+        (
+            '<strong>bolder</strong>and <a href="{ $mailto }">test@example.com</a>.<br><span>spanned</span> <em>emphasised</em> <abbr>abbreviation</abbr>',  # noqa: E501
+            '<strong>bolder</strong>and <a href="{ $mailto }">test@example.com</a>.<br><span>spanned</span> <em>emphasised</em> <abbr>abbreviation</abbr>',  # noqa: E501
+        ),
+        (
+            'test attrs <a href="https://www.mozilla.org/" class="test-link" id="target-for-link" rel="nofollow" title="test title">test</a>',
+            'test attrs <a href="https://www.mozilla.org/" class="test-link" id="target-for-link" rel="nofollow" title="test title">test</a>',
+        ),
+        (
+            "some string with a sneaky <script>alert('pwned');</script>",
+            "some string with a sneaky &lt;script&gt;alert('pwned');&lt;/script&gt;",
+        ),
+        (
+            'some string with disallowed attrs <a onclick="injectedScript()">test</a>',
+            "some string with disallowed attrs <a>test</a>",
+        ),
+        ("String with no markup in it", "String with no markup in it"),
+    ),
+    ids=[
+        "Tag test, no changes expected via sanitization",
+        "Attr test, no changes expected via sanitization",
+        "Proof that bleach is escaping tags that are not in the allowlists",
+        "Proof that bleach is stripping attrs that are not in the allowlists",
+        "String with no markup in it",
+    ],
+)
+@patch("lib.l10n_utils.templatetags.fluent.fluent.translate")
+@patch("lib.l10n_utils.templatetags.fluent.fluent.ftl")
+def test_ftl_bleaches_string(mock_ftl, mock_translate, mock_return_value, expected):
+    # Ensure the ftl() templatetag call bleaches the output of Markup
+    mock_ftl.return_value = mock_return_value
+    mock_translate.return_value = mock_return_value
+    dummy_ctx = {
+        "fluent_l10n": "dummy_fluent_l10n_in_ctx",
+        "fluent_files": "dummy_fluent_files_in_ctx",
+    }
+    assert ftl(dummy_ctx, "dummy") == expected
+
+
+def test_ftl_bleach_allowlists_are_comprehensive():
+    """This is a canary test to confirm our bleach() settings for
+    calls to the ftl() helper are appropriate for the content we expect.
+
+    FTL strings in `en-US` are our default, so ensure the bleaching call
+    expects all the _trusted_ markup that we can currently find in those
+    en-US strings. If non-en-US strings contain disallowed markup, it will
+    be escaped at runtime"""
+
+    # Build a list of the tags and attrs we find in our fluent strings and
+    # compare them with what's in our allowlists.
+
+    tag_re = re.compile(r"<\w+")
+    attr_re = re.compile(r"\s\w+=")
+
+    filepaths = set()
+
+    tags_found = set()
+    attrs_found = set()
+
+    def _clean_tag_matches(tag_matches: List) -> MutableSet:
+        return set([x.replace("<", "") for x in tag_matches])
+
+    def _clean_attr_matches(attr_matches: List) -> MutableSet:
+        return set([x.replace("=", "").strip() for x in attr_matches if "?" not in x])
+
+    for starting_dir in [
+        "l10n/",
+        "l10n-pocket",
+    ]:
+        for root, _, files in os.walk(starting_dir):
+            for file in files:
+                if file.endswith(".ftl"):
+                    filepaths.add(os.path.join(root, file))
+
+    for filepath in filepaths:
+        with open(filepath) as fp:
+            for line in fp.readlines():
+                if tag_matches := re.findall(tag_re, line):
+                    tags_found.update(_clean_tag_matches(tag_matches))
+                if attr_matches := re.findall(attr_re, line):
+                    attrs_found.update(_clean_attr_matches(attr_matches))
+
+    assert TAGS_ALLOWED_IN_FLUENT_STRINGS == tags_found, (
+        "DANGER! HTML tags were detected in en-US source FTL strings that are not in "
+        "lib.l10n_utils.templatetags.fluent.fluent.TAGS_ALLOWED_IN_FLUENT_STRINGS. "
+        f"Unexpected tag(s) found: {tags_found.difference(TAGS_ALLOWED_IN_FLUENT_STRINGS)}"
+    )
+
+    assert sorted(ATTRS_ALLOWED_IN_FLUENT_STRINGS) == sorted(attrs_found), (
+        "DANGER! HTML attributes were detected in en-US source FTL strings that are not in "
+        "lib.l10n_utils.templatetags.fluent.fluent.ATTRS_ALLOWED_IN_FLUENT_STRINGS. "
+        f"Unexpected attribute(s) found: {set(attrs_found).difference(set(ATTRS_ALLOWED_IN_FLUENT_STRINGS))}"
+    )
