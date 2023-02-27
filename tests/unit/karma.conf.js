e1f9c34b08951cd9387879343c0fbcad52e38eb2
Steve Jalim <stevejalim@mozilla.com>
2023-02-27 21:16:08 +0000

    Try a different version of Ubuntu, dodging a FF 'firstrun' blocker

--- tests/unit/karma.conf.js
+++ tests/unit/karma.conf.js
@@ -153,7 +153,7 @@ module.exports = function (config) {
         // - Safari (only Mac)
         // - PhantomJS
         // - IE (only Windows)
-        browsers: ['Firefox', 'Chrome'],
+        browsers: ['Chrome', 'Firefox'],
 
         // If browser does not capture in given timeout [ms], kill it
         captureTimeout: 60000,
