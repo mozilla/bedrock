1cc7f539b8723db47c3c7dae03fef58d486bdd5f
Alex Gibson <alexgibson@users.noreply.github.com>
2023-03-06 16:22:08 +0000

    Mark Pocket and Family pages tests as non-destructive

--- tests/functional/pocket/test_navigation.py
+++ tests/functional/pocket/test_navigation.py
@@ -22,6 +22,7 @@ def test_mobile_menu(pocket_base_url, selenium_mobile):
     assert not page.navigation.is_mobile_menu_nav_list_displayed
 
 
+@pytest.mark.nondestructive
 def test_accessible_mobile_menu_open_name(pocket_base_url, selenium_mobile):
     page = AboutPage(selenium_mobile, pocket_base_url).open()
     button_label_reference = page.navigation.mobile_menu_open_button.get_attribute("aria-labelledby")
@@ -29,6 +30,7 @@ def test_accessible_mobile_menu_open_name(pocket_base_url, selenium_mobile):
     assert len(string) > 0
 
 
+@pytest.mark.nondestructive
 def test_accessible_mobile_menu_close_name(pocket_base_url, selenium_mobile):
     page = AboutPage(selenium_mobile, pocket_base_url).open()
     page.navigation.open_mobile_menu()
@@ -36,6 +38,7 @@ def test_accessible_mobile_menu_close_name(pocket_base_url, selenium_mobile):
     assert len(string) > 0
 
 
+@pytest.mark.nondestructive
 def test_mobile_menu_not_displayed_on_desktop(pocket_base_url, selenium):
     page = AboutPage(selenium, pocket_base_url).open()
     assert not page.navigation.is_mobile_menu_open_button_displayed
