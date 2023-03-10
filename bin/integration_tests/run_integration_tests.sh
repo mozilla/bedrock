22a2cefb21141385168b6ed0817a8ea195f34929
Steve Jalim <stevejalim@mozilla.com>
2023-03-10 16:30:45 +0000

    Add support for functional tests via GH Actions - WIP

--- bin/integration_tests/run_integration_tests.sh
+++ bin/integration_tests/run_integration_tests.sh
@@ -20,7 +20,7 @@ if [ "${DRIVER}" = "SauceLabs" ]; then
   # Temporarily exclude logs for Saucelabs jobs https://github.com/pytest-dev/pytest-selenium/issues/288
   CMD="SAUCELABS_W3C=true SELENIUM_EXCLUDE_DEBUG=logs py.test"
 else
-  CMD="py.test"
+  CMD="pytest"
 fi
 
 CMD="${CMD} -r a"
