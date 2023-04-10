76cedc112c3c1f6570983e7f5bfb6f8cff8b2f5c
Stephanie Hobson <sjhobson@gmail.com>
2023-04-10 11:51:37 -0700

    first pass product_downlaod

---
+++ media/js/base/core-datalayer-events-init.es6.js
@@ -0,0 +1,11 @@
+/*
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at https://mozilla.org/MPL/2.0/.
+ */
+
+import init from './core-datalayer-events.es6.js';
+
+window.addEventListener('DOMContentLoaded', () => {
+    init();
+});
