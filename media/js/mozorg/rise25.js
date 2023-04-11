8c67169660413b1949412cfed7d1c3005cf78364
Nathan Barrett <30009669+nathan-barrett@users.noreply.github.com>
2023-04-11 11:25:32 -0700

    Add new rise25 video

--- media/js/mozorg/rise25.js
+++ media/js/mozorg/rise25.js
@@ -7,25 +7,77 @@
 (function () {
     'use strict';
 
-    var isReduced =
-        window.matchMedia('(prefers-reduced-motion: reduce)') === true ||
-        window.matchMedia('(prefers-reduced-motion: reduce)').matches === true;
+    function generateTweet() {
+        var tweetUrl = encodeURIComponent(
+            'https://mozilla.org/rise25?utm_campaign=rise25&utm_medium=organicsocial&utm_source=twitter&utm_content=rise25-share'
+        );
+        var tweetText = encodeURIComponent('Hey');
+        return (
+            'https://www.twitter.com/intent/tweet?url=' +
+            tweetUrl +
+            '&text=' +
+            tweetText
+        );
+    }
 
-    var heroImageWrapper = document.querySelector('.rise25-hero-image');
-    var images = heroImageWrapper.querySelectorAll('.hero-image');
+    function generateFacebookShare() {
+        var shareUrl = encodeURIComponent(
+            'https://mozilla.org/rise25?utm_campaign=rise25&utm_medium=organicsocial&utm_source=facebook&utm_content=rise25-share'
+        );
+        var shareText = encodeURIComponent('Hey');
+        return (
+            'https://facebook.com/sharer.php?u=' +
+            shareUrl +
+            '&quote=' +
+            shareText
+        );
+    }
 
-    function handleMouseMove(e) {
-        var amountMovedX = (e.clientX * -0.3) / 8;
-        var amountMovedY = (e.clientY * -0.5) / 8;
+    function handleCopyLink(e) {
+        e.preventDefault;
+        navigator.clipboard.writeText('https://mozilla.com/rise25');
+    }
 
-        for (var index = 0; index < images.length; index++) {
-            var image = images[index];
-            image.style.transform =
-                'translate(' + amountMovedX + 'px,' + amountMovedY + 'px)';
+    var facebookShare = document.querySelectorAll('.social-share .facebook');
+    var twitterShare = document.querySelectorAll('.social-share .twitter');
+    var emailShare = document.querySelectorAll('.social-share .email');
+    var copyLinks = document.querySelectorAll('.social-share .copy-text');
+
+    (function () {
+        for (var index = 0; index < facebookShare.length; index++) {
+            var element = facebookShare[index];
+            element.href = generateFacebookShare();
         }
+    })();
+
+    (function () {
+        for (var index = 0; index < twitterShare.length; index++) {
+            var element = twitterShare[index];
+            element.href = generateTweet();
+        }
+    })();
+
+    (function () {
+        var subject = encodeURIComponent('');
+        var body = encodeURIComponent('');
+        for (var index = 0; index < emailShare.length; index++) {
+            var element = emailShare[index];
+            element.href =
+                'mailto:example@website.com?subject=' +
+                subject +
+                '&body=' +
+                body;
+        }
+    })();
+
+    for (var index = 0; index < copyLinks.length; index++) {
+        var element = copyLinks[index];
+        element.addEventListener('click', handleCopyLink);
     }
 
-    if (!isReduced) {
-        heroImageWrapper.addEventListener('mousemove', handleMouseMove);
-    }
+    var video = document.querySelector('.rise25-hero video');
+
+    video.addEventListener('ended', function () {
+        video.currentTime = 0;
+    });
 })();
