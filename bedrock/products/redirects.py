7fc5b20602eeb45b089147ca0b6cd537afceb68c
Nathan Barrett <30009669+nathan-barrett@users.noreply.github.com>
2023-04-20 09:17:27 -0700

    Update redirects

--- bedrock/products/redirects.py
+++ bedrock/products/redirects.py
@@ -6,11 +6,11 @@ from bedrock.redirects.util import redirect
 
 redirectpatterns = (
     # Issue 12937 -
-    redirect(r"^vpn/more/what-is-an-ip-address/$", "/products/vpn/resource-center/what-is-an-ip-address/"),
-    redirect(r"^vpn/more/what-is-a-vpn$", "/products/vpn/resource-center/what-is-a-vpn/"),
-    redirect(r"^vpn/more/vpn-or-proxy/$", "/products/vpn/resource-center/the-difference-between-a-vpn-and-a-web-proxy/"),
-    redirect(r"^vpn/more/when-to-use-a-vpn/$", "/products/vpn/resource-center/5-reasons-you-should-use-a-vpn/"),
-    redirect(r"^vpn/more/why-mozilla-vpn/$", "products.vpn.landing"),
-    redirect(r"^vpn/more/do-i-need-a-vpn/$", "/products/vpn/resource-center/do-you-need-a-vpn-at-home-here-are-5-reasons-you-might/"),
-    redirect(r"^vpn/more/what-is-a-vpn-v2/$", "/products/vpn/resource-center/what-is-a-vpn/"),
+    redirect(r"^products/vpn/more/what-is-an-ip-address/$", "/products/vpn/resource-center/what-is-an-ip-address/"),
+    redirect(r"^products/vpn/more/what-is-a-vpn$", "/products/vpn/resource-center/what-is-a-vpn/"),
+    redirect(r"^products/vpn/more/vpn-or-proxy/$", "/products/vpn/resource-center/the-difference-between-a-vpn-and-a-web-proxy/"),
+    redirect(r"^products/vpn/more/when-to-use-a-vpn/$", "/products/vpn/resource-center/5-reasons-you-should-use-a-vpn/"),
+    redirect(r"^products/vpn/more/why-mozilla-vpn/$", "products.vpn.landing"),
+    redirect(r"^products/vpn/more/do-i-need-a-vpn/$", "/products/vpn/resource-center/do-you-need-a-vpn-at-home-here-are-5-reasons-you-might/"),
+    redirect(r"^products/vpn/more/what-is-a-vpn-v2/$", "/products/vpn/resource-center/what-is-a-vpn/"),
 )
