89b3000b12a5c8b23bc3537a6556ef645a3fddb0
Rob Hudson <robhudson@mozilla.com>
2023-05-10 07:05:12 -0700

    Fix #10252: Add `source_url` to newsletter management

--- media/js/newsletter/management.es6.js
+++ media/js/newsletter/management.es6.js
@@ -651,8 +651,12 @@ const NewsletterManagementForm = {
         const newsletters = encodeURIComponent(
             NewsletterManagementForm.getCheckedNewsletters().join(',')
         );
+        // Source URL (hidden field)
+        const sourceUrl = encodeURIComponent(
+            _form.querySelector('input[name="source_url"]').value
+        );
 
-        return `format=${format}&country=${country}&lang=${lang}&newsletters=${newsletters}&optin=Y`;
+        return `format=${format}&country=${country}&lang=${lang}&newsletters=${newsletters}&optin=Y&source_url=${sourceUrl}`;
     },
 
     /**
