20a509d34a93f7f7c79576f4c3ba8d6cdae0cab3
Steve Jalim <stevejalim@mozilla.com>
2023-05-22 22:08:21 +0100

    Fixup regex - was over-tuned

--- media/js/pocket/updates-signup.es6.js
+++ media/js/pocket/updates-signup.es6.js
@@ -101,7 +101,7 @@ const UpdatesForm = {
         // as default form values
         for (const [key, value] of UpdatesForm.getSearchParams()) {
             const strippedKey = key.replace('utm_', '');
-            const allowableRegex = RegExp(/^[\w\d/.%-_]{1,100}$/);
+            const allowableRegex = RegExp(/^[\w/.%-]{1,100}$/);
             if (soughtParams.includes(strippedKey)) {
                 // We only pre-populate the field if the value matches the regex
                 if (value.search(allowableRegex) !== -1) {
