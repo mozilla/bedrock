0417cfd08bcb1f290723fe6b9662c53a40228cde
Nathan Barrett <30009669+nathan-barrett@users.noreply.github.com>
2023-05-26 09:56:27 -0700

    Relay waitlist (#13061)
    * Add waitlist pages

--- bedrock/products/forms.py
+++ bedrock/products/forms.py
@@ -8,3 +8,13 @@ from bedrock.newsletter.forms import NewsletterFooterForm
 class VPNWaitlistForm(NewsletterFooterForm):
     def __init__(self, locale, data=None, *args, **kwargs):
         super().__init__("guardian-vpn-waitlist", locale, data, *args, **kwargs)
+
+
+class RelayBundleWaitlistForm(NewsletterFooterForm):
+    def __init__(self, locale, data=None, *args, **kwargs):
+        super().__init__("relay-vpn-bundle-waitlist", locale, data, *args, **kwargs)
+
+
+class RelayPhoneWaitlistForm(NewsletterFooterForm):
+    def __init__(self, locale, data=None, *args, **kwargs):
+        super().__init__("relay-phone-masking-waitlist", locale, data, *args, **kwargs)
