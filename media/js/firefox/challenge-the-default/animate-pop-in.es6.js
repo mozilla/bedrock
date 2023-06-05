c56b93553b5818cd94174d074104a8270a2783f8
Nathan Barrett <30009669+nathan-barrett@users.noreply.github.com>
2023-06-05 14:26:30 -0700

    Add animations

---
+++ media/js/firefox/challenge-the-default/animate-pop-in.es6.js
@@ -0,0 +1,59 @@
+/*
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at https://mozilla.org/MPL/2.0/.
+ */
+
+let observer;
+let chain = Promise.resolve();
+
+function createObserver() {
+    return new IntersectionObserver(function (entries) {
+        entries.forEach(function (entry) {
+            if (entry.isIntersecting) {
+                if (entry.target.classList.contains('mzp-c-picto')) {
+                    // chain promises with a 200ms delay in between each one
+                    chain = chain.then(() => popIn(entry.target));
+                    // remove target observer after triggering animation
+                    observer.unobserve(entry.target);
+                } else if (entry.target.classList.contains('toggle')) {
+                    const input = entry.target.querySelector('input');
+                    setTimeout(() => {
+                        entry.target.classList.add('animate-slide');
+                        input.checked = true;
+                    }, 250);
+                }
+            }
+        });
+    });
+}
+
+function init() {
+    const { Utils } = window.Mozilla;
+    if (Utils.supportsIntersectionObserver() && Utils.allowsMotion()) {
+        observer = createObserver();
+
+        //add picto observers
+        document
+            .querySelectorAll('.c-ctd-features .mzp-c-picto')
+            .forEach(function (element) {
+                observer.observe(element);
+            });
+
+        // add middle toggle
+        document.querySelectorAll('.toggle.middle').forEach(function (toggle) {
+            observer.observe(toggle);
+        });
+    }
+}
+
+function popIn(element) {
+    return new Promise((res) => {
+        setTimeout(() => {
+            element.classList.add('animate-pop-in');
+            res();
+        }, 200);
+    });
+}
+
+init();
--- media/js/firefox/challenge-the-default/challenge-the-default.es6.js
+++ media/js/firefox/challenge-the-default/challenge-the-default.es6.js
@@ -5,87 +5,67 @@
  */
 
 import '@mozilla-protocol/core/protocol/js/details';
-import MzpModal from '@mozilla-protocol/core/protocol/js/modal';
 
-const toggleGrid = document.querySelector('.toggle-grid');
-// because of the way the modal grid is set up, there are multiple modal buttons in the DOM
-const modalButtons = document.querySelectorAll('.modal-btn');
-const content = document.querySelector('.mzp-u-modal-content');
+// const toggleGrid = document.querySelector('.toggle-grid');
 
-function debounce(func, wait, immediate) {
-    let timeout;
-    return function () {
-        const context = this;
-        const args = arguments;
-        const later = () => {
-            timeout = null;
-            if (!immediate) {
-                func.apply(context, args);
-            }
-        };
-        const callNow = immediate && !timeout;
-        clearTimeout(timeout);
-        timeout = setTimeout(later, wait);
-        if (callNow) {
-            func.apply(context, args);
-        }
-    };
-}
+// function debounce(func, wait, immediate) {
+//     let timeout;
+//     return function () {
+//         const context = this;
+//         const args = arguments;
+//         const later = () => {
+//             timeout = null;
+//             if (!immediate) {
+//                 func.apply(context, args);
+//             }
+//         };
+//         const callNow = immediate && !timeout;
+//         clearTimeout(timeout);
+//         timeout = setTimeout(later, wait);
+//         if (callNow) {
+//             func.apply(context, args);
+//         }
+//     };
+// }
 
-function fillGrid() {
-    const gridComputedStyle = getComputedStyle(toggleGrid);
-    // get number of grid rows
-    const gridRowCount = gridComputedStyle
-        .getPropertyValue('grid-template-rows')
-        .split(' ').length;
-    // get number of grid columns
-    const gridColumnCount = gridComputedStyle
-        .getPropertyValue('grid-template-columns')
-        .split(' ').length;
+// function fillGrid() {
+//     const gridComputedStyle = getComputedStyle(toggleGrid);
+//     // get number of grid rows
+//     const gridRowCount = gridComputedStyle
+//         .getPropertyValue('grid-template-rows')
+//         .split(' ').length;
+//     // get number of grid columns
+//     const gridColumnCount = gridComputedStyle
+//         .getPropertyValue('grid-template-columns')
+//         .split(' ').length;
 
-    const toggleAmount = gridRowCount * gridColumnCount;
+//     const toggleAmount = gridRowCount * gridColumnCount;
 
-    if (toggleAmount) {
-        for (let index = 0; index < toggleAmount; index++) {
-            toggleGrid.insertAdjacentHTML(
-                'beforeend',
-                `<label class="toggle" for="toggle-${index}">
-                    <input type="checkbox" name="toggle" id="toggle-${index}" class="toggle-input" />
-                    <span class="toggle-display"></span>
-                </label>`
-            );
-        }
-    }
-}
+//     if (toggleAmount) {
+//         for (let index = 0; index < toggleAmount; index++) {
+//             toggleGrid.insertAdjacentHTML(
+//                 'beforeend',
+//                 `<label class="toggle" for="toggle-${index}">
+//                     <input type="checkbox" name="toggle" id="toggle-${index}" class="toggle-input" />
+//                     <span class="toggle-display"></span>
+//                 </label>`
+//             );
+//         }
+//     }
+// }
 
-function emptyGrid() {
-    while (toggleGrid.firstChild) {
-        toggleGrid.removeChild(toggleGrid.firstChild);
-    }
-}
+// function emptyGrid() {
+//     while (toggleGrid.firstChild) {
+//         toggleGrid.removeChild(toggleGrid.firstChild);
+//     }
+// }
 
-window.addEventListener(
-    'resize',
-    debounce(function () {
-        emptyGrid();
-        fillGrid();
-    }, 100)
-);
+// window.addEventListener(
+//     'resize',
+//     debounce(function () {
+//         emptyGrid();
+//         fillGrid();
+//     }, 100)
+// );
 
-fillGrid();
-
-for (let index = 0; index < modalButtons.length; index++) {
-    const button = modalButtons[index];
-    button.addEventListener(
-        'click',
-        (e) => {
-            e.preventDefault();
-            MzpModal.createModal(e.target, content, {
-                title: 'Von Anfang an für alle',
-                className: 'ctd-modal',
-                closeText: 'Close modal'
-            });
-        },
-        false
-    );
-}
+// fillGrid();
