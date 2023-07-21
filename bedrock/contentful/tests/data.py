a4d303c22d1d53b888e853bf9ac875f0c14283ae
Steve Jalim <stevejalim@mozilla.com>
2023-07-21 12:55:23 +0100

    Dependency bumps + related minor code tweaks
    Resolves #13411: Bump black from 23.3.0 to 23.7.0 in /requirements
    Resolves #13410: Bump django-allow-cidr from 0.6.0 to 0.7.1 in /requirements
    Resolves #13409: Bump ruff from 0.0.277 to 0.0.278 in /requirements
    Resolves #13408: Bump lxml from 4.9.2 to 4.9.3 in /requirements
    Resolves #13406: Bump basket-client from 1.0.0 to 1.1.0 in /requirements
    Resolves #13405: Bump sentry-sdk from 1.27.1 to 1.28.1 in /requirements
    Resolves #13403: Bump pipdeptree from 2.9.4 to 2.10.1 in /requirements
    Resolves #13402: Bump boto3 from 1.28.1 to 1.28.3 in /requirements
    Resolves #13401: Bump django-cors-headers from 4.1.0 to 4.2.0 in /requirements
    Resolves #13400: Bump cryptography from 41.0.1 to 41.0.2 in /requirements

--- bedrock/contentful/tests/data.py
+++ bedrock/contentful/tests/data.py
@@ -2,7 +2,7 @@
 # License, v. 2.0. If a copy of the MPL was not distributed with this
 # file, You can obtain one at https://mozilla.org/MPL/2.0/.
 
-# flake8: noqa E501
+# noqa: E501
 
 resource_center_page_data = {
     "page_type": "pagePageResourceCenter",
