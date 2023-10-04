88434b1486f9c0bf110c0e9fcf365df78f3dcc7f
Nathan Barrett <30009669+nathan-barrett@users.noreply.github.com>
2023-10-04 11:41:09 -0500

    Add redirect from rise25/ to external page
    Removed all old rise25 assets and templates and set up redirects
    and tests in map_globalconf.py

--- bedrock/mozorg/urls.py
+++ bedrock/mozorg/urls.py
@@ -150,8 +150,6 @@ urlpatterns = (
     page("analytics-tests/", "mozorg/analytics-tests/ga-index.html"),
     path("email-mieco/", views.mieco_email_form, name="mozorg.email_mieco"),
     page("santalocator/", "mozorg/santa-locator.html"),
-    page("rise25/", "mozorg/rise25/rise25.html"),
-    redirect(r"rise-25/?$", "rise25/"),
     page("advertising/", "mozorg/advertising.html"),
     page(
         "try-picture-in-picture/",
