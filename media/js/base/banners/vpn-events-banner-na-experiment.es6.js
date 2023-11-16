d19c0fb919dfc50bb1d79f4a70710298bdfedea6
Alex Gibson <alexgibson@users.noreply.github.com>
2023-11-16 10:23:17 +0000

    Simplify experiment logic

--- media/js/base/banners/vpn-events-banner-na-experiment.es6.js
+++ media/js/base/banners/vpn-events-banner-na-experiment.es6.js
@@ -9,32 +9,44 @@ import { isApprovedToRun } from '../experiment-utils.es6.js';
 
 const href = window.location.href;
 
-const initTrafficCop = () => {
-    if (href.indexOf('entrypoint_variation=') !== -1) {
-        if (href.indexOf('entrypoint_variation=1') !== -1) {
-            window.dataLayer.push({
-                'data-ex-variant': 'vpn-holidays-na-test-v1',
-                'data-ex-name': 'vpn-holidays-na-test'
-            });
-        } else if (href.indexOf('entrypoint_variation=2') !== -1) {
-            window.dataLayer.push({
-                'data-ex-variant': 'vpn-holidays-na-test-v2',
-                'data-ex-name': 'vpn-holidays-na-test'
-            });
-        }
-    } else if (TrafficCop) {
-        const cop = new TrafficCop({
-            id: 'vpn-holidays-na-test',
-            variations: {
-                'entrypoint_variation=1': 50,
-                'entrypoint_variation=2': 50
-            }
+const init = () => {
+    if (href.indexOf('entrypoint_variation=1') !== -1) {
+        window.dataLayer.push({
+            'data-ex-variant': 'vpn-holidays-na-test-v1',
+            'data-ex-name': 'vpn-holidays-na-test'
         });
-        cop.init();
+    } else if (href.indexOf('entrypoint_variation=2') !== -1) {
+        // set cookie to prevent banner from showing
+        const date = new Date();
+        const cookieDuration = 1 * 24 * 60 * 60 * 1000; // 1 day expiration
+        date.setTime(date.getTime() + cookieDuration); // 1 day expiration
+        Mozilla.Cookies.setItem(
+            'vpn-events-banner',
+            true,
+            date.toUTCString(),
+            '/',
+            undefined,
+            false,
+            'lax'
+        );
+
+        window.dataLayer.push({
+            'data-ex-variant': 'vpn-holidays-na-test-v2',
+            'data-ex-name': 'vpn-holidays-na-test'
+        });
+    } else if (TrafficCop) {
+        // Avoid entering automated tests into random experiments.
+        if (isApprovedToRun()) {
+            const cop = new TrafficCop({
+                id: 'vpn-holidays-na-test',
+                variations: {
+                    'entrypoint_variation=1': 50,
+                    'entrypoint_variation=2': 50
+                }
+            });
+            cop.init();
+        }
     }
 };
 
-// Avoid entering automated tests into random experiments.
-if (isApprovedToRun()) {
-    initTrafficCop();
-}
+init();
