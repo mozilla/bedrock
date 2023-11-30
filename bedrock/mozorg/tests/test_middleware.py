fa5a81ac905b4b1b2131d87bff17da5a6a475659
Rob Hudson <robhudson@mozilla.com>
2023-11-30 11:33:06 -0800

    Update `CacheMiddleware` conditions

--- bedrock/mozorg/tests/test_middleware.py
+++ bedrock/mozorg/tests/test_middleware.py
@@ -5,8 +5,51 @@ from django.conf import settings
 from django.http import HttpRequest, HttpResponse
 from django.test.utils import override_settings
 
-from bedrock.mozorg.middleware import ClacksOverheadMiddleware, HostnameMiddleware
+from freezegun import freeze_time
+
+from bedrock.mozorg.middleware import CacheMiddleware, ClacksOverheadMiddleware, HostnameMiddleware
 from bedrock.mozorg.tests import TestCase
+
+
+class TestCacheMiddleware(TestCase):
+    def setUp(self):
+        self.middleware = CacheMiddleware()
+        self.request = HttpRequest()
+        self.response = HttpResponse()
+
+    @freeze_time("2023-01-01 00:00:00.123456")
+    def test_good_response_has_headers(self):
+        for method in ("GET", "HEAD"):
+            for status in (200, 301, 302, 403, 404, 500):
+                self.request.method = method
+                self.response.status_code = status
+                self.middleware.process_response(self.request, self.response)
+                self.assertEqual(self.response["Cache-Control"], "max-age=600")
+                self.assertEqual(self.response["Expires"], "Sun, 01 Jan 2023 00:10:00 GMT")
+
+    def test_no_caching_methods(self):
+        for method in ("POST", "PUT", "DELETE", "OPTIONS"):
+            self.request.method = method
+            self.response.status_code = 200
+            self.middleware.process_response(self.request, self.response)
+            self.assertNotIn("Cache-Control", self.response)
+            self.assertNotIn("Expires", self.response)
+
+    def test_no_caching_headers(self):
+        self.request.method = "GET"
+        self.response.status_code = 200
+        self.response["Cache-Control"] = "no-cache"
+        self.middleware.process_response(self.request, self.response)
+        self.assertEqual(self.response["Cache-Control"], "no-cache")
+        self.assertNotIn("Expires", self.response)
+
+    def test_no_cache_streaming_response(self):
+        self.request.method = "GET"
+        self.response.status_code = 200
+        self.response.streaming = True
+        self.middleware.process_response(self.request, self.response)
+        self.assertNotIn("Cache-Control", self.response)
+        self.assertNotIn("Expires", self.response)
 
 
 class TestClacksOverheadMiddleware(TestCase):
