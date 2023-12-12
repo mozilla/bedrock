3664cfc6b1a5919520d1f54264f57cab3a43abd8
Alex Gibson <alexgibson@users.noreply.github.com>
2023-12-12 09:30:46 +0000

    Fundraising banner A/B test round 2 (Fixes #13999)

--- media/js/mozorg/home/banner-experiment.es6.js
+++ media/js/mozorg/home/banner-experiment.es6.js
@@ -13,23 +13,23 @@ const init = () => {
     if (href.indexOf('v=1') !== -1) {
         window.dataLayer.push({
             'data-ex-variant': 'exp-home-fundraising-banner-v1',
-            'data-ex-name': 'exp-home-fundraising-banner'
+            'data-ex-name': 'exp-home-fundraising-banner-exp-2'
         });
     } else if (href.indexOf('v=2') !== -1) {
         window.dataLayer.push({
             'data-ex-variant': 'exp-home-fundraising-banner-v2',
-            'data-ex-name': 'exp-home-fundraising-banner'
+            'data-ex-name': 'exp-home-fundraising-banner-exp-2'
         });
     } else if (href.indexOf('v=3') !== -1) {
         window.dataLayer.push({
             'data-ex-variant': 'exp-home-fundraising-banner-v3',
-            'data-ex-name': 'exp-home-fundraising-banner'
+            'data-ex-name': 'exp-home-fundraising-banner-exp-2'
         });
     } else if (TrafficCop) {
         // Avoid entering automated tests into random experiments.
         if (isApprovedToRun()) {
             const cop = new TrafficCop({
-                id: 'exp-home-fundraising-banner',
+                id: 'exp-home-fundraising-banner-exp-2',
                 cookieExpires: 0,
                 variations: {
                     'v=1': 33,
