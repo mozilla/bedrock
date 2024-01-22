8c3f1b6f58fa92b4f2a0a8666f33469b6e75161e
Reem H <42309026+reemhamz@users.noreply.github.com>
2024-01-22 16:57:17 +1100

    updated newsletter component

--- media/js/newsletter/newsletter-init.es6.js
+++ media/js/newsletter/newsletter-init.es6.js
@@ -1,3 +1,4 @@
+/* eslint-disable no-console */
 /*
  * This Source Code Form is subject to the terms of the Mozilla Public
  * License, v. 2.0. If a copy of the MPL was not distributed with this
@@ -5,7 +6,5 @@
  */
 
 import MzpNewsletter from '@mozilla-protocol/core/protocol/js/newsletter';
-import NewsletterForm from './newsletter.es6';
 
 MzpNewsletter.init();
-NewsletterForm.init();
