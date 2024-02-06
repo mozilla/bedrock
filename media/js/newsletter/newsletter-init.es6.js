4f275fe4671027726774f25c0b5a8f0c4bb3a032
Reem H <42309026+reemhamz@users.noreply.github.com>
2024-02-06 12:31:26 +1100

    added analytics events in newsletter

--- media/js/newsletter/newsletter-init.es6.js
+++ media/js/newsletter/newsletter-init.es6.js
@@ -1,4 +1,3 @@
-/* eslint-disable no-console */
 /*
  * This Source Code Form is subject to the terms of the Mozilla Public
  * License, v. 2.0. If a copy of the MPL was not distributed with this
@@ -7,4 +6,35 @@
 
 import MzpNewsletter from '@mozilla-protocol/core/protocol/js/newsletter';
 
-MzpNewsletter.init();
+const successCustomCallback = () => {
+    const newsletters = Array.from(
+        document.querySelectorAll(
+            ".mzp-c-newsletter-form input[name='newsletters']:checked"
+        )
+    ).map((newsletter) => newsletter.value);
+
+    if (window.dataLayer) {
+        // UA
+        window.dataLayer.push({
+            event: 'newsletter-signup-success',
+            newsletter: newsletters
+        });
+        // GA4
+        for (let i = 0; i < newsletters.length; ++i) {
+            window.dataLayer.push({
+                event: 'newsletter_subscribe',
+                newsletter_id: newsletters[i]
+            });
+        }
+    }
+
+    // Glean
+    if (typeof window.Mozilla.Glean !== 'undefined') {
+        window.Mozilla.Glean.pageEvent({
+            label: 'newsletter-sign-up-success',
+            type: newsletters.join(', ')
+        });
+    }
+};
+
+MzpNewsletter.init(successCustomCallback);
