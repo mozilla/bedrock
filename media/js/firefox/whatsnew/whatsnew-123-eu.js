353832d93aa96579f1614895d2db8266dfa302ef
Reem H <42309026+reemhamz@users.noreply.github.com>
2024-02-14 14:28:19 +1100

    added GA events

--- media/js/firefox/whatsnew/whatsnew-123-eu.js
+++ media/js/firefox/whatsnew/whatsnew-123-eu.js
@@ -36,6 +36,68 @@ function init() {
             false
         );
     });
+
+    // Log account status
+    Mozilla.Client.getFxaDetails(function (details) {
+        if (details.setup) {
+            // UA
+            window.dataLayer.push({
+                event: 'non-interaction',
+                eAction: 'whatsnew-123-eu',
+                eLabel: 'firefox-signed-in'
+            });
+
+            // GA4
+            window.dataLayer.push({
+                event: 'dimension_set',
+                firefox_is_signed_in: true
+            });
+        } else {
+            // UA
+            window.dataLayer.push({
+                event: 'non-interaction',
+                eAction: 'whatsnew-123-eu',
+                eLabel: 'firefox-signed-out'
+            });
+
+            // GA4
+            window.dataLayer.push({
+                event: 'dimension_set',
+                firefox_is_signed_in: false
+            });
+        }
+    });
+
+    // Log default status
+    Mozilla.UITour.getConfiguration('appinfo', function (details) {
+        if (details.defaultBrowser) {
+            // UA
+            window.dataLayer.push({
+                event: 'non-interaction',
+                eAction: 'whatsnew-123-eu',
+                eLabel: 'firefox-default'
+            });
+
+            // GA4
+            window.dataLayer.push({
+                event: 'dimension_set',
+                firefox_is_default: true
+            });
+        } else {
+            // UA
+            window.dataLayer.push({
+                event: 'non-interaction',
+                eAction: 'whatsnew-123-eu',
+                eLabel: 'firefox-not-default'
+            });
+
+            // GA4
+            window.dataLayer.push({
+                event: 'dimension_set',
+                firefox_is_default: false
+            });
+        }
+    });
 }
 
 if (
