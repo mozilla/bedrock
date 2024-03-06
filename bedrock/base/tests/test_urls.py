2a24e7dee59e49f6c890626f773e47861e340520
Steve Jalim <stevejalim@mozilla.com>
2024-03-06 12:21:23 +0000

    [Breaking] Add placeholder failing tests

---
+++ bedrock/base/tests/test_urls.py
@@ -0,0 +1,15 @@
+# This Source Code Form is subject to the terms of the Mozilla Public
+# License, v. 2.0. If a copy of the MPL was not distributed with this
+# file, You can obtain one at https://mozilla.org/MPL/2.0/.
+
+
+def test_healthz():
+    assert False, "Write me"
+
+
+def test_healthz_cron():
+    assert False, "Write me"
+
+
+def test_readiness():
+    assert False, "Write me"
