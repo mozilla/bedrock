b3ae0c9946cc28f86bbd100a7c4c29ccf6b7ad06
Steve Jalim <stevejalim@mozilla.com>
2024-03-19 12:15:46 +0000

    Add tests for language-code fixup + slight refactor to ensure it doesn't to _too_ much

--- lib/l10n_utils/tests/test_translation.py
+++ lib/l10n_utils/tests/test_translation.py
@@ -16,9 +16,10 @@ class TestTranslationUtils(TestCase):
         # nothing set, should return default lang
         self.assertEqual(translation.get_language(), settings.LANGUAGE_CODE)
 
-        translation.activate("non-default")
+        translation.activate("lang-xx")
         # 2024 Update: we're making Django work with uppercased territories in lang codes
-        self.assertEqual(translation.get_language(), "non-DEFAULT")
+        # but see test__fix_case below for more explanation
+        self.assertEqual(translation.get_language(), "lang-XX")
 
     def test_activate_deactivate(self):
         """Should activate and deactivate languages"""
@@ -48,3 +49,32 @@ class TestTranslationUtils(TestCase):
 
         translation.activate("skr")
         self.assertTrue(translation.get_language_bidi())
+
+    def test__fix_case(self):
+        cases = (
+            ("en-us", "en-US"),
+            ("EN-gB", "en-GB"),
+            ("en", "en"),
+            ("zh-hk", "zh-HK"),
+            # more complex codes that should not be changed - see function docstring
+            ("zh-Hant", "zh-Hant"),
+            ("zh-Hant-TW", "zh-Hant-TW"),
+            ("zh-HK", "zh-HK"),
+            ("zh-Hant-HK", "zh-Hant-HK"),
+            ("zh-hant", "zh-hant"),
+            ("zh-hant-tw", "zh-hant-tw"),
+            ("zh-Hant", "zh-Hant"),
+            ("zh-Hant-tw", "zh-Hant-tw"),
+            ("zh-Hant-TW", "zh-Hant-TW"),
+            ("ja-JP-mac", "ja-JP-mac"),
+            # no-ops
+            ("dude", "dude"),
+            ("the-DUDE", "the-DUDE"),
+            ("the-DUDE-abIDeS", "the-DUDE-abIDeS"),
+            ("xx", "xx"),
+            ("dq", "dq"),
+            # very special case
+        )
+        for val, expected in cases:
+            with self.subTest(val=val, expected=expected):
+                self.assertEqual(translation._fix_case(val), expected)
