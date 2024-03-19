609eae3d72332495350815d0d0d4bdb288bbae76
Steve Jalim <stevejalim@mozilla.com>
2024-03-19 12:15:46 +0000

    Add tests for language-code fixup + slight refactor to ensure it doesn't to _too_ much

--- lib/l10n_utils/translation.py
+++ lib/l10n_utils/translation.py
@@ -21,19 +21,30 @@ def deactivate():
     return translation.deactivate()
 
 
-def _fix_case(locale):
-    """Convert lowercase locales to uppercase: en-us -> en-US
+def _fix_case(lang_code):
+    """Convert lowercase language code to uppercase: en-us -> en-US
 
     Note: this is less exhaustive than bedrock.base.i18n.normalize_language
-    because we don't want it to potentially fall back to to a two-char lang code
+    because we don't want it to potentially fall back to to a two-char lang code.
+
+    Indeed, because we have normalize_language running earlier in the
+    request-response cycle the odds of getting a bad lang code are likely
+    zero, so our default for more complex locale codes (e.g. zh-Hant-TW) is
+    to leave them untouched. If this proves unreliable, we can merge this with
+    normalize_locales and add a do-not-fall-back option to that function
     """
-    if not locale:
+
+    if not lang_code:
         return None
-    parts = locale.split("-")
+    parts = lang_code.split("-")
+
     if len(parts) == 1:
-        return locale
+        return lang_code
+    if len(parts) > 2 or len(parts[1]) != 2:
+        # Don't touch complex codes
+        return lang_code
     else:
-        return f"{parts[0]}-{parts[1].upper()}"
+        return f"{parts[0].lower()}-{parts[1].upper()}"
 
 
 def get_language():
