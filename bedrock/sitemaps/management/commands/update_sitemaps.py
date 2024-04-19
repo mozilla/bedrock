aa307567f5aa92dbf5f98bec3500abe6e6541330
Steve Jalim <stevejalim@mozilla.com>
2024-04-19 12:49:36 +0100

    Add comment to help future selves

--- bedrock/sitemaps/management/commands/update_sitemaps.py
+++ bedrock/sitemaps/management/commands/update_sitemaps.py
@@ -7,6 +7,8 @@ from django.core.management.base import BaseCommand
 from bedrock.sitemaps.utils import update_sitemaps
 
 
+# Note that this command is only called by www-sitemap-generator, which uses the Docker image
+# of Bedrock to regenerate sitemap data
 class Command(BaseCommand):
     args = ""
     help = "Update XML sitemaps based on the list of localized pages."
