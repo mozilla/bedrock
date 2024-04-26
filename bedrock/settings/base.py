15641c2e7661a4fe552708936c291bbad718db41
Steve Jalim <stevejalim@mozilla.com>
2024-04-26 10:30:40 +0100

    Exclude the cms-admin path from the sitemap - doesn't need to be discoverable

--- bedrock/settings/base.py
+++ bedrock/settings/base.py
@@ -477,6 +477,7 @@ NOINDEX_URLS = [
     r"^(404|500)/",
     r"^firefox/welcome/",
     r"^contribute/(embed|event)/",
+    r"^cms-admin/",
     r"^firefox/set-as-default/thanks/",
     r"^firefox/unsupported/",
     r"^firefox/(sms-)?send-to-device-post",
