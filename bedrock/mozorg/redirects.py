b0ab5618131089d8878e07143bf868a0fb50aec4
Alex Gibson <alexgibson@users.noreply.github.com>
2024-04-29 15:36:29 +0100

    Remove firefox.com.cn redirect

--- bedrock/mozorg/redirects.py
+++ bedrock/mozorg/redirects.py
@@ -14,8 +14,6 @@ def to_uppercase(url):
 
 
 redirectpatterns = (
-    # bug 755826, 1222348, 1416798
-    redirect(r"^zh-CN/?$", "https://www.firefox.com.cn/", locale_prefix=False, query={"utm_medium": "referral", "utm_source": "mozilla.org"}),
     # bug 874913, 681572
     redirect(r"^(products/)?download\.html", "firefox.new", query=""),
     # bug 845580
