d94979142d145f4bf351223f1b3b38c0696d4bc5
Craig Cook <craigcook@users.noreply.github.com>
2024-04-30 19:23:36 -0700

    Remove redundant product landing tests [#14220]

--- tests/pages/products/landing.py
+++ tests/pages/products/landing.py
@@ -2,34 +2,13 @@
 # License, v. 2.0. If a copy of the MPL was not distributed with this
 # file, You can obtain one at https://mozilla.org/MPL/2.0/.
 
-from selenium.webdriver.common.by import By
 
 from pages.base import BasePage
 from pages.regions.join_firefox_form import JoinFirefoxForm
-from pages.regions.menu_list import MenuList
 
 
 class ProductsPage(BasePage):
     _URL_TEMPLATE = "/{locale}/products/"
-
-    _firefox_menu_list_locator = (By.ID, "menu-browsers-wrapper")
-    _focus_menu_list_locator = (By.ID, "menu-focus-wrapper")
-    _pocket_menu_list_locator = (By.ID, "menu-pocket-wrapper")
-
-    @property
-    def firefox_menu_list(self):
-        el = self.find_element(*self._firefox_menu_list_locator)
-        return MenuList(self, root=el)
-
-    @property
-    def focus_menu_list(self):
-        el = self.find_element(*self._focus_menu_list_locator)
-        return MenuList(self, root=el)
-
-    @property
-    def pocket_menu_list(self):
-        el = self.find_element(*self._pocket_menu_list_locator)
-        return MenuList(self, root=el)
 
     @property
     def join_firefox_form(self):
