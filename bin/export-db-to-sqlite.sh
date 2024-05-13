492d3322864c75e961f701db672aa16df6b653da
Steve Jalim <stevejalim@mozilla.com>
2024-05-13 14:57:22 +0100

    Expand DB export script to redact or skip certain Wagtail-related tables/columns

--- bin/export-db-to-sqlite.sh
+++ bin/export-db-to-sqlite.sh
@@ -4,18 +4,21 @@
 # License, v. 2.0. If a copy of the MPL was not distributed with this
 # file, You can obtain one at https://mozilla.org/MPL/2.0/.
 
-# Exports the postgres database to a sqlite file, which can be copied to the cloud bucket for consumption by web pods
+# Exports the postgres database to a sqlite file, which can be copied to the
+# cloud bucket for consumption by web pods.
 
 # Usage:
 # ./bin/export-db-to-sqlite.sh /path/to/output.db
 
 # CRITICAL: requires DATABASE_URL in the environment
 
-
-
 db-to-sqlite $DATABASE_URL $1 --all \
+    --skip django_session \
+    --skip wagtailsearch \
     --redact auth_user username \
     --redact auth_user email \
     --redact auth_user first_name \
     --redact auth_user last_name \
     --redact auth_user password \
+    --redact wagtailcore_comment text \
+    --redact wagtailcore_commentreply text \
