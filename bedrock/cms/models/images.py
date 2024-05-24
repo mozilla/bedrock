9045088be4d1d2f3a1de38e9707f905223e591c2
Steve Jalim <stevejalim@mozilla.com>
2024-05-24 15:41:42 +0100

    Ensure that renditions for images are automatically made from 2400px down to 200px in 200px increment, plus a 100px width

--- bedrock/cms/models/images.py
+++ bedrock/cms/models/images.py
@@ -6,10 +6,13 @@ from django.db import models
 
 from wagtail.images.models import AbstractImage, AbstractRendition, Image
 
+AUTOMATIC_RENDITION_FILTER_SPECS = [f"width-{size}" for size in range(2400, 0, -200)] + ["width-100"]
+
 
 class BedrockImage(AbstractImage):
     """
-    Custom image model from which we can hang extra methods.
+    Custom image model from which we can hang extra methods, such as the one that
+    pre-generates custom renditions for the image.
 
     While the docs show how we can add extra fields on this model
     (https://docs.wagtail.org/en/stable/advanced_topics/images/custom_image_model.html)
@@ -19,6 +22,35 @@ class BedrockImage(AbstractImage):
     """
 
     admin_form_fields = Image.admin_form_fields
+
+    def save(self, *args, **kwargs):
+        super().save(*args, **kwargs)
+        self._pre_generate_expected_renditions()
+
+    def _pre_generate_expected_renditions(self):
+        """We can't make renditions on the fly in production because the
+        public deployment will not have write access to the DB or storage.
+
+        To cope with this, we pre-generate the expected renditions for each image.
+        The simple algorithm is:
+
+        * For every step from 2400px wide down to 200px wide, generate a rendition
+          with that width, plus a rendition that's 100px wide
+
+        And then, in template markup, we'll ensure that developers ONLY use rendtions
+        that are width-2400, width-2200, width-2000... width-100px
+
+        Note that Wagtail does NOT upsize an image, but it will still create an
+        image as large as it can, in the same aspect ratio. So if the image
+        uploaded is 1600px wide, the renditions for 2400px, 2000px.... 1600px will
+        all be 1600px wide. Yes, this is wasteful, but it's the price we pay for
+        having to pre-generate renditions that match a certain set of possible
+        sizes we'll be expecting in template markup.
+
+        **** TODO: move this to be async via a worker queue ****
+        """
+
+        self.get_renditions(*AUTOMATIC_RENDITION_FILTER_SPECS)
 
 
 class BedrockRendition(AbstractRendition):
