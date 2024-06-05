b9a3c71c4ccde703bc4ec2295e87ce8b0c5ba15d
Craig Cook <craigcook@users.noreply.github.com>
2024-06-05 08:27:46 -0700

    Review fixes, copy and layout updates

--- media/js/firefox/whatsnew/whatsnew-127-eu.js
+++ media/js/firefox/whatsnew/whatsnew-127-eu.js
@@ -4,8 +4,6 @@
  * file, You can obtain one at https://mozilla.org/MPL/2.0/.
  */
 
-/* eslint no-unused-vars: [2, { "varsIgnorePattern": "onYouTubeIframeAPIReady" }] */
-
 // Log account status
 Mozilla.Client.getFxaDetails((details) => {
     'use strict';
