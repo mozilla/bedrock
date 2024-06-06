13d6fd74bb7b499475e3959846a683eae133a2d5
Steve Jalim <stevejalim@mozilla.com>
2024-06-06 16:40:35 +0100

    Add links to the task queue and the Django admin to the Wagtail Admin sidebar, for superusers only

---
+++ bedrock/cms/wagtail_hooks.py
@@ -0,0 +1,28 @@
+# This Source Code Form is subject to the terms of the Mozilla Public
+# License, v. 2.0. If a copy of the MPL was not distributed with this
+# file, You can obtain one at https://mozilla.org/MPL/2.0/.
+
+from django.urls import reverse
+
+from wagtail import hooks
+from wagtail.admin.menu import AdminOnlyMenuItem
+
+
+@hooks.register("register_admin_menu_item")
+def register_task_queue_link():
+    return AdminOnlyMenuItem(
+        "Task Queue",
+        reverse("rq_home"),
+        icon_name="tasks",
+        order=80000,
+    )
+
+
+@hooks.register("register_admin_menu_item")
+def register_django_admin_link():
+    return AdminOnlyMenuItem(
+        "Django Admin",
+        reverse("admin:index"),
+        icon_name="tasks",
+        order=80001,
+    )
