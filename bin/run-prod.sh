cf5a64c4e5de1d5c829a3727ecf6cfc6d276e8af
Rob Hudson <robhudson@mozilla.com>
2024-06-13 08:17:55 -0700

    Add granian env vars and sensible defaults

--- bin/run-prod.sh
+++ bin/run-prod.sh
@@ -17,9 +17,15 @@ for fname in "${STARTUP_FILES[@]}"; do
     fi
 done
 
+# Granian recommends containerized apps use the defaults of workers=1, threads=1.
+# The backgpressure defaults to backlog (1024) / workers (1), but should be adjusted to match the
+# number of database connections when configured for databases.
 granian --interface wsgi \
     --host 0.0.0.0 \
     --port "${PORT:-8000}" \
     --no-ws \
-    --workers "${WSGI_NUM_WORKERS:-8}" \
+    --workers "${GRANIAN_WORKERS:-1}" \
+    --threads "${GRANIAN_THREADS:-1}" \
+    --backpressure "${GRANIAN_BACKPRESSURE:-1024}" \
+    --log-level "${GRANIAN_LOG_LEVEL:-warning}" \
     wsgi.app:application
