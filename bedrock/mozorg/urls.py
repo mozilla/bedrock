d6c387188df907fa2793d3ff9979a47353a1895e
Craig Cook <craigcook@users.noreply.github.com>
2024-07-26 12:28:13 -0700

    Add SEI evergreen page (#14891)
    Co-authored-by: Reem H <42309026+reemhamz@users.noreply.github.com>

--- bedrock/mozorg/urls.py
+++ bedrock/mozorg/urls.py
@@ -123,6 +123,8 @@ urlpatterns = [
     page("sustainability/", "mozorg/sustainability/index.html"),
     page("sustainability/carbon-neutral/", "mozorg/sustainability/carbon-neutral.html"),
     page("sustainability/emissions-data/", "mozorg/sustainability/emissions-data.html"),
+    # SEI page
+    path("impact/", views.ImpactPageView.as_view(), name="mozorg.impact-report.index"),
     # Webvision
     # there's also a redirect in mozorg.nonlocale_urls
     path(
