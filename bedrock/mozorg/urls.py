dd2e406220d36b2c483ee8b4c37f7a9687d17fe4
Alex Gibson <alexgibson@users.noreply.github.com>
2024-08-19 12:43:34 +0100

    Fix 404 /impact/ page (Fixes #14971)

--- bedrock/mozorg/urls.py
+++ bedrock/mozorg/urls.py
@@ -124,7 +124,7 @@ urlpatterns = [
     page("sustainability/carbon-neutral/", "mozorg/sustainability/carbon-neutral.html"),
     page("sustainability/emissions-data/", "mozorg/sustainability/emissions-data.html"),
     # SEI page
-    path("impact/", views.ImpactPageView.as_view(), name="mozorg.impact-report.index"),
+    page("impact/", "mozorg/impact-report/index.html"),
     # Webvision
     # there's also a redirect in mozorg.nonlocale_urls
     path(
