e830c04aaae7aadabe8679920f4879ea26f68a26
Craig Cook <craigcook@users.noreply.github.com>
2024-08-21 10:08:53 -0700

    Update list of supported languages on translate feature page [fix #14987]

--- bedrock/firefox/views.py
+++ bedrock/firefox/views.py
@@ -814,6 +814,9 @@ def firefox_welcome_page1(request):
 def firefox_features_translate(request):
     translate_langs = [
         "bg",
+        "hr",
+        "cs",
+        "da",
         "nl",
         "en-US",
         "et",
@@ -822,14 +825,21 @@ def firefox_features_translate(request):
         "de",
         "el",
         "hu",
+        "id",
         "it",
+        "lv",
+        "lt",
         "pl",
         "pt-PT",
+        "ro",
         "ru",
+        "sr",
+        "sk",
         "sl",
         "es-ES",
         "tr",
         "uk",
+        "vi",
     ]
 
     names = get_translations_native_names(sorted(translate_langs))
