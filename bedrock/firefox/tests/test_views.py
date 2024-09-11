6657abd5a39154bdd3d217c0f4e161ee0079a71c
Stephanie Hobson <sjhobson@gmail.com>
2024-09-11 10:13:17 -0700

    Convert buttons with data-link-type="download" into CTAs (#14955)
    * Convert buttons with data-link-type="download" into CTAs Fix #14062

--- bedrock/firefox/tests/test_views.py
+++ bedrock/firefox/tests/test_views.py
@@ -485,17 +485,14 @@ class TestFirefoxGA(TestCase):
     def assert_ga_attr(self, response):
         doc = pq(response.content)
         links = doc(".mzp-c-button")
+        # test buttons all have appropriate attribute to trigger tracking in GA
         for link in links.items():
-            cta_data = link.attr("data-cta-type")
-            cta_link = link.attr("data-link-type")
-            if cta_data:
-                contains_cta = any(cta_data in item for item in ["link", "button"])
-                assert contains_cta or "fxa-" in cta_data
-            elif cta_link:
-                cta_link_types = ["download", "button", "link"]
-                assert cta_link in cta_link_types
+            cta_text = link.attr("data-cta-text")
+            link_text = link.attr("data-link-text")
+            if cta_text or link_text:
+                assert True
             else:
-                assert False, f"{link} does not contain attr cta-type or link-type"
+                assert False, f"{link} does not contain attr data-cta-text or data-link-text"
 
     def test_firefox_home_GA(self):
         req = RequestFactory().get("/en-US/firefox/")
