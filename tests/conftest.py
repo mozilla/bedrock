70e7bf300014a577756c2c7a7b31b41707794eeb
Alex Gibson <alexgibson@users.noreply.github.com>
2024-09-19 09:58:56 +0100

    Remove Pocket mode functional tests

--- tests/conftest.py
+++ tests/conftest.py
@@ -4,17 +4,7 @@
 
 import pytest
 
-from bedrock.base.config_manager import config
-
 
 @pytest.fixture(scope="session")
 def base_url(base_url, request):
     return base_url or request.getfixturevalue("live_server").url
-
-
-@pytest.fixture(scope="session")
-def pocket_base_url(request):
-    base_url = config("BASE_POCKET_URL", parser=str, default="")
-    if not base_url:
-        pytest.skip("No BASE_POCKET_URL detected in env vars")
-    return base_url
