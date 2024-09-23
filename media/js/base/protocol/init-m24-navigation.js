d19c2c4e8b12770925f3b02a81ad3ac407053638
wen-2018 <42974891+wen-2018@users.noreply.github.com>
2024-09-23 12:10:47 -0300

    update nav to remove existing nav styles

---
+++ media/js/base/protocol/init-m24-navigation.js
@@ -0,0 +1,52 @@
+/*
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at https://mozilla.org/MPL/2.0/.
+ */
+
+/**
+ * Initialize Protocol Navigation.
+ */
+(function () {
+    'use strict';
+
+    if (
+        typeof window.MzpMenu === 'undefined' ||
+        typeof window.MzpNavigation === 'undefined'
+    ) {
+        return;
+    }
+
+    var hasMediaQueries = typeof window.matchMedia !== 'undefined';
+
+    function handleOnMenuOpen() {
+        if (
+            !hasMediaQueries ||
+            !window.matchMedia('(min-width: 768px)').matches
+        ) {
+            return;
+        }
+    }
+
+    function initNavButton() {
+        if (typeof Mozilla.Client === 'undefined') {
+            return false;
+        }
+
+        var nav = document.querySelector('.m24-navigation-refresh');
+
+        // Nav should be present on page.
+        if (nav) {
+            // Add a CSS hook for animating the nav button (issue #9009)
+            nav.classList.add('nav-button-is-ready');
+        }
+    }
+
+    initNavButton();
+
+    window.MzpMenu.init({
+        onMenuOpen: handleOnMenuOpen
+    });
+
+    window.MzpNavigation.init();
+})();
