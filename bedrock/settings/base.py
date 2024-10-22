cbebf7998bd363dd43864b109b1f5f077031b144
Rob Hudson <robhudson@mozilla.com>
2024-10-22 10:02:43 -0700

    Fix 12986: Remove the rest of pocketfeed

--- bedrock/settings/base.py
+++ bedrock/settings/base.py
@@ -747,7 +747,6 @@ INSTALLED_APPS = [
     "bedrock.utils",
     "bedrock.wordpress",
     "bedrock.sitemaps",
-    "bedrock.pocketfeed",
     "bedrock.careers",
     # last so that redirects here will be last
     "bedrock.redirects",
