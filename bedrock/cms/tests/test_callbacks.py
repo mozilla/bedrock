3bb94e5c8361e80443273016651ba840fbe0a451
Steve Jalim <stevejalim@mozilla.com>
2024-11-19 22:56:51 +0000

    Support sending visual context to Smartling, for its CAT tool (#15519)
    * Support sending visual context to Smartling, for its CAT tool
    
    * Adds a callback for wagtail-localize-smartling to send visual context to Smartling
    
    * Update wagtail-localize-smartling to 0.6.0 (with Smartling CAT support)
    
    * Add a light test of the visual_context callback
    
    * Update bedrock/cms/wagtail_localize_smartling/callbacks.py

---
+++ bedrock/cms/tests/test_callbacks.py
@@ -0,0 +1,41 @@
+# This Source Code Form is subject to the terms of the Mozilla Public
+# License, v. 2.0. If a copy of the MPL was not distributed with this
+# file, You can obtain one at https://mozilla.org/MPL/2.0/.
+
+from unittest import mock
+
+import pytest
+import wagtail_factories
+from wagtaildraftsharing.models import WagtaildraftsharingLink
+
+from bedrock.cms.tests.factories import SimpleRichTextPageFactory, WagtailUserFactory
+from bedrock.cms.wagtail_localize_smartling.callbacks import visual_context
+
+
+@pytest.mark.django_db
+def test_visual_context(client):
+    top_level_page = SimpleRichTextPageFactory()
+
+    page = SimpleRichTextPageFactory(parent=top_level_page, slug="visual-context-text-page")
+    page.save_revision()
+
+    wagtail_factories.SiteFactory(
+        root_page=top_level_page,
+        is_default_site=True,
+        hostname=client._base_environ()["SERVER_NAME"],
+    )
+
+    user = WagtailUserFactory()
+
+    mock_job = mock.Mock()
+    mock_job.translation_source.get_source_instance.return_value = page
+    mock_job.user = user
+
+    url, html = visual_context(smartling_job=mock_job)
+
+    # light checks because we're not testing wagtaildraftsharing itself
+    assert "<body" in html
+    assert "Test SimpleRichTextPage" in html
+
+    sharing_link_key = WagtaildraftsharingLink.objects.get().key
+    assert url == f"http://testserver:81/_internal_draft_preview/{sharing_link_key}/"
