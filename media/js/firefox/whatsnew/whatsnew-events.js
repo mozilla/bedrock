a73a31212ded19c6ef64be7124d074f7a6e020cf
Alex Gibson <alexgibson@users.noreply.github.com>
2024-11-27 12:23:03 +0000

    Add check for Mozilla.Cient in whatsnew-events.js

--- media/js/firefox/whatsnew/whatsnew-events.js
+++ media/js/firefox/whatsnew/whatsnew-events.js
@@ -4,22 +4,31 @@
  * file, You can obtain one at https://mozilla.org/MPL/2.0/.
  */
 
-// Log account status
-Mozilla.Client.getFxaDetails((details) => {
+// Create namespace
+if (typeof window.Mozilla === 'undefined') {
+    window.Mozilla = {};
+}
+
+function sendEvents() {
     'use strict';
 
-    window.dataLayer.push({
-        event: 'dimension_set',
-        firefox_is_signed_in: details.setup ? true : false
+    // Log account status
+    window.Mozilla.Client.getFxaDetails((details) => {
+        window.dataLayer.push({
+            event: 'dimension_set',
+            firefox_is_signed_in: details.setup ? true : false
+        });
     });
-});
 
-// Log default status
-Mozilla.UITour.getConfiguration('appinfo', (details) => {
-    'use strict';
-
-    window.dataLayer.push({
-        event: 'dimension_set',
-        firefox_is_default: details.defaultBrowser ? true : false
+    // Log default status
+    window.Mozilla.UITour.getConfiguration('appinfo', (details) => {
+        window.dataLayer.push({
+            event: 'dimension_set',
+            firefox_is_default: details.defaultBrowser ? true : false
+        });
     });
-});
+}
+
+if (typeof window.Mozilla.Client !== 'undefined') {
+    sendEvents();
+}
