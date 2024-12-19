7f7a3fbb6c6970d7b235a911e8e2f0c3864622b1
Paul McLanahan <pmac@mozilla.com>
2024-12-19 10:11:04 -0500

    Add support for the USE_X_FORWARDED_HOST setting

--- bedrock/settings/base.py
+++ bedrock/settings/base.py
@@ -134,6 +134,10 @@ USE_TZ = True
 
 USE_ETAGS = config("USE_ETAGS", default=str(not DEBUG), parser=bool)
 
+# Use the "X-Forwarded-Host" header from the CDN to set the Hostname
+# https://mozilla-hub.atlassian.net/browse/SE-4263
+USE_X_FORWARDED_HOST = config("USE_X_FORWARDED_HOST", default="False", parser=bool)
+
 # just here so Django doesn't complain
 TEST_RUNNER = "django.test.runner.DiscoverRunner"
 
