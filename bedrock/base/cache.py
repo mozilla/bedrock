fd6026cbaf543e9cc7f7383ec65c006e7d252eb7
Steve Jalim <stevejalim@mozilla.com>
2025-01-14 12:35:39 +0000

    Add helpers to support a 'hybrid cache' option that uses locmem + DB cache
    In order to balance the need for a distributed cache with the speed of a
    local-memory cache, we've come up with a couple of helper functions that wrap
    the following behaviour:
    
    Getting values:
    * If it's in the local-memory cache, return that immediately.
    * If it's not, fall back to the DB cache, and if the key exists there,
    return that, cacheing it in local memory again on the way through
    * If the local memory cache and DB cache both miss, just return the
    default value for the helper function
    
    Setting values:
    * Set the value in the local memory cache and DB cache at (almost) the same time
    * If the DB cache is not reachable (eg the DB is a read-only replica), log
    this loudly, as it's a sign the helper has not been used appropriately, but
    still set the local-memory version for now, to prevent total failure.
    
    IMPORTANT: before this can be used in production, we need to create the
    cache table in the database with ./manage.py createcachetable AFTER this
    code has been deployed. This sounds a bit chicken-and-egg but we hopefully can
    do it via direct DB connection in the worst case.

--- bedrock/base/cache.py
+++ bedrock/base/cache.py
@@ -2,9 +2,15 @@
 # License, v. 2.0. If a copy of the MPL was not distributed with this
 # file, You can obtain one at https://mozilla.org/MPL/2.0/.
 
+import logging
+
+from django.conf import settings
+from django.core.cache import caches
 from django.core.cache.backends.locmem import DEFAULT_TIMEOUT, LocMemCache
 
 from bedrock.base import metrics
+
+logger = logging.getLogger(__name__)
 
 
 class SimpleDictCache(LocMemCache):
@@ -70,3 +76,89 @@ class SimpleDictCache(LocMemCache):
         with self._lock:
             self._cache[key] = new_value
         return new_value
+
+
+# === HYBRID CACHE BEHAVIOUR ===
+#
+# Our "hybrid cache" approach uses SimpleDictCache as a local, read-through
+# cache on the pod, which falls back to get values from a distributed
+# DB-backed cache.
+# The DB-backed cache is NOT a read-through cache and only has its values
+# set explicitly.
+
+local_cache = caches["default"]  # This is the SimpleDictCache
+db_cache = caches["db"]
+
+
+def get_from_hybrid_cache(key, default=None):
+    """
+    Retrieve a value from the hybrid cache. First checks local cache, then falls
+    back to DB cache.
+
+    If found in DB cache, the value is added to local cache for faster subsequent
+    access.
+
+    This can be called from any code, because it does not require write access to
+    the DB.
+
+    :param key: The cache key to retrieve.
+    :param default: Default value to return if the key is not found in either cache.
+
+    :return: The cached value, or the default if the key is not found.
+    """
+    # Check local cache
+    value = local_cache.get(key)
+    if value is not None:
+        return value
+
+    # Check DB cache and if it has a value, pop it into
+    # the local cache en route to returning the value
+    value = db_cache.get(key)
+    if value is not None:
+        local_cache.set(
+            key,
+            value,
+            timeout=settings.CACHE_TIME_SHORT,
+        )
+        return value
+
+    return default
+
+
+def set_in_hybrid_cache(key, value, timeout=None):
+    """
+    Set a value in the hybrid cache.
+
+    Writes to both the local cache and the DB cache.
+
+    IMPORTANT: this should only be called from somewhere with DB-write access -
+    i.e. the CMS deployment pod – if it is called from a Web deployment pod, it
+    will only set the local-memory cache and also log an exception, because
+    there will be unpredictable results if you're tryint to cache cache
+    something that should be available across pods -- and if you're not you
+    should just use the regular 'default' local-memory cache directly.
+
+    :param key: The cache key to set.
+    :param value: The value to cache.
+    :param timeout: Timeout for DB cache in seconds (local cache will use a shorter timeout by default).
+    """
+    # Set in DB cache first, with the provided optional timeout.
+    # In settings we have a timeout of None, so it will never expire
+    # but still can be replaced (via this helper function)
+
+    try:
+        db_cache.set(
+            key,
+            value,
+            timeout=timeout,
+        )
+    except Exception as ex:
+        # Cope with the DB cache not being available - eg
+        logger.exception(f"Could not set value in DB-backed cache: {ex}")
+
+    # Set in local cache with a short timeout
+    local_cache.set(
+        key,
+        value,
+        timeout=settings.CACHE_TIME_SHORT,
+    )
