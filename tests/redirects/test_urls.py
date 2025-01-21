c5d8c27e9916a0652676aea1aacf39a6d89f4c27
Steve Jalim <stevejalim@mozilla.com>
2025-01-21 16:02:05 +0000

    Strip out redirect rules but leave the infrastructure there

--- tests/redirects/test_urls.py
+++ tests/redirects/test_urls.py
@@ -11,6 +11,7 @@ from .base import assert_valid_url
 from .map_410 import URLS_410
 
 
+@pytest.mark.skip(reason="Disabled until we have redirects to test")
 @pytest.mark.headless
 @pytest.mark.nondestructive
 @pytest.mark.django_db
@@ -31,52 +32,25 @@ def test_404_url(base_url):
     )
 
 
+@pytest.mark.skip(reason="Disabled until we have redirects to test")
 @pytest.mark.headless
 @pytest.mark.nondestructive
 @pytest.mark.django_db
 @pytest.mark.parametrize(
     "url",
-    [
-        "/firefox/android/",
-        "/firefox/android/faq/",
-        "/firefox/brand/",
-        "/firefox/channel/",
-        "/firefox/desktop/",
-        "/firefox/interest-dashboard/",
-        "/firefox/mobile/",
-        "/firefox/os/",
-        "/firefox/os/notes/1.1/",
-        "/firefox/partners/",
-        "/firefox/speed/",
-        "/firefox/tiles/",
-        "/firefox/unsupported/EOL/",
-        # Legacy URLs (Bug 1110927)
-        "/firefox/start/central.html",
-        "/firefox/sync/firstrun.html",
-    ],
+    [],
 )
 def test_301_urls(url, base_url, follow_redirects=False):
     assert_valid_url(url, base_url=base_url, follow_redirects=follow_redirects)
 
 
+@pytest.mark.skip(reason="Disabled until we have redirects to test")
 @pytest.mark.headless
 @pytest.mark.nondestructive
 @pytest.mark.django_db
 @pytest.mark.parametrize(
     "url",
-    [
-        "/firefox/",
-        "/firefox/all/",
-        "/firefox/developer/",
-        "/firefox/installer-help/",
-        "/firefox/latest/releasenotes/",
-        "/firefox/new/",
-        "/firefox/nightly/firstrun/",
-        "/firefox/releases/",
-        "/firefox/unsupported-systems/",
-        "/firefox/android/releasenotes/",
-        "/firefox/ios/releasenotes/",
-    ],
+    [],
 )
 def test_302_urls(url, base_url, follow_redirects=False):
     assert_valid_url(url, base_url=base_url, follow_redirects=follow_redirects, status_code=requests.codes.found)
