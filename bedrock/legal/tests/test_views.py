a0463d66c0f72ca772ad3df6529ca80361bff3e6
Stephanie Hobson <sjhobson@gmail.com>
2025-02-23 22:11:35 -0800

    update tests

--- bedrock/legal/tests/test_views.py
+++ bedrock/legal/tests/test_views.py
@@ -5,13 +5,18 @@
 from unittest.mock import patch
 
 from django.http import HttpResponse
+from django.test import override_settings
 from django.test.client import RequestFactory
+
+import pytest
+from waffle.testutils import override_switch
 
 from bedrock.legal import views
 from bedrock.legal_docs import views as legal_docs_views
 from bedrock.mozorg.tests import TestCase
 
 
+@pytest.mark.django_db
 @patch("bedrock.firefox.views.l10n_utils.render", return_value=HttpResponse())
 @patch.object(legal_docs_views, "load_legal_doc")
 class TestFirefoxSimpleDocView(TestCase):
@@ -23,6 +28,8 @@ class TestFirefoxSimpleDocView(TestCase):
         template = lld_mock.call_args[0][1]
         assert template == "legal/terms/firefox.html"
 
+    @override_settings(DEV=False)
+    @override_switch("FIREFOX_TOU", active=False)
     def test_simple_template(self, render_mock, lld_mock):
         req = RequestFactory().get("/about/legal/terms/firefox/?v=product")
         req.locale = "en-US"
@@ -30,3 +37,13 @@ class TestFirefoxSimpleDocView(TestCase):
         view(req)
         template = lld_mock.call_args[0][1]
         assert template == "legal/terms/firefox-simple.html"
+
+    @override_settings(DEV=False)
+    @override_switch("FIREFOX_TOU", active=True)
+    def test_simple_template_2025(self, render_mock, lld_mock):
+        req = RequestFactory().get("/about/legal/terms/firefox/?v=product")
+        req.locale = "en-US"
+        view = views.FirefoxTermsOfServiceDocView.as_view()
+        view(req)
+        template = lld_mock.call_args[0][1]
+        assert template == "legal/terms/firefox-simple-2025.html"
