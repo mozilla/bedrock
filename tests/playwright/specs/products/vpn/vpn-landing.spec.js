1923399b3ed09d59df5c5ce6d55210af7bcedfd1
Jingwen Zhu <42974891+wen-2018@users.noreply.github.com>
2025-06-05 13:43:39 -0300

    update integration test for VPN landing page (#16316)

--- tests/playwright/specs/products/vpn/vpn-landing.spec.js
+++ tests/playwright/specs/products/vpn/vpn-landing.spec.js
@@ -13,6 +13,11 @@ const availableCountries = ['us', 'ca', 'gb', 'de', 'fr'];
 const mobileOnlyCountries = ['au', 'in', 'mx', 'ua'];
 const androidOnlyCountries = ['br', 'ma'];
 const unavailableCountries = ['cn'];
+const vpnBundleAvailableCountry = ['us'];
+const experimentVariant =
+    '&entrypoint_experiment=vpn-landing-bundle-promo&entrypoint_variation=c';
+const experimenControltVariant =
+    '&entrypoint_experiment=vpn-landing-bundle-promo&entrypoint_variation=a';
 
 test.describe(
     `${url} page`,
@@ -22,9 +27,23 @@ test.describe(
     () => {
         for (const country of availableCountries) {
             test.describe('VPN available', () => {
-                test.beforeEach(async ({ page, browserName }) => {
-                    await openPage(url + `?geo=${country}`, page, browserName);
-                });
+                if (country === 'us') {
+                    test.beforeEach(async ({ page, browserName }) => {
+                        await openPage(
+                            url + `?geo=${country}${experimenControltVariant}`,
+                            page,
+                            browserName
+                        );
+                    });
+                } else {
+                    test.beforeEach(async ({ page, browserName }) => {
+                        await openPage(
+                            url + `?geo=${country}`,
+                            page,
+                            browserName
+                        );
+                    });
+                }
 
                 test(`Country code: ${country}`, async ({ page }) => {
                     const getVpnHeroButton = page.getByTestId(
@@ -304,6 +323,105 @@ test.describe(
                     await expect(getVpnAppStorebutton).not.toBeVisible();
                     await expect(getVpnPlayStorebutton).not.toBeVisible();
                     await expect(getVpnQrCode).not.toBeVisible();
+                });
+            });
+        }
+    }
+);
+
+test.describe(
+    `${url} page`,
+    {
+        tag: '@vpn'
+    },
+    () => {
+        for (const country of vpnBundleAvailableCountry) {
+            test.describe('VPN bundle available', () => {
+                test.beforeEach(async ({ page, browserName }) => {
+                    await openPage(
+                        url +
+                            `?geo=${vpnBundleAvailableCountry}${experimentVariant}`,
+                        page,
+                        browserName
+                    );
+                });
+
+                test(`Country code: ${country}`, async ({ page }) => {
+                    const getVpnHeroButton = page.getByTestId(
+                        'get-mozilla-vpn-hero-button'
+                    );
+
+                    // tab yearly
+                    const getVpnTwelveMonthButton = page.getByTestId(
+                        'vpn-pricing-grid-12-month-button'
+                    );
+
+                    // tab monthly
+                    const getVpnMonthlyButton = page.getByTestId(
+                        'vpn-pricing-grid-monthly-button'
+                    );
+
+                    // banner bundle button
+                    const privacyProductBundleBannerButton = page.getByTestId(
+                        'privacy-product-bundle-banner-button'
+                    );
+                    // pricing grid bundle button
+                    const privacyProductBundlePricingGridButton =
+                        page.getByTestId(
+                            'privacy-product-bundle-pricing-grid-button'
+                        );
+
+                    const getVpnFooterButton = page.getByTestId(
+                        'get-mozilla-vpn-footer-button'
+                    );
+                    const getVpnAppStorebutton = page.getByTestId(
+                        'get-vpn-ios-app-store'
+                    );
+                    const getVpnPlayStorebutton = page.getByTestId(
+                        'get-vpn-google-play-store'
+                    );
+                    const getVpnQrCode = page.getByTestId('get-vpn-qr-code');
+                    const waitlistHeroButton = page.getByTestId(
+                        'join-waitlist-hero-button'
+                    );
+                    const waitlistNotAvailableButton = page.getByTestId(
+                        'join-waitlist-not-available-button'
+                    );
+                    const waitlistSecondaryButton = page.getByTestId(
+                        'join-waitlist-secondary-button'
+                    );
+                    const waitlistTertiaryButton = page.getByTestId(
+                        'join-waitlist-tertiary-button'
+                    );
+                    const waitlistFooterButton = page.getByTestId(
+                        'join-waitlist-footer-button'
+                    );
+
+                    // Assert Get Mozilla VPN buttons are displayed.
+                    await expect(getVpnHeroButton).toBeVisible();
+                    await expect(getVpnTwelveMonthButton).toBeVisible();
+                    await expect(
+                        privacyProductBundleBannerButton
+                    ).toBeVisible();
+                    await expect(
+                        privacyProductBundlePricingGridButton
+                    ).toBeVisible();
+                    await expect(getVpnFooterButton).toBeVisible();
+
+                    // tab monthly is hidden by default
+                    await expect(getVpnMonthlyButton).not.toBeVisible();
+
+                    // Assert mobile only CTAs are not displayed.
+                    await expect(getVpnAppStorebutton).not.toBeVisible();
+                    await expect(getVpnPlayStorebutton).not.toBeVisible();
+                    await expect(getVpnQrCode).not.toBeVisible();
+
+                    // Assert Join Waitlist buttons are not displayed.
+                    await expect(waitlistHeroButton).not.toBeVisible();
+                    await expect(waitlistNotAvailableButton).not.toBeVisible();
+                    await expect(waitlistSecondaryButton).not.toBeVisible();
+                    await expect(waitlistTertiaryButton).not.toBeVisible();
+                    await expect(waitlistFooterButton).not.toBeVisible();
                 });
             });
         }
