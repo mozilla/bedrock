880c00e3f833ff7f81a743629e05bcbc0e716f03
maureenlholland <19650432+maureenlholland@users.noreply.github.com>
2025-07-21 19:33:09 +0100

    Add WNP141 (#16403)
    * Add WNP141
    
    * Add SVG title based on new tab customize button label
    
    * Stakeholder feedback: keep loop setting
    
    * Stakeholder feedback: DE copy updates
    
    * Review fixes
    
    * Use smaller jpg, cut off corners with border-radius

--- bedrock/firefox/views.py
+++ bedrock/firefox/views.py
@@ -511,6 +511,7 @@ class WhatsnewView(L10nTemplateView):
         "firefox/whatsnew/whatsnew-fx138-tab-groups.html": ["firefox/whatsnew/whatsnew"],
         "firefox/whatsnew/whatsnew-fx139-awesome-bar.html": ["firefox/whatsnew/whatsnew"],
         "firefox/whatsnew/whatsnew-fx140-productivity-shortcuts.html": ["firefox/whatsnew/whatsnew"],
+        "firefox/whatsnew/whatsnew-fx141-customize-new-tab.html": ["firefox/whatsnew/whatsnew"],
     }
 
     # specific templates that should not be rendered in
@@ -615,6 +616,11 @@ class WhatsnewView(L10nTemplateView):
                     template = "firefox/whatsnew/whatsnew-fx142beta.html"
                 else:
                     template = "firefox/whatsnew/index.html"
+            else:
+                template = "firefox/whatsnew/index.html"
+        elif version.startswith("141."):
+            if locale in ["en-US", "en-CA", "en-GB", "de", "fr"]:
+                template = "firefox/whatsnew/whatsnew-fx141-customize-new-tab.html"
             else:
                 template = "firefox/whatsnew/index.html"
         elif version.startswith("140."):
