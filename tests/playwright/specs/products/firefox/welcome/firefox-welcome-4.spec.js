e222d4760731c62570f112d85d8cfe67a31b324d
Jan Brasna <1784648+janbrasna@users.noreply.github.com>
2025-08-20 17:37:52 +0200

    Update firefox-welcome-4.spec.js

--- tests/playwright/specs/products/firefox/welcome/firefox-welcome-4.spec.js
+++ tests/playwright/specs/products/firefox/welcome/firefox-welcome-4.spec.js
@@ -13,7 +13,7 @@ const url = '/firefox/welcome/4/';
 test.describe(
     `${url} page`,
     {
-        tag: ['@firefox', '@wfc-redirect']
+        tag: '@firefox'
     },
     () => {
         test('Send to device form success', async ({ page, browserName }) => {
