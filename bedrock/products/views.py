3313ab474e83166c463c0d327628a15de328d203
wen-2018 <42974891+wen-2018@users.noreply.github.com>
2025-09-17 11:37:49 -0300

    end VPN landing page experiment in favor of variant c design

--- bedrock/products/views.py
+++ bedrock/products/views.py
@@ -83,10 +83,10 @@ def vpn_landing_page(request):
     country = get_country_from_request(request)
 
     # ensure experiment parameters matches pre-defined values
-    if entrypoint_variation not in ["a", "b", "c"]:
+    if entrypoint_variation not in []:
         entrypoint_variation = None
 
-    if entrypoint_experiment not in ["vpn-landing-bundle-promo"]:
+    if entrypoint_experiment not in []:
         entrypoint_experiment = None
 
     if request.locale.startswith("en") and country == "US":
