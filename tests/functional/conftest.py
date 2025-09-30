80e2048f521025d2b0504cb1d02db92acd67ce5b
Steve Jalim <stevejalim@mozilla.com>
2025-09-30 09:51:06 +0100

    Remove functional download tests and GHA that invokes them
    Note: this changeset was written by Codex, supervised by @stevejalim
    
    Resolves #16765

--- tests/functional/conftest.py
+++ tests/functional/conftest.py
@@ -3,10 +3,6 @@
 # file, You can obtain one at https://mozilla.org/MPL/2.0/.
 
 import pytest
-import requests
-from pyquery import PyQuery as pq
-
-TIMEOUT = 60
 
 
 @pytest.fixture
@@ -74,49 +70,3 @@ def selenium(selenium):
 def selenium_mobile(selenium):
     selenium.set_window_size(320, 480)  # width, height
     return selenium
-
-
-def pytest_generate_tests(metafunc):
-    markexpr = metafunc.config.getoption("markexpr")
-    if markexpr == "download":
-        base_url = metafunc.config.getoption("base_url")
-        if "download_path" in metafunc.fixturenames:
-            doc = get_web_page(f"{base_url}/en-US/firefox/download/thanks/")
-            urls = [a.attrib["href"] for a in doc("ul.download-list a")]
-            # Bug 1266682 remove links to Play Store to avoid rate limiting in automation.
-            skip_urls = ["https://play.google.com", "https://apps.apple.com"]
-            urls = list(filter(lambda url: not any([s in url for s in skip_urls]), urls))
-            assert urls
-            metafunc.parametrize("download_path", urls)
-
-        elif "download_path_l10n" in metafunc.fixturenames:
-            urls = []
-            doc = get_web_page(f"{base_url}/en-US/firefox/all/")
-            product_urls = [a.attrib["href"] for a in doc("ul.c-product-list a")]
-            # FIXME: sanity checks after first scene is redirected to FXC:
-            if product_urls and product_urls[0].startswith("/en-US/download/all/"):
-                product_urls = ["/en-US/firefox/all/desktop-release/", "/en-US/firefox/all/desktop-nightly/"]
-            # If product url links outside of /firefox/all/ ignore it. (e.g. testflight)
-            product_urls = [url for url in product_urls if url.startswith("/en-US/firefox/all/")]
-            for url in product_urls:
-                doc = get_web_page(f"{base_url}{url}")
-                platform_urls = [a.attrib["href"] for a in doc("ul.c-platform-list a")]
-                for url in platform_urls:
-                    doc = get_web_page(f"{base_url}{url}")
-                    lang_urls = [a.attrib["href"] for a in doc("ul.c-lang-list a")]
-                    for url in lang_urls:
-                        doc = get_web_page(f"{base_url}{url}")
-                        download_urls = [a.attrib["href"] for a in doc("a.download-link")]
-                        for url in download_urls:
-                            urls.append(url)
-            assert urls
-            metafunc.parametrize("download_path_l10n", urls)
-
-
-def get_web_page(url):
-    try:
-        r = requests.get(url, timeout=TIMEOUT, headers={"accept-language": "en"})
-    except requests.RequestException:
-        # retry
-        r = requests.get(url, timeout=TIMEOUT, headers={"accept-language": "en"})
-    return pq(r.content)
