8df7baa78c4adeeb6c690ebcd9713674af31963f
Jo Sprague <github@iamlocaljo.com>
2025-10-10 14:19:14 +0700

    Add l10n images

---
+++ media/js/firefox/whatsnew/whatsnew-144.es6.js
@@ -0,0 +1,55 @@
+/*
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at https://mozilla.org/MPL/2.0/.
+ */
+
+// Show Shake to Summarize section only on Firefox for iOS
+if (
+    typeof window.Mozilla !== 'undefined' &&
+    typeof window.Mozilla.Client !== 'undefined' &&
+    window.Mozilla.Client.isFirefoxiOS
+) {
+    document.body.classList.add('is-firefox-ios');
+}
+
+function init() {
+    Mozilla.UITour.ping(() => {
+        // show new tab CTA
+        document.body.classList.add('wnp-uitour');
+
+        // new tab CTA should open new tab
+        const newtabCta = document.querySelector('.wnp144-uitour-newtab');
+
+        if (typeof window.dataLayer === 'undefined') {
+            window.dataLayer = [];
+        }
+
+        if (newtabCta) {
+            newtabCta.addEventListener(
+                'click',
+                (e) => {
+                    e.preventDefault();
+
+                    window.dataLayer.push({
+                        event: 'widget_action',
+                        type: 'new tab',
+                        action: 'open',
+                        label: 'Personalize my new tab'
+                    });
+
+                    Mozilla.UITour.showNewTab();
+                },
+                false
+            );
+        }
+    });
+}
+
+if (
+    typeof window.Mozilla.Client !== 'undefined' &&
+    typeof window.Mozilla.UITour !== 'undefined' &&
+    window.Mozilla.Client.isFirefoxDesktop
+) {
+    init();
+}
