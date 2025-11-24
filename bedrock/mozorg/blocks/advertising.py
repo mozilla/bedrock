acb8f16d0d6452d2c43897485fc3fe36d894081b
D Chukhin <6025153+dchukhin@users.noreply.github.com>
2025-11-24 15:03:59 -0500

    WT-334 - Build CMS Components For Advertising Pages - Part 1 (#16813)
    * Stylelint
    
    * WIP: add new solutions page
    
    * Stylelint
    
    * Move base styles from the home-solutions to the advertising and main-dark-section css files
    
    * Add base advertising template for next pages
    
    * Remove unused code
    
    * Prevent specific issues with white lines below the sticky subnav
    
    * Add base why mozilla view and template
    
    * Update ads notification bar to make arrow clickable
    
    * Add why mozilla, contact, notification, and highlight text to the why mozilla page
    
    * Wrap why mozilla content on #mozads-main
    
    * Add sample gallery section
    
    * Redirect formats page to the WIP solutions page
    
    * solutions template fix
    
    * First step toward refactoring typography to align advertising, protocol, and m24 sizing.
    
    * finished refactoring font sizes
    
    * Move shared css from principles page to a generic two-column css file
    
    * Add new contact sales page
    
    * Change url for the contact page
    
    * Add link to the contact sales page
    
    * Wrap contact and principles pages with #mozads-main
    
    * Add required field indicators to the contact sales form fields
    
    * Add base static content to the ads solutions page
    
    * Add placeholder modal implementation to ads solutions page
    
    * Add accordion component using the details and summary html tags
    
    * add an AdvertisingIndexPage to the project, with template identical to mozorg/advertising/landing.html
    
    * make sure that AdvertisingIndexPage is included in db export script
    
    * turn advertising index page hero into a block
    
    * turn advertising section heading into a block
    
    * Migrate ads solutions to an es6 file
    
    * Add skeleton for carousels in modals on ads solutions page
    
    * turn advertising statistics sections into a block
    
    * turn advertising feature list section into a block
    
    * turn advertising contact banner into a snippet
    
    * turn advertising notification into a page field
    
    * be more specific about rendered image dimensions
    
    * add a unit test for the AdvertisingIndexPage
    
    * design revisions
    
    * modal FE finished
    
    * solutions PDF, contact link updated
    
    * minor FE edits to account for predictable CMS behavior
    
    * copy and design revisions
    
    * update for design revisions
    
    * design revisions
    
    * fixing linting
    
    * prettier JS updates
    
    * carousel caption JS updates
    
    * new Impact page design
    
    * add ability to add top divider to section heading block
    
    * allow image alt text to be set on page, but also have default if page has not alt text
    
    * typography revisions
    
    * use image description for alt-text
    
    simplify user editing experience by limiting the
    number of editable fields; since the images will
    likely be used only 1 time, the alt text can be
    defined at the image level
    
    * use wagtail-link-block to create consistent link choosing experience
    
    * update test for AdvertisingIndexPage based on changes to fields
    
    * fix lint error
    
    * updated type scale
    
    * linting fixes
    
    * linting fixes
    
    * fix formatting error
    
    * allow FigureWithStatisticBlock to have a link
    
    * new images, another round of design QA
    
    * advertising index page can have multiple links for a notification
    
    * new copy added
    
    * MDN ads images added, captions revised
    
    * Mozilla Ads and Web Docs find/replace
    
    * CSS prefix revision to remove "mozads" in favor of "mza"
    
    * replacing mozads with mza in template files
    
    * Solutions refactor in prep for CMS work
    
    * photo update
    
    * make sure to refer to correct CTA attribute in template
    
    * add rel noopener attribute to links that open in a new tab
    
    * "nearly" removed. Apostrophes fixed. Principles added to nav.
    
    * precommit installed and run on all relevant files
    
    * Update bedrock/mozorg/blocks/advertising.py
    
    Co-authored-by: Steve Jalim <stevejalim@mozilla.com>
    
    * add migration for label text change
    
    * remove twitter as a social icon choice
    
    ---------
    
    Co-authored-by: Luciano Ratamero <luciano@ratamero.com>
    Co-authored-by: Kasey Kelly <kasey@servee.com>
    Co-authored-by: Steve Jalim <stevejalim@mozilla.com>

---
+++ bedrock/mozorg/blocks/advertising.py
@@ -0,0 +1,146 @@
+# This Source Code Form is subject to the terms of the Mozilla Public
+# License, v. 2.0. If a copy of the MPL was not distributed with this
+# file, You can obtain one at https://mozilla.org/MPL/2.0/.
+
+from wagtail import blocks
+from wagtail.images.blocks import ImageChooserBlock
+from wagtail_link_block.blocks import LinkBlock
+
+CAPTION_TEXT_FEATURES = [
+    "bold",
+    "italic",
+    "link",
+    "superscript",
+    "subscript",
+    "strikethrough",
+    "link",
+]
+
+
+SOCIAL_MEDIA_ICON_CHOICES = [
+    ("linkedin", "LinkedIn"),
+    ("tiktok", "TikTok"),
+    ("spotify", "Spotify"),
+    ("bluesky", "BlueSky"),
+    ("instagram", "Instagram"),
+    ("youtube", "YouTube"),
+]
+
+
+class AdvertisingHeroBlock(blocks.StructBlock):
+    """Advertising page hero block."""
+
+    heading_text = blocks.CharBlock(char_max_length=255)
+    primary_cta_text = blocks.CharBlock(char_max_length=255)
+    primary_cta_link = LinkBlock(label="Primary CTA Link")
+    supporting_text = blocks.TextBlock()
+    secondary_cta_text = blocks.CharBlock(char_max_length=255)
+    secondary_cta_link = LinkBlock(label="Secondary CTA Link")
+
+    class Meta:
+        label = "Hero"
+        label_format = "{heading_text}"
+        template = "mozorg/cms/advertising/blocks/advertising_hero_block.html"
+        form_classname = "compact-form struct-block"
+
+
+class SectionHeaderBlock(blocks.StructBlock):
+    """Section header block."""
+
+    has_top_divider = blocks.BooleanBlock(
+        default=False,
+        required=False,
+        label="Should the section have a divider line on top?",
+        inline_form=True,
+    )
+    superheading_text = blocks.CharBlock(char_max_length=255, required=False)
+    heading_text = blocks.CharBlock(char_max_length=255)
+    subheading_text = blocks.CharBlock(char_max_length=255, required=False)
+    image = ImageChooserBlock(required=False)
+
+    class Meta:
+        icon = "title"
+        label = "Section Heading"
+        label_format = "{heading_text}"
+        template = "mozorg/cms/advertising/blocks/section_heading_block.html"
+        form_classname = "compact-form struct-block"
+
+
+class FigureWithStatisticBlock(blocks.StructBlock):
+    """Figure with a statistic block."""
+
+    image = ImageChooserBlock(required=False)
+    image_caption = blocks.RichTextBlock(char_max_length=255)
+    statistic_value = blocks.CharBlock(char_max_length=255)
+    statistic_label = blocks.CharBlock(char_max_length=255)
+    align_image_on_right = blocks.BooleanBlock(
+        default=False,
+        required=False,
+        label="Position the image to the right of the statistic",
+        inline_form=True,
+    )
+    cta_text = blocks.CharBlock(char_max_length=255, required=False)
+    cta_link = LinkBlock(label="Link", required=False)
+
+    class Meta:
+        icon = "decimal"
+        label = "Figure With Statistic"
+        label_format = "{statistic_value} {statistic_label}"
+        template = "mozorg/cms/advertising/blocks/figure_with_statistic_block.html"
+        form_classname = "compact-form struct-block"
+
+
+class FeatureListItemBlock(blocks.StructBlock):
+    """Feature list item block."""
+
+    heading_text = blocks.CharBlock(char_max_length=255)
+    supporting_text = blocks.TextBlock()
+
+    class Meta:
+        label = "Feature Item"
+        label_format = "{heading_text}"
+        template = "mozorg/cms/advertising/blocks/feature_list_item_block.html"
+        form_classname = "compact-form struct-block"
+
+
+class FeatureListBlock(blocks.StructBlock):
+    """Feature list block."""
+
+    feature_list_items = blocks.ListBlock(FeatureListItemBlock(), min_num=1)
+
+    class Meta:
+        icon = "list-ul"
+        label = "Feature List"
+        label_format = "Feature List"
+        template = "mozorg/cms/advertising/blocks/feature_list_block.html"
+        form_classname = "compact-form struct-block"
+
+
+class LinkWithIcon(blocks.StructBlock):
+    """Link with an icon."""
+
+    icon = blocks.ChoiceBlock(required=False, choices=SOCIAL_MEDIA_ICON_CHOICES, inline_form=True)
+    link = LinkBlock()
+
+    class Meta:
+        icon = "link"
+        label = "Link With Icon"
+        label_format = "Link With Icon"
+        template = "mozorg/cms/advertising/blocks/link_with_icon_block.html"
+        form_classname = "compact-form struct-block"
+
+
+class NotificationBlock(blocks.StructBlock):
+    notification_text = blocks.RichTextBlock(
+        char_max_length=255,
+        features=["bold", "italic", "superscript", "subscript", "strikethrough", "code", "link"],
+    )
+    links = blocks.StreamBlock(
+        [
+            ("link_with_icon", LinkWithIcon()),
+        ],
+        required=False,
+    )
+
+    class Meta:
+        template = "mozorg/cms/advertising/blocks/notification_block.html"
--- bedrock/mozorg/tests/test_models.py
+++ bedrock/mozorg/tests/test_models.py
@@ -49,3 +49,74 @@ def test_leadership_page(minimal_site, rf, serving_method):  # noqa
     assert 'href="https://example.com/photos.zip"' in page_content
     assert "Test Job Title" in page_content
     assert "Test Biography" in page_content
+
+
+@pytest.mark.parametrize("serving_method", ("serve", "serve_preview"))
+def test_advertising_index_page(minimal_site, rf, serving_method):  # noqa
+    root_page = minimal_site.root_page
+
+    # Create a ContactBannerSnippet
+    contact_banner = factories.ContactBannerSnippetFactory(
+        heading="Contact Our Advertising Team",
+        button_text="Contact Sales",
+        button_link="https://example.com/contact-sales",
+    )
+
+    # Create the AdvertisingIndexPage with content blocks and contact banner
+    advertising_page = factories.AdvertisingIndexPageFactory(
+        parent=root_page,
+        contact_banner=contact_banner,
+        notifications__0__notification_block__notification_text=RichText("<p>Test notification text</p>"),
+        notifications__0__notification_block__links__0__link_with_icon=factories.LinkWithIconFactory(
+            icon="linkedin",
+            link=factories.LinkBlockFactory(
+                link_to="custom_url",
+                custom_url="https://example.com/notification",
+            ),
+        ),
+        content__0__advertising_hero_block=factories.AdvertisingHeroBlockFactory(
+            heading_text="Test Hero Heading",
+            primary_cta_text="Primary CTA",
+            primary_cta_link=factories.LinkBlockFactory(
+                link_to="custom_url",
+                custom_url="https://example.com/primary",
+            ),
+            supporting_text="Test supporting text",
+            secondary_cta_text="Secondary CTA",
+            secondary_cta_link=factories.LinkBlockFactory(
+                link_to="custom_url",
+                custom_url="https://example.com/secondary",
+            ),
+        ),
+        content__1__section_header_block=factories.SectionHeaderBlockFactory(
+            heading_text="Test Section Header",
+            subheading_text="Test subheading",
+        ),
+    )
+
+    advertising_page.save()
+
+    _relative_url = advertising_page.relative_url(minimal_site)
+    assert _relative_url == "/en-US/advertising/"
+    request = rf.get(_relative_url)
+
+    resp = getattr(advertising_page, serving_method)(request)
+    page_content = resp.text
+
+    # Assert content from the hero block
+    assert "Test Hero Heading" in page_content
+    assert "Primary CTA" in page_content
+    assert "Test supporting text" in page_content
+    assert "Secondary CTA" in page_content
+
+    # Assert content from the section header block
+    assert "Test Section Header" in page_content
+    assert "Test subheading" in page_content
+
+    # Assert content from the contact banner snippet
+    assert "Contact Our Advertising Team" in page_content
+    assert "Contact Sales" in page_content
+    assert "https://example.com/contact-sales" in page_content
+
+    # Assert notification text
+    assert "Test notification text" in page_content
