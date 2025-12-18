42ae3cc98675376893199f18ff321fe74da4092b
Dmitriy Chukhin <6025153+dchukhin@users.noreply.github.com>
2025-12-18 15:29:35 -0500

    add unit tests for remove_p_tag template tag

---
+++ bedrock/base/tests/test_templatetags.py
@@ -0,0 +1,13 @@
+from bedrock.base.templatetags.helpers import remove_p_tag
+
+
+def test_remove_p_tag():
+    input_html = '<p class="test-class">This is a paragraph.</p><p>This is another paragraph.</p>'
+    expected_output = "This is a paragraph.<br/>This is another paragraph."
+    assert remove_p_tag(input_html) == expected_output
+
+
+def test_remove_p_tag_with_inner_tags():
+    input_html = '<p class="test-class">This is a <strong>paragraph</strong>.</p><p>This is another paragraph.</p>'
+    expected_output = "This is a <strong>paragraph</strong>.<br/>This is another paragraph."
+    assert remove_p_tag(input_html) == expected_output
