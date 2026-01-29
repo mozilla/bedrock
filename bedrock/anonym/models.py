7cbe364cc71df9d87a5da84ac4becb88f41391e5
D Chukhin <6025153+dchukhin@users.noreply.github.com>
2026-01-29 17:49:43 -0500

    Anonym Part 9: Anonym Directory (#16989)
    * add an AnonymTopAndBottomPage to the project
    
    * add counter to section templates
    
    * only show icon element if icon is set
    
    * add CSS class to sections with 'top glow' theme
    
    * add table block to SectionBlocks
    
    * make sure first section heading is an h1 element, and others are h2 elements on AnonymTopAndBottomPage
    
    * add an AnonymContentSubPage to the project
    
    * add toggleable items component to AnonymContentSubPage
    
    * update migration, since CardsListBlock.cards is now a StreamBlock, to better handle the need for other card types in the future
    
    * add logo cards, to allow creating case study cards on AnonymContentSubPage
    
    * update toggle to handle dark and light icons automatically
    
    * add a configurable navigation to anonym pages
    
    * make sure migration has a license
    
    * update body class to be anonym-specific
    
    * update body class to be anonym-specific
    
    * add a PersonCard, to allow adding cards with people's photos to the 'About' page
    
    * add a TwoColumnBlock, to allow adding them to 'About' page
    
    * add ability to create static pages as AnonymIndexPage children
    
    * fill in content for static pages
    
    * make sure AnonymTopAndBottomPage can be added in production
    
    * make sure AnonymTopAndBottomPage is included in db export script
    
    * make sure AnonymContentSubPage can be added in production
    
    * make sure AnonymContentSubPage is included in db export script
    
    * make sure AnonymStaticPage can be added in production
    
    * make sure AnonymStaticPage is included in db export script
    
    * make sure that there is only 1 AnonymNewsPage and 1 AnonymContactPage
    
    * add AnonymArticlePage to project, with ability to add rich text blocks to sections
    
    * update advertising pages to pass CSS class to notification snippet
    
    * make instances of AnonymTopAndBottomPage more easily identifiable by setting verbose_name
    
    * make instances of AnonymContentSubPage more easily identifiable by setting verbose_name
    
    * make CMS process DRYer by updating PeopleListBlock to reference Person objects
    
    * make sure migration has a license
    
    * create anonym app for anonym files
    
    * move anonym files to anonym app
    
    * make sure to preserve anonym data in database
    
    * add comment about use of db_table in models file
    
    * update imports in scss files based on new directory structure
    
    * fix lint error
    
    * add license to files that are missing it
    
    * remove unused anonym templates from mozorg app
    
    * move Person snippet to anonym app
    
    * add removed migration for moving person snippet to anonym app

---
+++ bedrock/anonym/models.py
@@ -0,0 +1,266 @@
+# This Source Code Form is subject to the terms of the Mozilla Public
+# License, v. 2.0. If a copy of the MPL was not distributed with this
+# file, You can obtain one at https://mozilla.org/MPL/2.0/.
+
+from django.db import models
+
+from wagtail.admin.panels import FieldPanel, MultiFieldPanel
+from wagtail.fields import StreamField
+from wagtail.models import TranslatableMixin
+from wagtail.snippets.models import register_snippet
+
+from bedrock.anonym.blocks import (
+    CallToActionBlock as AnonymCallToActionBlock,
+    SectionBlock as AnonymSectionBlock,
+    ToggleableItemsBlock as AnonymToggleableItemsBlock,
+)
+from bedrock.cms.models.base import AbstractBedrockCMSPage
+from bedrock.mozorg.blocks.navigation import NavigationLinkBlock
+from bedrock.mozorg.models import SubNavigationMixin
+
+
+class AnonymStaticPage(AbstractBedrockCMSPage):
+    """
+    Abstract base class for static Anonym pages.
+    Subclasses only need to define the template path.
+    """
+
+    parent_page_types = ["AnonymIndexPage"]
+    subpage_types = []
+
+    def get_context(self, request, *args, **kwargs):
+        context = super().get_context(request, *args, **kwargs)
+        # Add the parent AnonymIndexPage to context
+        context["anonym_index_page"] = self.get_parent().specific
+        return context
+
+    class Meta:
+        abstract = True
+
+
+class AnonymNewsPage(AnonymStaticPage):
+    """Static news page for Anonym."""
+
+    max_count = 1
+
+    template = "anonym/anonym_news.html"
+
+    class Meta:
+        verbose_name = "Anonym News Page"
+        # This database table was originally created in the mozorg app, then
+        # the Django model was moved to the anonym app. To preserve data, we
+        # refer to the original database table here.
+        db_table = "mozorg_anonymnewspage"
+
+
+class AnonymContactPage(AnonymStaticPage):
+    """Static contact page for Anonym."""
+
+    max_count = 1
+
+    template = "anonym/anonym_contact.html"
+
+    class Meta:
+        verbose_name = "Anonym Contact Page"
+        # This database table was originally created in the mozorg app, then
+        # the Django model was moved to the anonym app. To preserve data, we
+        # refer to the original database table here.
+        db_table = "mozorg_anonymcontactpage"
+
+
+class AnonymIndexPage(SubNavigationMixin, AbstractBedrockCMSPage):
+    subpage_types = ["AnonymTopAndBottomPage", "AnonymContentSubPage", "AnonymNewsPage", "AnonymContactPage", "AnonymArticlePage"]
+    navigation_field_name = "navigation"
+
+    navigation = StreamField(
+        [("link", NavigationLinkBlock())],
+        blank=True,
+        null=True,
+        use_json_field=True,
+        help_text="Configure the navigation menu items.",
+    )
+    content = StreamField(
+        [
+            ("section", AnonymSectionBlock()),
+            ("call_to_action", AnonymCallToActionBlock()),
+        ],
+        blank=True,
+        null=True,
+        collapsed=True,
+    )
+
+    content_panels = AbstractBedrockCMSPage.content_panels + [
+        FieldPanel("content"),
+    ]
+    settings_panels = AbstractBedrockCMSPage.settings_panels + [
+        FieldPanel("navigation"),
+    ]
+
+    template = "anonym/anonym_index_page.html"
+
+    def get_available_sections(self):
+        """Collect all anchor IDs from content blocks."""
+        available_sections = []
+
+        for block in self.content:
+            # Anchor IDs are nested under settings
+            settings = block.value.get("settings", {})
+            anchor_id = settings.get("anchor_id") if settings else None
+            if anchor_id:
+                available_sections.append(anchor_id)
+
+        return available_sections
+
+    class Meta:
+        verbose_name = "Anonym Index Page"
+        # This database table was originally created in the mozorg app, then
+        # the Django model was moved to the anonym app. To preserve data, we
+        # refer to the original database table here.
+        db_table = "mozorg_anonymindexpage"
+
+
+class AnonymTopAndBottomPage(AbstractBedrockCMSPage):
+    parent_page_types = ["AnonymIndexPage"]
+    subpage_types = []
+
+    top_content = StreamField(
+        [
+            ("section", AnonymSectionBlock()),
+            ("call_to_action", AnonymCallToActionBlock()),
+        ],
+        blank=True,
+        null=True,
+        collapsed=True,
+    )
+    bottom_content = StreamField(
+        [
+            ("section", AnonymSectionBlock()),
+            ("call_to_action", AnonymCallToActionBlock()),
+        ],
+        blank=True,
+        null=True,
+        collapsed=True,
+    )
+
+    content_panels = AbstractBedrockCMSPage.content_panels + [
+        FieldPanel("top_content"),
+        FieldPanel("bottom_content"),
+    ]
+
+    template = "anonym/anonym_top_and_bottom_page.html"
+
+    class Meta:
+        verbose_name = "Anonym Top And Bottom Page"
+        # This database table was originally created in the mozorg app, then
+        # the Django model was moved to the anonym app. To preserve data, we
+        # refer to the original database table here.
+        db_table = "mozorg_anonymtopandbottompage"
+
+
+class AnonymContentSubPage(AbstractBedrockCMSPage):
+    parent_page_types = ["AnonymIndexPage"]
+    subpage_types = []
+
+    content = StreamField(
+        [
+            ("section", AnonymSectionBlock()),
+            ("toggle_items", AnonymToggleableItemsBlock()),
+            ("call_to_action", AnonymCallToActionBlock()),
+        ],
+        blank=True,
+        null=True,
+        collapsed=True,
+    )
+    content_panels = AbstractBedrockCMSPage.content_panels + [
+        FieldPanel("content"),
+    ]
+
+    template = "anonym/anonym_content_sub_page.html"
+
+    class Meta:
+        verbose_name = "Anonym Content Subpage"
+        # This database table was originally created in the mozorg app, then
+        # the Django model was moved to the anonym app. To preserve data, we
+        # refer to the original database table here.
+        db_table = "mozorg_anonymcontentsubpage"
+
+
+class AnonymArticlePage(AbstractBedrockCMSPage):
+    parent_page_types = ["AnonymIndexPage"]
+    subpage_types = []
+
+    content = StreamField(
+        [
+            ("section", AnonymSectionBlock()),
+            ("call_to_action", AnonymCallToActionBlock()),
+        ],
+        blank=True,
+        null=True,
+        collapsed=True,
+    )
+    notification = models.ForeignKey(
+        "mozorg.NotificationSnippet",
+        null=True,
+        blank=True,
+        on_delete=models.SET_NULL,
+        related_name="+",
+    )
+
+    content_panels = AbstractBedrockCMSPage.content_panels + [
+        FieldPanel("content"),
+        FieldPanel("notification"),
+    ]
+
+    template = "anonym/anonym_article_page.html"
+
+    class Meta:
+        verbose_name = "Anonym Article Page"
+        # This database table was originally created in the mozorg app, then
+        # the Django model was moved to the anonym app. To preserve data, we
+        # refer to the original database table here.
+        db_table = "mozorg_anonymarticlepage"
+
+
+@register_snippet
+class Person(TranslatableMixin):
+    image = models.ForeignKey(
+        "cms.BedrockImage",
+        null=True,
+        blank=False,
+        on_delete=models.SET_NULL,
+        related_name="+",
+    )
+    name = models.CharField(
+        max_length=255,
+        blank=False,
+    )
+    position = models.CharField(
+        max_length=255,
+        blank=False,
+    )
+    description = models.TextField(
+        blank=True,
+    )
+
+    panels = [
+        MultiFieldPanel(
+            [
+                FieldPanel("image", heading="Image"),
+                FieldPanel("name", heading="Name"),
+                FieldPanel("position", heading="Position"),
+                FieldPanel("description", heading="Description"),
+            ],
+            heading="Person",
+        ),
+    ]
+
+    class Meta(TranslatableMixin.Meta):
+        verbose_name = "Person"
+        verbose_name_plural = "People"
+        # This database table was originally created in the mozorg app, then
+        # the Django model was moved to the anonym app. To preserve data, we
+        # refer to the original database table here.
+        db_table = "mozorg_person"
+
+    def __str__(self):
+        return f"{self.name} - {self.position}"
