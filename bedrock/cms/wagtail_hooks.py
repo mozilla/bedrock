26f43c9686d3662b973f62b681f61e51a8c23fb2
luciano ratamero <luciano@ratamero.com>
2026-04-09 14:28:52 -0300

    Add underline button to cms' rich text fields (#17127)
    * Add underline button to cms' rich text fields
    
    * Improve spacing and add styles for third indentation on legal text fields

--- bedrock/cms/wagtail_hooks.py
+++ bedrock/cms/wagtail_hooks.py
@@ -6,8 +6,10 @@ from django.templatetags.static import static
 from django.urls import reverse
 from django.utils.html import format_html
 
+import wagtail.admin.rich_text.editors.draftail.features as draftail_features
 from wagtail import hooks
 from wagtail.admin.menu import MenuItem
+from wagtail.admin.rich_text.converters.html_to_contentstate import InlineStyleElementHandler
 
 
 @hooks.register("register_admin_menu_item")
@@ -33,3 +35,34 @@ def register_django_admin_link():
 @hooks.register("insert_global_admin_css")
 def global_admin_css():
     return format_html('<link rel="stylesheet" href="{}">', static("css/cms/wagtail_admin.css"))
+
+
+@hooks.register("register_rich_text_features")
+def register_underline_feature(features):
+    """
+    Registering the `underline` feature, which uses the `UNDERLINE` Draft.js inline style type,
+    and is stored as HTML with a `<u>` tag.
+    """
+    feature_name = "underline"
+    type_ = "UNDERLINE"
+    tag = "u"
+
+    control = {
+        "type": type_,
+        "label": "_",
+        "description": "Underline",
+    }
+
+    features.register_editor_plugin("draftail", feature_name, draftail_features.InlineStyleFeature(control))
+
+    db_conversion = {
+        "from_database_format": {tag: InlineStyleElementHandler(type_)},
+        "to_database_format": {"style_map": {type_: tag}},
+    }
+
+    # 5. Call register_converter_rule to register the content transformation conversion.
+    features.register_converter_rule("contentstate", feature_name, db_conversion)
+
+    # 6. (optional) Add the feature to the default features list to make it available
+    # on rich text fields that do not specify an explicit 'features' list
+    features.default_features.append("underline")
