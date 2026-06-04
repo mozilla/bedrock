74f8d05a03202ae04187e713cb40954a1aa6918a
Matthew Semeniuk <9813994+bluewave41@users.noreply.github.com>
2026-06-04 09:27:33 -0700

    WT 1131 june fundraiser (#17194)
    * Add translations for mid year fundraiser
    
    * Add initial green design
    
    * add trafficcop color variants
    
    * add background color changes
    
    * Programatically set traffic cop URL's
    
    * Handle URL tampering
    
    * Update translations
    
    * Add missing quotation marks
    
    * Small text edits
    
    * Remove safe flag for titles
    
    * Revert old fundraising template changes
    
    * Use correct previous fundraising template
    
    * Add missing semicolon
    
    * Remove safe and unused link

---
+++ media/js/base/banners/foundation-fundraising-banner-midyear2026-init.es6.js
@@ -0,0 +1,86 @@
+/*
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at https://mozilla.org/MPL/2.0/.
+ */
+
+import MozBanner from './mozilla-banner.es6';
+import TrafficCop from '@mozmeao/trafficcop';
+import { isApprovedToRun } from '../../base/experiment-utils.es6.js';
+
+MozBanner.init('foundation-fundraising-banner-midyear2026', true);
+
+if (typeof window.dataLayer === 'undefined') {
+    window.dataLayer = [];
+}
+
+const href = window.location.href;
+
+const initTrafficCop = () => {
+    if (href.indexOf('v=') !== -1) {
+        if (href.indexOf('v=2') !== -1) {
+            // GA4
+            window.dataLayer.push({
+                event: 'experiment_view',
+                id: 'MOFO-donation-green',
+                variant: 'mofo-donation-v2'
+            });
+        } else if (href.indexOf('v=3') !== -1) {
+            // GA4
+            window.dataLayer.push({
+                event: 'experiment_view',
+                id: 'MOFO-donation-orange',
+                variant: 'mofo-donation-v3'
+            });
+        }
+    } else if (TrafficCop) {
+        const cop = new TrafficCop({
+            variations: {
+                'v=2': 50, // MoFo donate green
+                'v=3': 50 // MoFo donate orange
+            }
+        });
+        cop.init();
+    }
+};
+
+const applyVariant = () => {
+    const bannerImage = document.querySelector('.c-banner-image');
+    const donateButton = document.querySelector(
+        '#foundation-fundraising-banner-midyear2026-link'
+    );
+
+    if (href.indexOf('v=3') !== -1) {
+        bannerImage.setAttribute(
+            'src',
+            '/media/img/banners/fundraiser/banner-orange.png'
+        );
+        document
+            .querySelector('.c-banner-fundraising')
+            .classList.add('c-variant-orange');
+        donateButton.setAttribute(
+            'href',
+            'https://www.mozillafoundation.org/?form=26MY-PBB'
+        );
+    } else {
+        bannerImage.setAttribute(
+            'src',
+            '/media/img/banners/fundraiser/banner-green.png'
+        );
+        document
+            .querySelector('.c-banner-fundraising')
+            .classList.add('c-variant-green');
+        donateButton.setAttribute(
+            'href',
+            'https://www.mozillafoundation.org/?form=26MY-PBA'
+        );
+    }
+};
+
+// Avoid entering automated tests into random experiments.
+if (isApprovedToRun()) {
+    initTrafficCop();
+}
+
+// Run on page load (after TrafficCop may have redirected back)
+applyVariant();
