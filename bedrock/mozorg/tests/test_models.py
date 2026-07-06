8e134a86d68ffd0514980d39ac0add611043d923
Stephanie Hobson <stephaniehobson@users.noreply.github.com>
2026-07-06 13:04:18 -0700

    Remove `combined-manifesto` switch, and take old Leadership page out of circulation (#17253)
    * Remove combined-manifesto switch, content is now permanent

--- bedrock/mozorg/tests/test_models.py
+++ bedrock/mozorg/tests/test_models.py
@@ -19,44 +19,6 @@ pytestmark = [
 
 
 @pytest.mark.parametrize("serving_method", ("serve", "serve_preview"))
-def test_leadership_page(minimal_site, rf, serving_method):  # noqa
-    root_page = minimal_site.root_page
-
-    leadership_page = factories.LeadershipPageFactory(
-        parent=root_page,
-        leadership_sections__0__section__title="Test Section Title",
-        leadership_sections__0__section__leadership_group__0__title="Test Leadership Group Title",
-        leadership_sections__0__section__leadership_group__0__leaders__0__name="Test Name",
-        leadership_sections__0__section__leadership_group__0__leaders__0__headshot=factories.LeadershipHeadshotBlockFactory(
-            image_alt_text="Text Alt Text",
-            photos_link="https://example.com/photos.zip",
-        ),
-        leadership_sections__0__section__leadership_group__0__leaders__0__job_title="Test Job Title",
-        leadership_sections__0__section__leadership_group__0__leaders__0__biography=RichText("Test Biography"),
-        leadership_sections__0__section__leadership_group__0__leaders__0__external_links__0__url="https://example.com",
-        leadership_sections__0__section__leadership_group__0__leaders__0__external_links__0__type="Website",
-        leadership_sections__0__section__leadership_group__0__leaders__0__external_links__0__text="Test Blog link",
-    )
-
-    leadership_page.save()
-
-    _relative_url = leadership_page.relative_url(minimal_site)
-    assert _relative_url == "/en-US/leadership/"  # Note: not the real site URL just for testing.
-    request = rf.get(_relative_url)
-
-    resp = getattr(leadership_page, serving_method)(request)
-    page_content = resp.text
-    assert "Test Section Title" in page_content
-    assert "Test Leadership Group" in page_content
-    assert "Test Name" in page_content
-    assert 'alt="Text Alt Text"' in page_content
-    assert "Test Blog link" and 'href="https://example.com"' in page_content
-    assert 'href="https://example.com/photos.zip"' in page_content
-    assert "Test Job Title" in page_content
-    assert "Test Biography" in page_content
-
-
-@pytest.mark.parametrize("serving_method", ("serve", "serve_preview"))
 def test_advertising_index_page(minimal_site, rf, serving_method):  # noqa
     root_page = minimal_site.root_page
 
@@ -480,7 +442,7 @@ def test_about_us_page_utm_parameters(minimal_site):  # noqa: F811
 
 
 def test_about_us_page_subpage_types():
-    assert models.LeadershipPage in models.AboutUsPage.subpage_types
+    assert models.OrganizationLeadershipIndexPage in models.AboutUsPage.subpage_types
 
 
 def test_notification_snippet_analytics_id_auto_generated():
